d2c52fd322538f3dfe4282134de7666c2da39b0d
Steven Salter <ssalter@chemsw.com>
2010-10-13 23:00:02 +0400

    uses kiln paths

--- Schema/_reset_nbt_01G32.bat
+++ Schema/_reset_nbt_01G32.bat
@@ -1,8 +1,8 @@
-echo exit | sqlplus nbt/nbt@orcl @D:\vault\Dn\Nbt\Schema\nbt_nuke.sql
+echo exit | sqlplus nbt/nbt@orcl @C:\kiln\Dn\Nbt\Schema\nbt_nuke.sql
 
-imp.exe nbt/nbt@orcl FILE=D:\vault\Dn\Nbt\Schema\Dumps\Nbt_Master_01G32.dmp FULL=Y TOUSER=nbt
+imp.exe nbt/nbt@orcl FILE=C:\kiln\Dn\Nbt\Schema\Dumps\Nbt_Master_01G32.dmp FULL=Y TOUSER=nbt
 
-echo exit | sqlplus nbt/nbt@orcl @D:\vault\Dn\Nbt\Schema\nbt_finalize_01H06_ora.sql
+echo exit | sqlplus nbt/nbt@orcl @C:\kiln\Dn\Nbt\Schema\nbt_finalize_01H06_ora.sql
 
 
 
