bd618f5df9262ecf15b5727024e169e5aed673ea
Christopher Froehlich <cfroehlich@chemsw.com>
2010-10-26 21:27:06 +0400

    Re: comment #1 on review 20054. Fixed for case 20037.

--- NbtWebApp/Popup_DesignAdd.aspx.cs
+++ NbtWebApp/Popup_DesignAdd.aspx.cs
@@ -503,9 +503,8 @@ namespace ChemSW.Nbt.WebPages
                 {
                     // Temporarily skip unimplemented ones
                     // If Inspection, filter to allowed question field types
-                    if( _Mode == NbtDesignMode.Standard &&
-                          ( FieldType.FieldType != CswNbtMetaDataFieldType.NbtFieldType.External &&
-                            FieldType.FieldType != CswNbtMetaDataFieldType.NbtFieldType.MOL ) )
+                    if( FieldType.FieldType != CswNbtMetaDataFieldType.NbtFieldType.External &&
+                            FieldType.FieldType != CswNbtMetaDataFieldType.NbtFieldType.MOL ) 
                     {
                         AddPropNewFieldTypeIdSelect.Items.Add( new ListItem( FieldType.FieldType.ToString(),
                                                                              FieldType.FieldTypeId.ToString() ) );
