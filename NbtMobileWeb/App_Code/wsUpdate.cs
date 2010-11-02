59a42be8474385410893112d2d5a43925b011c7a
Phil Glaser <pglaser@chemsw.com>
2010-11-02 17:43:53 +0400

    case 20073

---
+++ NbtMobileWeb/App_Code/wsUpdate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using ChemSW.Core;
+using ChemSW.Nbt;
+using ChemSW.Nbt.ObjClasses;
+using ChemSW.Nbt.Actions;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Config;
+using ChemSW.Nbt.PropTypes;
+
+/// <summary>
+/// Summary description for wsUpdate
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class wsUpdate : System.Web.Services.WebService {
+
+    private CswNbtResources _CswNbtResources;
+    public wsUpdate()
+    {
+        CswSessionResourcesNbt CswInitialization = new CswSessionResourcesNbt( Context.Application, Context.Session, Context.Request, Context.Response, string.Empty, _FilesPath, SetupMode.Web );
+        _CswNbtResources = CswInitialization.CswNbtResources;
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    private string _FilesPath
+    {
+        get
+        {
+            return ( System.Web.Hosting.HostingEnvironment.ApplicationPhysicalPath + "\\etc" );
+        }
+    }
+
+    [WebMethod]
+    public string HelloWorld() {
+        return "Hello World";
+    }
+    
+}
---
+++ NbtMobileWeb/Db.js
@@ -0,0 +1,65 @@
+﻿
+
+var DbId = {
+    DBShortName: 'main.html',
+    DBVersion: '1.0',
+    DBDisplayName: 'main.html',
+    DBMaxSize: 65536
+};
+
+
+function _DoSql(sql, params, onSuccess) {
+
+    if (window.openDatabase) {
+
+        db = openDatabase(DbId.DBShortName, DbId.DBVersion, DbId.DBDisplayName, DbId.DBMaxSize);
+        db.transaction(
+                function (transaction) {
+                    transaction.executeSql(sql, params, onSuccess, _errorHandler);
+                }
+            );
+    } else 
+    {
+        console.log("database is not opened");
+    }
+} //_DoSql
+
+function _errorHandler(transaction, error) {
+    alert('Database Error: ' + error.message + ' (Code ' + error.code + ')');
+    return true;
+} //_errorHandler() 
+
+
+
+function _initDB(doreset) {
+
+        if (doreset) {
+            _DoSql('DROP TABLE IF EXISTS sublevels; ');
+            _DoSql('DROP TABLE IF EXISTS changes; ');
+        }
+
+        _createDB();
+
+} //_initDb()
+
+
+
+function _createDB() {
+    _DoSql('CREATE TABLE IF NOT EXISTS sublevels ' +
+            '  (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ' +
+            '   rootid TEXT NOT NULL, ' +
+            '   rootname TEXT NOT NULL, ' +
+            '   rootxml TEXT, ' +
+            '   sublevelxml TEXT );'
+            );
+
+    _DoSql('CREATE TABLE IF NOT EXISTS changes ' +
+            '  (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ' +
+            '   propid TEXT NOT NULL, ' +
+            '   newvalue TEXT, ' +
+            '   applied CHAR ); '
+            );
+}//_createDB()
+
+
+
