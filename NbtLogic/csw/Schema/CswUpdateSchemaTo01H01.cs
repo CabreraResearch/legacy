d47ba8f397213886981a28ea7d271be86c78532a
Steven Salter <ssalter@chemsw.com>
2010-11-17 22:02:53 +0500

    case 20083 in progress

--- NbtLogic/csw/Schema/CswUpdateSchemaTo01H01.cs
+++ NbtLogic/csw/Schema/CswUpdateSchemaTo01H01.cs
@@ -53,8 +53,12 @@ namespace ChemSW.Nbt.Schema
                                                false, false, false, string.Empty, false, DataDictionaryPortableDataType.Long, false,
                                                false, "jct_nodes_props", DataDictionaryUniqueType.None, false, string.Empty );
 
-            // BZ 20081
-            _CswNbtSchemaModTrnsctn.addBooleanColumn( "nodetype_props", "hideinmobile", "Exclude this property in Mobile", false, false );
+            // Case 20081
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( "nodetype_props", "hideinmobile", "Exclude this property from Mobile", false, false );
+            
+            // Case 20083
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( "nodetype_props", "mobilesearch", "Include this property in Mobile Search", false, false );
+            
 
             // New UserSelect Fieldtype
             CswTableUpdate FieldTypesUpdate = _CswNbtSchemaModTrnsctn.makeCswTableUpdate( "01H-05_FT_Update", "field_types" );
---
+++ NbtLogic/csw/Schema/CswUpdateSchemaTo01H10.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Text;
+using ChemSW.Core;
+using ChemSW.Nbt;
+using ChemSW.Nbt.MetaData;
+using ChemSW.DB;
+using ChemSW.Nbt.ObjClasses;
+using ChemSW.Nbt.MetaData.FieldTypeRules;
+using ChemSW.Nbt.PropTypes;
+using ChemSW.Nbt.Actions;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Updates the schema to version 01H-10
+    /// </summary>
+    public class CswUpdateSchemaTo01H10 : ICswUpdateSchemaTo
+    {
+        private CswNbtSchemaModTrnsctn _CswNbtSchemaModTrnsctn;
+
+        public CswSchemaVersion SchemaVersion { get { return new CswSchemaVersion( 1, 'H', 10 ); } }
+        public CswUpdateSchemaTo01H10( CswNbtSchemaModTrnsctn CswNbtSchemaModTrnsctn )
+        {
+            _CswNbtSchemaModTrnsctn = CswNbtSchemaModTrnsctn;
+        }
+
+        public void update()
+        {
+            // Case 20083 - set MobileSearch for nodetypeprops
+            //CswNbtMetaDataNodeTypeProp PhysicalInspectionNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType(
+    
+
+        } // update()
+
+    }//class CswUpdateSchemaTo01H10
+
+}//namespace ChemSW.Nbt.Schema
+
+
