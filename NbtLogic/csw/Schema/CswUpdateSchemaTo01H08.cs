c1676ec4a5cd19ff85044d6d536c66c86d468350
Christopher Froehlich <cfroehlich@chemsw.com>
2010-11-23 01:27:20 +0500

    Removed Waste* node types for case 20328.

--- NbtLogic/csw/Schema/CswUpdateSchemaTo01H08.cs
+++ NbtLogic/csw/Schema/CswUpdateSchemaTo01H08.cs
@@ -42,6 +42,50 @@ namespace ChemSW.Nbt.Schema
 
             _CswNbtSchemaModTrnsctn.MetaData.makeMissingNodeTypeProps();
 
+            // Case 20328
+            CswNbtMetaDataNodeType WasteAreaInspectionNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "Waste Area Inspection" );
+            if( null != WasteAreaInspectionNT )
+                _CswNbtSchemaModTrnsctn.MetaData.DeleteNodeType( WasteAreaInspectionNT );
+
+            CswNbtMetaDataNodeType WasteAreaRouteNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "Waste Area Route" );
+            if( null != WasteAreaRouteNT )
+                _CswNbtSchemaModTrnsctn.MetaData.DeleteNodeType( WasteAreaRouteNT );
+
+            CswNbtMetaDataNodeType WasteInspectionScheduleNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "Waste Inspection Schedule" );
+            if( null != WasteInspectionScheduleNT )
+                _CswNbtSchemaModTrnsctn.MetaData.DeleteNodeType( WasteInspectionScheduleNT );
+
+            CswNbtView WasteAreaInspectionsV = _CswNbtSchemaModTrnsctn.restoreView( "Waste Area Inspections" );
+            if( null != WasteAreaInspectionsV )
+                WasteAreaInspectionsV.Delete();
+            
+            //CswNbtMetaDataNodeType WasteAreaNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "Waste Area" );
+            //if( null != WasteAreaNT )
+            //{
+            //    _CswNbtSchemaModTrnsctn.MetaData.DeleteNodeTypeAllVersions( WasteAreaNT );
+            //}
+
+            String deleteWasteAreaJnps = @"delete from jct_nodes_props
+											where nodetypepropid in (select nodetypepropid from nodetype_props where nodetypeid in 
+											(select nodetypeid from nodetypes where lower(nodetypename) like 'waste area%'))";
+            String deleteWasteAreaNtps = @"delete from nodetype_props
+										   where nodetypeid in (select nodetypeid from nodetypes where lower(nodetypename) like 'waste area%')";
+            String deleteWasteAreaJmn = @"delete from jct_modules_nodetypes
+										  where nodetypeid in (select nodetypeid from nodetypes where lower(nodetypename) like 'waste area%')";
+            String deleteWasteAreaNtts = @"delete from nodetype_tabset
+										  where nodetypeid in (select nodetypeid from nodetypes where lower(nodetypename) like 'waste area%')";
+            String deleteWasteAreaNodes = @"delete from nodes
+										  where nodetypeid in (select nodetypeid from nodetypes where lower(nodetypename) like 'waste area%')";
+            String deleteWasteAreaNts = @"delete from nodetypes
+										  where lower(nodetypename) like 'waste area%'";
+
+            _CswNbtSchemaModTrnsctn.execArbitraryPlatformNeutralSql( deleteWasteAreaJnps );
+            _CswNbtSchemaModTrnsctn.execArbitraryPlatformNeutralSql( deleteWasteAreaNtps );
+            _CswNbtSchemaModTrnsctn.execArbitraryPlatformNeutralSql( deleteWasteAreaJmn );
+            _CswNbtSchemaModTrnsctn.execArbitraryPlatformNeutralSql( deleteWasteAreaNtts );
+            _CswNbtSchemaModTrnsctn.execArbitraryPlatformNeutralSql( deleteWasteAreaNodes );
+            _CswNbtSchemaModTrnsctn.execArbitraryPlatformNeutralSql( deleteWasteAreaNts );
+
         } // update()
 
     }//class CswUpdateSchemaTo01H08
