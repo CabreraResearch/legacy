2c9391e6f564e7b432e22c20b9917b22cb35ef6f
Christopher Froehlich <cfroehlich@chemsw.com>
2010-12-13 19:27:21 +0500

    Reverting delimiter change for case 20424. Steve will make CswDelimiter class.

--- NbtLogic/csw/Tree/CswNbtNodeKey.cs
+++ NbtLogic/csw/Tree/CswNbtNodeKey.cs
@@ -48,7 +48,7 @@ namespace ChemSW.Nbt
             //_ParentKey = ParentKey;
         }
 
-        private static char delimiter = '^';
+        private static char delimiter = ':';
         /// <summary>
         /// Convert the NodeKey information into a delimited string
         /// </summary>
