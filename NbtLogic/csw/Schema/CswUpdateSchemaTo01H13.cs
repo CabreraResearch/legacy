4a1ac30143460ba0bc2ca21cbacf522988ab2300
Steven Salter <ssalter@chemsw.com>
2010-12-17 21:45:16 +0500

    new empty 01H-13 script

---
+++ NbtLogic/csw/Schema/CswUpdateSchemaTo01H13.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Text;
+using ChemSW.Core;
+using ChemSW.Nbt;
+using ChemSW.Nbt.MetaData;
+using ChemSW.DB;
+using ChemSW.Nbt.ObjClasses;
+using ChemSW.Nbt.MetaData.FieldTypeRules;
+using ChemSW.Nbt.PropTypes;
+using ChemSW.Nbt.Actions;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Updates the schema to version 01H-13
+    /// </summary>
+    public class CswUpdateSchemaTo01H13 : ICswUpdateSchemaTo
+    {
+        private CswNbtSchemaModTrnsctn _CswNbtSchemaModTrnsctn;
+
+        public CswSchemaVersion SchemaVersion { get { return new CswSchemaVersion( 1, 'H', 13 ); } }
+        public CswUpdateSchemaTo01H13( CswNbtSchemaModTrnsctn CswNbtSchemaModTrnsctn )
+        {
+            _CswNbtSchemaModTrnsctn = CswNbtSchemaModTrnsctn;
+        }
+
+        public void update()
+        {
+
+
+        } // update()
+
+    }//class CswUpdateSchemaTo01H13
+
+}//namespace ChemSW.Nbt.Schema
+
+
