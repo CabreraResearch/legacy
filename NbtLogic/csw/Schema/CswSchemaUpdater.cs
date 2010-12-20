d6dbd55479d6343854640cfd399bcbbb07679360
pglaser@chemsw.com <unknown@kiln.example.com>
2010-12-20 21:23:43 +0500

    Case 20533

--- NbtLogic/csw/Schema/CswSchemaUpdater.cs
+++ NbtLogic/csw/Schema/CswSchemaUpdater.cs
@@ -75,6 +75,8 @@ namespace ChemSW.Nbt.Schema
             _UpdateDrivers.Add( Schema01H11Driver.SchemaVersion, Schema01H11Driver );
             CswSchemaUpdateDriver Schema01H12Driver = new CswSchemaUpdateDriver( _CswNbtSchemaModTrnsctn, new CswUpdateSchemaTo01H12( _CswNbtSchemaModTrnsctn ) );
             _UpdateDrivers.Add( Schema01H12Driver.SchemaVersion, Schema01H12Driver );
+            CswSchemaUpdateDriver Schema01H13Driver = new CswSchemaUpdateDriver( _CswNbtSchemaModTrnsctn, new CswUpdateSchemaTo01H13( _CswNbtSchemaModTrnsctn ) );
+            _UpdateDrivers.Add( Schema01H13Driver.SchemaVersion, Schema01H13Driver );
             // This automatically detects the latest version
             foreach( CswSchemaVersion Version in _UpdateDrivers.Keys )
             {
