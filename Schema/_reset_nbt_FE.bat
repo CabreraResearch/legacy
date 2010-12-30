e54fa5f0a3bc4a56d7d5d7315db9897d5a1a6e7d
Steven Salter <ssalter@chemsw.com>
2010-12-30 23:56:45 +0500

    updated for new nbt_master.dmp file

--- Schema/_reset_nbt_FE.bat
+++
@@ -1,5 +0,0 @@
-echo exit | sqlplus nbt/nbt@orcl @nbt_nuke.sql
-
-impdp.exe nbt/nbt@orcl DUMPFILE=FE_Sales_Demo.dmp DIRECTORY=NBTDUMPS
-
-echo exit | sqlplus nbt/nbt@orcl @nbt_finalize_01H11_ora.sql
