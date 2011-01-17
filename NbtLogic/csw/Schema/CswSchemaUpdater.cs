776e735f071b62f53d217d38bd7283c4c74145a5
Christopher Froehlich <cfroehlich@chemsw.com>
2011-01-18 00:50:46 +0500

    Case 20706. Limit Assembly and Equipment Schedule to IMCS Module. Limit PI Schedule to FE Module.

--- NbtLogic/csw/Schema/CswSchemaUpdater.cs
+++ NbtLogic/csw/Schema/CswSchemaUpdater.cs
@@ -83,6 +83,8 @@ namespace ChemSW.Nbt.Schema
             _UpdateDrivers.Add( Schema01H15Driver.SchemaVersion, Schema01H15Driver );
             CswSchemaUpdateDriver Schema01H16Driver = new CswSchemaUpdateDriver( _CswNbtSchemaModTrnsctn, new CswUpdateSchemaTo01H16( _CswNbtSchemaModTrnsctn ) );
             _UpdateDrivers.Add( Schema01H16Driver.SchemaVersion, Schema01H16Driver );
+            CswSchemaUpdateDriver Schema01H17Driver = new CswSchemaUpdateDriver( _CswNbtSchemaModTrnsctn, new CswUpdateSchemaTo01H17( _CswNbtSchemaModTrnsctn ) );
+            _UpdateDrivers.Add( Schema01H17Driver.SchemaVersion, Schema01H17Driver );
 
             // This automatically detects the latest version
             foreach( CswSchemaVersion Version in _UpdateDrivers.Keys )
---
+++ NbtLogic/csw/Schema/CswUpdateSchemaTo01H17.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Text;
+using ChemSW.Core;
+using ChemSW.Nbt;
+using ChemSW.Nbt.MetaData;
+using ChemSW.DB;
+using ChemSW.Nbt.ObjClasses;
+using ChemSW.Nbt.MetaData.FieldTypeRules;
+using ChemSW.Nbt.PropTypes;
+using ChemSW.Nbt.Actions;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Updates the schema to version 01H-17
+    /// </summary>
+    public class CswUpdateSchemaTo01H17 : ICswUpdateSchemaTo
+    {
+        private CswNbtSchemaModTrnsctn _CswNbtSchemaModTrnsctn;
+
+        public CswSchemaVersion SchemaVersion { get { return new CswSchemaVersion( 1, 'H', 17 ); } }
+        public CswUpdateSchemaTo01H17( CswNbtSchemaModTrnsctn CswNbtSchemaModTrnsctn )
+        {
+            _CswNbtSchemaModTrnsctn = CswNbtSchemaModTrnsctn;
+        }
+
+        public void update()
+        {
+            // Case 20706
+            CswNbtMetaDataNodeType AssemblyScheduleNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "Assembly Schedule" );
+            CswNbtMetaDataNodeType EquipmentScheduleNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "Equipment Schedule" );
+            CswNbtMetaDataNodeType PhysicalInspectionScheduleNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( CswSchemaUpdater.HamletNodeTypesAsString( CswSchemaUpdater.HamletNodeTypes.Physical_Inspection_Schedule ) );
+            
+            CswTableSelect ModulesTableSelect = _CswNbtSchemaModTrnsctn.makeCswTableSelect( "modules_select", "modules" );
+            DataTable FETable = ModulesTableSelect.getTable( "where name = 'FE'" );
+            Int32 FEModuleId = CswConvert.ToInt32( FETable.Rows[0]["moduleid"] );
+
+            DataTable ImcsTable = ModulesTableSelect.getTable( "where name = 'IMCS'" );
+            Int32 ImcsModuleID = CswConvert.ToInt32( FETable.Rows[0]["moduleid"] );
+
+            if( null != AssemblyScheduleNT )
+            {
+                _CswNbtSchemaModTrnsctn.createModuleNodeTypeJunction( ImcsModuleID, AssemblyScheduleNT.NodeTypeId );
+            }
+            if( null != EquipmentScheduleNT )
+            {
+                _CswNbtSchemaModTrnsctn.createModuleNodeTypeJunction( ImcsModuleID, EquipmentScheduleNT.NodeTypeId );
+            }
+            if( null != PhysicalInspectionScheduleNT )
+            {
+                _CswNbtSchemaModTrnsctn.createModuleNodeTypeJunction( FEModuleId, PhysicalInspectionScheduleNT.NodeTypeId );
+            }
+        } // update()
+
+    }//class CswUpdateSchemaTo01H17
+
+}//namespace ChemSW.Nbt.Schema
+
+
