eee4dad50e5337f076e9ae17e06c89a6669f1eec
Christopher Froehlich <cfroehlich@chemsw.com>
2011-01-20 01:57:17 +0500

    Case 20689. Add isdemo column to appropriate tables.

--- NbtLogic/csw/Schema/CswUpdateSchemaTo01H17.cs
+++ NbtLogic/csw/Schema/CswUpdateSchemaTo01H17.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections;
-using System.Collections.ObjectModel;
 using System.Data;
-using System.Text;
 using ChemSW.Core;
-using ChemSW.Nbt;
 using ChemSW.Nbt.MetaData;
 using ChemSW.DB;
-using ChemSW.Nbt.ObjClasses;
-using ChemSW.Nbt.MetaData.FieldTypeRules;
-using ChemSW.Nbt.PropTypes;
-using ChemSW.Nbt.Actions;
 
 namespace ChemSW.Nbt.Schema
 {
@@ -54,6 +45,50 @@ namespace ChemSW.Nbt.Schema
             {
                 _CswNbtSchemaModTrnsctn.createModuleNodeTypeJunction( FEModuleId, PhysicalInspectionScheduleNT.NodeTypeId );
             }
+            
+            // Case 20689
+            String ConfigVarSQL = "update configuration_variables set variablename=lower(variablename)";
+            _CswNbtSchemaModTrnsctn.execArbitraryPlatformNeutralSql( ConfigVarSQL );
+            _CswNbtSchemaModTrnsctn.createConfigurationVariable( CswResources.NbtConfigurationVariables.Is_Demo, "If 1, Schema is in Demo mode", "1", true );
+            
+            // isdemo columns
+            const String DemoColumnDescription = "1 if the row is demo data";
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.containers.ToString(), CswSchemaVersion.ContainersColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.inventory_groups.ToString(), CswSchemaVersion.InventoryGroupsColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.jct_modules_actions.ToString(), CswSchemaVersion.JctModulesActionsColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.jct_modules_nodetypes.ToString(), CswSchemaVersion.JctModulesNodeTypesColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.jct_nodes_props.ToString(), CswSchemaVersion.JctNodesPropsColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.jct_nodes_props_audit.ToString(), CswSchemaVersion.JctNodesPropsAuditColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.license_accept.ToString(), CswSchemaVersion.LicenseAcceptColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.locations.ToString(), CswSchemaVersion.LocationsColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.materials.ToString(), CswSchemaVersion.MaterialsColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.materials_subclass.ToString(), CswSchemaVersion.MaterialsSubclassColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.materials_synonyms.ToString(), CswSchemaVersion.MaterialsSynonymsColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.nodes.ToString(), CswSchemaVersion.NodesColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.nodes_audit.ToString(), CswSchemaVersion.NodesAuditColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.nodetypes.ToString(), CswSchemaVersion.NodeTypesColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.nodetypes_audit.ToString(), CswSchemaVersion.NodeTypesAuditColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.nodetype_props.ToString(), CswSchemaVersion.NodeTypePropsColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.nodetype_props_audit.ToString(), CswSchemaVersion.NodeTypePropsAuditColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.nodetype_tabset.ToString(), CswSchemaVersion.NodeTypeTabsetColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.node_views.ToString(), CswSchemaVersion.NodeViewsColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.packages.ToString(), CswSchemaVersion.PackagesColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.packdetail.ToString(), CswSchemaVersion.PackdetailColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.schedule_items.ToString(), CswSchemaVersion.ScheduleItemsColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.sequences.ToString(), CswSchemaVersion.SequencesColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.sessionlist.ToString(), CswSchemaVersion.SessionListColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.statistics.ToString(), CswSchemaVersion.StatisticsColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.statistics_actions.ToString(), CswSchemaVersion.StatisticsActionsColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.statistics_nodetypes.ToString(), CswSchemaVersion.StatisticsNodeTypesColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.statistics_reports.ToString(), CswSchemaVersion.StatisticsReportsColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.statistics_searches.ToString(), CswSchemaVersion.StatisticsSearchesColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.statistics_views.ToString(), CswSchemaVersion.StatisticsViewsColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.units_of_measure.ToString(), CswSchemaVersion.UnitsOfMeasureColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.update_history.ToString(), CswSchemaVersion.UpdateHistoryColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.users.ToString(), CswSchemaVersion.UsersColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.vendors.ToString(), CswSchemaVersion.VendorsColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+            _CswNbtSchemaModTrnsctn.addBooleanColumn( CswSchemaVersion.NbtTables.welcome.ToString(), CswSchemaVersion.WelcomeColumns.isdemo.ToString(), DemoColumnDescription, true, false );
+
         } // update()
 
     }//class CswUpdateSchemaTo01H17
