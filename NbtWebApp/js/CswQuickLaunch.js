8ad637f92563d99a60e877b4650b8f6e5b368d3b
Christopher Froehlich <cfroehlich@chemsw.com>
2011-02-03 00:46:34 +0500

    Whole lotta stuff for you.

--- NbtWebApp/js/CswQuickLaunch.js
+++ NbtWebApp/js/CswQuickLaunch.js
@@ -15,24 +15,25 @@
             url: o.Url,
             data: "{ UserId: '' }",
             success: function ($xml) {
-                var $QuickLaunchDiv = $('<div id="quicklaunchdiv"><table class="QuickLaunchTable" align="center" cellpadding="20"></table></div>')
+                var $QuickLaunchDiv = $('<div id="quicklaunchdiv"><ul id="breadcrumbs"></ul></div>')
                                     .appendTo($this);
-                var $table = $QuickLaunchDiv.children('table');
+                var $list = $QuickLaunchDiv.children();
                 
                 $xml.children().each(function() {
 
                     var $item = $(this);
-                    var $cell = getTableCell($table, $item.attr('displayrow'), $item.attr('displaycol'));
-
+                    //var $cell = getTableCell($table, $item.attr('displayrow'), $item.attr('displaycol'));
                     switch($item.attr('type'))
                     {
                         case 'View':
-                            $cell.append( $('<a href="">' + $item.attr('text') + '</a>') );
-                            $cell.find('a').click(function() { o.onLinkClick($item.attr('viewid')); return false; });
+                            $('<li><a href="#">' + $item.attr('text') + '</a></li>')
+                                 .appendTo($list) 
+                                 .children('a')
+                                 .click(function() { o.onLinkClick($item.attr('viewid')); return false; });
                             break;
                         case 'Action': 
-                            $cell.append( $('<a href=' + $item.attr('url') + '>' + $item.attr('text') + '</a>') );
-                            $cell.find('a').click(function() { o.onSearchClick($item.attr('actionid')); return false; });
+                            $('<li><a href=' + $item.attr('url') + '>' + $item.attr('text') + '</a></li>') 
+                                .appendTo($list);
                             break;
                     }
                 });
