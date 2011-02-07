17d06f9bb57f8127698ac4eed15f270d38d774d8
Steven Salter <ssalter@chemsw.com>
2011-02-07 23:29:45 +0500

    Automated commit for release: Hamlet.13

--- NbtSchedService/Properties/AssemblyInfo.cs
+++ NbtSchedService/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "4.1.1.13" )]
-[assembly: AssemblyFileVersion( "4.1.1.13" )]
+[assembly: AssemblyVersion( "4.1.1.14" )]
+[assembly: AssemblyFileVersion( "4.1.1.14" )]
