5103e2f32685e981dcad8fcb4e32ba38b98fbbdd
Christopher Froehlich <cfroehlich@chemsw.com>
2011-02-11 02:11:04 +0500

    Bad merge.

--- NbtLogic/csw/Schema/CswUpdateSchemaTo01H20.cs
+++ NbtLogic/csw/Schema/CswUpdateSchemaTo01H20.cs
@@ -8,82 +8,81 @@ using System.IO;
 
 namespace ChemSW.Nbt.Schema
 {
-    /// <summary>
-    /// Updates the schema to version 01H-20
-    /// </summary>
-    public class CswUpdateSchemaTo01H20 : ICswUpdateSchemaTo
-    {
-        private CswNbtSchemaModTrnsctn _CswNbtSchemaModTrnsctn;
+	/// <summary>
+	/// Updates the schema to version 01H-20
+	/// </summary>
+	public class CswUpdateSchemaTo01H20 : ICswUpdateSchemaTo
+	{
+		private CswNbtSchemaModTrnsctn _CswNbtSchemaModTrnsctn;
 
-        public CswSchemaVersion SchemaVersion { get { return new CswSchemaVersion( 1, 'H', 20 ); } }
-        public CswUpdateSchemaTo01H20( CswNbtSchemaModTrnsctn CswNbtSchemaModTrnsctn )
-        {
-            _CswNbtSchemaModTrnsctn = CswNbtSchemaModTrnsctn;
-        }
+		public CswSchemaVersion SchemaVersion { get { return new CswSchemaVersion( 1, 'H', 20 ); } }
+		public CswUpdateSchemaTo01H20( CswNbtSchemaModTrnsctn CswNbtSchemaModTrnsctn )
+		{
+			_CswNbtSchemaModTrnsctn = CswNbtSchemaModTrnsctn;
+		}
 
-        public void update()
-        {
-            CswNbtMetaDataObjectClass InspectionOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswNbtMetaDataObjectClass.NbtObjectClass.InspectionDesignClass );
-            foreach( CswNbtMetaDataNodeType InspectionNT in InspectionOC.NodeTypes )
-            if( null != PiSetupTab )
-            {
-                CswNbtMetaDataNodeType LatestVersionInspectionNT = InspectionNT.LatestVersionNodeType;
-                if( !LatestVersionInspectionNT.IsLocked )
-                {
-                    // case 20951
-                    CswNbtMetaDataNodeTypeTab ActionTab = LatestVersionInspectionNT.getNodeTypeTab( "Action" );
-                    if( ActionTab == null )
-                    {
-                        ActionTab = _CswNbtSchemaModTrnsctn.MetaData.makeNewTab( LatestVersionInspectionNT, "Action", 2 );
-                    }
+		public void update()
+		{
+			CswNbtMetaDataObjectClass InspectionOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswNbtMetaDataObjectClass.NbtObjectClass.InspectionDesignClass );
+			foreach( CswNbtMetaDataNodeType InspectionNT in InspectionOC.NodeTypes )
+			{
+				CswNbtMetaDataNodeType LatestVersionInspectionNT = InspectionNT.LatestVersionNodeType;
+				if( !LatestVersionInspectionNT.IsLocked )
+				{
+					// case 20951
+					CswNbtMetaDataNodeTypeTab ActionTab = LatestVersionInspectionNT.getNodeTypeTab( "Action" );
+					if( ActionTab == null )
+					{
+						ActionTab = _CswNbtSchemaModTrnsctn.MetaData.makeNewTab( LatestVersionInspectionNT, "Action", 2 );
+					}
 
-                    CswNbtMetaDataNodeTypeProp FinishedProp = LatestVersionInspectionNT.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.FinishedPropertyName );
-                    FinishedProp.NodeTypeTab = ActionTab;
-                    FinishedProp.DisplayRow = 1;
-                    FinishedProp.DisplayColumn = 1;
+					CswNbtMetaDataNodeTypeProp FinishedProp = LatestVersionInspectionNT.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.FinishedPropertyName );
+					FinishedProp.NodeTypeTab = ActionTab;
+					FinishedProp.DisplayRow = 1;
+					FinishedProp.DisplayColumn = 1;
 
-                    CswNbtMetaDataNodeTypeProp CancelledProp = LatestVersionInspectionNT.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.CancelledPropertyName );
-                    CancelledProp.NodeTypeTab = ActionTab;
-                    CancelledProp.DisplayRow = 2;
-                    CancelledProp.DisplayColumn = 1;
+					CswNbtMetaDataNodeTypeProp CancelledProp = LatestVersionInspectionNT.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.CancelledPropertyName );
+					CancelledProp.NodeTypeTab = ActionTab;
+					CancelledProp.DisplayRow = 2;
+					CancelledProp.DisplayColumn = 1;
 
-                    CswNbtMetaDataNodeTypeProp CancelReasonProp = LatestVersionInspectionNT.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.CancelReasonPropertyName );
-                    CancelReasonProp.NodeTypeTab = ActionTab;
-                    CancelReasonProp.DisplayRow = 3;  // even though webapp interprets this independently, Mobile needs this to be 3
-                    CancelReasonProp.DisplayColumn = 1;
-
-
-                    // case 20955
-                    CswNbtMetaDataNodeTypeProp GeneratorProp = LatestVersionInspectionNT.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.GeneratorPropertyName );
-                    CswNbtMetaDataNodeTypeProp IsFutureProp = LatestVersionInspectionNT.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.IsFuturePropertyName );
-                    CswNbtMetaDataNodeTypeProp RouteProp = LatestVersionInspectionNT.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.RoutePropertyName );
-                    CswNbtMetaDataNodeTypeProp RouteOrderProp = LatestVersionInspectionNT.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.RouteOrderPropertyName );
-
-                    GeneratorProp.HideInMobile = true;
-                    IsFutureProp.HideInMobile = true;
-                    RouteProp.HideInMobile = true;
-                    RouteOrderProp.HideInMobile = true;
-
-                    // case 20948
-                    CswNbtMetaDataNodeTypeProp DueDateProp = LatestVersionInspectionNT.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.DatePropertyName );
-                    DueDateProp.DisplayRow = 4;
-                    
-                    // case 20956
-            	    var PiSetupTab = LatestVersionInspectionNT.getNodeTypeTab( "Setup" );
-                    if( null != PiSetupTab )
-                    {
-                      PiSetupTab.TabName = "Details";
-                      PiSetupTab.TabOrder = 3;
-		    }
-
-                } // if( !InspectionNT.IsLocked )
-            } // foreach( CswNbtMetaDataNodeType InspectionNT in InspectionOC.NodeTypes )
+					CswNbtMetaDataNodeTypeProp CancelReasonProp = LatestVersionInspectionNT.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.CancelReasonPropertyName );
+					CancelReasonProp.NodeTypeTab = ActionTab;
+					CancelReasonProp.DisplayRow = 3;  // even though webapp interprets this independently, Mobile needs this to be 3
+					CancelReasonProp.DisplayColumn = 1;
 
 
+					// case 20955
+					CswNbtMetaDataNodeTypeProp GeneratorProp = LatestVersionInspectionNT.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.GeneratorPropertyName );
+					CswNbtMetaDataNodeTypeProp IsFutureProp = LatestVersionInspectionNT.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.IsFuturePropertyName );
+					CswNbtMetaDataNodeTypeProp RouteProp = LatestVersionInspectionNT.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.RoutePropertyName );
+					CswNbtMetaDataNodeTypeProp RouteOrderProp = LatestVersionInspectionNT.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.RouteOrderPropertyName );
 
-        } // update()
+					GeneratorProp.HideInMobile = true;
+					IsFutureProp.HideInMobile = true;
+					RouteProp.HideInMobile = true;
+					RouteOrderProp.HideInMobile = true;
 
-    }//class CswUpdateSchemaTo01H20
+					// case 20948
+					CswNbtMetaDataNodeTypeProp DueDateProp = LatestVersionInspectionNT.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.DatePropertyName );
+					DueDateProp.DisplayRow = 4;
+					
+					// case 20956
+					var PiSetupTab = LatestVersionInspectionNT.getNodeTypeTab( "Setup" );
+					if( null != PiSetupTab )
+					{
+					  PiSetupTab.TabName = "Details";
+					  PiSetupTab.TabOrder = 3;
+					}
+
+				} // if( !InspectionNT.IsLocked )
+			} // foreach( CswNbtMetaDataNodeType InspectionNT in InspectionOC.NodeTypes )
+
+
+
+		} // update()
+
+	}//class CswUpdateSchemaTo01H20
 
 }//namespace ChemSW.Nbt.Schema
 
