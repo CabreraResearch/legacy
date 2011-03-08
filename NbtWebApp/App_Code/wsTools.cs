b962b2eb5833e52904f586668f8317bd88142f18
Christopher Froehlich <cfroehlich@chemsw.com>
2011-03-08 08:01:36 +0500

    Case 20792. Added tools class for webservices.

---
+++ NbtWebApp/App_Code/wsTools.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Script.Serialization;
+
+namespace ChemSW.Nbt.WebServices
+{
+
+	public class wsTools : System.Web.Services.WebService
+	{
+		#region Conversion
+
+        /// <summary>
+        /// Serializes a string into a safe JavaScript string 
+        /// </summary>
+        public static string ToSafeJavaScriptParam( string ParamText )
+        {
+            JavaScriptSerializer Serializer = new JavaScriptSerializer();
+            string ReturnText = Serializer.Serialize(ParamText);
+            return ReturnText;
+        }
+
+        /// <summary>
+        /// Deserializes a string into from a safe JavaScript string 
+        /// </summary>
+        public static string FromSafeJavaScriptParam( string ParamText )
+        {
+            JavaScriptSerializer Serializer = new JavaScriptSerializer();
+            string ReturnText = Serializer.Deserialize<string>( ParamText );
+            return ReturnText;
+        }
+
+        #endregion
+
+    }//wsNBT
+
+} // namespace ChemSW.WebServices
