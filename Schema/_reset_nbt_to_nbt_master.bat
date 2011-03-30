ab6e3f6ed12aa55e324560f55f7741b9bea5465d
Steven Salter <ssalter@chemsw.com>
2011-03-30 19:25:52 +0400

    update finalize in schema bat

--- Schema/_reset_nbt_to_nbt_master.bat
+++ Schema/_reset_nbt_to_nbt_master.bat
@@ -2,4 +2,4 @@ echo exit | sqlplus nbt/nbt@orcl @nbt_nuke.sql
 
 impdp.exe nbt/nbt@orcl DUMPFILE=NBT_MASTER_11g.dmp DIRECTORY=NBTDUMPS REMAP_SCHEMA=nbt_master:nbt
 
-echo exit | sqlplus nbt/nbt@orcl @nbt_finalize_01H21_ora.sql
+echo exit | sqlplus nbt/nbt@orcl @nbt_finalize_01H24_ora.sql
