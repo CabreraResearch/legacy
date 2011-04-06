1325d8f45f9dd49d2856b4df010b47add70d9952
Christopher Froehlich <cfroehlich@chemsw.com>
2011-04-07 00:57:16 +0400

    Case 21351. Implemented CswButton.

---
+++ NbtWebApp/js/CswButton.js
@@ -0,0 +1,67 @@
+﻿; (function ($)
+{
+
+	var PluginName = 'CswButton';
+
+	var methods = {
+		init: function (options)
+		{
+			var o = {
+                'ID': '',
+				'enabledText': '',
+                'disabledText': '',
+                'hasText': true,
+                'disableOnClick': true,
+                'primaryicon': '',
+                'secondaryicon': '',
+				'ReadOnly': false,
+				'Required': false,
+				'onclick': function() { }
+			};
+			if(options) $.extend(o, options);
+
+			var $parent = $(this);
+
+            var buttonOpt = {
+                text: (o.hasText),
+                label: o.enabledText,
+                disabled: (o.ReadOnly),
+                icons: {
+                            primary: o.primaryicon,
+                            secondary: o.secondaryicon
+                        }
+            };
+            if( buttonOpt.disabled )
+            {
+                buttonOpt.label = o.disabledText;
+            }
+            $parent.button(buttonOpt)
+                   .click(function() {
+                        $thisButton = $(this);
+                        if(o.disableOnClick) $thisButton.button({label: o.disabledText, disabled: true});
+                        o.onclick();
+                        if(o.disableOnClick) $thisButton.button({label: o.enabledText, disabled: false});
+                   })
+                   .attr('id',o.ID)
+                   .attr('name',o.ID);
+
+		}
+	};
+
+	// Method calling logic
+	$.fn.CswButton = function (method)
+	{
+
+		if (methods[method])
+		{
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		} else if (typeof method === 'object' || !method)
+		{
+			return methods.init.apply(this, arguments);
+		} else
+		{
+			$.error('Method ' + method + ' does not exist on ' + PluginName);
+		}
+
+	};
+})(jQuery);
