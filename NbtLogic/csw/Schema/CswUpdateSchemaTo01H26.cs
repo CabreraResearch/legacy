8184f6ac743ab9e5571f53b0dd5c7edd4d390df7
Christopher Froehlich <cfroehlich@chemsw.com>
2011-04-14 00:35:05 +0400

    Add to review 20871. Missing schema script. For case 21395.

---
+++ NbtLogic/csw/Schema/CswUpdateSchemaTo01H26.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Collections.ObjectModel;
+using ChemSW.Core;
+using ChemSW.Nbt.MetaData;
+using ChemSW.DB;
+using ChemSW.Nbt.ObjClasses;
+using System.IO;
+
+namespace ChemSW.Nbt.Schema
+{
+	/// <summary>
+	/// Updates the schema to version 01H-26
+	/// </summary>
+	public class CswUpdateSchemaTo01H26 : ICswUpdateSchemaTo
+	{
+		private CswNbtSchemaModTrnsctn _CswNbtSchemaModTrnsctn;
+
+		public CswSchemaVersion SchemaVersion { get { return new CswSchemaVersion( 1, 'H', 26 ); } }
+		public CswUpdateSchemaTo01H26( CswNbtSchemaModTrnsctn CswNbtSchemaModTrnsctn )
+		{
+			_CswNbtSchemaModTrnsctn = CswNbtSchemaModTrnsctn;
+		}
+
+		public void update()
+		{
+			// case 20871
+		    List<CswNbtView> AllMountPoints = _CswNbtSchemaModTrnsctn.restoreViews( "All FE Inspection Points" );
+            CswNbtMetaDataNodeType MountPointNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( CswSchemaUpdater.HamletNodeTypesAsString( CswSchemaUpdater.HamletNodeTypes.FE_Inspection_Point ) );
+            if( null != MountPointNT )
+            {
+                CswNbtMetaDataNodeTypeProp BarcodeNtp = MountPointNT.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionTarget.BarcodePropertyName );
+                CswNbtMetaDataNodeTypeProp DescriptionNtp = MountPointNT.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionTarget.DescriptionPropertyName );
+                CswNbtMetaDataNodeTypeProp StatusNtp = MountPointNT.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionTarget.StatusPropertyName );
+                CswNbtMetaDataNodeTypeProp TypeNtp = MountPointNT.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionTarget.TypePropertyName );
+                CswNbtMetaDataNodeTypeProp LocationNtp = MountPointNT.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionTarget.LocationPropertyName );
+                CswNbtMetaDataNodeTypeProp MountPointGroupNtp = MountPointNT.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionTarget.InspectionTargetGroupPropertyName );
+                foreach( CswNbtView View in AllMountPoints )
+                {
+                    View.Root.ChildRelationships.Clear();
+                    CswNbtViewRelationship MountPointVr = View.AddViewRelationship( MountPointNT, false );
+                    CswNbtViewProperty BarcodeVp = View.AddViewProperty( MountPointVr, BarcodeNtp );
+                    View.AddViewPropertyFilter( BarcodeVp, CswNbtSubField.SubFieldName.Barcode, CswNbtPropFilterSql.PropertyFilterMode.Begins, string.Empty, false );
+
+                    CswNbtViewProperty DescriptionVp = View.AddViewProperty( MountPointVr, DescriptionNtp );
+                    View.AddViewPropertyFilter( DescriptionVp, CswNbtSubField.SubFieldName.Text, CswNbtPropFilterSql.PropertyFilterMode.Begins, string.Empty, false );
+
+                    CswNbtViewProperty StatusVp = View.AddViewProperty( MountPointVr, StatusNtp );
+                    View.AddViewPropertyFilter( StatusVp, CswNbtSubField.SubFieldName.Value, CswNbtPropFilterSql.PropertyFilterMode.Equals, string.Empty, false );
+
+                    CswNbtViewProperty TypeVp = View.AddViewProperty( MountPointVr, TypeNtp );
+                    View.AddViewPropertyFilter( TypeVp, CswNbtSubField.SubFieldName.Value, CswNbtPropFilterSql.PropertyFilterMode.Equals, string.Empty, false );
+
+                    CswNbtViewProperty LocationVp = View.AddViewProperty( MountPointVr, LocationNtp );
+                    View.AddViewPropertyFilter( LocationVp, CswNbtSubField.SubFieldName.Name, CswNbtPropFilterSql.PropertyFilterMode.Begins, string.Empty, false );
+
+                    CswNbtViewProperty MountPointGroupVp = View.AddViewProperty( MountPointVr, MountPointGroupNtp );
+                    View.AddViewPropertyFilter( MountPointGroupVp, CswNbtSubField.SubFieldName.Name, CswNbtPropFilterSql.PropertyFilterMode.Begins, string.Empty, false );
+                    View.save();
+                }
+            }
+            //No else, just leave the OC-based views
+		} // update()
+
+	}//class CswUpdateSchemaTo01H26
+
+}//namespace ChemSW.Nbt.Schema
+
