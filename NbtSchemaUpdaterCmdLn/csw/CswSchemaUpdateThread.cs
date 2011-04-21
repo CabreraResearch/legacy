f7f09338763a5422e8183f982fe29e0025783eb4
Phil Glaser <pglaser@chemsw.com>
2011-04-21 20:47:44 +0400

    case 21451: added background thread

---
+++ NbtSchemaUpdaterCmdLn/csw/CswSchemaUpdateThread.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Threading;
+using System.Reflection;
+using System.Text;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using ChemSW.Exceptions;
+using ChemSW.Config;
+using ChemSW.Core;
+using ChemSW.Nbt.Config;
+using ChemSW.Nbt.Schema;
+using ChemSW.Nbt;
+
+
+namespace ChemSW.Nbt.Schema.CmdLn
+{
+    /// <summary>
+    /// Keeps the schema up-to-date
+    /// </summary>
+    public enum UpdateState { Idle, Running, Complete };
+    public class CswSchemaUpdateThread
+    {
+        private CswSchemaUpdater _CswSchemaUpdater = null;
+        public CswSchemaUpdateThread( CswSchemaUpdater CswSchemaUpdater )
+        {
+            _CswSchemaUpdater = CswSchemaUpdater;
+        }//ctor
+
+        private UpdateState _UpdateState = UpdateState.Idle;
+        public UpdateState UpdateState
+        {
+            get
+            {
+                return ( _UpdateState );
+            }
+        }
+
+        private string _Message = string.Empty;
+        public string Message
+        {
+            get
+            {
+                return ( _Message );
+            }
+        }
+
+
+
+        public void start()
+        {
+            _Message = string.Empty;
+            _UpdateState = UpdateState.Running;
+            _UpdateThread = new Thread( new ThreadStart( _doUpdate ) );
+            _UpdateThread.Start();
+
+
+        }//start() 
+
+
+        private Thread _UpdateThread = null;
+        private void _doUpdate()
+        {
+            try
+            {
+                _CswSchemaUpdater.Update();
+                _Message = "Update to schema version " + _CswSchemaUpdater.TargetVersion.ToString() + " is complete";
+
+            }
+
+            catch( Exception Exception )
+            {
+                _Message = "Update to schema version " + _CswSchemaUpdater.TargetVersion.ToString() + " failed: " + Exception.Message;
+            }
+
+            _UpdateState = CmdLn.UpdateState.Complete;
+
+        }//_doUpdate()
+
+
+    }//CswSchemaUpdateThread
+
+}//ChemSW.Nbt.Schema.CmdLn
--- NbtSchemaUpdaterCmdLn/csw/CswSchemaUpdaterConsole.cs
+++ NbtSchemaUpdaterCmdLn/csw/CswSchemaUpdaterConsole.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Threading;
+using System.Reflection;
 using System.Text;
 using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using ChemSW.Exceptions;
+using ChemSW.Config;
+using ChemSW.Core;
+using ChemSW.Nbt.Config;
+using ChemSW.Nbt.Schema;
+using ChemSW.Nbt;
 
 
 namespace ChemSW.Nbt.Schema.CmdLn
@@ -16,17 +23,63 @@ namespace ChemSW.Nbt.Schema.CmdLn
 
         private const string NuLine = "\n\r";
         private const string ParamSpacer = "- ";
-        private const string _Help = NuLine + "Usage: " +
-                                     NuLine + "NbtSchemaUpdate <AccessId> | all" +
-                                     NuLine + ParamSpacer + "<AccessId>: The AccessId, as per CswDbConfig.xml, of the schema to be updated" +
-                                     NuLine + ParamSpacer + "all: appdate all schemata specified CswDbConfig.xml";
+
+        private string _VersionInfo
+        {
+            get
+            {
+                string ReturnVal = string.Empty;
+
+                ReturnVal = NuLine + "ChemSW NBT SchemaUpdater version " + Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+                if( null != _CswSchemaUpdater )
+                {
+                    ReturnVal += NuLine + NuLine + "Minimum schema version: " + _CswSchemaUpdater.MinimumVersion;
+                    ReturnVal += NuLine + "Latest schema version: " + _CswSchemaUpdater.TargetVersion;
+                }
+
+                return ( ReturnVal );
+            }
+        }
+
+        private string _Help
+        {
+            get
+            {
+
+                string ReturnVal = string.Empty;
+
+                ReturnVal = _VersionInfo;
+
+
+
+
+                ReturnVal += NuLine + NuLine + "Usage: " +
+                                            NuLine + "NbtSchemaUpdate <AccessId> | all" +
+                                            NuLine + ParamSpacer + "<AccessId>: The AccessId, as per CswDbConfig.xml, of the schema to be updated" +
+                                            NuLine + ParamSpacer + "all: appdate all schemata specified CswDbConfig.xml";
+                return ( ReturnVal );
+            }
+        }
+
+
+        private CswDbCfgInfoNbt _CswDbCfgInfoNbt = null;
+        private CswSchemaUpdater _CswSchemaUpdater = null;
+        private CswNbtResources _CswNbtResources = null;
+        private CswSchemaUpdateThread _CswSchemaUpdateThread = null;
+
+
 
         private string[] _args;
         public CswSchemaUpdaterConsole( string[] args )
         {
             _args = args;
+            _CswDbCfgInfoNbt = new CswDbCfgInfoNbt( SetupMode.Executable );
+            _CswNbtResources = CswNbtResourcesFactory.makeCswNbtResources( AppType.Nbt, new CswSetupVblsNbt( SetupMode.Executable ), _CswDbCfgInfoNbt, CswTools.getConfigurationFilePath( SetupMode.Executable ), false, false );
+            _CswSchemaUpdater = new CswSchemaUpdater( _CswNbtResources );
+            _CswSchemaUpdateThread = new CswSchemaUpdateThread( _CswSchemaUpdater );
 
-        }
+        }//ctor
 
         public string process()
         {
@@ -34,23 +87,61 @@ namespace ChemSW.Nbt.Schema.CmdLn
 
             if( 1 == _args.Length )
             {
-                if( "all" != _args[0].ToString().ToLower() )
+                string Arg1 = _args[0].ToString();
+
+                if( "all" != Arg1.ToLower() )
                 {
-                    ReturnVal = "This is not implemented yet"; 
+                    if( _CswDbCfgInfoNbt.AccessIds.Contains( Arg1 ) )
+                    {
+                        ReturnVal = _updateAccessId( Arg1 );
+                    }
+                    else
+                    {
+                        ReturnVal = "AccessId " + Arg1 + " unknown";
+                    }
                 }
                 else
                 {
+
                     ReturnVal = "This is not implemented yet";
                 }
             }
             else
             {
-                ReturnVal = _Help; 
+                ReturnVal = _Help;
             }//
 
 
             return ( ReturnVal );
         }
+
+        private string _updateAccessId( string AccessId )
+        {
+            string ReturnVal = string.Empty;
+
+            _CswNbtResources.AccessId = AccessId;
+
+            if( _CswSchemaUpdater.LatestVersion != _CswSchemaUpdater.TargetVersion )
+            {
+                Console.WriteLine( "Updating AccessId " + AccessId + " to schema " + _CswSchemaUpdater.TargetVersion.ToString() );
+                _CswSchemaUpdateThread.start();
+                while( UpdateState.Complete != _CswSchemaUpdateThread.UpdateState )
+                {
+                    Console.Write( " ." );
+                    Thread.Sleep( 1000 );
+                }
+
+                ReturnVal = _CswSchemaUpdateThread.Message;
+
+            }
+            else
+            {
+                ReturnVal = NuLine + _VersionInfo + NuLine + NuLine + "AccessId " + AccessId + ": Schema version -- " + _CswSchemaUpdater.LatestVersion.ToString() + "-- is current";
+            }
+
+            return ( ReturnVal );
+
+        }//_updateAccessId() 
 
     }//CswSchemaUpdaterConsole
 
