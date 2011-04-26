7cef95aa4d52afd82f889783cf038fa897366862
Christopher Froehlich <cfroehlich@chemsw.com>
2011-04-26 19:47:30 +0400

    Automated commit for release: Horatio.5 2011.4.26.1

--- NbtConfig/Properties/AssemblyInfo.cs
+++ NbtConfig/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion( "4.2.1.5" )]
-[assembly: AssemblyFileVersion( "4.2.1.5" )]
+[assembly: AssemblyVersion( "2011.4.26.1" )]
+[assembly: AssemblyFileVersion( "2011.4.26.1" )]
