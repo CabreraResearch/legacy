e8483407f862eaf3f877758597554e0a2d947c9c
Christopher Froehlich <cfroehlich@chemsw.com>
2011-04-29 19:24:19 +0400

    Update to my schema script.

--- Schema/_christopher_reset_nbt_to_nbt_master.bat
+++ Schema/_christopher_reset_nbt_to_nbt_master.bat
@@ -2,4 +2,4 @@ echo exit | sqlplus cf_nbt/nbt@madeye @nbt_nuke.sql
 
 impdp.exe cf_nbt/nbt@madeye DUMPFILE=NBT_MASTER_11g.dmp DIRECTORY=NBTDUMPS REMAP_SCHEMA=nbt_master:cf_nbt
 
-echo exit | sqlplus cf_nbt/nbt@madeye @nbt_finalize_01H24_ora.sql
+echo exit | sqlplus cf_nbt/nbt@madeye @nbt_finalize_ora.sql
