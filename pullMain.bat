687506255a0177a5814df47015d79ed958eaeecc
Steven Salter <ssalter@chemsw.com>
2011-05-06 00:16:01 +0400

    took the drive letter off for compatibility

--- pullMain.bat
+++ pullMain.bat
@@ -1,6 +1,6 @@
-hg pull -R c:/kiln/Common/CswCommon -u main
-hg pull -R c:/kiln/Common/CswConfigUI -u main
-hg pull -R c:/kiln/Common/CswWebControls -u main
-hg pull -R c:/kiln/Common/CswLogService -u main
-hg pull -R c:/kiln/nbt/nbt -u main
+hg pull -R /kiln/Common/CswCommon -u main
+hg pull -R /kiln/Common/CswConfigUI -u main
+hg pull -R /kiln/Common/CswWebControls -u main
+hg pull -R /kiln/Common/CswLogService -u main
+hg pull -R /kiln/nbt/nbt -u main
 pause
