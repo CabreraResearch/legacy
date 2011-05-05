4e435763980686804c7e2f495b5d940fb87d2969
Steven Salter <ssalter@chemsw.com>
2011-05-05 04:28:33 +0400

    more post merge cleanup

--- NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01H34.cs
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01H34.cs
@@ -14,11 +14,15 @@ namespace ChemSW.Nbt.Schema
 	public class CswUpdateSchemaTo01H34 : ICswUpdateSchemaTo
 	{
 		private CswNbtSchemaModTrnsctn _CswNbtSchemaModTrnsctn;
+		private CswProdUpdtRsrc _CswProdUpdtRsrc = null; 
 
 		public CswSchemaVersion SchemaVersion { get { return new CswSchemaVersion( 1, 'H', 34 ); } }
+		public string Description { get { return ( _CswProdUpdtRsrc.makeTestCaseDescription( SchemaVersion ) ); } }
+
 		public CswUpdateSchemaTo01H34( CswNbtSchemaModTrnsctn CswNbtSchemaModTrnsctn )
 		{
 			_CswNbtSchemaModTrnsctn = CswNbtSchemaModTrnsctn;
+            _CswProdUpdtRsrc = new CswProdUpdtRsrc( _CswNbtSchemaModTrnsctn );
 		}
 
 		public void update()
