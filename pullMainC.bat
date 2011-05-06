a1587c2822d2aa86e5af21f4a46d3fb883bd7dcc
Christopher Froehlich <cfroehlich@chemsw.com>
2011-05-06 18:20:14 +0400

    No longer needed

--- pullMainC.bat
+++
@@ -1,7 +0,0 @@
-set /p KilnDrive=
-
-hg pull -R %KilnDrive%:/kiln/Common/CswCommon -u main
-hg pull -R %KilnDrive%:/kiln/Common/CswConfigUI -u main
-hg pull -R %KilnDrive%:/kiln/Common/CswWebControls -u main
-hg pull -R %KilnDrive%:/kiln/Common/CswLogService -u main
-hg pull -R %KilnDrive%:/kiln/nbt/nbt -u main
