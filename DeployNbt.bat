025696b8fd6f4f3e46f7cce7678deb5a4656f41d
Christopher Froehlich <cfroehlich@chemsw.com>
2011-05-06 18:20:05 +0400

    Deploy Nbt now performs ASP precompile.

--- DeployNbt.bat
+++ DeployNbt.bat
@@ -1,3 +1,13 @@
 set /p ThisVersionNo=
 
 DeployNbt.pl %ThisVersionNo% > C:\nbtlog\dailylog.txt 2>&1
+
+echo "Deploy Finished"
+
+pause
+
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\aspnet_compiler.exe" -v /NbtWebApp -p "c:\kiln\Nbt\Nbt\NbtWebApp"
+
+echo "ASP Precompile Finished"
+
+pause
