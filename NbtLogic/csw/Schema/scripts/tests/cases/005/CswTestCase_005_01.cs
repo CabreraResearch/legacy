3b9f1f4b07b93a6a2595a816f31326972ea29f52
Phil Glaser <pglaser@chemsw.com>
2011-05-09 17:22:30 +0400

    case 21547: beginning of test case 005

---
+++ NbtLogic/csw/Schema/scripts/tests/cases/005/CswTestCase_005_01.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Text;
+using ChemSW.Nbt;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Exceptions;
+using ChemSW.DB;
+using ChemSW.Nbt.Schema;
+using ChemSW.Core;
+
+namespace ChemSW.Nbt.Schema
+{
+
+    public class CswTestCase_005_01 : ICswUpdateSchemaTo
+    {
+
+
+        private CswNbtSchemaModTrnsctn _CswNbtSchemaModTrnsctn;
+
+        public string Description { get { return ( _CswTstCaseRsrc.makeTestCaseDescription( this.GetType().Name, _CswTstCaseRsrc_005.Purpose, "verify test data tear-down" ) ); } }
+
+        private CswTestCaseRsrc _CswTstCaseRsrc = null;
+        private CswTstCaseRsrc_005 _CswTstCaseRsrc_005 = null;
+
+        private CswSchemaVersion _CswSchemaVersion = null;
+        public CswSchemaVersion SchemaVersion { get { return ( _CswSchemaVersion ); } }
+        public CswTestCase_005_01( CswNbtSchemaModTrnsctn CswNbtSchemaModTrnsctn, CswSchemaVersion CswSchemaVersion )
+        {
+            _CswSchemaVersion = CswSchemaVersion;
+            _CswNbtSchemaModTrnsctn = CswNbtSchemaModTrnsctn;
+            _CswTstCaseRsrc = new CswTestCaseRsrc( _CswNbtSchemaModTrnsctn );
+            _CswTstCaseRsrc_005 = new CswTstCaseRsrc_005( _CswNbtSchemaModTrnsctn );
+        }//ctor
+
+        public void update()
+        {
+
+
+        }//runTest()
+
+    }//CswSchemaUpdaterTestCaseDropColumnRollback
+
+}//ChemSW.Nbt.Schema
