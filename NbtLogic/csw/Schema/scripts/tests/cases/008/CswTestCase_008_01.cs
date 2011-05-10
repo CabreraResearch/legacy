169edf894ea8109cf7bcf86cc5585f9fd43541a7
Phil Glaser <pglaser@chemsw.com>
2011-05-10 17:32:50 +0400

    case 21547: test case 008

---
+++ NbtLogic/csw/Schema/scripts/tests/cases/008/CswTestCase_008_01.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Text;
+using ChemSW.Nbt;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Exceptions;
+using ChemSW.DB;
+using ChemSW.Nbt.Schema;
+using ChemSW.Core;
+
+namespace ChemSW.Nbt.Schema
+{
+
+    public class CswTestCase_008_01 : ICswUpdateSchemaTo
+    {
+
+
+        private CswNbtSchemaModTrnsctn _CswNbtSchemaModTrnsctn;
+
+        public string Description { get { return ( _CswTstCaseRsrc.makeTestCaseDescription( this.GetType().Name, _CswTstCaseRsrc_008.Purpose, "verify read" ) ); } }
+
+        private CswTestCaseRsrc _CswTstCaseRsrc = null;
+        private CswTstCaseRsrc_008 _CswTstCaseRsrc_008 = null;
+
+        private CswSchemaVersion _CswSchemaVersion = null;
+        public CswSchemaVersion SchemaVersion { get { return ( _CswSchemaVersion ); } }
+        public CswTestCase_008_01( CswNbtSchemaModTrnsctn CswNbtSchemaModTrnsctn, CswSchemaVersion CswSchemaVersion )
+        {
+            _CswSchemaVersion = CswSchemaVersion;
+            _CswNbtSchemaModTrnsctn = CswNbtSchemaModTrnsctn;
+            _CswTstCaseRsrc = new CswTestCaseRsrc( _CswNbtSchemaModTrnsctn );
+            _CswTstCaseRsrc_008 = new CswTstCaseRsrc_008( _CswNbtSchemaModTrnsctn );
+        }//ctor
+
+        public void update()
+        {
+            _CswNbtSchemaModTrnsctn.CswDataDictionary.setCurrentColumn( "nodetype_props", "nodetypepropid" );
+
+            Int32 TableColId = _CswNbtSchemaModTrnsctn.CswDataDictionary.TableColId;
+
+            if( TableColId <= 0 )
+            {
+                throw ( new CswDniException( "tableclid received from data dictionry is suspect: " + TableColId.ToString() ) );
+            }
+
+        }//runTest()
+
+    }//CswSchemaUpdaterTestCaseDropColumnRollback
+
+}//ChemSW.Nbt.Schema
