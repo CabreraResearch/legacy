1d01d3a82d8f829d6f834bb0bd47906cecced074
Phil Glaser <pglaser@chemsw.com>
2011-05-10 20:31:58 +0400

    case 21547: added test case 010

---
+++ NbtLogic/csw/Schema/scripts/tests/rsrcs/CswTstCaseRsrc_010.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using ChemSW.Nbt;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Exceptions;
+//using ChemSW.RscAdo;
+using ChemSW.DB;
+using ChemSW.Core;
+using ChemSW.Nbt.Schema;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Test Case: 001, part 01
+    /// </summary>
+    public class CswTstCaseRsrc_010
+    {
+
+        private CswNbtSchemaModTrnsctn _CswNbtSchemaModTrnsctn;
+
+        private CswTestCaseRsrc _CswTestCaseRsrc = null;
+        public CswTstCaseRsrc_010( CswNbtSchemaModTrnsctn CswNbtSchemaModTrnsctn )
+        {
+            _CswNbtSchemaModTrnsctn = CswNbtSchemaModTrnsctn;
+            _CswTestCaseRsrc = new CswTestCaseRsrc( _CswNbtSchemaModTrnsctn );
+        }//ctor
+
+
+        public string Purpose = "Update Preserve Blob Column";
+
+    }//CswSchemaUpdaterTestCaseDropColumnRollback
+
+}//ChemSW.Nbt.Schema
