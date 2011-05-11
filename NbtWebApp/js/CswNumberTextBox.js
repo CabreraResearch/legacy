daaee93ce6a18c23a97a246e968f2196d0e7cc74
Christopher Froehlich <cfroehlich@chemsw.com>
2011-05-11 23:04:31 +0400

    References while we're in here.

--- NbtWebApp/js/CswNumberTextBox.js
+++ NbtWebApp/js/CswNumberTextBox.js
@@ -1,5 +1,9 @@
-﻿; (function ($)
-{
+﻿/// <reference path="../jquery/jquery-1.6-vsdoc.js" />
+/// <reference path="../jquery/linq.js_ver2.2.0.2/linq-vsdoc.js" />
+/// <reference path="../jquery/linq.js_ver2.2.0.2/jquery.linq-vsdoc.js" />
+/// <reference path="_Global.js" />
+
+; (function ($) { /// <param name="$" type="jQuery" />
 
 	var PluginName = 'CswNumberTextBox';
 
