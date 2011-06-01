cc06b4652d7c194eef6734767a5864ff06becb38
Phil Glaser <pglaser@chemsw.com>
2011-06-02 00:57:47 +0400

    case 21597: added test case 023 for rollback back of audit data

---
+++ NbtLogic/csw/Schema/scripts/tests/cases/023/CswTestCase_023_04.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Text;
+using ChemSW.Nbt;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Exceptions;
+using ChemSW.DB;
+using ChemSW.Nbt.Schema;
+using ChemSW.Core;
+using ChemSW.Audit;
+
+namespace ChemSW.Nbt.Schema
+{
+
+    public class CswTestCase_023_04 : ICswUpdateSchemaTo
+    {
+
+
+        private CswNbtSchemaModTrnsctn _CswNbtSchemaModTrnsctn;
+
+        public string Description { get { return ( _CswTstCaseRsrc.makeTestCaseDescription( this.GetType().Name, _CswTstCaseRsrc_023.Purpose, "clean up test data" ) ); } }
+
+        private CswTestCaseRsrc _CswTstCaseRsrc = null;
+        private CswTstCaseRsrc_023 _CswTstCaseRsrc_023 = null;
+
+        private CswSchemaVersion _CswSchemaVersion = null;
+        public CswSchemaVersion SchemaVersion { get { return ( _CswSchemaVersion ); } }
+        public CswTestCase_023_04( CswNbtSchemaModTrnsctn CswNbtSchemaModTrnsctn, CswSchemaVersion CswSchemaVersion, object CswTstCaseRsrc )
+        {
+            _CswSchemaVersion = CswSchemaVersion;
+            _CswNbtSchemaModTrnsctn = CswNbtSchemaModTrnsctn;
+            _CswTstCaseRsrc = new CswTestCaseRsrc( _CswNbtSchemaModTrnsctn );
+            _CswTstCaseRsrc_023 = (CswTstCaseRsrc_023) CswTstCaseRsrc;
+
+        }//ctor
+
+
+        public void update()
+        {
+            CswAuditMetaData CswAuditMetaData = new CswAuditMetaData();
+            _CswNbtSchemaModTrnsctn.dropTable( _CswTstCaseRsrc_023.ArbitraryTableName_01 );
+            _CswNbtSchemaModTrnsctn.dropTable( CswAuditMetaData.makeAuditTableName( _CswTstCaseRsrc_023.ArbitraryTableName_01 ) );
+            _CswTstCaseRsrc_023.restoreAuditSetting(); 
+        }//runTest()
+
+    }//CswSchemaUpdaterTestCaseDropColumnRollback
+
+}//ChemSW.Nbt.Schema
---
+++ NbtLogic/csw/Schema/scripts/tests/cases/023/CswTestCase_023_05.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Text;
+using ChemSW.Nbt;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Exceptions;
+using ChemSW.DB;
+using ChemSW.Nbt.Schema;
+using ChemSW.Core;
+using ChemSW.Audit;
+
+namespace ChemSW.Nbt.Schema
+{
+
+    public class CswTestCase_023_05 : ICswUpdateSchemaTo
+    {
+
+
+        private CswNbtSchemaModTrnsctn _CswNbtSchemaModTrnsctn;
+
+        public string Description { get { return ( _CswTstCaseRsrc.makeTestCaseDescription( this.GetType().Name, _CswTstCaseRsrc_023.Purpose, "verify test data was cleaned up" ) ); } }
+
+        private CswTestCaseRsrc _CswTstCaseRsrc = null;
+        private CswTstCaseRsrc_023 _CswTstCaseRsrc_023 = null;
+
+        private CswSchemaVersion _CswSchemaVersion = null;
+        public CswSchemaVersion SchemaVersion { get { return ( _CswSchemaVersion ); } }
+        public CswTestCase_023_05( CswNbtSchemaModTrnsctn CswNbtSchemaModTrnsctn, CswSchemaVersion CswSchemaVersion, object CswTstCaseRsrc )
+        {
+            _CswSchemaVersion = CswSchemaVersion;
+            _CswNbtSchemaModTrnsctn = CswNbtSchemaModTrnsctn;
+            _CswTstCaseRsrc = new CswTestCaseRsrc( _CswNbtSchemaModTrnsctn );
+            _CswTstCaseRsrc_023 = (CswTstCaseRsrc_023) CswTstCaseRsrc;
+
+        }//ctor
+
+
+        public void update()
+        {
+            CswAuditMetaData CswAuditMetaData = new CswAuditMetaData();
+            _CswTstCaseRsrc.assertTableIsAbsent( _CswTstCaseRsrc_023.ArbitraryTableName_01 );
+            _CswTstCaseRsrc.assertTableIsAbsent( CswAuditMetaData.makeAuditTableName( _CswTstCaseRsrc_023.ArbitraryTableName_01 ) );
+            _CswTstCaseRsrc_023.assertAuditSettingIsRestored(); 
+        }//runTest()
+
+    }//CswSchemaUpdaterTestCaseDropColumnRollback
+
+}//ChemSW.Nbt.Schema
