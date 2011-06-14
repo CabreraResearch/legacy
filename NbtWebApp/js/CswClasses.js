8f313985e38e6cc6b4d30b1efd806d12c2a4c424
Christopher Froehlich <cfroehlich@chemsw.com>
2011-06-14 23:20:53 +0400

    CswArray works. Not in use atm.

--- NbtWebApp/js/CswClasses.js
+++ NbtWebApp/js/CswClasses.js
@@ -3,6 +3,8 @@
 ///// <reference path="../js/thirdparty/js/linq.js_ver2.2.0.2/jquery.linq-vsdoc.js" />
 ///// <reference path="_Global.js" />
 
+
+//#region CswString
 function CswString(string)
 {
     var value = string;
@@ -22,3 +24,43 @@ function CswString(string)
 }
 
 CswString.prototype.toString = function () { return this.value; };
+
+//#endregion CswString
+
+//#region CswArray
+function CswArray()
+{
+    var ctor = [];
+    ctor.push.apply(arr, arguments);
+    ctor.__proto__ = CswArray.prototype;
+    return ctor;
+}
+CswArray.prototype = new Array;
+CswArray.prototype.last = function ()
+{
+    return this[this.length - 1];
+};
+
+//#endregion CswArray
+
+//#region CswLocalStorage
+//function CswLocalStorage()
+//{
+//    localStorage.clear();
+//    var ctor = localStorage;
+//    ctor.__proto__ = CswLocalStorage.prototype;
+//    return ctor;
+//}
+
+//CswLocalStorage.prototype.parseArray = function (name, arr)
+//{
+//    log(name, true);
+//    log(arr, true);
+//    if (arguments.length === 2)
+//    {
+//        this[name] = JSON.stringify(arr);
+//    }
+
+//    return JSON.parse(this[name]);
+//};
+//#endregion CswLocalStorage
