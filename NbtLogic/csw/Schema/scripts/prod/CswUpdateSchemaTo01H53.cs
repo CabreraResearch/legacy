9fca841e08592b6c60d5beec29975e88aa5da109
Steven Salter <ssalter@chemsw.com>
2011-06-24 20:46:20 +0400

    case 22339

--- NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01H53.cs
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01H53.cs
@@ -28,11 +28,43 @@ namespace ChemSW.Nbt.Schema
 
 		public void update()
 		{
+			// case 22339
+			// this failed in 01H-52
+			// update Location views
+			CswNbtMetaDataNodeType SiteNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "Site" );
+			CswNbtMetaDataNodeType BuildingNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "Building" );
+			CswNbtMetaDataNodeType FloorNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "Floor" );
+			CswNbtMetaDataNodeType RoomNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "Room" );
+			CswNbtMetaDataNodeType CabinetNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "Cabinet" );
+			CswNbtMetaDataNodeType ShelfNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "Shelf" );
+			CswNbtMetaDataNodeType BoxNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "Box" );
+
+			//Locations
+			foreach( CswNbtView LocationsView in _CswNbtSchemaModTrnsctn.restoreViews( "Locations" ) )
+			{
+				LocationsView.Root.ChildRelationships.Clear();
+
+				CswNbtViewRelationship SiteRel = _addLocationToView( LocationsView, null, SiteNT );
+				CswNbtViewRelationship BuildingRel = _addLocationToView( LocationsView, SiteRel, BuildingNT );
+				CswNbtViewRelationship FloorRel = _addLocationToView( LocationsView, BuildingRel, FloorNT );
+				CswNbtViewRelationship RoomRel = _addLocationToView( LocationsView, FloorRel, RoomNT );
+				CswNbtViewRelationship CabinetRel = _addLocationToView( LocationsView, RoomRel, CabinetNT );
+				CswNbtViewRelationship ShelfRel = _addLocationToView( LocationsView, CabinetRel, ShelfNT );
+				CswNbtViewRelationship BoxRel = _addLocationToView( LocationsView, ShelfRel, BoxNT );
+
+				CswNbtViewRelationship RoomRel2 = _addLocationToView( LocationsView, BuildingRel, RoomNT );
+				CswNbtViewRelationship CabinetRel2 = _addLocationToView( LocationsView, RoomRel2, CabinetNT );
+				CswNbtViewRelationship ShelfRel2 = _addLocationToView( LocationsView, CabinetRel2, ShelfNT );
+				CswNbtViewRelationship BoxRel2 = _addLocationToView( LocationsView, ShelfRel2, BoxNT );
+
+				LocationsView.save();
+			} // if( LocationsView != null )
+
 
 			// case 22176
 
 			//New Inspection Status action
-			_CswNbtSchemaModTrnsctn.createAction( CswNbtActionName.Inspection_Status, true, "", "" );
+			_CswNbtSchemaModTrnsctn.createAction( CswNbtActionName.Inspection_Status, true, "", "Inspections" );
 
 			// Add permissions
 			CswNbtObjClassRole AdministratorRoleNode = CswNbtNodeCaster.AsRole( _CswNbtSchemaModTrnsctn.Nodes.makeRoleNodeFromRoleName( "Administrator" ) );
@@ -41,6 +73,48 @@ namespace ChemSW.Nbt.Schema
 			_CswNbtSchemaModTrnsctn.Permit.set( CswNbtActionName.Inspection_Status, ChemSWAdministratorRoleNode, true );
 
 		} // update()
+
+
+
+		private CswNbtViewRelationship _addLocationToView( CswNbtView View, CswNbtViewRelationship ParentRel, CswNbtMetaDataNodeType LocationNodeType )
+		{
+			CswNbtViewRelationship Rel = null;
+			if( LocationNodeType != null )
+			{
+				CswNbtMetaDataNodeTypeProp LocationNTP = LocationNodeType.getNodeTypePropByObjectClassPropName( CswNbtObjClassLocation.LocationPropertyName );
+				CswNbtMetaDataNodeTypeProp NameNTP = LocationNodeType.getNodeTypePropByObjectClassPropName( CswNbtObjClassLocation.NamePropertyName );
+				CswNbtMetaDataNodeTypeProp BarcodeNTP = LocationNodeType.getNodeTypePropByObjectClassPropName( CswNbtObjClassLocation.BarcodePropertyName );
+
+				if( ParentRel == null )
+				{
+					Rel = View.AddViewRelationship( LocationNodeType, true );
+				}
+				else
+				{
+					Rel = View.AddViewRelationship( ParentRel, CswNbtViewRelationship.PropOwnerType.Second, LocationNTP, true );
+				}
+
+				if( NameNTP != null )
+				{
+					CswNbtViewProperty NameViewProp = View.AddViewProperty( Rel, NameNTP );
+					View.AddViewPropertyFilter( NameViewProp,
+												NameNTP.FieldTypeRule.SubFields.Default.Name,
+												NameNTP.FieldTypeRule.SubFields.Default.DefaultFilterMode,
+												string.Empty,
+												false );
+				}
+				if( BarcodeNTP != null )
+				{
+					CswNbtViewProperty BarcodeViewProp = View.AddViewProperty( Rel, BarcodeNTP );
+					View.AddViewPropertyFilter( BarcodeViewProp,
+												BarcodeNTP.FieldTypeRule.SubFields.Default.Name,
+												BarcodeNTP.FieldTypeRule.SubFields.Default.DefaultFilterMode,
+												string.Empty,
+												false );
+				}
+			} // if( LocationNodetype != null )
+			return Rel;
+		} // _addLocationToView()
 
 	}//class CswUpdateSchemaTo01H53
 
