180a2b1980f1403d85c20c242b2039872953546c
Steven Salter <ssalter@chemsw.com>
2011-06-30 19:17:26 +0400

    case 22551

--- NbtWebApp/App_Code/CswNbtWebServiceTabsAndProps.cs
+++ NbtWebApp/App_Code/CswNbtWebServiceTabsAndProps.cs
@@ -279,15 +279,17 @@ namespace ChemSW.Nbt.WebServices
 			CswXmlDocument.AppendXmlAttribute( PropXmlNode, "required", "0" );
 			CswXmlDocument.AppendXmlAttribute( PropXmlNode, "readonly", "0" );
 
-			string SQL = @"select na.recordcreated as ChangeDate, na.auditeventtype as EventType
+			string SQL = @"select na.recordcreated as ChangeDate, na.auditeventtype as EventType, x.transactionusername as Username
  from nodes n
  join nodes_audit na on n.nodeid = na.nodeid
+ join audit_transactions x on na.audittransactionid = x.audittransactionid
  where n.nodeid = " + Node.NodeId.PrimaryKey.ToString() + @"
  UNION
- select ja.recordcreated as ChangeDate, ja.auditeventtype as EventType
+ select ja.recordcreated as ChangeDate, ja.auditeventtype as EventType, x.transactionusername as Username
  from nodes n
  join jct_nodes_props j on n.nodeid = j.nodeid
  join jct_nodes_props_audit ja on j.jctnodepropid = ja.jctnodepropid
+ join audit_transactions x on ja.audittransactionid = x.audittransactionid
  where n.nodeid = " + Node.NodeId.PrimaryKey.ToString() + @" 
  order by ChangeDate desc";
 
