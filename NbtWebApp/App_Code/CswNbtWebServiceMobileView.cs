823e8e6e92b73569c523cb61f50013c1c7c86f4a
Christopher Froehlich <cfroehlich@chemsw.com>
2011-07-06 21:36:35 +0400

    Case 22607. Evaluate node's status as well.

--- NbtWebApp/App_Code/CswNbtWebServiceMobileView.cs
+++ NbtWebApp/App_Code/CswNbtWebServiceMobileView.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Xml;
 using System.Collections.ObjectModel;
-using System.Data;
 using System.Linq;
+using System.Xml;
 using System.Xml.Linq;
 using ChemSW.Core;
-using ChemSW.Nbt.ObjClasses;
 using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.ObjClasses;
 using ChemSW.Nbt.PropTypes;
 using ChemSW.Nbt.Security;
-using ChemSW.NbtWebControls;
 
 namespace ChemSW.Nbt.WebServices
 {
@@ -31,13 +29,13 @@ namespace ChemSW.Nbt.WebServices
         {
             // All Views
             XElement RetXml = new XElement( "views" );
-			Collection<CswNbtView> MobileViews = _CswNbtResources.ViewSelect.getVisibleViews( string.Empty, CurrentUser, false, _ForMobile, false, NbtViewRenderingMode.Any );
-			foreach( CswNbtView MobileView in MobileViews )
-			{
-				RetXml.Add( new XElement( "view",
-						new XAttribute( "id", MobileView.ViewId ),
-						new XAttribute( "name", MobileView.ViewName ) ) );
-			}
+            Collection<CswNbtView> MobileViews = _CswNbtResources.ViewSelect.getVisibleViews( string.Empty, CurrentUser, false, _ForMobile, false, NbtViewRenderingMode.Any );
+            foreach( CswNbtView MobileView in MobileViews )
+            {
+                RetXml.Add( new XElement( "view",
+                        new XAttribute( "id", MobileView.ViewId ),
+                        new XAttribute( "name", MobileView.ViewName ) ) );
+            }
 
             return RetXml;
         } // Run()
@@ -47,8 +45,8 @@ namespace ChemSW.Nbt.WebServices
             XElement RetXml = new XElement( "node" );
 
             // Get the full XML for the entire view
-			CswNbtViewId NbtViewId = new CswNbtViewId( ViewId );
-			CswNbtView View = _CswNbtResources.ViewSelect.restoreView( NbtViewId );
+            CswNbtViewId NbtViewId = new CswNbtViewId( ViewId );
+            CswNbtView View = _CswNbtResources.ViewSelect.restoreView( NbtViewId );
 
             // case 20083
             if( _ForMobile )
@@ -65,8 +63,8 @@ namespace ChemSW.Nbt.WebServices
             else
             {
                 RetXml = new XElement( "node",
-							new XAttribute( "id", NodeIdPrefix + Int32.MinValue ),
-                            new XAttribute( "name", "No Results" ) 
+                            new XAttribute( "id", NodeIdPrefix + Int32.MinValue ),
+                            new XAttribute( "name", "No Results" )
                         );
             }
 
@@ -90,74 +88,74 @@ namespace ChemSW.Nbt.WebServices
         } // _getSearchNodes
 
 
-		private void _runTreeNodesRecursive( ICswNbtTree Tree, ref XElement ParentXmlNode )
-		{
-			for( Int32 c = 0; c < Tree.getChildNodeCount(); c++ )
-			{
-				Tree.goToNthChild( c );
+        private void _runTreeNodesRecursive( ICswNbtTree Tree, ref XElement ParentXmlNode )
+        {
+            for( Int32 c = 0; c < Tree.getChildNodeCount(); c++ )
+            {
+                Tree.goToNthChild( c );
 
-				CswNbtNode ThisNode = Tree.getNodeForCurrentPosition();
-				string ThisNodeName = Tree.getNodeNameForCurrentPosition();
-				string ThisNodeId = ThisNode.NodeId.ToString();
+                CswNbtNode ThisNode = Tree.getNodeForCurrentPosition();
+                string ThisNodeName = Tree.getNodeNameForCurrentPosition();
+                string ThisNodeId = ThisNode.NodeId.ToString();
 
-				XElement ThisXmlNode = new XElement( "node" );
-				XElement ThisSubItems = new XElement( "subitems" );
-				ThisXmlNode.Add( ThisSubItems );
+                XElement ThisXmlNode = new XElement( "node" );
+                XElement ThisSubItems = new XElement( "subitems" );
+                ThisXmlNode.Add( ThisSubItems );
 
-				_runTreeNodesRecursive( Tree, ref ThisSubItems );
+                _runTreeNodesRecursive( Tree, ref ThisSubItems );
 
-				if( Tree.getNodeShowInTreeForCurrentPosition() )
-				{
-					if( _ForMobile && Tree.getChildNodeCount() == 0 )   // is a leaf
-					{
-						_runProperties( ThisNode, ref ThisSubItems );
-					}
+                if( Tree.getNodeShowInTreeForCurrentPosition() )
+                {
+                    if( _ForMobile && Tree.getChildNodeCount() == 0 )   // is a leaf
+                    {
+                        _runProperties( ThisNode, ref ThisSubItems );
+                    }
 
-					if( _ForMobile )
-					{
-						ThisXmlNode.SetAttributeValue( "id", NodeIdPrefix + ThisNodeId );
-						ThisXmlNode.SetAttributeValue( "name", CswTools.SafeJavascriptParam( ThisNodeName ) );
-						ThisXmlNode.SetAttributeValue( "nodetype", CswTools.SafeJavascriptParam( ThisNode.NodeType.NodeTypeName ) );
-						ThisXmlNode.SetAttributeValue( "objectclass", CswTools.SafeJavascriptParam( ThisNode.ObjectClass.ObjectClass.ToString() ) );
-						ThisXmlNode.SetAttributeValue( "iconfilename", CswTools.SafeJavascriptParam( ThisNode.NodeType.IconFileName ) );
-					}
+                    if( _ForMobile )
+                    {
+                        ThisXmlNode.SetAttributeValue( "id", NodeIdPrefix + ThisNodeId );
+                        ThisXmlNode.SetAttributeValue( "name", CswTools.SafeJavascriptParam( ThisNodeName ) );
+                        ThisXmlNode.SetAttributeValue( "nodetype", CswTools.SafeJavascriptParam( ThisNode.NodeType.NodeTypeName ) );
+                        ThisXmlNode.SetAttributeValue( "objectclass", CswTools.SafeJavascriptParam( ThisNode.ObjectClass.ObjectClass.ToString() ) );
+                        ThisXmlNode.SetAttributeValue( "iconfilename", CswTools.SafeJavascriptParam( ThisNode.NodeType.IconFileName ) );
+                    }
 
-					// case 20083 - search values
-					foreach( CswNbtMetaDataNodeTypeProp MetaDataProp in ThisNode.NodeType.NodeTypeProps.Cast<CswNbtMetaDataNodeTypeProp>().Where( MetaDataProp => MetaDataProp.MobileSearch ) )
-					{
-					    if( MetaDataProp.ObjectClassProp != null )
-					    {
-					        //ret += " search_ocp_" + MetaDataProp.ObjectClassPropId.ToString() + "=\"" + CswTools.SafeJavascriptParam( ThisNode.Properties[MetaDataProp].Gestalt ) + "\"";
-					        ThisXmlNode.SetAttributeValue( "search_ocp_" + MetaDataProp.ObjectClassPropId.ToString(), CswTools.SafeJavascriptParam( ThisNode.Properties[MetaDataProp].Gestalt ) );
-					    }
-					    else
-					    {
-					        //ret += " search_ntp_" + MetaDataProp.PropId.ToString() + "=\"" + CswTools.SafeJavascriptParam( ThisNode.Properties[MetaDataProp].Gestalt ) + "\"";
-					        ThisXmlNode.SetAttributeValue( "search_ntp_" + MetaDataProp.PropId.ToString(), CswTools.SafeJavascriptParam( ThisNode.Properties[MetaDataProp].Gestalt ) );
-					    }
-					}
+                    // case 20083 - search values
+                    foreach( CswNbtMetaDataNodeTypeProp MetaDataProp in ThisNode.NodeType.NodeTypeProps.Cast<CswNbtMetaDataNodeTypeProp>().Where( MetaDataProp => MetaDataProp.MobileSearch ) )
+                    {
+                        if( MetaDataProp.ObjectClassProp != null )
+                        {
+                            //ret += " search_ocp_" + MetaDataProp.ObjectClassPropId.ToString() + "=\"" + CswTools.SafeJavascriptParam( ThisNode.Properties[MetaDataProp].Gestalt ) + "\"";
+                            ThisXmlNode.SetAttributeValue( "search_ocp_" + MetaDataProp.ObjectClassPropId.ToString(), CswTools.SafeJavascriptParam( ThisNode.Properties[MetaDataProp].Gestalt ) );
+                        }
+                        else
+                        {
+                            //ret += " search_ntp_" + MetaDataProp.PropId.ToString() + "=\"" + CswTools.SafeJavascriptParam( ThisNode.Properties[MetaDataProp].Gestalt ) + "\"";
+                            ThisXmlNode.SetAttributeValue( "search_ntp_" + MetaDataProp.PropId.ToString(), CswTools.SafeJavascriptParam( ThisNode.Properties[MetaDataProp].Gestalt ) );
+                        }
+                    }
 
-					//if( _ForMobile )
-					//{
-					//    ret += "><subitems>" + ThisSubItems + "</subitems>";
-					//    ret += "</node>";
-					//}
-					//else
-					//{
-					//    ret += ThisSubItems;
-					//    ret += "</item>";
-					//}
-					ParentXmlNode.Add( ThisXmlNode );
-				} // if( Tree.getNodeShowInTreeForCurrentPosition() )
-				else
-				{
-					//ret += ThisSubItems;
-					ParentXmlNode.Add( ThisSubItems );
-				}
-				Tree.goToParentNode();
-			}
-			//return ret;
-		} // _runTreeNodesRecursive()
+                    //if( _ForMobile )
+                    //{
+                    //    ret += "><subitems>" + ThisSubItems + "</subitems>";
+                    //    ret += "</node>";
+                    //}
+                    //else
+                    //{
+                    //    ret += ThisSubItems;
+                    //    ret += "</item>";
+                    //}
+                    ParentXmlNode.Add( ThisXmlNode );
+                } // if( Tree.getNodeShowInTreeForCurrentPosition() )
+                else
+                {
+                    //ret += ThisSubItems;
+                    ParentXmlNode.Add( ThisSubItems );
+                }
+                Tree.goToParentNode();
+            }
+            //return ret;
+        } // _runTreeNodesRecursive()
 
 
         private static void _runProperties( CswNbtNode Node, ref XElement SubItemsXmlNode )
@@ -166,36 +164,36 @@ namespace ChemSW.Nbt.WebServices
             {
                 foreach( CswNbtMetaDataNodeTypeProp Prop in Tab.NodeTypePropsByDisplayOrder )
                 {
-					if( !Prop.HideInMobile &&
-						Prop.FieldType.FieldType != CswNbtMetaDataFieldType.NbtFieldType.Password &&
-						Prop.FieldType.FieldType != CswNbtMetaDataFieldType.NbtFieldType.Grid ) // Case 20772
-					{
-						CswNbtNodePropWrapper PropWrapper = Node.Properties[Prop];
-						XmlDocument XmlDoc = new XmlDocument();
-						CswXmlDocument.SetDocumentElement( XmlDoc, "root" );
-						PropWrapper.ToXml( XmlDoc.DocumentElement );
+                    if( !Prop.HideInMobile &&
+                        Prop.FieldType.FieldType != CswNbtMetaDataFieldType.NbtFieldType.Password &&
+                        Prop.FieldType.FieldType != CswNbtMetaDataFieldType.NbtFieldType.Grid ) // Case 20772
+                    {
+                        CswNbtNodePropWrapper PropWrapper = Node.Properties[Prop];
+                        XmlDocument XmlDoc = new XmlDocument();
+                        CswXmlDocument.SetDocumentElement( XmlDoc, "root" );
+                        PropWrapper.ToXml( XmlDoc.DocumentElement );
+                        string ReadOnly = ( Node.ReadOnly || Prop.ReadOnly ) ? "true" : "false";
+                        XElement PropXElement = new XElement( "prop",
+                                            new XAttribute( "id", PropIdPrefix + Prop.PropId + "_" + NodeIdPrefix + Node.NodeId.ToString() ),
+                                            new XAttribute( "name", CswTools.SafeJavascriptParam( Prop.PropNameWithQuestionNo ) ),
+                                            new XAttribute( "tab", CswTools.SafeJavascriptParam( Tab.TabName ) ),
+                                            new XAttribute( "isreadonly", ReadOnly ),
+                                            new XAttribute( "fieldtype", Prop.FieldType.FieldType.ToString() ),
+                                            new XAttribute( "gestalt", CswTools.SafeJavascriptParam( PropWrapper.Gestalt ) ),
+                                            new XAttribute( "ocpname", CswTools.SafeJavascriptParam( PropWrapper.ObjectClassPropName ) ) );
+                        SubItemsXmlNode.Add( PropXElement );
+                        if( null != XmlDoc.DocumentElement )
+                            foreach( XElement ChildXElement in from XmlNode PropValueChildNode
+                                                                   in XmlDoc.DocumentElement.ChildNodes
+                                                               select XElement.Parse( PropValueChildNode.OuterXml ) )
+                            {
+                                PropXElement.Add( ChildXElement );
+                            }
+                    }
+                } //foreach( CswNbtMetaDataNodeTypeProp Prop in Tab.NodeTypePropsByDisplayOrder )
+            } // foreach( CswNbtMetaDataNodeTypeTab Tab in Node.NodeType.NodeTypeTabs )
 
-						XElement PropXElement = new XElement( "prop",
-											new XAttribute( "id", PropIdPrefix + Prop.PropId + "_" + NodeIdPrefix + Node.NodeId.ToString() ),
-											new XAttribute( "name", CswTools.SafeJavascriptParam( Prop.PropNameWithQuestionNo ) ),
-											new XAttribute( "tab", CswTools.SafeJavascriptParam( Tab.TabName ) ),
-											new XAttribute( "isreadonly", Prop.ReadOnly.ToString().ToLower() ),
-											new XAttribute( "fieldtype", Prop.FieldType.FieldType.ToString() ),
-											new XAttribute( "gestalt", CswTools.SafeJavascriptParam( PropWrapper.Gestalt ) ),
-											new XAttribute( "ocpname", CswTools.SafeJavascriptParam( PropWrapper.ObjectClassPropName ) ) );
-						SubItemsXmlNode.Add( PropXElement );
-					    if( null != XmlDoc.DocumentElement )
-					        foreach( XElement ChildXElement in from XmlNode PropValueChildNode 
-					                                               in XmlDoc.DocumentElement.ChildNodes 
-					                                           select XElement.Parse( PropValueChildNode.OuterXml ) )
-					        {
-					            PropXElement.Add( ChildXElement );
-					        }
-					}
-				} //foreach( CswNbtMetaDataNodeTypeProp Prop in Tab.NodeTypePropsByDisplayOrder )
-			} // foreach( CswNbtMetaDataNodeTypeTab Tab in Node.NodeType.NodeTypeTabs )
-
-		} // _runProperties()
+        } // _runProperties()
 
 
     } // class CswNbtWebServiceMobileView
