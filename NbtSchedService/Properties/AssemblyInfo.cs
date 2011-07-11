2f096883571d9fd110580c5d8d006b69079882f8
Automatic Build <autobuild@chemsw.com>
2011-07-11 08:26:03 +0400

    Automated commit for release: Horatio 2011.7.11.1

--- NbtSchedService/Properties/AssemblyInfo.cs
+++ NbtSchedService/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "2011.7.8.2" )]
-[assembly: AssemblyFileVersion( "2011.7.8.2" )]
+[assembly: AssemblyVersion( "2011.7.11.1" )]
+[assembly: AssemblyFileVersion( "2011.7.11.1" )]
