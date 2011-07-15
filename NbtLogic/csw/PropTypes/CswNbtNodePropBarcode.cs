3a09321c7585575d7d2a8c816e341e71c6958e50
Christopher Froehlich <cfroehlich@chemsw.com>
2011-07-15 21:46:12 +0400

    Re: comment 1 on review 22809. Case 22808.

--- NbtLogic/csw/PropTypes/CswNbtNodePropBarcode.cs
+++ NbtLogic/csw/PropTypes/CswNbtNodePropBarcode.cs
@@ -142,11 +142,7 @@ namespace ChemSW.Nbt.PropTypes
 
         public override void ReadXml( XmlNode XmlNode, Dictionary<Int32, Int32> NodeMap, Dictionary<Int32, Int32> NodeTypeMap )
         {
-            string ProspectiveBarcode = CswXmlDocument.ChildXmlNodeValueAsString( XmlNode, _BarcodeSubField.ToXmlNodeName() );
-            if( ProspectiveBarcode != string.Empty )
-            {
-                setBarcodeValueOverride( ProspectiveBarcode, false );
-            }
+            _setPropVals( CswXmlDocument.ChildXmlNodeValueAsString( XmlNode, _BarcodeSubField.ToXmlNodeName() ) );
         } // ReadXml()
 
         public override void ToXElement( XElement ParentNode )
