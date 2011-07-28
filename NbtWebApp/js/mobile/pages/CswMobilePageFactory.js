ec254de687a00794fc0eb5feabb93891177d569b
Christopher Froehlich <cfroehlich@chemsw.com>
2011-07-28 23:30:12 +0400

    Case 22944. Placeholders.

---
+++ NbtWebApp/js/mobile/pages/CswMobilePageFactory.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="/js/thirdparty/jquery/core/jquery-1.6.1-vsdoc.js" />
+/// <reference path="../../_Global.js" />
+/// <reference path="../../_CswPrototypeExtensions.js" />
+
+
+//#region CswMobilePageFactory
+
+function CswMobilePageFactory() {
+
+}
+
+//#endregion CswMobilePageFactory
---
+++ NbtWebApp/js/mobile/pages/CswMobilePageFooter.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="/js/thirdparty/jquery/core/jquery-1.6.1-vsdoc.js" />
+/// <reference path="../../_Global.js" />
+/// <reference path="../../_CswPrototypeExtensions.js" />
+/// <reference path="CswMobileMenuButton.js" />
+
+
+//#region CswMobilePageFooter
+
+function CswMobilePageFooter() {
+
+}
+
+//#endregion CswMobilePageFooter
