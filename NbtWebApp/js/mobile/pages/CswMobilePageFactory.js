e085a07d9942cf691837a1296583160333682225
Christopher Froehlich <cfroehlich@chemsw.com>
2011-08-02 00:24:34 +0400

    Case 22973. Implemented CswMobilePageFactory and CswMobilePageLogin.

--- NbtWebApp/js/mobile/pages/CswMobilePageFactory.js
+++ NbtWebApp/js/mobile/pages/CswMobilePageFactory.js
@@ -1,12 +1,93 @@
-﻿/// <reference path="/js/thirdparty/jquery/core/jquery-1.6.1-vsdoc.js" />
-/// <reference path="../../_Global.js" />
-/// <reference path="../../_CswPrototypeExtensions.js" />
-
+﻿/// <reference path="../../_Global.js" />
+/// <reference path="../../thirdparty/jquery/core/jquery-1.6.1-vsdoc.js" />
+/// <reference path="../../jquery/common/CswAttr.js" />
+/// <reference path="../controls/ICswMobileWebControls.js" />
+/// <reference path="../controls/CswMobilePageHeader.js" />
+/// <reference path="../controls/CswMobilePageFooter.js" />
+/// <reference path="../controls/CswMobileMenuButton.js" />
+/// <reference path="../../CswEnums.js" />
+/// <reference path="../CswMobileTools.js" />
 
 //#region CswMobilePageFactory
 
-function CswMobilePageFactory() {
+function CswMobilePageFactory(pageDef, $parent) {
+	/// <summary>
+	///   Page factory class. Responsible for generating a Mobile page.
+	/// </summary>
+	/// <param name="pageDef" type="Object">JSON definition of content to display, including header/footerDef properties.</param>
+	/// <param name="$parent" type="jQuery">Parent element to attach to.</param>
+	/// <returns type="CswMobilePageFactory">Instance of itself. Must instance with 'new' keyword.</returns>
 
+	//#region private
+	var mobileHeader, mobileFooter, $content;
+	
+	var p = {
+		ParentId: undefined,
+		level: 1,
+		DivId: '',       // required
+		HeaderText: '',
+		$content: $(''),
+		headerDef: { },
+		footerDef: { },
+	    theme: 'b'
+	};
+
+	if (pageDef) {
+		$.extend(p, pageDef);
+	}
+
+	p.DivId = makeSafeId({ ID: p.DivId });
+
+	var $pageDiv = $('#' + p.DivId);
+
+	var firstInit = (isNullOrEmpty($pageDiv) || $pageDiv.length === 0);
+			
+	if (firstInit) {
+		$pageDiv = $parent.CswDiv('init', { ID: p.DivId })
+			.CswAttrXml({
+					'data-role': 'page',
+					'data-url': p.DivId,
+					'data-title': p.HeaderText,
+					'data-rel': 'page'
+				});
+	}
+	var headerDef = {
+		buttons: { },
+		ID: p.DivId,
+		text: p.HeaderText,
+		dataId: 'csw_header',
+		dataTheme: p.theme
+	};
+	if(p.headerDef) $.extend(headerDef, p.headerDef);
+	
+	mobileHeader = new CswMobilePageHeader(headerDef, $pageDiv);
+
+	if(firstInit) {
+		$content = $pageDiv.CswDiv('init', { ID: p.DivId + '_content' })
+			.CswAttrXml({ 'data-role': 'content', 'data-theme': p.theme })
+			.append(p.$content);
+	}
+
+	var footerDef = {
+		buttons: { },
+		ID: p.DivId,
+		dataId: 'csw_footer',
+		dataTheme: p.theme
+	};
+	if(p.footerDef) $.extend(footerDef, p.footerDef);
+	
+	mobileFooter = new CswMobilePageFooter(footerDef, $pageDiv);
+			
+	//#endregion private	
+	
+	//#region public, priveleged
+
+	this.mobileHeader = mobileHeader;
+	this.mobileFooter = mobileFooter;
+	this.$content = $content;
+    this.$pageDiv = $pageDiv;
+	
+	//#region public, priveleged
 }
 
 //#endregion CswMobilePageFactory
---
+++ NbtWebApp/js/mobile/pages/CswMobilePageLogin.js
@@ -0,0 +1,127 @@
+﻿/// <reference path="../../_Global.js" />
+/// <reference path="../../thirdparty/jquery/core/jquery-1.6.1-vsdoc.js" />
+/// <reference path="../../jquery/common/CswAttr.js" />
+/// <reference path="../controls/ICswMobileWebControls.js" />
+/// <reference path="../controls/CswMobilePageHeader.js" />
+/// <reference path="../controls/CswMobilePageFooter.js" />
+/// <reference path="../controls/CswMobileMenuButton.js" />
+/// <reference path="../CswMobile.js" />
+/// <reference path="../CswMobileTools.js" />
+/// <reference path="../../CswEnums.js" />
+/// <reference path="../../jquery/common/CswCookie.js" />
+/// <reference path="CswMobilePageFactory.js" />
+
+//#region CswMobilePageLogin
+
+function CswMobilePageLogin(loginDef,$parent,mobileStorage,loginSuccess) {
+	/// <summary>
+	///   Login Page class. Responsible for generating a Mobile login page.
+	/// </summary>
+    /// <param name="loginDef" type="Object">Login definitional data.</param>
+	/// <param name="$parent" type="jQuery">Parent element to attach to.</param>
+    /// <param name="mobileStorage" type="CswMobileClientDbResources">Client DB Resources</param>
+    /// <param name="loginSuccess" type="Function">Function to execute on login success.</param>
+	/// <returns type="CswMobilePageLogin">Instance of itself. Must instance with 'new' keyword.</returns>
+
+	//#region private
+
+    var loginContent = '<p style="text-align: center;">Login to Mobile Inspection Manager</p>';
+	loginContent += '<input type="textbox" id="login_customerid" placeholder="Customer Id" /><br>';
+	loginContent += '<input type="textbox" id="login_username" placeholder="User Name" /><br>';
+	loginContent += '<input type="password" id="login_password" placeholder="Password" /><br>';
+	loginContent += '<a id="loginsubmit" data-role="button" data-identity="loginsubmit" data-url="loginsubmit" href="javascript:void(0);">Continue</a>';
+    
+    var authenticateUrl = '/NbtWebApp/wsNBT.asmx/Authenticate';
+    
+    var p = {
+		level: -1,
+		DivId: 'logindiv',       // required
+		HeaderText: 'ChemSW Live',
+        headerDef: { },
+        onSuccess: function() {},
+        theme: 'b',
+        $content: $(loginContent)
+    };
+    if(loginDef) $.extend(p, loginDef);
+
+    if( isNullOrEmpty(p.footerDef)) {
+        
+		p.footerDef = {
+		    buttons: {
+					fullsite: { ID: p.DivId + '_main',
+								text: 'Full Site',
+								href: 'Main.html', 
+								rel: 'external',
+								dataIcon: 'home' },
+					help: { ID: p.DivId + '_help',
+								text: 'Help',
+								dataIcon: 'info' }
+				}
+		};
+    }
+    
+    var loginDiv = new CswMobilePageFactory(p, $parent);
+    var loginHeader = loginDiv.mobileHeader;
+    var loginFooter = loginDiv.mobileFooter;
+    var $content = loginDiv.$content;
+    			
+	var loginFailure = mobileStorage.getItem('loginFailure');
+	if( !isNullOrEmpty(loginFailure) ) {
+		loginHeader.pageHeader(loginFailure)
+			       .css('color','yellow');
+	}
+			
+	$('#loginsubmit')
+		.unbind('click')
+		.bind('click', function() {
+			return startLoadingMsg(function() { onLoginSubmit(); });
+	});
+	$('#login_customerid').clickOnEnter($('#loginsubmit'));
+	$('#login_username').clickOnEnter($('#loginsubmit'));
+	$('#login_password').clickOnEnter($('#loginsubmit'));
+	
+	function onLoginSubmit() {
+		if (mobileStorage.amOnline()) {
+			var userName = $('#login_username').val();
+			var accessId = $('#login_customerid').val();
+
+			var ajaxData = {
+				'AccessId': accessId, //We're displaying "Customer ID" but processing "AccessID"
+				'UserName': userName,
+				'Password': $('#login_password').val(),
+				ForMobile: true
+			};
+
+			CswAjaxJSON({
+					formobile: true,
+					//async: false,
+					url: authenticateUrl,
+					data: ajaxData,
+					onloginfail: function(text) {
+						onLoginFail(text);
+					},
+					success: function(data) {
+					    if( !isNullOrEmpty(loginSuccess) ) {
+					        loginSuccess(data, userName, accessId);
+					    }
+					},
+					error: function() {
+						onError();
+					}
+				});
+		}
+
+	} //onLoginSubmit() 
+	
+	//#endregion private
+    
+    //#region public, priveleged
+
+    this.$content = $content;
+    this.mobileHeader = loginHeader;
+    this.mobileFooter = loginFooter;
+    this.$pageDiv = loginDiv.$pageDiv;
+    //#endregion public, priveleged
+}
+
+//#endregion CswMobilePageLogin
