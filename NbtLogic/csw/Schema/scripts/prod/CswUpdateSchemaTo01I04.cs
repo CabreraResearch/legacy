d09720003c80533ab877fce1c11a8cb0aaa88f38
Keith Baldwin <kbaldwin@chemsw.com>
2011-08-03 00:43:05 +0400

    case 22984

--- NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01I04.cs
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01I04.cs
@@ -36,13 +36,10 @@ namespace ChemSW.Nbt.Schema
             // 4. Add a property "Conversion Factor" to Unit Of Measure, which is a Scientific fieldtype value, that scales the quantity to the "Base Unit".  Each Unit of measure will have a conversion factor that converts its value and unit to a value in base units.  That way all values in a unit type, but different units of measure, can be compared
 
             // create the class
-            Int32 NewObjectClassId = _CswNbtSchemaModTrnsctn.createObjectClass("UnitTypeClass", "scales.gif", false, false);
+            Int32 NewUnitTypeClassId = _CswNbtSchemaModTrnsctn.createObjectClass("UnitTypeClass", "scales.gif", false, false);
 
             // refresh so that meta data has the new class
             _CswNbtSchemaModTrnsctn.MetaData.refreshAll();
-
-            // create the node type
-            CswNbtMetaDataNodeType NewNodeType = _CswNbtSchemaModTrnsctn.MetaData.makeNewNodeType("UnitTypeClass", "Unit Type", string.Empty);
 
             // get an updateable table so we can add properties to the classes
             CswTableUpdate OCPUpdate = _CswNbtSchemaModTrnsctn.makeCswTableUpdate("01I-04_OCP_Update", "object_class_props");
@@ -51,21 +48,47 @@ namespace ChemSW.Nbt.Schema
             // pull back the newly created Unit Type class
             CswNbtMetaDataObjectClass UnitTypeOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass(CswNbtMetaDataObjectClass.NbtObjectClass.UnitTypeClass);
 
+            // get UnitOfMeasure class
+            CswNbtMetaDataObjectClass UnitOfMeasureOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass(CswNbtMetaDataObjectClass.NbtObjectClass.UnitOfMeasureClass);
+   
             // add properties to UnitType class
             _CswNbtSchemaModTrnsctn.addObjectClassPropRow(OCPTable, UnitTypeOC.ObjectClassId, CswNbtObjClassUnitType.NamePropertyName, CswNbtMetaDataFieldType.NbtFieldType.Text, Int32.MinValue, Int32.MinValue);
-            _CswNbtSchemaModTrnsctn.addObjectClassPropRow(OCPTable, UnitTypeOC.ObjectClassId, CswNbtObjClassUnitType.BaseUnitPropertyName, CswNbtMetaDataFieldType.NbtFieldType.Relationship, Int32.MinValue, Int32.MinValue);
-
-            // we have to add properties to Unit Of Measure class -s o get UnitOfMeasure class
-            CswNbtMetaDataObjectClass UnitOfMeasureOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass(CswNbtMetaDataObjectClass.NbtObjectClass.UnitOfMeasureClass);
+            //_CswNbtSchemaModTrnsctn.addObjectClassPropRow(OCPTable, UnitTypeOC.ObjectClassId, CswNbtObjClassUnitType.BaseUnitPropertyName, CswNbtMetaDataFieldType.NbtFieldType.Relationship, Int32.MinValue, Int32.MinValue);
+            _CswNbtSchemaModTrnsctn.addObjectClassPropRow(OCPTable, UnitTypeOC, CswNbtObjClassUnitType.BaseUnitPropertyName, CswNbtMetaDataFieldType.NbtFieldType.Relationship, false, false, true, CswNbtViewRelationship.RelatedIdType.ObjectClassId.ToString(), UnitOfMeasureOC.ObjectClassId, false, false, false, false, string.Empty, Int32.MinValue, Int32.MinValue);
 
             // add properties to UnitOfMeasure class
-            _CswNbtSchemaModTrnsctn.addObjectClassPropRow(OCPTable, UnitOfMeasureOC.ObjectClassId, CswNbtObjClassUnitOfMeasure.UnitTypePropertyName, CswNbtMetaDataFieldType.NbtFieldType.Relationship, Int32.MinValue, Int32.MinValue);
+            //_CswNbtSchemaModTrnsctn.addObjectClassPropRow(OCPTable, UnitOfMeasureOC.ObjectClassId, CswNbtObjClassUnitOfMeasure.UnitTypePropertyName, CswNbtMetaDataFieldType.NbtFieldType.Relationship, Int32.MinValue, Int32.MinValue);
+            _CswNbtSchemaModTrnsctn.addObjectClassPropRow(OCPTable, UnitOfMeasureOC, CswNbtObjClassUnitOfMeasure.UnitTypePropertyName, CswNbtMetaDataFieldType.NbtFieldType.Relationship, false, false, true, CswNbtViewRelationship.RelatedIdType.ObjectClassId.ToString(), UnitTypeOC.ObjectClassId, false, false, false, false, string.Empty, Int32.MinValue, Int32.MinValue);
             _CswNbtSchemaModTrnsctn.addObjectClassPropRow(OCPTable, UnitOfMeasureOC.ObjectClassId, CswNbtObjClassUnitOfMeasure.ConversionFactorPropertyName, CswNbtMetaDataFieldType.NbtFieldType.Scientific, Int32.MinValue, Int32.MinValue);
 
             // save changes to properties
             OCPUpdate.update(OCPTable);
 
 			_CswNbtSchemaModTrnsctn.MetaData.makeMissingNodeTypeProps();
+
+            // create the UnitType node type
+            CswNbtMetaDataNodeType UnitTypeNT = _CswNbtSchemaModTrnsctn.MetaData.makeNewNodeType("UnitTypeClass", "Unit Type", string.Empty);
+
+            // Have the Node name based on the Name property
+            UnitTypeNT.NameTemplateText = CswNbtMetaData.MakeTemplateEntry(CswNbtObjClassUnitType.NamePropertyName);
+
+            // get Generic class to swap ForeignKey
+            CswNbtMetaDataObjectClass GenericOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass(CswNbtMetaDataObjectClass.NbtObjectClass.GenericClass);
+
+            // We have to set the property type again
+            CswNbtMetaDataNodeTypeProp BaseUnitNTP = UnitTypeNT.getNodeTypePropByObjectClassPropName(CswNbtObjClassUnitType.BaseUnitPropertyName);
+            CswNbtMetaDataNodeType UnitOfMeasureNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType("Unit of Measure");
+            CswNbtMetaDataNodeTypeProp UnitTypeNTP = UnitOfMeasureNT.getNodeTypePropByObjectClassPropName(CswNbtObjClassUnitOfMeasure.UnitTypePropertyName);
+
+            // We have to toggle the Foreign Key - so first set it to the wrong value
+            BaseUnitNTP.SetFK(CswNbtViewRelationship.RelatedIdType.NodeTypeId.ToString(), UnitOfMeasureNT.NodeTypeId, string.Empty, Int32.MinValue);
+             // now set it to the correct value
+            BaseUnitNTP.SetFK(CswNbtViewRelationship.RelatedIdType.ObjectClassId.ToString(), UnitOfMeasureOC.ObjectClassId, string.Empty, Int32.MinValue);
+
+            // We have to toggle the Foreign Key - so first set it to the wrong value
+            UnitTypeNTP.SetFK(CswNbtViewRelationship.RelatedIdType.NodeTypeId.ToString(), UnitTypeNT.NodeTypeId, string.Empty, Int32.MinValue);
+            // now set it to the correct value
+            UnitTypeNTP.SetFK(CswNbtViewRelationship.RelatedIdType.ObjectClassId.ToString(), UnitTypeOC.ObjectClassId, string.Empty, Int32.MinValue);
 
             //_CswNbtSchemaModTrnsctn.Permit.set(
             // Grant permission to Administrator
@@ -75,7 +98,7 @@ namespace ChemSW.Nbt.Schema
 												CswNbtPermit.NodeTypePermission.Create, 
 												CswNbtPermit.NodeTypePermission.Edit, 
 												CswNbtPermit.NodeTypePermission.View },
-                                        NewNodeType,
+                                        UnitTypeNT,
                                         CswNbtNodeCaster.AsRole(RoleNode),
                                         true);
             CswNbtNode RoleNode2 = _CswNbtSchemaModTrnsctn.Nodes.makeRoleNodeFromRoleName("chemsw_admin_role");
@@ -84,7 +107,7 @@ namespace ChemSW.Nbt.Schema
 												CswNbtPermit.NodeTypePermission.Create, 
 												CswNbtPermit.NodeTypePermission.Edit, 
 												CswNbtPermit.NodeTypePermission.View },
-                                        NewNodeType,
+                                        UnitTypeNT,
                                         CswNbtNodeCaster.AsRole(RoleNode2),
                                         true);
 
