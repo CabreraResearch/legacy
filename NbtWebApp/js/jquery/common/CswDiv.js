e8839cd68a511f776b6921529af704fadc3c24e0
Christopher Froehlich <cfroehlich@chemsw.com>
2011-08-04 18:42:57 +0400

    References.

--- NbtWebApp/js/jquery/common/CswDiv.js
+++ NbtWebApp/js/jquery/common/CswDiv.js
@@ -1,7 +1,5 @@
-﻿/// <reference path="../js/thirdparty/jquery/core/jquery-1.6.1-vsdoc.js" />
-/// <reference path="../js/thirdparty/js/linq.js_ver2.2.0.2/linq-vsdoc.js" />
-/// <reference path="../js/thirdparty/js/linq.js_ver2.2.0.2/jquery.linq-vsdoc.js" />
-/// <reference path="../_Global.js" />
+﻿/// <reference path="../../thirdparty/jquery/core/jquery-1.6.1-vsdoc.js" />
+/// <reference path="../../_Global.js" />
 
 ; (function ($) { /// <param name="$" type="jQuery" />
 	
