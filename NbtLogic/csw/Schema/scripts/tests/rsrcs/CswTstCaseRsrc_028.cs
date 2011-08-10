091f0ff03c291ba08a89dee118e584e2b5e690fb
Phil Glaser <pglaser@chemsw.com>
2011-08-10 18:33:36 +0400

    case 23063: Test cases for new multiple accessid infrastructure

---
+++ NbtLogic/csw/Schema/scripts/tests/rsrcs/CswTstCaseRsrc_028.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections;
+using System.Data;
+using System.Text;
+using System.Diagnostics;
+using ChemSW.Core;
+using ChemSW.Nbt;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Exceptions;
+//using ChemSW.RscAdo;
+using ChemSW.DB;
+using ChemSW.Nbt.Schema;
+using ChemSW.Audit;
+using ChemSW.Nbt.ObjClasses;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Test Case: 001, part 01
+    /// </summary>
+    public class CswTstCaseRsrc_028
+    {
+
+        private CswNbtSchemaModTrnsctn _CswNbtSchemaModTrnsctn;
+
+        private CswTestCaseRsrc _CswTestCaseRsrc = null;
+        private CswAuditMetaData _CswAuditMetaData = new CswAuditMetaData();
+        public Process Process = null;
+        public CswTstCaseRsrc_028( CswNbtSchemaModTrnsctn CswNbtSchemaModTrnsctn )
+        {
+
+            _CswNbtSchemaModTrnsctn = CswNbtSchemaModTrnsctn;
+            _CswTestCaseRsrc = new CswTestCaseRsrc( _CswNbtSchemaModTrnsctn );
+            Process = System.Diagnostics.Process.GetCurrentProcess();
+        }//ctor
+
+        public string Purpose = "Multiple resource objects per request cycle";
+
+
+    }//CswSchemaUpdaterTestCaseDropColumnRollback
+
+}//ChemSW.Nbt.Schema
