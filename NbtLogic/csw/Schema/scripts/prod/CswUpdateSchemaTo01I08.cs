36b2af8aabc720f7efcdd557f4b79bc9dedfb417
Keith Baldwin <kbaldwin@chemsw.com>
2011-08-15 19:41:25 +0400

    case 20939

---
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01I08.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using ChemSW.Core;
+using ChemSW.DB;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.ObjClasses;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Updates the schema to version 01I-08
+    /// </summary>
+    public class CswUpdateSchemaTo01I08 : ICswUpdateSchemaTo
+    {
+        private CswNbtSchemaModTrnsctn _CswNbtSchemaModTrnsctn;
+
+        private CswProdUpdtRsrc _CswProdUpdtRsrc = null;
+        public CswSchemaVersion SchemaVersion { get { return new CswSchemaVersion( 1, 'I', 08 ); } }
+        public CswUpdateSchemaTo01I08( CswNbtSchemaModTrnsctn CswNbtSchemaModTrnsctn )
+        {
+            _CswNbtSchemaModTrnsctn = CswNbtSchemaModTrnsctn;
+            _CswProdUpdtRsrc = new CswProdUpdtRsrc( _CswNbtSchemaModTrnsctn );
+        }
+
+
+        public string Description { get { return ( _CswProdUpdtRsrc.makeTestCaseDescription( SchemaVersion ) ); } }
+
+
+        public void update()
+        {
+
+        }//Update()
+
+    }//class CswUpdateSchemaTo01I08
+
+}//namespace ChemSW.Nbt.Schema
+
+
---
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01I09.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using ChemSW.Core;
+using ChemSW.DB;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.ObjClasses;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Updates the schema to version 01I-09
+    /// </summary>
+    public class CswUpdateSchemaTo01I09 : ICswUpdateSchemaTo
+    {
+        private CswNbtSchemaModTrnsctn _CswNbtSchemaModTrnsctn;
+
+        private CswProdUpdtRsrc _CswProdUpdtRsrc = null;
+        public CswSchemaVersion SchemaVersion { get { return new CswSchemaVersion( 1, 'I', 09 ); } }
+        public CswUpdateSchemaTo01I09( CswNbtSchemaModTrnsctn CswNbtSchemaModTrnsctn )
+        {
+            _CswNbtSchemaModTrnsctn = CswNbtSchemaModTrnsctn;
+            _CswProdUpdtRsrc = new CswProdUpdtRsrc( _CswNbtSchemaModTrnsctn );
+        }
+
+
+        public string Description { get { return ( _CswProdUpdtRsrc.makeTestCaseDescription( SchemaVersion ) ); } }
+
+
+        public void update()
+        {
+
+        }//Update()
+
+    }//class CswUpdateSchemaTo01I09
+
+}//namespace ChemSW.Nbt.Schema
+
+
