497a0f416c99b6be193888ca24494d7e31a95ad9
Automatic Build <autobuild@chemsw.com>
2011-08-18 08:21:42 +0400

    Automated commit for release: Iago 2011.8.18.1

--- NbtSchedService/Properties/AssemblyInfo.cs
+++ NbtSchedService/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "2011.8.17.3" )]
-[assembly: AssemblyFileVersion( "2011.8.17.3" )]
+[assembly: AssemblyVersion( "2011.8.18.1" )]
+[assembly: AssemblyFileVersion( "2011.8.18.1" )]
