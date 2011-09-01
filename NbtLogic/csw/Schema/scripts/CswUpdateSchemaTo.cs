82e765f0c093a743802610d741bf00ea818d26ae
Steven Salter <ssalter@chemsw.com>
2011-09-02 01:31:42 +0400

    case 23373

---
+++ NbtLogic/csw/Schema/scripts/CswUpdateSchemaTo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChemSW.Nbt.Schema
+{
+    public abstract class CswUpdateSchemaTo
+    {
+		protected CswNbtSchemaModTrnsctn _CswNbtSchemaModTrnsctn = null;
+		public CswNbtSchemaModTrnsctn CswNbtSchemaModTrnsctn
+		{
+			set { _CswNbtSchemaModTrnsctn = value; }
+		}
+
+		public abstract CswSchemaVersion SchemaVersion { get; }
+
+		public string Description
+		{
+			get
+			{
+				return "Update to schema version " + SchemaVersion.ToString();
+			}
+		}
+	
+		public abstract void update();
+	}
+}
