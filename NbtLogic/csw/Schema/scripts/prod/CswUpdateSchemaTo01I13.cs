ed6891a012647ca7fa734bb80953db3fa5f10cac
Steven Salter <ssalter@chemsw.com>
2011-09-02 18:36:31 +0400

    Moved my schema changes to script 01I-13

--- NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01I13.cs
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01I13.cs
@@ -1,45 +1,29 @@
 ﻿using System;
 using System.Data;
-using ChemSW.Nbt.MetaData;
-using ChemSW.DB;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using ChemSW.Core;
+using ChemSW.DB;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.ObjClasses;
+using ChemSW.Nbt.PropTypes;
+using ChemSW.Nbt.Security;
+using ChemSW.Nbt.Actions;
 
 namespace ChemSW.Nbt.Schema
 {
-    /// <summary>
-    /// Updates the schema to version 01I-01
-    /// </summary>
-    public class CswUpdateSchemaTo01I01 : CswUpdateSchemaTo
-    {
-        public override CswSchemaVersion SchemaVersion { get { return new CswSchemaVersion( 1, 'I', 01 ); } }
+	/// <summary>
+	/// Updates the schema to version 01I-13
+	/// </summary>
+	public class CswUpdateSchemaTo01I13 : CswUpdateSchemaTo
+	{
+		public override CswSchemaVersion SchemaVersion { get { return new CswSchemaVersion( 1, 'I', 13 ); } }
 
 		public override void update()
-        {
-            // This script is reserved for schema changes, 
-            // such as adding tables or columns, 
-            // which need to take place before any other changes can be made.
-
-            if( false == _CswNbtSchemaModTrnsctn.isColumnDefinedInDataBase( "jct_nodes_props", "field2_numeric" ) )
-            {
-                _CswNbtSchemaModTrnsctn.addDoubleColumn( "jct_nodes_props", "field2_numeric", "A second numeric value", false, false, 6 );
-            }
-
-            // case 8411
-            if( false == _CswNbtSchemaModTrnsctn.isColumnDefinedInDataBase( "nodetype_tabset", "firsttabversionid" ) )
-            {
-                _CswNbtSchemaModTrnsctn.addForeignKeyColumn( "nodetype_tabset", "firsttabversionid", "Foreign key to original tab version", false, false, "nodetype_tabset", "nodetypetabsetid" );
-            }
-            if( false == _CswNbtSchemaModTrnsctn.isColumnDefinedInDataBase( "nodetype_tabset", "priortabversionid" ) )
-            {
-                _CswNbtSchemaModTrnsctn.addForeignKeyColumn( "nodetype_tabset", "priortabversionid", "Foreign key to previous tab version", false, false, "nodetype_tabset", "nodetypetabsetid" );
-            }
-
-
-
-
+		{
 			// case 22960
 			// Change how we store layouts
-			// also in 01I-13
+			// also in 01I-01
 			string LayoutTableName = "nodetype_layout";
 			if( false == _CswNbtSchemaModTrnsctn.isTableDefinedInDataBase( LayoutTableName ) )
 			{
@@ -53,10 +37,10 @@ namespace ChemSW.Nbt.Schema
 				_CswNbtSchemaModTrnsctn.addLongColumn( LayoutTableName, "display_column", "Display Column", false, false );
 
 				// copy existing layouts from nodetype_props
-				CswTableSelect PropSelect = _CswNbtSchemaModTrnsctn.makeCswTableSelect( "01I-11_Prop_Select", "nodetype_props" );
+				CswTableSelect PropSelect = _CswNbtSchemaModTrnsctn.makeCswTableSelect( "01I-13_Prop_Select", "nodetype_props" );
 				DataTable PropTable = PropSelect.getTable();
 
-				CswTableUpdate LayoutUpdate = _CswNbtSchemaModTrnsctn.makeCswTableUpdate( "01I-11_Layout_Update", LayoutTableName );
+				CswTableUpdate LayoutUpdate = _CswNbtSchemaModTrnsctn.makeCswTableUpdate( "01I-13_Layout_Update", LayoutTableName );
 				DataTable LayoutTable = LayoutUpdate.getEmptyTable();
 				foreach( DataRow PropRow in PropTable.Rows )
 				{
@@ -90,15 +74,10 @@ namespace ChemSW.Nbt.Schema
 				_CswNbtSchemaModTrnsctn.dropColumn( "nodetype_props", "setvalonadd" );
 			} // if( false == _CswNbtSchemaModTrnsctn.isTableDefinedInDataBase( LayoutTableName ) )
 
+		} // Update()
 
-            //Case 23132
-            if( false == _CswNbtSchemaModTrnsctn.isColumnDefinedInDataBase( "session_data", "keepinquicklaunch" ) )
-            {
-                _CswNbtSchemaModTrnsctn.addBooleanColumn( "session_data", "keepinquicklaunch", "Determines whether to keep the item in User's Quick Launch", true, false );
-            }
-        }//Update()
 
-    }//class CswUpdateSchemaTo01I01
+	}//class CswUpdateSchemaTo01I13
 
 }//namespace ChemSW.Nbt.Schema
 
