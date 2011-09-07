39fedf7245a5d5ebab30e4f89e688c1ec7645075
Phil Glaser <pglaser@chemsw.com>
2011-09-07 22:17:04 +0400

    case 23373: fixed test case 012

--- NbtLogic/csw/Schema/scripts/tests/cases/012/CswTestCase_012_06.cs
+++ NbtLogic/csw/Schema/scripts/tests/cases/012/CswTestCase_012_06.cs
@@ -38,7 +38,10 @@ namespace ChemSW.Nbt.Schema
             Collection<OrderByClause> OrderByClauses = new Collection<OrderByClause>();
             OrderByClauses.Add( new OrderByClause( _CswTstCaseRsrc_012.FakePkColumnName, OrderByType.Ascending ) );
             CswTableSelect CswTableSelect = _CswNbtSchemaModTrnsctn.makeCswTableSelect( Description, _CswTstCaseRsrc_012.FakeTestTableName );
-            DataTable DataTable = _CswTstCaseRsrc_012.TheSuspectUpdateTablesUpdater.getTable( "where " + _CswTstCaseRsrc_012.FakePkColumnName + "> 0", OrderByClauses );
+
+            CswTableUpdate CswTableUpdate = _CswNbtSchemaModTrnsctn.makeCswTableUpdate( Description, _CswTstCaseRsrc_012.FakeTestTableName );
+            CswTableUpdate.StorageMode = StorageMode.Cached; // causes the rolback behavior we want
+            DataTable DataTable = CswTableUpdate.getTable( "where " + _CswTstCaseRsrc_012.FakePkColumnName + "> 0", OrderByClauses );
             
             if( DataTable.Rows[0][_CswTstCaseRsrc_012.FakeValColumnName].ToString() != _CswTstCaseRsrc_012.Val_Row_1 )
                 throw ( new CswDniException( "Row one does not have value " + _CswTstCaseRsrc_012.Val_Row_1 ) );
