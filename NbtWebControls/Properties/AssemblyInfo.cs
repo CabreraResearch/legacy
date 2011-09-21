de18edf7df696ea077ac17bba9f800feb7cf90f0
Automatic Build <955583a3-168d-41dc-afd6-77b3216a431b>
2011-09-21 08:20:57 +0400

    Automated commit for release: Iago_2011.9.21.1

--- NbtWebControls/Properties/AssemblyInfo.cs
+++ NbtWebControls/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion( "2011.9.20.1" )]
-[assembly: AssemblyFileVersion( "2011.9.20.1" )]
+[assembly: AssemblyVersion( "2011.9.21.1" )]
+[assembly: AssemblyFileVersion( "2011.9.21.1" )]
