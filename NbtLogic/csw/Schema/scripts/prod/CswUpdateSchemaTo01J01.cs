529a8c46f8ad25e130c0d93cba8e98ccc7af6243
Steven Salter <ssalter@chemsw.com>
2011-09-30 21:55:21 +0400

    Prep for Juliet

---
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01J01.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using ChemSW.Nbt.MetaData;
+using ChemSW.DB;
+using ChemSW.Core;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Updates the schema to version 01J-01
+    /// </summary>
+    public class CswUpdateSchemaTo01J01 : CswUpdateSchemaTo
+    {
+        public override CswSchemaVersion SchemaVersion { get { return new CswSchemaVersion( 1, 'J', 01 ); } }
+		public override string Description { get { return "Update to schema version " + SchemaVersion.ToString(); } }
+
+		public override void update()
+        {
+            // This script is reserved for schema changes, 
+            // such as adding tables or columns, 
+            // which need to take place before any other changes can be made.
+
+
+		
+		
+		}//Update()
+
+    }//class CswUpdateSchemaTo01J01
+
+}//namespace ChemSW.Nbt.Schema
+
+
