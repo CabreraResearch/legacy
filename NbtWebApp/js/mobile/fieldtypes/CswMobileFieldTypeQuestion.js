71bdfc1f51aad7db5b4e902789bf6fef15923950
Christopher Froehlich <cfroehlich@chemsw.com>
2011-10-03 23:36:06 +0400

    Case 23718. Fixed some issues with the display of CorrectiveAction. This also fixes some of the footer position display issues on the property page.

--- NbtWebApp/js/mobile/fieldtypes/CswMobileFieldTypeQuestion.js
+++ NbtWebApp/js/mobile/fieldtypes/CswMobileFieldTypeQuestion.js
@@ -18,7 +18,7 @@ function CswMobileFieldTypeQuestion(ftDef) {
 
     var divSuffix = '_propdiv',
         propSuffix = '_input',
-        $content, contentDivId, elementId, propId, propName, subfields, value, gestalt, compliantAnswers;
+        $content, contentDivId, elementId, propId, propName, subfields, value, gestalt, compliantAnswers, correctiveAction;
     
     //ctor
     (function () {
@@ -38,7 +38,6 @@ function CswMobileFieldTypeQuestion(ftDef) {
             answer = tryParseString(propVals.answer),
             allowedAnswers = tryParseString(propVals.allowedanswers).split(','),
             comments = tryParseString(propVals[subfields.Comments.name]),
-            correctiveAction = tryParseString(propVals[subfields.CorrectiveAction.name]),
             suffix = 'ans',
             $fieldset, answerName, i, answerid, $answer, isCompliant, $prop;
         
@@ -46,8 +45,7 @@ function CswMobileFieldTypeQuestion(ftDef) {
         propName = p.propName;
         contentDivId = propId + divSuffix;
         elementId = propId + propSuffix;
-        
-        
+        correctiveAction = tryParseString(propVals[subfields.CorrectiveAction.name]),        
         
         value = tryParseString(propVals[subfields.Answer.name]);
         gestalt = tryParseString(p.gestalt, '');
@@ -83,8 +81,8 @@ function CswMobileFieldTypeQuestion(ftDef) {
                 }
             }
         }
-        isCompliant = inCompliance(answer,correctiveAction);
-        $prop = $('<div data-role="collapsible" class="' + CswMobileCssClasses.collapsible.name + '" data-collapsed="' + isCompliant + '"><h3>Comments</h3></div>')
+        isCompliant = inCompliance(answer);
+        $prop = $('<div id="' + propId + '_collapsible" data-role="collapsible" class="' + CswMobileCssClasses.collapsible.name + '" data-collapsed="' + (isCompliant && isNullOrEmpty(correctiveAction)) + '"><h3>Comments</h3></div>')
             .appendTo($content);
 
         $('<textarea id="' + propId + '_cor" name="' + propId + '_cor" placeholder="Corrective Action">' + correctiveAction + '</textarea>')
@@ -96,15 +94,15 @@ function CswMobileFieldTypeQuestion(ftDef) {
         subfields.comments = propId + '_com';
     })(); //ctor
     
-    function inCompliance(currentAnswer,correctiveAction) {
+    function inCompliance(currentAnswer) {
         var ret = true;
         if (isNullOrEmpty(currentAnswer)) {
-            currentAnswer = $content.find('input:checked').val();
+            currentAnswer = tryParseString($content.find('input:checked').val());
         }
         if (isNullOrEmpty(correctiveAction)) {
-            correctiveAction = $content.find('#' + propId + '_cor').val();
+            correctiveAction = tryParseString($content.find('#' + propId + '_cor').val());
         }
-        if (!isNullOrEmpty(currentAnswer) &&
+        if (false === isNullOrEmpty(currentAnswer) &&
             compliantAnswers.indexOf(currentAnswer) === -1 &&
                 isNullOrEmpty(correctiveAction)) {
             ret = false;
@@ -113,13 +111,22 @@ function CswMobileFieldTypeQuestion(ftDef) {
     }
     
     function applyFieldTypeLogicToContent($control) {
-        if (!isNullOrEmpty($control)) {
-            if (inCompliance()) {
-                $control.find('#' + propId + '_cor').css('display','none').hide();
+        var $collapsible = $control.find('#' + propId + '_collapsible'),
+            compliant = inCompliance();
+        if (false === isNullOrEmpty($control)) {
+            if (compliant) {
                 $control.find('h2').removeClass(CswMobileCssClasses.OOC.name);
             } else {
                 $control.find('#' + propId + '_cor').css('display','').show();
                 $control.find('h2').addClass(CswMobileCssClasses.OOC.name);
+            }
+            if(isNullOrEmpty(correctiveAction) && compliant) {
+                $collapsible.CswAttrXml('data-collapsed', true);
+                $collapsible.trigger('collapse');
+                $control.find('#' + propId + '_cor').css('display', 'none').hide();
+            } else {
+                $collapsible.CswAttrXml('data-collapsed', false);
+                $collapsible.trigger('expand');
             }
         }
         //fixGeometry();
@@ -134,6 +141,7 @@ function CswMobileFieldTypeQuestion(ftDef) {
             subFieldToUpdate = subfields.Answer.name;
         } 
         else if (id.contains(makeSafeId({ ID: propId, suffix: 'cor' }))) {
+            correctiveAction = newValue;
             subFieldToUpdate = subfields.CorrectiveAction.name;
         }
 
