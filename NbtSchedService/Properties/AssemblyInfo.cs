61731c19d782585778b9acc7a7b0c57df4ab3eb9
Automatic Build <955583a3-168d-41dc-afd6-77b3216a431b>
2011-10-06 19:39:27 +0400

    Automated commit for release: Iago_2011.10.6.4

--- NbtSchedService/Properties/AssemblyInfo.cs
+++ NbtSchedService/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "2011.10.6.3" )]
-[assembly: AssemblyFileVersion( "2011.10.6.3" )]
+[assembly: AssemblyVersion( "2011.10.6.4" )]
+[assembly: AssemblyFileVersion( "2011.10.6.4" )]
