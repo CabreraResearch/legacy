8e9602771a5522d48f3f77503a8628a6eb1a4dca
Christopher Froehlich <cfroehlich@chemsw.com>
2011-10-14 22:20:49 +0400

    Case 23782.

--- NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01J02.cs
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01J02.cs
@@ -1,5 +1,8 @@
 ﻿
 
+using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.ObjClasses;
+
 namespace ChemSW.Nbt.Schema
 {
     /// <summary>
@@ -18,6 +21,65 @@ namespace ChemSW.Nbt.Schema
                 View.Category = "System";
                 View.save();
             }
+
+            //Case 23782
+            CswNbtMetaDataNodeType RouteNt = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "Physical Inspection Route" );
+            if( null != RouteNt )
+            {
+                CswNbtMetaDataNodeTypeProp MpGridNtp = RouteNt.getNodeTypeProp( "FE Inspection Points Grid" );
+                if( null != MpGridNtp && MpGridNtp.FieldType.FieldType == CswNbtMetaDataFieldType.NbtFieldType.Grid )
+                {
+                    if( null != MpGridNtp.ViewId )
+                    {
+                        CswNbtView MpgView = _CswNbtSchemaModTrnsctn.restoreView( MpGridNtp.ViewId );
+                        CswNbtView BackupView = _CswNbtSchemaModTrnsctn.restoreView( MpGridNtp.ViewId );
+
+                        CswNbtMetaDataNodeType PiNt = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "FE Inspection Point" );
+                        if( null != PiNt )
+                        {
+                            CswNbtMetaDataNodeTypeProp RouteNtp = PiNt.getNodeTypeProp( "Route" );
+                            if( null != RouteNtp )
+                            {
+                                MpgView.Clear();
+                                CswNbtViewRelationship RouteRel = MpgView.AddViewRelationship( RouteNt, false );
+                                CswNbtViewRelationship InspectPointRel = MpgView.AddViewRelationship( RouteRel, CswNbtViewRelationship.PropOwnerType.Second, RouteNtp, false );
+
+                                CswNbtMetaDataNodeTypeProp BarcodeNtp = PiNt.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionTarget.BarcodePropertyName );
+                                MpgView.AddViewProperty( InspectPointRel, BarcodeNtp );
+
+                                CswNbtMetaDataNodeTypeProp DescNtp = PiNt.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionTarget.DescriptionPropertyName );
+                                MpgView.AddViewProperty( InspectPointRel, DescNtp );
+
+                                CswNbtMetaDataNodeTypeProp LocationNtp = PiNt.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionTarget.LocationPropertyName );
+                                MpgView.AddViewProperty( InspectPointRel, LocationNtp );
+
+                                CswNbtMetaDataNodeTypeProp LastIDateNtp = PiNt.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionTarget.LastInspectionDatePropertyName );
+                                MpgView.AddViewProperty( InspectPointRel, LastIDateNtp );
+
+                                CswNbtMetaDataNodeTypeProp StatusNtp = PiNt.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionTarget.StatusPropertyName );
+                                MpgView.AddViewProperty( InspectPointRel, StatusNtp );
+
+                                MpgView.ViewName = BackupView.ViewName;
+                                MpgView.ViewMode = BackupView.ViewMode;
+                                MpgView.Visibility = BackupView.Visibility;
+                                MpgView.VisibilityRoleId = BackupView.VisibilityRoleId;
+                                MpgView.VisibilityUserId = BackupView.VisibilityUserId;
+                                MpgView.Width = BackupView.Width;
+                                MpgView.Category = MpgView.Category;
+                                MpgView.save();
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            //foreach( CswNbtView View in _CswNbtSchemaModTrnsctn.restoreViews( "Mount Points Grid" ) )
+            //{
+            //    if( View.ViewMode == NbtViewRenderingMode.Grid && 
+            //        View.Visibility == NbtViewVisibility.Property && 
+            //        View.Root. )
+            //}
 
         }//Update()
 
