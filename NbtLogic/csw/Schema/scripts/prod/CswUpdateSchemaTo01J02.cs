f1bfa29d54b14a343d78503b9496d5b9034ed8d6
Christopher Froehlich <cfroehlich@chemsw.com>
2011-10-14 19:31:28 +0400

    Case 23809.

---
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01J02.cs
@@ -0,0 +1,29 @@
+﻿
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Updates the schema to version 01J-02
+    /// </summary>
+    public class CswUpdateSchemaTo01J02 : CswUpdateSchemaTo
+    {
+        public override CswSchemaVersion SchemaVersion { get { return new CswSchemaVersion( 1, 'J', 02 ); } }
+        public override string Description { get { return "Update to schema version " + SchemaVersion.ToString(); } }
+
+        public override void update()
+        {
+            // This script is reserved for schema changes, 
+            // such as adding tables or columns, 
+            // which need to take place before any other changes can be made.
+
+            foreach( CswNbtView View in _CswNbtSchemaModTrnsctn.restoreViews( "Roles and Users" ) )
+            {
+                View.Category = "System";
+                View.save();
+            }
+        }//Update()
+
+    }//class CswUpdateSchemaTo01J02
+
+}//namespace ChemSW.Nbt.Schema
+
+
