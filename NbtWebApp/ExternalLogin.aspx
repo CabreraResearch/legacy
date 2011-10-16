61801c9837b702b73b91c46aaf6c8071b266532b
Steven Salter <ssalter@chemsw.com>
2011-10-16 23:28:43 +0400

    case 23879

---
+++ NbtWebApp/ExternalLogin.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ExternalLogin.aspx.cs" Inherits="ChemSW.Nbt.WebPages.ExternalLogin" %>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+	<!--#include file="MainIncludes.html" -->
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+		Please Wait...
+
+		<script language="javascript">
+			<asp:PlaceHolder ID="JSPlaceHolder" runat="server"></asp:PlaceHolder>
+		</script>
+	</div>
+    </form>
+</body>
+</html>
---
+++ NbtWebApp/ExternalLogin.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChemSW.Nbt.WebPages
+{
+	public partial class ExternalLogin : System.Web.UI.Page
+	{
+		protected void Page_Load( object sender, EventArgs e )
+		{
+			string username = Request.Form["username"];
+			string password = Request.Form["password"];
+			string accessid = Request.Form["accessid"];
+
+			if( false == String.IsNullOrEmpty( username ) &&
+				false == String.IsNullOrEmpty( password ) &&
+				false == String.IsNullOrEmpty( accessid ) )
+			{
+				Literal JSLiteral = new Literal();
+				JSLiteral.Text = @"
+							$(document).ready(function () {
+								var dataJson = {
+									AccessId: '" + accessid + @"', 
+									UserName: '" + username + @"', 
+									Password: '" + password + @"',
+									ForMobile: false
+								};
+
+								CswAjaxJson({
+											url: '/NbtWebApp/wsNBT.asmx/authenticate',
+											data: dataJson,
+											success: function (data) 
+												{
+													$.CswCookie('set', CswCookieName.Username, dataJson.UserName);
+													window.location = 'Main.html';
+												},
+											onloginfail: function(txt) 
+												{
+													window.location = 'Main.html';
+												},
+											error: function() 
+												{
+													window.location = 'Main.html';
+												}
+								});
+							});";
+				JSPlaceHolder.Controls.Add( JSLiteral );
+			}
+			else
+			{
+				Response.Redirect( "Main.html" );
+			}
+		} // Page_Load()
+	}
+}
