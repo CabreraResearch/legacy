ba50d86ac167c3b3144b2c3f3eb3a54501255b4e
Christopher Froehlich <cfroehlich@chemsw.com>
2011-10-20 00:05:17 +0400

    Review 23926.

--- NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01J06.cs
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01J06.cs
@@ -22,37 +22,40 @@ namespace ChemSW.Nbt.Schema
             {
                 foreach( CswNbtView PiView in _CswNbtSchemaModTrnsctn.restoreViews( "Physical Inspections" ) )
                 {
-                    PiView.Root.ChildRelationships.Clear();
-                    CswNbtMetaDataNodeTypeProp DueDateNtp = PiNt.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.DatePropertyName );
-                    CswNbtMetaDataNodeTypeProp StatusNtp = PiNt.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.StatusPropertyName );
-                    CswNbtMetaDataNodeTypeProp TargetNtp = PiNt.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.TargetPropertyName );
-                    CswNbtMetaDataNodeTypeProp LocationNtp = PiNt.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.LocationPropertyName );
+                    if( PiView.ViewMode == NbtViewRenderingMode.Grid && PiView.Visibility != NbtViewVisibility.Property )
+                    {
+                        PiView.Root.ChildRelationships.Clear();
+                        CswNbtMetaDataNodeTypeProp DueDateNtp = PiNt.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.DatePropertyName );
+                        CswNbtMetaDataNodeTypeProp StatusNtp = PiNt.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.StatusPropertyName );
+                        CswNbtMetaDataNodeTypeProp TargetNtp = PiNt.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.TargetPropertyName );
+                        CswNbtMetaDataNodeTypeProp LocationNtp = PiNt.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionDesign.LocationPropertyName );
 
-                    CswNbtMetaDataNodeTypeProp IpGroupNtp = IpNt.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionTarget.InspectionTargetGroupPropertyName );
+                        CswNbtMetaDataNodeTypeProp IpGroupNtp = IpNt.getNodeTypePropByObjectClassPropName( CswNbtObjClassInspectionTarget.InspectionTargetGroupPropertyName );
 
-                    CswNbtViewRelationship PiRel = PiView.AddViewRelationship( PiNt, false );
-                    CswNbtViewRelationship IpRel = PiView.AddViewRelationship( PiRel, CswNbtViewRelationship.PropOwnerType.First, TargetNtp, false );
+                        CswNbtViewRelationship PiRel = PiView.AddViewRelationship( PiNt, false );
+                        CswNbtViewRelationship IpRel = PiView.AddViewRelationship( PiRel, CswNbtViewRelationship.PropOwnerType.First, TargetNtp, false );
 
-                    CswNbtViewProperty Dp = PiView.AddViewProperty( PiRel, DueDateNtp );
-                    Dp.Order = 1;
+                        CswNbtViewProperty Dp = PiView.AddViewProperty( PiRel, DueDateNtp );
+                        Dp.Order = 1;
 
-                    CswNbtViewProperty Sp = PiView.AddViewProperty( PiRel, StatusNtp );
-                    Sp.Order = 2;
-                    PiView.AddViewPropertyFilter( Sp, StatusNtp.FieldTypeRule.SubFields.Default.Name, CswNbtPropFilterSql.PropertyFilterMode.Equals, string.Empty, false );
+                        CswNbtViewProperty Sp = PiView.AddViewProperty( PiRel, StatusNtp );
+                        Sp.Order = 2;
+                        PiView.AddViewPropertyFilter( Sp, StatusNtp.FieldTypeRule.SubFields.Default.Name, CswNbtPropFilterSql.PropertyFilterMode.Equals, string.Empty, false );
 
-                    CswNbtViewProperty Tp = PiView.AddViewProperty( PiRel, TargetNtp );
-                    Tp.Order = 3;
-                    PiView.AddViewPropertyFilter( Tp, TargetNtp.FieldTypeRule.SubFields.Default.Name, CswNbtPropFilterSql.PropertyFilterMode.Equals, string.Empty, false );
+                        CswNbtViewProperty Tp = PiView.AddViewProperty( PiRel, TargetNtp );
+                        Tp.Order = 3;
+                        PiView.AddViewPropertyFilter( Tp, TargetNtp.FieldTypeRule.SubFields.Default.Name, CswNbtPropFilterSql.PropertyFilterMode.Equals, string.Empty, false );
 
-                    CswNbtViewProperty Lp = PiView.AddViewProperty( PiRel, LocationNtp );
-                    Lp.Order = 4;
-                    PiView.AddViewPropertyFilter( Lp, LocationNtp.FieldTypeRule.SubFields.Default.Name, CswNbtPropFilterSql.PropertyFilterMode.Equals, string.Empty, false );
+                        CswNbtViewProperty Lp = PiView.AddViewProperty( PiRel, LocationNtp );
+                        Lp.Order = 4;
+                        PiView.AddViewPropertyFilter( Lp, LocationNtp.FieldTypeRule.SubFields.Default.Name, CswNbtPropFilterSql.PropertyFilterMode.Equals, string.Empty, false );
 
-                    CswNbtViewProperty Ip = PiView.AddViewProperty( IpRel, IpGroupNtp );
-                    Ip.Order = 5;
-                    PiView.AddViewPropertyFilter( Ip, IpGroupNtp.FieldTypeRule.SubFields.Default.Name, CswNbtPropFilterSql.PropertyFilterMode.Equals, string.Empty, false );
+                        CswNbtViewProperty Ip = PiView.AddViewProperty( IpRel, IpGroupNtp );
+                        Ip.Order = 5;
+                        PiView.AddViewPropertyFilter( Ip, IpGroupNtp.FieldTypeRule.SubFields.Default.Name, CswNbtPropFilterSql.PropertyFilterMode.Equals, string.Empty, false );
 
-                    PiView.save();
+                        PiView.save();
+                    }
                 }
             }
 
