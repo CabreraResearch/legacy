736e60995890c43bc8e3b6d69ab9bf44aefe7331
Christopher Froehlich <cfroehlich@chemsw.com>
2011-10-20 04:07:05 +0400

    Fix merge errors.

--- NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01J06.cs
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01J06.cs
@@ -1,44 +1,40 @@
-﻿using System;
-using System.Data;
-using ChemSW.Core;
-using ChemSW.DB;
+﻿using ChemSW.Core;
+using ChemSW.Nbt.Actions;
 using ChemSW.Nbt.MetaData;
 using ChemSW.Nbt.ObjClasses;
-using ChemSW.Nbt.Actions;
 
 namespace ChemSW.Nbt.Schema
 {
-	/// <summary>
-	/// Updates the schema to version 01J-06
-	/// </summary>
-	public class CswUpdateSchemaTo01J06 : CswUpdateSchemaTo
-	{
-		public override CswSchemaVersion SchemaVersion { get { return new CswSchemaVersion( 1, 'J', 06 ); } }
-		public override string Description { get { return "Update to schema version " + SchemaVersion.ToString(); } }
+    /// <summary>
+    /// Updates the schema to version 01J-06
+    /// </summary>
+    public class CswUpdateSchemaTo01J06 : CswUpdateSchemaTo
+    {
+        public override CswSchemaVersion SchemaVersion { get { return new CswSchemaVersion( 1, 'J', 06 ); } }
+        public override string Description { get { return "Update to schema version " + SchemaVersion.ToString(); } }
 
-		public override void update()
-		{
+        public override void update()
+        {
 
-			// case 23920 - Grant permissions to new action to administrator roles
-			CswNbtMetaDataObjectClass RoleOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswNbtMetaDataObjectClass.NbtObjectClass.RoleClass );
-			foreach( CswNbtNode RoleNode in RoleOC.getNodes( false, true ) )
-			{
-				CswNbtObjClassRole RoleNodeAsRole = (CswNbtObjClassRole) CswNbtNodeCaster.AsRole( RoleNode );
-				if( RoleNodeAsRole.Administrator.Checked == Tristate.True || RoleNodeAsRole.Name.Text == CswNbtObjClassRole.ChemSWAdminRoleName )
-				{
-					_CswNbtSchemaModTrnsctn.Permit.set( CswNbtActionName.Sessions, RoleNodeAsRole, true );
-				}
-			}
-			
-			 //Case 23888
+            // case 23920 - Grant permissions to new action to administrator roles
+            CswNbtMetaDataObjectClass RoleOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswNbtMetaDataObjectClass.NbtObjectClass.RoleClass );
+            foreach( CswNbtNode RoleNode in RoleOC.getNodes( false, true ) )
+            {
+                CswNbtObjClassRole RoleNodeAsRole = (CswNbtObjClassRole) CswNbtNodeCaster.AsRole( RoleNode );
+                if( RoleNodeAsRole.Administrator.Checked == Tristate.True || RoleNodeAsRole.Name.Text == CswNbtObjClassRole.ChemSWAdminRoleName )
+                {
+                    _CswNbtSchemaModTrnsctn.Permit.set( CswNbtActionName.Sessions, RoleNodeAsRole, true );
+                }
+            }
+
+            //Case 23888
 
             CswNbtMetaDataNodeType PiNt = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "Physical Inspection" );
             CswNbtMetaDataNodeType IpNt = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "FE Inspection Point" );
 
             if( null != PiNt && null != IpNt )
             {
-                List<CswNbtView> PiViews = _CswNbtSchemaModTrnsctn.restoreViews( "Physical Inspections" );
-                foreach( CswNbtView PiView in PiViews )
+                foreach( CswNbtView PiView in _CswNbtSchemaModTrnsctn.restoreViews( "Physical Inspections" ) )
                 {
                     if( PiView.ViewMode == NbtViewRenderingMode.Grid && PiView.Visibility != NbtViewVisibility.Property )
                     {
@@ -81,9 +77,9 @@ namespace ChemSW.Nbt.Schema
             }
 
 
-		}//Update()
+        }//Update()
 
-	}//class CswUpdateSchemaTo01J06
+    }//class CswUpdateSchemaTo01J06
 
 }//namespace ChemSW.Nbt.Schema
 
