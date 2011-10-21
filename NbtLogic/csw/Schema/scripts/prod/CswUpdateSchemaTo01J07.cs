52877b0596ecf08307a95ce664ad92211ff4ce68
Christopher Froehlich <cfroehlich@chemsw.com>
2011-10-21 18:00:14 +0400

    Review 23944. Comment 1.

--- NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01J07.cs
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01J07.cs
@@ -13,27 +13,42 @@ namespace ChemSW.Nbt.Schema
         public override void update()
         {
             #region Case 23666
-            CswNbtNode CswAdminNode = _CswNbtSchemaModTrnsctn.Nodes.makeRoleNodeFromRoleName( CswNbtObjClassRole.ChemSWAdminRoleName );
+            CswNbtNode CswAdminRoleNode = _CswNbtSchemaModTrnsctn.Nodes.makeRoleNodeFromRoleName( CswNbtObjClassRole.ChemSWAdminRoleName );
+            CswNbtNode CswAdminUserNode = _CswNbtSchemaModTrnsctn.Nodes.makeUserNodeFromUsername( CswNbtObjClassUser.ChemSWAdminUsername );
+            CswNbtView RoleUserView = null;
+            bool RuViewExists = false;
             foreach( CswNbtView RauView in _CswNbtSchemaModTrnsctn.restoreViews( "Roles and Users" ) )
             {
-                if( RauView.ViewMode == NbtViewRenderingMode.Tree && RauView.Visibility != NbtViewVisibility.Property )
+                if( null == RoleUserView && RauView.ViewMode == NbtViewRenderingMode.Tree && RauView.Visibility != NbtViewVisibility.Property )
                 {
-                    CswNbtView NewRauView = _CswNbtSchemaModTrnsctn.makeView();
-                    NewRauView.makeNew( "Roles and Users", NbtViewVisibility.Role, CswAdminNode.NodeId, null, RauView.ViewId.get() );
-                    NewRauView.save();
-                    break;
+                    RoleUserView = RauView;
                 }
+                RuViewExists = ( RuViewExists || RauView.VisibilityRoleId == CswAdminRoleNode.NodeId || RauView.VisibilityUserId == CswAdminUserNode.NodeId );
             }
 
+            if( false == RuViewExists && null != RoleUserView )
+            {
+                CswNbtView NewRauView = _CswNbtSchemaModTrnsctn.makeView();
+                NewRauView.makeNew( "Roles and Users", NbtViewVisibility.Role, CswAdminRoleNode.NodeId, null, RoleUserView.ViewId.get() );
+                NewRauView.save();
+            }
+
+            CswNbtView UserView = null;
+            bool UlViewExists = false;
             foreach( CswNbtView UlView in _CswNbtSchemaModTrnsctn.restoreViews( "User List" ) )
             {
-                if( UlView.ViewMode == NbtViewRenderingMode.Grid && UlView.Visibility != NbtViewVisibility.Property )
+                if( null == UserView && UlView.ViewMode == NbtViewRenderingMode.Grid && UlView.Visibility != NbtViewVisibility.Property )
                 {
-                    CswNbtView NewUlView = _CswNbtSchemaModTrnsctn.makeView();
-                    NewUlView.makeNew( "User List", NbtViewVisibility.Role, CswAdminNode.NodeId, null, UlView.ViewId.get() );
-                    NewUlView.save();
-                    break;
+                    UserView = UlView;
                 }
+                UlViewExists = ( UlViewExists || UlView.VisibilityRoleId == CswAdminRoleNode.NodeId || UlView.VisibilityUserId == CswAdminUserNode.NodeId );
+            }
+
+            if( false == UlViewExists && null != UserView )
+            {
+                CswNbtView NewUlView = _CswNbtSchemaModTrnsctn.makeView();
+                NewUlView.makeNew( "User List", NbtViewVisibility.Role, CswAdminRoleNode.NodeId, null, UserView.ViewId.get() );
+                NewUlView.save();
             }
             #endregion Case 23666
         }//Update()
