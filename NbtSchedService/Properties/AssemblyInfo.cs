09acfea74a834e5878fff5f8cdf2198f41a43024
Automatic Build <955583a3-168d-41dc-afd6-77b3216a431b>
2011-10-21 18:33:23 +0400

    Automated commit for release: Juliet_2011.10.21.1

--- NbtSchedService/Properties/AssemblyInfo.cs
+++ NbtSchedService/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "2011.10.20.1" )]
-[assembly: AssemblyFileVersion( "2011.10.20.1" )]
+[assembly: AssemblyVersion( "2011.10.21.1" )]
+[assembly: AssemblyFileVersion( "2011.10.21.1" )]
