b56e6698cf52d62a87fa3700086aa047d83b7786
Automatic Build <955583a3-168d-41dc-afd6-77b3216a431b>
2011-10-24 17:17:32 +0400

    Automated commit for release: Juliet_2011.10.24.1

--- Schema/_export_nbt_11g.bat
+++ Schema/_export_nbt_11g.bat
@@ -2,4 +2,4 @@ set /p username=
 set /p database=
 
 del Dumps\Nbt_Master_11g.dmp
-expdp.exe %username%/nbt@%database% DUMPFILE=NBT_MASTER_11g.dmp DIRECTORY=NBTDUMPS
+expdp.exe %username%/nbt@%database% DUMPFILE=%username%.dmp DIRECTORY=NBTDUMPS
