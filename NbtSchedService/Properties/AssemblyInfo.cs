f7b402e7478b02b31b6ec5d27ace7ef9bfa1edcb
Christopher Froehlich <cfroehlich@chemsw.com>
2011-11-01 22:06:53 +0400

    Juliet_2011.11.1.2

--- NbtSchedService/Properties/AssemblyInfo.cs
+++ NbtSchedService/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "2011.10.31.1" )]
+[assembly: AssemblyVersion( "2011.11.1.2" )]
 [assembly: AssemblyFileVersion( "2011.10.31.1" )]
