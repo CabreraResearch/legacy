f1f98b1a10ebe31e5947f0d9cc59872d2f5fd421
Automatic Build <955583a3-168d-41dc-afd6-77b3216a431b>
2011-11-14 20:45:48 +0500

    CISPro.

--- Schema/_reset_nbt_madeye.bat
+++ Schema/_reset_nbt_madeye.bat
@@ -3,9 +3,10 @@ set /p database=
 set /p datapumpdir= 
 set /p filename=
 set /p password=
+set /p fromuser=
 
 echo exit | sqlplus %username%/%password%@%database% @nbt_nuke.sql
 
-impdp.exe %username%/%password%@%database% DUMPFILE=%filename% DIRECTORY=%datapumpdir% REMAP_SCHEMA=nbt_master:%username%
+impdp.exe %username%/%password%@%database% DUMPFILE=%filename% DIRECTORY=%datapumpdir% REMAP_SCHEMA=%fromuser%:%username%
 
 echo exit | sqlplus %username%/%password%@%database% @nbt_finalize_ora.sql
