f51ed4644a610333f560787cb48adff4ed6996de
Automatic Build <955583a3-168d-41dc-afd6-77b3216a431b>
2011-11-16 10:19:24 +0500

    Automated commit for release: Katarina_2011.11.16.1

--- NbtSchedService/Properties/AssemblyInfo.cs
+++ NbtSchedService/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "2011.11.15.2" )]
-[assembly: AssemblyFileVersion( "2011.11.15.2" )]
+[assembly: AssemblyVersion( "2011.11.16.1" )]
+[assembly: AssemblyFileVersion( "2011.11.16.1" )]
