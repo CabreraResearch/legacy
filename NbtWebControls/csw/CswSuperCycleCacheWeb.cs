4464d8d811910eb83333c98e35c2d7f5d0edde63
Phil Glaser <pglaser@chemsw.com>
2011-11-22 02:36:19 +0500

    case 23935: add missing file

---
+++ NbtWebControls/csw/CswSuperCycleCacheWeb.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Web;
+using System.Web.Caching;
+using ChemSW.Config;
+using ChemSW.Core;
+using ChemSW.DB;
+using ChemSW.Log;
+using ChemSW.RscAdo;
+using ChemSW.Security;
+
+namespace ChemSW.Nbt
+{
+    public class CswSuperCycleCacheWeb : ICswSuperCycleCache
+    {
+        Cache _Cache = null;
+        public CswSuperCycleCacheWeb( Cache Cache )
+        {
+            _Cache = Cache;
+        }//ctor
+
+        public void put( string Name, object Object )
+        {
+
+           string Key = Name.ToLower();
+
+           _Cache.Add( Key, Object, null, Cache.NoAbsoluteExpiration, Cache.NoSlidingExpiration, CacheItemPriority.Normal, null );
+
+        }//put() 
+
+        public object get( string Name )
+        {
+            object Returnval = null;
+
+            string Key = Name.ToLower();
+
+            _Cache.Get( Key ); 
+
+            return ( Returnval );
+        }
+
+    } // class ICswSuperCycleCache
+
+} // namespace ChemSW
+
