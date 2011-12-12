c728ecd6efc8b8ef64a8fced04fd6232d714d623
Christopher Froehlich <cfroehlich@chemsw.com>
2011-12-12 21:49:03 +0500

    Spec Case 24425. Step 3.

--- NbtWebApp/js/main/fieldtypes/CswFieldTypeButton.js
+++ NbtWebApp/js/main/fieldtypes/CswFieldTypeButton.js
@@ -3,44 +3,84 @@
 /// <reference path="../../globals/CswGlobalTools.js" />
 /// <reference path="../../globals/Global.js" />
 /// <reference path="../../../Scripts/jquery-1.6.4-vsdoc.js" />
+/// <reference path="../controls/CswButton.js" />
 
 ; (function ($) { /// <param name="$" type="jQuery" />
         
     var pluginName = 'CswFieldTypeButton';
 
+    var onButtonClick = function(nodeid, nodetypepropid, $button) {
+        var nodePk = tryParseString(nodeid), 
+            nodeTypePropPk = tryParseString(nodetypepropid),
+            params;
+
+        $button.CswButton('disable');
+        if(isNullOrEmpty(nodePk)) {
+            $.CswDialog('ErrorDialog', ChemSW.makeClientSideError(ChemSW.enums.ErrorType.warning.name, 'Cannot execute a property\'s button click event without a valid node.', 'Attempted to click a property button with a null or empty nodeid.' ));
+            $button.CswButton('enable');
+        }
+        else if(isNullOrEmpty(nodeTypePropPk)) {
+            $.CswDialog('ErrorDialog', ChemSW.makeClientSideError(ChemSW.enums.ErrorType.warning.name, 'Cannot execute a property\'s button click event without a valid node.', 'Attempted to click a property button with a null or empty nodeid.' ));
+            $button.CswButton('enable');
+        } else {
+            params = {
+                NodePk: nodePk,
+                NodeTypePropPk: nodeTypePropPk
+            };
+            
+            CswAjaxJson({
+                    url: '/NbtWebApp/wsNBT.asmx/OnObjectClassButtonClick',
+                    data: params,
+                    success: function(data) {
+                        $button.CswButton('enable');
+                    },
+                    error: function() {
+                        $button.CswButton('enable');
+                    }
+                });
+        }
+    };
+    
     var methods = {
         init: function(o) { //nodepk = o.nodeid, $xml = o.$propxml, onchange = o.onchange, ID = o.ID, Required = o.Required, ReadOnly = o.ReadOnly , cswnbtnodekey
 
             var $Div = $(this);
             $Div.contents().remove();
 
-            var propVals = o.propData.values;
-            var value = tryParseString(propVals.text,o.propData.name);
-            var mode = tryParseString(propVals.mode,'button');
+            var propVals = o.propData.values,
+                value = tryParseString(propVals.text, o.propData.name),
+                mode = tryParseString(propVals.mode, 'button'),
+                $button;
 
             if(o.ReadOnly) {
                 $Div.append(value);
             } else {
-                var $Ctrl = "";
-                if(mode==="button"){
-                    $Ctrl = $Div.CswButton('init', {'ID': o.ID,
-                                                        'enabledText': value,
-                                                        'disabledText': value,
-                                                        'disableOnClick': false,
-                                                        'onclick': function () { alert('clicked!'); }
-                                                      });
+                $button = "";
+                if (mode === 'button'){
+                    $button = $Div.CswButton('init', {
+                                    ID: o.ID,
+                                    enabledText: value,
+                                    disabledText: value,
+                                    disableOnClick: false,
+                                    onclick: function () {
+                                        onButtonClick(o.nodeid, o.propid, $button);
+                                    }
+                                });
                 }
                 else{
-                    $Ctrl = $Div.CswLink('init', {'ID': o.ID,
-                                                        'value': value,
-                                                        'href': '#',
-                                                        'onClick': function() { alert('clicked!'); }
-                                                      });
+                    $button = $Div.CswLink('init', {
+                                    ID: o.ID,
+                                    value: value,
+                                    href: '#',
+                                    onClick: function() {
+                                        onButtonClick(o.nodeid, o.propid, $button);
+                                    }
+                                });
                 }
 
 
                 if(o.Required) {
-                    $Ctrl.addClass("required");
+                    $button.addClass('required');
                 }
             }
         },
