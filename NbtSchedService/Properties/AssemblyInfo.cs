795cec4083cbfc75a936d5bbf2bfa74fb2d4ce87
Automatic Build <955583a3-168d-41dc-afd6-77b3216a431b>
2012-01-20 10:21:33 +0500

    Automated commit for release: Lear_2012.1.20.1

--- NbtSchedService/Properties/AssemblyInfo.cs
+++ NbtSchedService/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "2012.1.19.2" )]
-[assembly: AssemblyFileVersion( "2012.1.19.2" )]
+[assembly: AssemblyVersion( "2012.1.20.1" )]
+[assembly: AssemblyFileVersion( "2012.1.20.1" )]
