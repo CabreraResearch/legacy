c279e1a31eaee0ffabcd9a7ef0013379567998d7
Christopher Froehlich <cfroehlich@chemsw.com>
2012-01-25 19:16:17 +0500

    Case 24786. Review 24803.

--- NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01L17.cs
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01L17.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Data;
-using ChemSW.DB;
-using ChemSW.Nbt.MetaData;
+﻿using ChemSW.Nbt.MetaData;
 using ChemSW.Nbt.ObjClasses;
 
 namespace ChemSW.Nbt.Schema
@@ -19,37 +16,14 @@ namespace ChemSW.Nbt.Schema
             #region case 24587
 
             // The FKType and FKValue of all Location properties on Location nodetypes should be the Location object class in the master.
-            CswNbtMetaDataObjectClass LocationOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswNbtMetaDataObjectClass.NbtObjectClass.LocationClass);
-            foreach(CswNbtMetaDataNodeType LocationNT in LocationOC.NodeTypes)
+            CswNbtMetaDataObjectClass LocationOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswNbtMetaDataObjectClass.NbtObjectClass.LocationClass );
+            foreach( CswNbtMetaDataNodeType LocationNT in LocationOC.NodeTypes )
             {
-                CswNbtMetaDataNodeTypeProp LocationProp = LocationNT.getNodeTypePropByObjectClassPropName(CswNbtObjClassLocation.LocationPropertyName);
-                LocationProp.SetFK(CswNbtViewRelationship.RelatedIdType.ObjectClassId.ToString(), LocationOC.ObjectClassId);
+                CswNbtMetaDataNodeTypeProp LocationProp = LocationNT.getNodeTypePropByObjectClassPropName( CswNbtObjClassLocation.LocationPropertyName );
+                LocationProp.SetFK( CswNbtViewRelationship.RelatedIdType.ObjectClassId.ToString(), LocationOC.ObjectClassId );
             }
-            
+
             #endregion case 24587
-
-            #region Case 24786
-
-            CswTableUpdate TableUpdate = _CswNbtSchemaModTrnsctn.makeCswTableUpdate( SchemaVersion.ToString() + "_jct_nodes_props_update", "jct_nodes_props" );
-            string UpdateWhere = @"where jctnodepropid in ( 
-                                        select n.defaultvalueid from nodetype_props n 
-                                        join field_types f on n.fieldtypeid=f.fieldtypeid
-                                        where lower(f.fieldtype)='question' 
-                                  )";
-            DataTable UpdateTable = TableUpdate.getTable( UpdateWhere, false );
-
-            foreach( DataRow Row in UpdateTable.Rows )
-            {
-                Row["field1_date"] = DBNull.Value;
-            }
-            TableUpdate.update( UpdateTable );
-
-
-            #endregion Case 24786
-
-
-       
-
 
         }//Update()
 
