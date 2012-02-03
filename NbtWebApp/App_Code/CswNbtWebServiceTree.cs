24f72ad1be37b2292f4c23e30919920062af5e0a
Steven Salter <ssalter@chemsw.com>
2012-02-03 21:34:15 +0500

    case 24911

--- NbtWebApp/App_Code/CswNbtWebServiceTree.cs
+++ NbtWebApp/App_Code/CswNbtWebServiceTree.cs
@@ -186,11 +186,21 @@ namespace ChemSW.Nbt.WebServices
                                 break;
                             case "firstchild":
                                 Tree.goToRoot();
-                                Tree.goToNthChild( 0 );
-                                ReturnObj["selectid"] = _IdPrefix + Tree.getNodeIdForCurrentPosition().ToString();
+                                CswNbtNodeKey CurrentKey = Tree.getNodeKeyForCurrentPosition();
+                                while( CurrentKey != null &&
+                                       CurrentKey.NodeSpecies != NodeSpecies.Plain &&
+                                       Tree.getChildNodeCount() > 0 )
+                                {
+                                    Tree.goToNthChild( 0 );
+                                    CurrentKey = Tree.getNodeKeyForCurrentPosition();
+                                }
+                                if( CurrentKey != null && CurrentKey.NodeSpecies == NodeSpecies.Plain )
+                                {
+                                    ReturnObj["selectid"] = _IdPrefix + Tree.getNodeIdForCurrentPosition().ToString();
+                                }
                                 break;
-                        }
-                    }
+                        } // switch( DefaultSelect )
+                    } // if( ReturnObj["selectid"] == null )
                 } // if( HasResults )
                 else
                 {
