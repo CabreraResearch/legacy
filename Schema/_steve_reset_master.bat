138c91e016181f9be7d7c00e73aee74f71c68de2
David Hessler <dhessler@chemsw.com>
2012-02-07 00:04:34 +0500

    revised steve's bat

--- Schema/_steve_reset_master.bat
+++ Schema/_steve_reset_master.bat
@@ -1,9 +1,9 @@
-echo exit | sqlplus david/david@golem @nbt_nuke.sql
+echo exit | sqlplus nbt_master/nbt_master@orcl @nbt_nuke.sql
 
-impdp.exe david/david@golem DUMPFILE=NBT_MASTER_11G.dmp DIRECTORY=NBTDUMPS
+impdp.exe nbt_master/nbt_master@orcl DUMPFILE=NBT_MASTER_11G.dmp DIRECTORY=NBTDUMPS
 
-echo exit | sqlplus david/david@golem @nbt_finalize_ora.sql
+echo exit | sqlplus nbt_master/nbt_master@orcl @nbt_finalize_ora.sql
 
-rem C:\kiln\Nbt\Nbt\NbtSchemaUpdaterCmdLn\bin\Debug\NbtUpdt.exe -accessid 1 -mode prod
+C:\kiln\Nbt\Nbt\NbtSchemaUpdaterCmdLn\bin\Debug\NbtUpdt.exe -accessid master -mode prod
 
 pause
