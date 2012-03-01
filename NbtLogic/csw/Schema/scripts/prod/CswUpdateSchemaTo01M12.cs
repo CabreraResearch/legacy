c9038a38b4e62bcc8c8873cbf3f1c720b3167a4d
David Hessler <dhessler@chemsw.com>
2012-03-01 21:11:29 +0500

    case 24975

--- NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01M12.cs
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaTo01M12.cs
@@ -48,9 +48,35 @@ namespace ChemSW.Nbt.Schema
             {
                 CswNbtNode rptNode = _CswNbtSchemaModTrnsctn.Nodes.makeNodeFromNodeTypeId( rptNT.NodeTypeId, CswNbtNodeCollection.MakeNodeOperation.WriteNode );
                 CswNbtObjClassReport rptNodeAsReport = CswNbtNodeCaster.AsReport( rptNode );
+                rptNodeAsReport.Category = "System";
                 rptNodeAsReport.ReportName.Text = "SQL Report View Dictionary";
                 rptNodeAsReport.SQL.Text = "select * from vwAutoViewColNames";                
-                rptNodeAsReport.postChanges(true);                
+                rptNodeAsReport.postChanges(true);
+
+
+                CswNbtNode rptNode2 = _CswNbtSchemaModTrnsctn.Nodes.makeNodeFromNodeTypeId( rptNT.NodeTypeId, CswNbtNodeCollection.MakeNodeOperation.WriteNode );
+                CswNbtObjClassReport rptNodeAsReport2 = CswNbtNodeCaster.AsReport( rptNode2 );
+                rptNodeAsReport2.Category = "SI_Example";
+                rptNodeAsReport2.ReportName.Text = "OOC Issues last 30 days";
+                rptNodeAsReport2.SQL.Text = @"select dept.P17 dept,bldg.P24 bldg,targ.P05_LOCATION,targ.nodeid ip_nodeid, targ.P01_BARCODE ip_barcode,
+                                                 targ.P02_DESCRIPTION ip_desc,
+                                                 targ.P10_STATUS ip_status,
+                                                 targ.P04_LASTINSPECTIONDATE ip_lastinspected,
+                                                 p4391 prot_descr,q.questionno,q.propname question,q.iscompliant,
+                                                 q.answer,q.correctiveaction,q.comments 
+                                                from ntsi_protocol pr
+                                                 join ocinspectiontargetclass targ on targ.nodeid=pr.P4390INSPECTIONTARGETCL_OCFK
+                                                 join vwquestioncompliance q on q.nodeid=pr.nodeid
+                                                 join ntroom room on room.nodeid=targ.P05_Location_fk
+                                                 join ntbuilding bldg on bldg.nodeid=room.P2339LOCATIONCLASS_OCFK
+                                                 left outer join ntdepartment dept on dept.nodeid=bldg.P25_DEPARTMENT_NTFK
+                                                where (q.iscompliant='0' or q.correctiveaction is not null )
+                                                 and to_date(targ.P04_LASTINSPECTIONDATE,'mm/dd/yyyy') between SYSDATE-30 and SYSDATE
+                                                order by targ.P05_LOCATION,pr.nodeid,q.questionno ";
+
+                rptNodeAsReport2.postChanges( true );
+
+
             }
             #endregion case 24975
 
