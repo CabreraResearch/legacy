71b2ef75543e3ab0a12b2ec7722abc959971671f
Steven Salter <ssalter@chemsw.com>
2012-03-08 03:04:25 +0500

    case 24777

--- NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
+++ NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
@@ -46,6 +46,7 @@ namespace ChemSW.Nbt.Schema
             addReleaseDmlDriver( new CswSchemaUpdateDriver( new CswUpdateSchemaToCase25226() ) );  //17
             addReleaseDmlDriver( new CswSchemaUpdateDriver( new CswUpdateSchemaToCase24515() ) );  //18
             addReleaseDmlDriver( new CswSchemaUpdateDriver( new CswUpdateSchemaCase25381() ) );    //19
+            addReleaseDmlDriver( new CswSchemaUpdateDriver( new CswUpdateSchemaCase24777() ) );    //20
 
             // This automatically detects the latest version
             _LatestVersion = _MinimumVersion;
