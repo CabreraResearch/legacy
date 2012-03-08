29eccf30e294e799cc6d0db60745cc181aba1b4d
Steven Salter <ssalter@chemsw.com>
2012-03-09 02:29:26 +0500

    Schema script uniformity now!

--- NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase24988.cs
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase24988.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 
-
-
 namespace ChemSW.Nbt.Schema
 {
     /// <summary>
-    /// Updates the schema to case 24988
+    /// Updates the schema for case 24988
     /// </summary>
-    public class CswUpdateSchemaTo24988 : CswUpdateSchemaTo
+    public class CswUpdateSchemaCase24988 : CswUpdateSchemaTo
     {
-        //public override CswSchemaVersion SchemaVersion { get { return new CswSchemaVersion( 1, 'M', 12 ); } }
-        //public override string Description { get { return "Update to schema version " + SchemaVersion.ToString(); } }
-
         public override void update()
         {
             string SqlConstraint = "alter table sessionlist add constraint sessionlist_sessionid unique (sessionid);";
@@ -25,6 +20,6 @@ namespace ChemSW.Nbt.Schema
 
         }//Update()
 
-    }//class CswUpdateSchemaTo24988
+    }//class CswUpdateSchemaCase24988
 
 }//namespace ChemSW.Nbt.Schema
--- NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase25290.cs
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase25290.cs
@@ -7,17 +7,12 @@ using ChemSW.Nbt.MetaData;
 namespace ChemSW.Nbt.Schema
 {
     /// <summary>
-    /// Updates the schema to case 25290
+    /// Updates the schema for case 25290
     /// </summary>
-    public class CswUpdateSchemaTo25290 : CswUpdateSchemaTo
+    public class CswUpdateSchemaCase25290 : CswUpdateSchemaTo
     {
-        //public override CswSchemaVersion SchemaVersion { get { return new CswSchemaVersion( 1, 'M', 12 ); } }
-        //public override string Description { get { return "Update to schema version " + SchemaVersion.ToString(); } }
-
         public override void update()
         {
-
-            
             //validate role nodetype permissions
             foreach( CswNbtNode roleNode in _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswNbtMetaDataObjectClass.NbtObjectClass.RoleClass ).getNodes( false, true ) )
             {
@@ -28,6 +23,6 @@ namespace ChemSW.Nbt.Schema
 
         }//Update()
 
-    }//class CswUpdateSchemaTo25290
+    }//class CswUpdateSchemaCase25290
 
 }//namespace ChemSW.Nbt.Schema
