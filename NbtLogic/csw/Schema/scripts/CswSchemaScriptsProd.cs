d160449e2627f57d551008b5b3f3b9dd72586f4b
Steven Salter <ssalter@chemsw.com>
2012-03-09 23:52:58 +0500

    case 25374 - set .rpt in database, from resource file

--- NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
+++ NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
@@ -50,6 +50,7 @@ namespace ChemSW.Nbt.Schema
             addReleaseDmlDriver( new CswSchemaUpdateDriver( new CswUpdateSchemaCase25377() ) );      //21
             addReleaseDmlDriver( new CswSchemaUpdateDriver( new CswUpdateSchemaCase24777() ) );      //22
             addReleaseDmlDriver( new CswSchemaUpdateDriver( new CswUpdateSchemaCase25374() ) );      //23
+            addReleaseDmlDriver( new CswSchemaUpdateDriver( new CswUpdateSchemaCase25374B() ) );     //24
 
 
             // This automatically detects the latest version
