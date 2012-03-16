0c3d9f8465d51996d3ffcd4bbea8aad8eddfdc56
Steven Salter <ssalter@chemsw.com>
2012-03-16 21:15:15 +0400

    case 25493 part 1 - use new images

--- NbtWebApp/App_Code/CswNbtWebServiceTable.cs
+++ NbtWebApp/App_Code/CswNbtWebServiceTable.cs
@@ -157,7 +157,7 @@ namespace ChemSW.Nbt.WebServices
             if( NodeType != null )
             {
                 // default image, overridden below
-                ret["thumbnailurl"] = "Images/icons/" + NodeType.IconFileName;
+                ret["thumbnailurl"] = "Images/icons/300/" + NodeType.IconFileName;
             }
 
             // Map property order to insert position
