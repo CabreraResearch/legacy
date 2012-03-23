96318f975bdca28c5d1310df0f3416707620730b
Phil Glaser <pglaser@chemsw.com>
2012-03-23 19:46:14 +0400

    case 25551: first version

--- NbtLogic/csw/Schema/scripts/infr/RunAfterEveryExecutionOfUpdater_02.cs
+++ NbtLogic/csw/Schema/scripts/infr/RunAfterEveryExecutionOfUpdater_02.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Windows.Forms;
 using System.Data;
 using System.Collections.Generic;
 using ChemSW.Core;
@@ -21,21 +23,29 @@ namespace ChemSW.Nbt.Schema
             //***************  ADD your own code
             _CswNbtSchemaModTrnsctn.CswLogger.reportAppState( "Running nbt_finalize_ora.bat after updates." ); //this one doesn't blocks        
             //"Initialize" is only for updateSequences()!!!
+
+            string FileLocations = Application.StartupPath;
+            string BatchFilePath = FileLocations + "\\nbt_finalize_ora.bat";
+            string SqlFilePath = FileLocations + "\\nbt_finalize_ora.sql";
+            File.WriteAllBytes( BatchFilePath, ChemSW.Nbt.Properties.Resources.nbt_initialize_ora_bat );
+            File.WriteAllBytes( SqlFilePath, ChemSW.Nbt.Properties.Resources.nbt_initialize_ora_sql );
+
+
             _CswNbtSchemaModTrnsctn.CswDbCfgInfo.makeConfigurationCurrent( _CswNbtSchemaModTrnsctn.Accessid );
             string serverName = _CswNbtSchemaModTrnsctn.CswDbCfgInfo.CurrentServerName;
             string userName = _CswNbtSchemaModTrnsctn.CswDbCfgInfo.CurrentUserName;
             string passWord = _CswNbtSchemaModTrnsctn.CswDbCfgInfo.CurrentPlainPwd;
 
 
-            System.Diagnostics.Process p = new System.Diagnostics.Process();
-            p.StartInfo.UseShellExecute = false;
-            p.StartInfo.FileName = _CswNbtSchemaModTrnsctn.ConfigFileLocation + "\\nbt_finalize_ora.bat";
-            p.StartInfo.Arguments = " " + serverName + " " + userName + " " + passWord + " " + _CswNbtSchemaModTrnsctn.ConfigFileLocation;
-            p.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
-            p.StartInfo.UseShellExecute = false;
-            p.StartInfo.RedirectStandardOutput = false;
+            System.Diagnostics.Process Process = new System.Diagnostics.Process();
+            Process.StartInfo.UseShellExecute = false;
+            Process.StartInfo.FileName = BatchFilePath;
+            Process.StartInfo.Arguments = " " + serverName + " " + userName + " " + passWord + " " + FileLocations;
+            Process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            Process.StartInfo.UseShellExecute = false;
+            Process.StartInfo.RedirectStandardOutput = false;
 
-            System.Diagnostics.Process.Start( p.StartInfo );
+            System.Diagnostics.Process.Start( Process.StartInfo );
             //we don't wait 
             _CswNbtSchemaModTrnsctn.CswLogger.reportAppState( "Left nbt_finalize_ora.bat after updates without waiting." ); //this one doesn't block
 
--- NbtLogic/csw/Schema/scripts/infr/RunBeforeEveryExecutionOfUpdater_02.cs
+++ NbtLogic/csw/Schema/scripts/infr/RunBeforeEveryExecutionOfUpdater_02.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Windows.Forms;
 using System.Data;
 using System.Collections.Generic;
 using ChemSW.Core;
@@ -21,6 +23,17 @@ namespace ChemSW.Nbt.Schema
             //***************  ADD your own code
             _CswNbtSchemaModTrnsctn.CswLogger.reportAppState( "Running nbt_initialize_ora.bat prior to updates." ); //this one blocks        
             //"Initialize" is only for updateSequences()!!!
+
+
+
+            //Retrieve files from resource
+            string FileLocations = Application.StartupPath;
+            string BatchFilePath = FileLocations + "\\nbt_initialize_ora.bat";
+            string SqlFilePath = FileLocations + "\\nbt_initialize_ora.sql";
+            File.WriteAllBytes( BatchFilePath, ChemSW.Nbt.Properties.Resources.nbt_initialize_ora_bat );
+            File.WriteAllBytes( SqlFilePath, ChemSW.Nbt.Properties.Resources.nbt_initialize_ora_sql );
+
+
             _CswNbtSchemaModTrnsctn.CswDbCfgInfo.makeConfigurationCurrent( _CswNbtSchemaModTrnsctn.Accessid );
             string serverName = _CswNbtSchemaModTrnsctn.CswDbCfgInfo.CurrentServerName;
             string userName = _CswNbtSchemaModTrnsctn.CswDbCfgInfo.CurrentUserName;
@@ -29,8 +42,8 @@ namespace ChemSW.Nbt.Schema
 
             System.Diagnostics.Process p = new System.Diagnostics.Process();
             p.StartInfo.UseShellExecute = false;
-            p.StartInfo.FileName = _CswNbtSchemaModTrnsctn.ConfigFileLocation + "\\nbt_initialize_ora.bat";
-            p.StartInfo.Arguments = " " + serverName + " " + userName + " " + passWord + " " + _CswNbtSchemaModTrnsctn.ConfigFileLocation;
+            p.StartInfo.FileName = BatchFilePath;
+            p.StartInfo.Arguments = " " + serverName + " " + userName + " " + passWord + " " + FileLocations;
             p.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
             p.StartInfo.UseShellExecute = false;
             p.StartInfo.RedirectStandardOutput = false;
