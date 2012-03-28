935265697ec39dd37d74418f6212b3c28f0d3ca5
Steven Salter <ssalter@chemsw.com>
2012-03-28 21:25:59 +0400

    refactor sql script execution into function call

--- NbtLogic/csw/Schema/scripts/infr/RunBeforeEveryExecutionOfUpdater_02.cs
+++ NbtLogic/csw/Schema/scripts/infr/RunBeforeEveryExecutionOfUpdater_02.cs
@@ -13,59 +13,19 @@ using System.Diagnostics;
 namespace ChemSW.Nbt.Schema
 {
     /// <summary>
-    /// Updates the schema to version 01J-01
+    /// Runs the initialize script
     /// </summary>
     public class RunBeforeEveryExecutionOfUpdater_02 : CswUpdateSchemaTo
     {
-        public static string Title = "nbt_initialize_ora.bat";
+        public static string Title = FileName;
+
+        private const string FileName = "nbt_initialize_ora.sql";
 
         public override void update()
         {
-            //_CswNbtSchemaModTrnsctn.CswLogger.reportAppState("Running nbt_initialize_ora.bat prior to updates."); //this one blocks        
-            
-            //"Initialize" is only for updateSequences()!!!
+            _CswNbtSchemaModTrnsctn.runExternalSqlScript( FileName, ChemSW.Nbt.Properties.Resources.nbt_initialize_ora_sql );
+        }//update()
 
-
-
-            //Retrieve files from resource
-            string FileLocations = Application.StartupPath;
-            string BatchFilePath = FileLocations + "\\nbt_initialize_ora.bat";
-            string SqlFilePath = FileLocations + "\\nbt_initialize_ora.sql";
-            File.WriteAllBytes( BatchFilePath, ChemSW.Nbt.Properties.Resources.nbt_initialize_ora_bat );
-            File.WriteAllBytes( SqlFilePath, ChemSW.Nbt.Properties.Resources.nbt_initialize_ora_sql );
-
-
-            _CswNbtSchemaModTrnsctn.CswDbCfgInfo.makeConfigurationCurrent( _CswNbtSchemaModTrnsctn.Accessid );
-            string serverName = _CswNbtSchemaModTrnsctn.CswDbCfgInfo.CurrentServerName;
-            string userName = _CswNbtSchemaModTrnsctn.CswDbCfgInfo.CurrentUserName;
-            string passWord = _CswNbtSchemaModTrnsctn.CswDbCfgInfo.CurrentPlainPwd;
-
-
-            System.Diagnostics.Process p = new System.Diagnostics.Process();
-            p.StartInfo.UseShellExecute = false;
-            p.StartInfo.FileName = BatchFilePath;
-            p.StartInfo.Arguments = " " + serverName + " " + userName + " " + passWord + " " + FileLocations;
-            p.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
-            p.StartInfo.UseShellExecute = false;
-            p.StartInfo.RedirectStandardOutput = false;
-
-            Process SpawnedProcess = System.Diagnostics.Process.Start( p.StartInfo );
-            if( false == SpawnedProcess.WaitForExit( _CswNbtSchemaModTrnsctn.UpdtShellWaitMsec ) )
-            {
-                _CswNbtSchemaModTrnsctn.CswLogger.reportAppState( "Timed out will running nbt_initialize_ora.bat prior to updates." );
-            }
-            //else
-            //{
-            //    _CswNbtSchemaModTrnsctn.CswLogger.reportAppState( "Finished nbt_initialize_ora.bat prior to updates." );
-            //}
-
-            File.Delete( BatchFilePath );
-            File.Delete( SqlFilePath );
-
-        }//Update()
-
-    }//class CswUpdateSchema_Infr_TakeDump
+    }//class RunBeforeEveryExecutionOfUpdater_02
 
 }//namespace ChemSW.Nbt.Schema
-
-
