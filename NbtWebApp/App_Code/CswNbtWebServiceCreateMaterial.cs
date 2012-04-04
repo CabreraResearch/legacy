eaf909727cee8b56af172aab813ce1e9338be73d
Christopher Froehlich <unknown@kiln.example.com>
2012-04-05 00:09:18 +0400

    Spec case 25722. Part 5. Case 24447.

---
+++ NbtWebApp/App_Code/CswNbtWebServiceCreateMaterial.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using ChemSW.Core;
+using ChemSW.Exceptions;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.ObjClasses;
+using ChemSW.Nbt.MetaData.FieldTypeRules;
+using ChemSW.Nbt.Security;
+using ChemSW.Nbt.Actions;
+using Newtonsoft.Json.Linq;
+
+namespace ChemSW.Nbt.WebServices
+{
+    public class CswNbtWebServiceCreateMaterial
+    {
+        #region ctor
+
+        private CswNbtResources _CswNbtResources;
+        
+        public CswNbtWebServiceCreateMaterial( CswNbtResources CswNbtResources )
+        {
+            _CswNbtResources = CswNbtResources;
+
+            if( false == _CswNbtResources.Permit.can(CswNbtActionName.Create_Material, _CswNbtResources.CurrentNbtUser ) )
+            {
+                throw new CswDniException( ErrorType.Error, "You do not have permission to use the Create Material wizard.", "Attempted to access the Create Material wizard with role of " + _CswNbtResources.CurrentNbtUser.Rolename );
+            }
+        }
+
+        #endregion ctor
+
+
+        #region Public
+
+        
+        #endregion Public
+
+    }
+}
