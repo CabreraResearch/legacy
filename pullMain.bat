6a0ed2f2cefa15af23227de7d2dcbac90c15b3d8
Christopher Froehlich <unknown@kiln.example.com>
2012-04-13 00:57:06 +0400

    pullMain gets a little quieter.

--- pullMain.bat
+++ pullMain.bat
@@ -1,20 +1,17 @@
+hg pull -R /kiln/nbt/nbt -u main
+
 hg pull -R /kiln/Common/CswCommon -u main
 hg pull -R /kiln/Common/CswConfigUI -u main
 hg pull -R /kiln/Common/CswWebControls -u main
 hg pull -R /kiln/Common/CswLogService -u main
 
-echo "Pull from Common Complete"
-
-hg pull -R /kiln/nbt/nbt -u main
 hg pull -R /kiln/nbt/NbtImport -u main
 hg pull -R /kiln/nbt/nbt/NbtWebApp/help -u
-
-echo "Pull from Nbt Complete"
 
 hg pull -R /kiln/ThirdParty/ClosureCompiler -u
 hg pull -R /kiln/ThirdParty/OracleDataAccess -u
 hg pull -R /kiln/ThirdParty/YUICompressor -u
 
-echo "Pull from ThirdParty Complete"
+exit
 
-pause
+
