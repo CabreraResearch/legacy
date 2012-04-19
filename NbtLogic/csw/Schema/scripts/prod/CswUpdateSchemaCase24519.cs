e887a47fdd936ee5928c5134639b118bab9d075d
Christopher Froehlich <unknown@kiln.example.com>
2012-04-20 00:04:33 +0400

    Case 24519. Last but not least.

--- NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase24519.cs
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase24519.cs
@@ -173,6 +173,21 @@ namespace ChemSW.Nbt.Schema
 
             #endregion
 
+            #region MSDS View
+
+            CswNbtView MSDSView = _CswNbtSchemaModTrnsctn.makeView();
+            MSDSView.makeNew( "MSDS Expiring Next Month", NbtViewVisibility.Global );
+            MSDSView.ViewMode = NbtViewRenderingMode.Tree;
+            MSDSView.Category = "Materials";
+
+            CswNbtViewRelationship DocumentVr = MSDSView.AddViewRelationship( DocumentOc, true );
+            MSDSView.AddViewPropertyAndFilter( DocumentVr, ExpirationDateOcp, "today+30", FilterMode: CswNbtPropFilterSql.PropertyFilterMode.LessThanOrEquals );
+            MSDSView.AddViewPropertyAndFilter( DocumentVr, DocumentClassOcp, "MSDS" );
+
+            MSDSView.save();
+
+            #endregion
+
 
         }//Update()
 
