ea39d4321d96a931174ec40d3c8855838cc672fc
Christopher Froehlich <unknown@kiln.example.com>
2012-05-04 00:51:55 +0400

    Review 26125. Comment 1.

--- NbtLogic/csw/View/CswNbtView.cs
+++ NbtLogic/csw/View/CswNbtView.cs
@@ -858,33 +858,7 @@ namespace ChemSW.Nbt
         /// </summary>
         public bool IsFullyEnabled()
         {
-            bool Ret = true;
-            /* Case 26117 */
-            if( Visibility == NbtViewVisibility.User &&
-                null != VisibilityUserId )
-            {
-                CswNbtNode ChemSwAdminUser = _CswNbtResources.Nodes.makeUserNodeFromUsername( CswNbtObjClassUser.ChemSWAdminUsername );
-                if( VisibilityUserId == ChemSwAdminUser.NodeId &&
-                    _CswNbtResources.CurrentNbtUser.Username != CswNbtObjClassUser.ChemSWAdminUsername )
-                {
-                    Ret = false;
-                }
-            }
-            else if( Visibility == NbtViewVisibility.Role &&
-                null != VisibilityRoleId )
-            {
-                CswNbtNode ChemSwAdminRole = _CswNbtResources.Nodes.makeRoleNodeFromRoleName( CswNbtObjClassRole.ChemSWAdminRoleName );
-                if( VisibilityRoleId == ChemSwAdminRole.NodeId &&
-                    _CswNbtResources.CurrentNbtUser.Rolename != CswNbtObjClassRole.ChemSWAdminRoleName )
-                {
-                    Ret = false;
-                }
-            }
-            if( Ret )
-            {
-                Ret = IsFullyEnabledRecursive( Root );
-            }
-            return Ret;
+            return IsFullyEnabledRecursive( Root );
         }
         private bool IsFullyEnabledRecursive( CswNbtViewNode ViewNode )
         {
