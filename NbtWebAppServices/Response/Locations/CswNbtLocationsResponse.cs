715015e40eee1aecd6321659c485d8cb67d024cc
Christopher Froehlich <unknown@kiln.example.com>
2012-05-07 19:07:22 +0400

    Case 26119.

---
+++ NbtWebAppServices/Response/Locations/CswNbtLocationsResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+using System.Web;
+
+namespace NbtWebAppServices.Response
+{
+    [DataContract]
+    public class CswNbtLocationsResponse : CswNbtWebServiceResponseNoData
+    {
+        public CswNbtLocationsResponse( HttpContext Context )
+            : base( Context )
+        {
+        }
+
+        [DataMember]
+        public CswNbtWsLocationsModel.CswNbtWsLocationListModel Data { get; set; }
+    }
+}
---
+++ NbtWebAppServices/Response/Locations/CswNbtWsLocationsModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+
+namespace NbtWebAppServices.Response
+{
+    public class CswNbtWsLocationsModel
+    {
+        public class CswNbtWsLocationNodeModel
+        {
+            public string Name { get; set; }
+            public string Path { get; set; }
+            public string LocationId { get; set; }
+        }
+
+        public class CswNbtWsLocationListModel
+        {
+            public Collection<CswNbtWsLocationNodeModel> LocationsList { get; set; }
+        }
+    }
+}
