e98c20e668d8a8b6371bf52947eea9ab711b1cbb
NBT Daily Build <b3851691-5f24-46c9-9188-5f0a2e70960a>
2012-05-09 08:16:53 +0400

    Automated commit for release: Othello_2012.5.9.1

--- NbtSchedService/Properties/AssemblyInfo.cs
+++ NbtSchedService/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "2012.5.8.1" )]
-[assembly: AssemblyFileVersion( "2012.5.8.1" )]
+[assembly: AssemblyVersion( "2012.5.9.1" )]
+[assembly: AssemblyFileVersion( "2012.5.9.1" )]
