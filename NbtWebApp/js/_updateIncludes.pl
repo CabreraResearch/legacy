503fb811a2060a1ffc2c61ae425826cbd48ba75a
Christopher Froehlich <unknown@kiln.example.com>
2012-05-10 20:54:28 +0400

    Case 25926. Delete existing minified files.

--- NbtWebApp/js/_updateIncludes.pl
+++ NbtWebApp/js/_updateIncludes.pl
@@ -1,4 +1,6 @@
 use strict;
+use warnings;
+use Time::Local;
 
 my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
 my $datestr = ($year + 1900).".". ($mon + 1) .".$mday";
@@ -6,6 +8,26 @@ my $datestr = ($year + 1900).".". ($mon + 1) .".$mday";
 my $dir = $ARGV[0];
 my $includesFile = "$dir\\MainCswIncludes.html";
 unlink($includesFile);
+
+sub deleteYesterdays;
+sub deleteYesterdays {
+    my $path = $_[0];
+    opendir(JSDIR, $path) or die("Cannot open js directory: $path ; $!");
+    while((my $filename = readdir(JSDIR)))
+    {
+        if(($filename =~ /.*\.js$/ &&
+           $filename =~ /Csw.*\.min\.js/) ||
+           ($filename =~ /.*\.css$/ &&
+           $filename =~ /ChemSW.*\.min\.css/)
+          ) 
+        {
+            unlink("$path\\$filename");
+        }
+    }
+    closedir(JSDIR);
+}
+deleteYesterdays("$dir\\css") or die $!;
+deleteYesterdays("$dir") or die $!;
 
 my $includesContent = <<HTML;
 <link rel="stylesheet" type="text/css" href="css/ChemSW.$datestr.min.css" />
