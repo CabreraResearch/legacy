8cae8e050263460b3a97bf56a7984e85e15381ec
Christopher Froehlich <unknown@kiln.example.com>
2012-05-15 19:23:10 +0400

    Improve script naming conventions and organization.

--- Scripts/Daily/NbtDailyBuild.bat
+++ Scripts/Daily/NbtDailyBuild.bat
@@ -1,5 +1,3 @@
-set /p ThisVersionNo=
-
 net stop W3SVC
 
 net stop "ChemSW Log Service" > D:\log\dailylog.txt
@@ -8,7 +6,7 @@ net stop "ChemSW NBT Schedule Service" >> D:\log\dailylog.txt
 
 taskkill -f /IM "nbtschedservice.exe" >> D:\log\dailylog.txt
 
-DeployNbt.pl %ThisVersionNo% >> D:\log\dailylog.txt 2>&1
+NbtDailyBuild.pl 1 >> D:\log\dailylog.txt 2>&1
 
 echo "Deploy Finished" >> D:\log\dailylog.txt
 
@@ -26,4 +24,4 @@ C:\Windows\Microsoft.NET\Framework64\v4.0.30319\aspnet_compiler.exe -v /NbtWebAp
 
 iisreset
 
-echo "ASP Precompile Finished" >> D:\log\dailylog.txt    
+echo "ASP Precompile Finished" >> D:\log\dailylog.txt
