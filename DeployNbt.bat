030ac97debcd4d5e92c0e50cb3cfb3c50d949951
dhessler@chemsw.com <unknown@kiln.example.com>
2012-05-15 19:10:55 +0400

    reset nbt_master each night...

--- DeployNbt.bat
+++ DeployNbt.bat
@@ -18,6 +18,11 @@ net start "ChemSW Log Service" >> D:\log\dailylog.txt
 
 D:\kiln\Nbt\Nbt\NbtSchemaUpdaterCmdLn\bin\Release\NbtUpdt.exe -all >> D:\log\dailylog.txt
 
+REM must reset nbt_master before schemaupdater runs
+echo sqlplus nbt_master/hj345defwu9@nbttest @nbt_nuke.sql
+
+impdp.exe nbt_master/hj345defwu9@nbttest DUMPFILE=d:\kiln\nbt\nbt\schema\NBT_MASTER_11G.DMP DIRECTORY=NBTDUMPS REMAP_SCHEMA=nbt_master:nbt_master
+
 net start "ChemSW NBT Schedule Service" >> D:\log\dailylog.txt
 
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\aspnet_compiler.exe -v /NbtWebApp -p d:\kiln\Nbt\Nbt\NbtWebApp >> D:\log\dailylog.txt
