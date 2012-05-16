747af07d27bb8bcf69c0a0ac1e189bbf3a939b3a
Steven Salter <ssalter@chemsw.com>
2012-05-16 16:22:44 +0400

    manual versions for Nestor_2012.5.16.1

--- NbtSchedService/Properties/AssemblyInfo.cs
+++ NbtSchedService/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "2012.5.8.1" )]
-[assembly: AssemblyFileVersion( "2012.5.8.1" )]
+[assembly: AssemblyVersion( "2012.5.16.1" )]
+[assembly: AssemblyFileVersion( "2012.5.16.1" )]
