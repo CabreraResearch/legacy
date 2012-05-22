f12ef95437bb1d6a313a5fa38390b7b42fa3a5d8
Christopher Froehlich <unknown@kiln.example.com>
2012-05-22 20:18:51 +0400

    Backed out changeset: 5a5a3af6a19e. Case 26415. Review 26419.

--- NbtWcfServices/Inspections/CswNbtWcfInspectionsGet.cs
+++ NbtWcfServices/Inspections/CswNbtWcfInspectionsGet.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
-using System.Linq;
 using System.Web;
 using ChemSW.Core;
 using ChemSW.Nbt;
@@ -271,22 +270,6 @@ namespace NbtWebAppServices.Response
             }
         }
 
-                        CswCommaDelimitedString PossibleAnswers = new CswCommaDelimitedString();
-                        PossibleAnswers.FromString( NodeTypeProp.ListOptions );
-                        CswCommaDelimitedString CompliantAnswers = new CswCommaDelimitedString();
-                        CompliantAnswers.FromString( NodeTypeProp.ValueOptions );
-                        foreach( string Answer in PossibleAnswers )
-                        {
-                            var Choice =
-                                new CswNbtWcfInspectionsDataModel.CswNbtInspectionDesign.AnswerChoice
-                                {
-                                    Text = Answer,
-                                    IsCompliant = CompliantAnswers.Contains( Answer, false )
-                                };
-                            ResponseProperty.Choices.Add( Choice );
-                        }
-                        ResponseSection.Properties.Add( ResponseProperty );
-                    }
 
 
         public CswNbtWcfInspectionsResponseWithDesigns finalize()
