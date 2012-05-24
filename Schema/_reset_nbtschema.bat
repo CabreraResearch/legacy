56e40eca88ca5f3bb2d75a65e72f829c91b11b27
dhessler@chemsw.com <unknown@kiln.example.com>
2012-05-25 00:44:19 +0400

    case 26469, review 26481

--- Schema/_reset_nbtschema.bat
+++ Schema/_reset_nbtschema.bat
@@ -2,7 +2,7 @@ set /p accessid=
 set /p username=
 set /p password=
 
-echo exit | sqlplus %username%/%password%@nbttest @nbt_nuke.sql
+exit | sqlplus %username%/%password%@nbttest @nbt_nuke.sql
 
 impdp.exe %username%/%password%@nbttest DUMPFILE=NBT_MASTER_11G.DMP DIRECTORY=EXPORTS REMAP_SCHEMA=nbt_master:%username%
 
