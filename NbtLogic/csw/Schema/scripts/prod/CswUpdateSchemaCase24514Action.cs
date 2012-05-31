0ed2557f23e14a91e8ac11d4a09b1f450b6f548d
Christopher Froehlich <unknown@kiln.example.com>
2012-05-31 21:49:30 +0400

    Case 24514. Action.

---
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase24514Action.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ChemSW.Nbt.Actions;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update for case 24514
+    /// </summary>
+    public class CswUpdateSchemaCase24514Action : CswUpdateSchemaTo
+    {
+        public override void update()
+        {
+            Int32 SubmitRequestActionId = _CswNbtSchemaModTrnsctn.createAction( CswNbtActionName.Submit_Request, true, "", "Requests" );
+            _CswNbtSchemaModTrnsctn.createModuleActionJunction( CswNbtResources.CswNbtModule.CISPro, CswNbtActionName.Submit_Request );
+
+        }//Update()
+
+    }//class CswUpdateSchemaCase24514Action
+
+}//namespace ChemSW.Nbt.Schema
