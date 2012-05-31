c0a3c873bff092566d870d95a3c0b61ec6b054e9
bvavra@chemsw.com <unknown@kiln.example.com>
2012-05-31 21:22:48 +0400

    Review 26521, comment 8

--- NbtLogic/csw/Conversion/CswNbtUnitConversion.cs
+++ NbtLogic/csw/Conversion/CswNbtUnitConversion.cs
@@ -119,5 +119,29 @@ namespace ChemSW.Nbt.csw.Conversion
 
             return UnitRelationship;
         }
+
+        private class CswNbtUnitConversionEnums
+        {
+            /// <summary>
+            /// Enum: Used to identify the UnitType relationship between two UnitOfMeasure Nodes in order to apply the correct conversion logic
+            /// </summary>
+            public sealed class UnitTypeRelationship : CswEnum<UnitTypeRelationship>
+            {
+                private UnitTypeRelationship( string Name ) : base( Name ) { }
+                public static IEnumerable<UnitTypeRelationship> _All { get { return CswEnum<UnitTypeRelationship>.All; } }
+
+                public static explicit operator UnitTypeRelationship( string str )
+                {
+                    UnitTypeRelationship ret = Parse( str );
+                    return ( ret != null ) ? ret : UnitTypeRelationship.Unknown;
+                }
+
+                public static readonly UnitTypeRelationship Unknown = new UnitTypeRelationship( "Unknown" );
+                public static readonly UnitTypeRelationship Same = new UnitTypeRelationship( "Same" );
+                public static readonly UnitTypeRelationship NotSupported = new UnitTypeRelationship( "NotSupported" );
+                public static readonly UnitTypeRelationship WeightToVolume = new UnitTypeRelationship( "WeightToVolume" );
+                public static readonly UnitTypeRelationship VolumeToWeight = new UnitTypeRelationship( "VolumeToWeight" );
+            }
+        }
     }
 }
