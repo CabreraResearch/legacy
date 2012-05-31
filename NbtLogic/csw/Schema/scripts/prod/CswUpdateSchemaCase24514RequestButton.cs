ee8c016184f81f95bec58a306924a4d387aaef66
Christopher Froehlich <unknown@kiln.example.com>
2012-05-31 22:45:25 +0400

    Case 24514. Request button is on first tab.

--- NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase24514RequestButton.cs
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase24514RequestButton.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+using System.Linq;
 using ChemSW.Nbt.MetaData;
 using ChemSW.Nbt.ObjClasses;
 
@@ -13,28 +15,42 @@ namespace ChemSW.Nbt.Schema
         {
             CswNbtMetaDataObjectClass ContainerOc = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswNbtMetaDataObjectClass.NbtObjectClass.ContainerClass );
             CswNbtMetaDataObjectClassProp ContainerRequestOcp = _CswNbtSchemaModTrnsctn.createObjectClassProp(
-                ContainerOc, new CswNbtWcfMetaDataModel.ObjectClassProp()
+                ContainerOc, new CswNbtWcfMetaDataModel.ObjectClassProp
                 {
                     FieldType = CswNbtMetaDataFieldType.NbtFieldType.Button,
                     PropName = CswNbtObjClassContainer.RequestPropertyName
                 } );
-            foreach( CswNbtMetaDataNodeType Container in ContainerOc.getNodeTypes() )
+            foreach( CswNbtMetaDataNodeType ContainerNt in ContainerOc.getNodeTypes() )
             {
-                CswNbtMetaDataNodeTypeProp RequestNtp = Container.getNodeTypePropByObjectClassProp( CswNbtObjClassContainer.RequestPropertyName );
-                RequestNtp.updateLayout( CswNbtMetaDataNodeTypeLayoutMgr.LayoutType.Table, true );
+                Int32 FirsTabId = Int32.MinValue;
+                foreach( CswNbtMetaDataNodeTypeTab Tab in from _Tab in ContainerNt.getNodeTypeTabs() orderby _Tab.TabOrder, _Tab.TabId select _Tab )
+                {
+                    FirsTabId = Tab.TabId;
+                    break;
+                }
+                CswNbtMetaDataNodeTypeProp RequestNtp = ContainerNt.getNodeTypePropByObjectClassProp( CswNbtObjClassContainer.RequestPropertyName );
+                RequestNtp.updateLayout( CswNbtMetaDataNodeTypeLayoutMgr.LayoutType.Edit, true, FirsTabId );
+                RequestNtp.updateLayout( CswNbtMetaDataNodeTypeLayoutMgr.LayoutType.Table, true, FirsTabId );
             }
 
             CswNbtMetaDataObjectClass MaterialOc = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswNbtMetaDataObjectClass.NbtObjectClass.MaterialClass );
             CswNbtMetaDataObjectClassProp MaterialRequestOcp = _CswNbtSchemaModTrnsctn.createObjectClassProp(
-                MaterialOc, new CswNbtWcfMetaDataModel.ObjectClassProp()
+                MaterialOc, new CswNbtWcfMetaDataModel.ObjectClassProp
                 {
                     FieldType = CswNbtMetaDataFieldType.NbtFieldType.Button,
                     PropName = CswNbtObjClassMaterial.RequestPropertyName
                 } );
             foreach( CswNbtMetaDataNodeType MaterialNt in MaterialOc.getNodeTypes() )
             {
+                Int32 FirsTabId = Int32.MinValue;
+                foreach( CswNbtMetaDataNodeTypeTab Tab in from _Tab in MaterialNt.getNodeTypeTabs() orderby _Tab.TabOrder, _Tab.TabId select _Tab )
+                {
+                    FirsTabId = Tab.TabId;
+                    break;
+                }
                 CswNbtMetaDataNodeTypeProp RequestNtp = MaterialNt.getNodeTypePropByObjectClassProp( CswNbtObjClassMaterial.RequestPropertyName );
-                RequestNtp.updateLayout( CswNbtMetaDataNodeTypeLayoutMgr.LayoutType.Table, true );
+                RequestNtp.updateLayout( CswNbtMetaDataNodeTypeLayoutMgr.LayoutType.Edit, true, FirsTabId );
+                RequestNtp.updateLayout( CswNbtMetaDataNodeTypeLayoutMgr.LayoutType.Table, true, FirsTabId );
             }
 
         }//Update()
