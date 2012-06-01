26ea9f7fe6f50924b6f2fdde2f6c1d7151de9f19
pglaser@chemsw.com <unknown@kiln.example.com>
2012-06-01 22:27:51 +0400

    case 9111: Add script to create log table

---
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase9111.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using ChemSW.Core;
+using ChemSW.DB;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.ObjClasses;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update for case 9111
+    /// </summary>
+    public class CswUpdateSchemaCase9111 : CswUpdateSchemaTo
+    {
+        public override void update()
+        {
+            string LogFileName = "logmessages";
+            string LogFileNamePk = "logmessageid";
+            _CswNbtSchemaModTrnsctn.addTable( LogFileName, LogFileNamePk );
+
+
+            foreach( ChemSW.Log.LegalAttribute CurrentLegalAttribute in Enum.GetValues( typeof( ChemSW.Log.LegalAttribute ) ) )
+            {
+                _CswNbtSchemaModTrnsctn.addStringColumn( LogFileName, CurrentLegalAttribute.ToString(), "The " + CurrentLegalAttribute.ToString() + " ", false, false, 32 );
+            }
+
+
+            //bulk loader complains about PKs, so we need to nuke pk column
+            _CswNbtSchemaModTrnsctn.dropColumn( LogFileName, LogFileNamePk );
+
+        }//Update()
+
+    }//class CswUpdateSchemaCase9111
+
+}//namespace ChemSW.Nbt.Schema
