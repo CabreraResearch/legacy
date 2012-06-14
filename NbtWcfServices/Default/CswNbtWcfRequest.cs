03ac3ae7957af5be68f9add79b05d92c2a7c8a4f
Christopher Froehlich <unknown@kiln.example.com>
2012-06-15 01:01:15 +0400

    Improve session/init validation.

--- NbtWcfServices/Default/CswNbtWcfRequest.cs
+++ NbtWcfServices/Default/CswNbtWcfRequest.cs
@@ -1,13 +1,20 @@
 ﻿
+using System.Runtime.Serialization;
+
 namespace NbtWebAppServices.Response
 {
     public class CswNbtWcfRequest : ICswNbtWcfRequest
     {
+        [DataContract]
         public class CswNbtSessionRequest
         {
+            [DataMember( IsRequired = true )]
             public string Password { get; set; }
+            [DataMember( IsRequired = true )]
             public string CustomerId { get; set; }
+            [DataMember( IsRequired = true )]
             public string UserName { get; set; }
+            [DataMember( IsRequired = true )]
             public bool IsMobile { get; set; }
         }
     }
