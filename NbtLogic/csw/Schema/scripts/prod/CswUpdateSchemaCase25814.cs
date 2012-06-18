483433fed7e912eb5f97e42b34683584da0b8a08
bvavra@chemsw.com <unknown@kiln.example.com>
2012-06-18 23:52:16 +0400

    Review 26658, Comment 6

--- NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase25814.cs
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase25814.cs
@@ -26,22 +26,6 @@ namespace ChemSW.Nbt.Schema
             CswNbtMetaDataObjectClassProp DisposedProp = _CswNbtSchemaModTrnsctn.MetaData.getObjectClassProp( ContainerOC.ObjectClassId, CswNbtObjClassContainer.DisposedPropertyName );
             _CswNbtSchemaModTrnsctn.MetaData.UpdateObjectClassProp( DisposedProp, CswNbtMetaDataObjectClassProp.ObjectClassPropAttributes.servermanaged, true );
 
-            //Rename existing buttons to Request
-            _CswNbtSchemaModTrnsctn.MetaData.UpdateObjectClassProp(
-                ContainerOC.getObjectClassProp( CswNbtObjClassContainer.RequestDispensePropertyName ),
-                CswNbtMetaDataObjectClassProp.ObjectClassPropAttributes.propname,
-                "Request Dispense" );
-
-            _CswNbtSchemaModTrnsctn.MetaData.UpdateObjectClassProp(
-                ContainerOC.getObjectClassProp( CswNbtObjClassContainer.RequestDisposePropertyName ),
-                CswNbtMetaDataObjectClassProp.ObjectClassPropAttributes.propname,
-                "Request Dispose" );
-
-            _CswNbtSchemaModTrnsctn.MetaData.UpdateObjectClassProp(
-                ContainerOC.getObjectClassProp( CswNbtObjClassContainer.RequestMovePropertyName ),
-                CswNbtMetaDataObjectClassProp.ObjectClassPropAttributes.propname,
-                "Request Move" );
-
         }//Update()
 
     }//class CswUpdateSchemaCase25814
