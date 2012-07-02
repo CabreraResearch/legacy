9b95b719cf5ba6887b90e37645aae6fdd6d4ecb1
Christopher Froehlich <unknown@kiln.example.com>
2012-07-02 22:38:11 +0400

    Case 26965. Part 2.

--- NbtWebApp/js/nbt/wizards/csw.receivematerial.js
+++ NbtWebApp/js/nbt/wizards/csw.receivematerial.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="~/js/CswNbt-vsdoc.js" />
+/// <reference path="~/js/CswNbt-vsdoc.js" />
 /// <reference path="~/js/CswCommon-vsdoc.js" />
 
 (function () {
@@ -109,7 +109,7 @@
                             isStepTwoAndHasQuantity = cswPrivate.getQuantity(false);
                         }
                     }
-                    return isNotStepTwoOrIsValid || isStepTwoAndHasQuantity;
+                    return isNotStepTwoOrIsValid && isStepTwoAndHasQuantity;
                 };
 
                 cswPrivate.finalize = function () {
