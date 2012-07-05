7c370ea37d913acc5d06415d325a4dca7c845829
Steven Salter <ssalter@chemsw.com>
2012-07-06 00:10:51 +0400

    case 27024 - ramifications

--- NbtWebApp/js/nbt/pagecmp/CswMenuHeader.js
+++ NbtWebApp/js/nbt/pagecmp/CswMenuHeader.js
@@ -23,60 +23,76 @@
 
         var $MenuDiv = $(this);
 
-        Csw.ajax.post({
-            url: o.Url,
-            data: {},
-            success: function (data) {
-                var $ul = $('<ul class="topnav"></ul>');
-                $MenuDiv.text('')
-                        .append($ul);
-                
-                for (var menuItem in data) {
-                    if (data.hasOwnProperty(menuItem)) {
-                        var thisItem = data[menuItem];
-                        if (!Csw.isNullOrEmpty(menuItem))
-                        {
-                            var $li = Csw.handleMenuItem({ $ul: $ul, 
-                                                        itemKey: menuItem,
-                                                        itemJson: thisItem, 
-                                                        onLogout: o.onLogout,
-                                                        onQuotas: o.onQuotas,
-                                                        onModules: o.onModules,
-                                                        onSessions: o.onSessions,
-                                                        onImpersonate: o.onImpersonate,
-                                                        onEndImpersonation: o.onEndImpersonation,
-                                                        onSubmitRequest: o.onSubmitRequest });
+        var menuOpts = { 
+            width: '100%',
+            ajax: { 
+                urlMethod: 'getHeaderMenu', 
+                data: {} 
+            },
+            onLogout: o.onLogout,
+            onQuotas: o.onQuotas,
+            onModules: o.onModules,
+            onSessions: o.onSessions,
+            onImpersonate: o.onImpersonate,
+            onEndImpersonation: o.onEndImpersonation,
+            onSubmitRequest: o.onSubmitRequest 
+        };
+        Csw.composites.menu( Csw.literals.factory($MenuDiv), menuOpts ); // menu()
 
-                            if (Csw.bool(thisItem.haschildren)) {
-                                delete thisItem.haschildren;
-                                var $subul = $('<ul class="subnav"></ul>')
-                                    .appendTo($li);
-                                for (var childItem in thisItem) {
-                                    if (thisItem.hasOwnProperty(childItem)) {
-                                        var thisChild = thisItem[childItem];
-                                        Csw.handleMenuItem({ $ul: $subul, 
-                                                         itemKey: childItem,
-                                                         itemJson: thisChild, 
-                                                         onLogout: o.onLogout,
-                                                         onQuotas: o.onQuotas,
-                                                         onModules: o.onModules,
-                                                         onSessions: o.onSessions,
-                                                         onImpersonate: o.onImpersonate,
-                                                         onEndImpersonation: o.onEndImpersonation,
-                                                         onSubmitRequest: o.onSubmitRequest
-                                                     });
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-                $ul.CswMenu();
+//        Csw.ajax.post({
+//            url: o.Url,
+//            data: {},
+//            success: function (data) {
+//                var $ul = $('<ul class="topnav"></ul>');
+//                $MenuDiv.text('')
+//                        .append($ul);
+//                
+//                for (var menuItem in data) {
+//                    if (data.hasOwnProperty(menuItem)) {
+//                        var thisItem = data[menuItem];
+//                        if (!Csw.isNullOrEmpty(menuItem))
+//                        {
+//                            var $li = Csw.handleMenuItem({ $ul: $ul, 
+//                                                        itemKey: menuItem,
+//                                                        itemJson: thisItem, 
+//                                                        onLogout: o.onLogout,
+//                                                        onQuotas: o.onQuotas,
+//                                                        onModules: o.onModules,
+//                                                        onSessions: o.onSessions,
+//                                                        onImpersonate: o.onImpersonate,
+//                                                        onEndImpersonation: o.onEndImpersonation,
+//                                                        onSubmitRequest: o.onSubmitRequest });
 
-                o.onSuccess();
+//                            if (Csw.bool(thisItem.haschildren)) {
+//                                delete thisItem.haschildren;
+//                                var $subul = $('<ul class="subnav"></ul>')
+//                                    .appendTo($li);
+//                                for (var childItem in thisItem) {
+//                                    if (thisItem.hasOwnProperty(childItem)) {
+//                                        var thisChild = thisItem[childItem];
+//                                        Csw.handleMenuItem({ $ul: $subul, 
+//                                                         itemKey: childItem,
+//                                                         itemJson: thisChild, 
+//                                                         onLogout: o.onLogout,
+//                                                         onQuotas: o.onQuotas,
+//                                                         onModules: o.onModules,
+//                                                         onSessions: o.onSessions,
+//                                                         onImpersonate: o.onImpersonate,
+//                                                         onEndImpersonation: o.onEndImpersonation,
+//                                                         onSubmitRequest: o.onSubmitRequest
+//                                                     });
+//                                    }
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//                $ul.CswMenu();
 
-            } // success{}
-        }); // $.ajax({
+//                o.onSuccess();
+
+//            } // success{}
+//        }); // $.ajax({
 
         // For proper chaining support
         return this;
