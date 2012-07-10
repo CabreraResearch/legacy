f7cd0adba44e72097f916106350b5a7ce13d4886
Steven Salter <ssalter@chemsw.com>
2012-07-11 00:35:01 +0400

    case 26683 - fix existing data

---
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase26683.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data;
+using ChemSW.Core;
+using ChemSW.DB;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.ObjClasses;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update for case 26683
+    /// </summary>
+    public class CswUpdateSchemaCase26683 : CswUpdateSchemaTo
+    {
+        public override void update()
+        {
+            // Clear hidden/readonly flags on certain fields:
+            CswNbtMetaDataObjectClass InspectionDesignOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswNbtMetaDataObjectClass.NbtObjectClass.InspectionDesignClass );
+            CswNbtMetaDataObjectClass UserOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswNbtMetaDataObjectClass.NbtObjectClass.UserClass );
+            CswNbtMetaDataObjectClass FeedbackOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswNbtMetaDataObjectClass.NbtObjectClass.FeedbackClass );
+
+            foreach( CswNbtObjClassUser UserNode in UserOC.getNodes( false, true ) )
+            {
+                UserNode.AccountLocked.setReadOnly( value: false, SaveToDb: true );
+                UserNode.AccountLocked.setHidden( value: false, SaveToDb: true );
+
+                UserNode.FailedLoginCount.setReadOnly( value: false, SaveToDb: true );
+                UserNode.FailedLoginCount.setHidden( value: false, SaveToDb: true );
+                UserNode.postChanges( true );
+            }
+
+            foreach( CswNbtObjClassFeedback FeedbackNode in FeedbackOC.getNodes( false, true ) )
+            {
+                FeedbackNode.LoadUserContext.setReadOnly( value: false, SaveToDb: true );
+                FeedbackNode.LoadUserContext.setHidden( value: false, SaveToDb: true );
+                FeedbackNode.postChanges( true );
+            }
+
+            foreach( CswNbtObjClassInspectionDesign InspectionDesignNode in InspectionDesignOC.getNodes( false, true ) )
+            {
+                InspectionDesignNode.Status.setReadOnly( value: false, SaveToDb: true );
+                InspectionDesignNode.Status.setHidden( value: false, SaveToDb: true );
+                InspectionDesignNode.postChanges( true );
+            }
+
+
+
+        }//Update()
+
+    }//class CswUpdateSchemaCase26683
+
+}//namespace ChemSW.Nbt.Schema
