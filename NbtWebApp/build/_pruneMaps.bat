0c46439d3ecf33a640d5b16318a1e495e62c647c
Christopher Froehlich <unknown@kiln.example.com>
2012-07-13 18:53:25 +0400

    Suppress documentation errors.

---
+++ NbtWebApp/build/_pruneMaps.bat
@@ -0,0 +1,7 @@
+﻿set farpath=%1
+
+%1build\far.exe %1js\*.js.map "C:\\kiln\\Nbt\\Nbt\\NbtWebApp\\js\\" "\\"
+%1build\far.exe %1js\*.js.map "D:\\kiln\\Nbt\\Nbt\\NbtWebApp\\js\\" "\\"
+
+%1build\far.exe %1js\*.js.map "C:\\kiln\\Nbt\\Nbt\\NbtWebApp\\" "\\"
+%1build\far.exe %1js\*.js.map "D:\\kiln\\Nbt\\Nbt\\NbtWebApp\\" "\\"
