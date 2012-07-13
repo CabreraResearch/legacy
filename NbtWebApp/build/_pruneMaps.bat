42cb9c96a0284365742d084ad18e8510dce0239e
Christopher Froehlich <unknown@kiln.example.com>
2012-07-13 19:02:13 +0400

    Review 27143. Find and Replace.exe

--- NbtWebApp/build/_pruneMaps.bat
+++
@@ -1,7 +0,0 @@
-﻿set farpath=%1
-
-%1build\far.exe %1js\*.js.map "C:\\kiln\\Nbt\\Nbt\\NbtWebApp\\js\\" "\\"
-%1build\far.exe %1js\*.js.map "D:\\kiln\\Nbt\\Nbt\\NbtWebApp\\js\\" "\\"
-
-%1build\far.exe %1js\*.js.map "C:\\kiln\\Nbt\\Nbt\\NbtWebApp\\" "\\"
-%1build\far.exe %1js\*.js.map "D:\\kiln\\Nbt\\Nbt\\NbtWebApp\\" "\\"
