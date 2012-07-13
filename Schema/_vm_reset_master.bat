c5b0e9ffaec251c074832a78f3533b30980db5e3
Christopher Froehlich <unknown@kiln.example.com>
2012-07-13 21:02:46 +0400

    Review 27131. Case 26602.

--- Schema/_vm_reset_master.bat
+++ Schema/_vm_reset_master.bat
@@ -5,5 +5,3 @@ impdp.exe nbt/nbt@w2008x64Db DUMPFILE=NBT_MASTER_11G.dmp DIRECTORY=NBTDUMPS REMA
 echo exit | sqlplus nbt/nbt@w2008x64Db @nbt_finalize_ora.sql
 
 C:\kiln\Nbt\Nbt\NbtSchemaUpdaterCmdLn\bin\Debug\NbtUpdt.exe -accessid 1 -mode prod
-
-exit
