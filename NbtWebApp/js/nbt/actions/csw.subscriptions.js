c21fa3818b3db6d77e1379c956dc6e29ee59d6ca
bvavra@chemsw.com <unknown@kiln.example.com>
2012-07-16 23:59:34 +0400

    Case 27046, part of Part 2.1 - setting up tables (punted to Quince)

--- NbtWebApp/js/nbt/actions/csw.subscriptions.js
+++ NbtWebApp/js/nbt/actions/csw.subscriptions.js
@@ -9,12 +9,52 @@
                 urlMethod: 'getSubscriptions',
                 ID: 'action_subscriptions'
             };
-            if (options) $.extend(o, options);
+            if (options) $.extend(o, options)
 
-            cswParent.div().css({ 'text-align': 'center' }).span({ text: "Edit Your Subscriptions to Notifications and Mail Reports:" }).br();
+            var description,
+                controlTables,
+                notificationControl = { table: null, row: 1 },
+                mailReportControl = { table: null, row: 1 };
 
-            //ajax call - loading of two tables - one for notifications, and one for mail reports, with a CheckAll func below
-            //save button, onClick ajax call - takes all checked items and subscribes them
+            var initializeNotificationTable = function () {
+                notificationControl.table = controlTables.cell(2, 1).table({
+                    suffix: 'tbl',
+                    border: 1,
+                    cellpadding: 5
+                });
+
+                notificationControl.table.cell(notificationControl.row, 1).b({ text: 'Notification' });
+                notificationControl.table.cell(notificationControl.row, 2).b({ text: 'Subscribe' });
+                notificationControl.row += 1;
+            }
+
+            var initializeMailReportTable = function () {
+                mailReportControl.table = controlTables.cell(2, 2).table({
+                    suffix: 'tbl',
+                    border: 1,
+                    cellpadding: 5
+                });
+
+                mailReportControl.table.cell(mailReportControl.row, 1).b({ text: 'Mail Report' });
+                mailReportControl.table.cell(mailReportControl.row, 2).b({ text: 'Subscribe' });
+                mailReportControl.row += 1;
+            }
+
+            var initializeUI = function () {
+                cswParent.br();
+                
+                controlTables = cswParent.table({
+                    suffix: 'tbl',
+                    cellpadding: 10
+                });
+                description = controlTables.cell(1,1).span({ text: "Edit Your Subscriptions to Notifications and Mail Reports:" });
+                initializeNotificationTable();
+                initializeMailReportTable();
+                //ajax call - loading of two tables - one for notifications, and one for mail reports, with a CheckAll func below
+                //save button, onClick ajax call - takes all checked items and subscribes them
+            }
+
+            initializeUI();
 
         });
 } ());
