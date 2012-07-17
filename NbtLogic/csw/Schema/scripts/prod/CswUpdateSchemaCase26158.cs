9a70961b0ef7f049b6dffcae4d10f59cd256c610
pglaser@chemsw.com <unknown@kiln.example.com>
2012-07-17 23:20:10 +0400

    case 26158: comitting adding script (empty) file (before I forget)

---
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase26158.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update for case 26158
+    /// </summary>
+    public class CswUpdateSchemaCase26158 : CswUpdateSchemaTo
+    {
+        public override void update()
+        {
+            // This is a placeholder script that does nothing.
+        }//Update()
+
+    }//class CswUpdateSchemaCaseXXXXX
+
+}//namespace ChemSW.Nbt.Schema
