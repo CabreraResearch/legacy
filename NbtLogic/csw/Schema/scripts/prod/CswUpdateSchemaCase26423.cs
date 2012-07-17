55912f750429c0b2ee438d5c976ef40cf20fdf51
mbischoff@chemsw.com <unknown@kiln.example.com>
2012-07-17 23:53:54 +0400

    review 27213 - did it BV's (aka better) way

--- NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase26423.cs
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase26423.cs
@@ -16,32 +16,16 @@ namespace ChemSW.Nbt.Schema
         public override void update()
         {
 
-
             CswNbtWelcomeTable welcomeTable = _CswNbtSchemaModTrnsctn.getWelcomeTable();
 
-            //get the role id of chemsw_admin
-            CswNbtMetaDataNodeType roleNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "Role" );
-            string strRoleId = null;
-            if( null != roleNT )
-            {
-                foreach( CswNbtNode node in roleNT.getNodes( false, true ) )
-                {
-                    if( node.NodeName.Equals( "chemsw_admin_role" ) )
-                    {
-                        strRoleId = node.NodeId.ToString();
-                    }
-                }
-            }
+            CswNbtNode cswAdminRoleNode = _CswNbtSchemaModTrnsctn.Nodes.makeRoleNodeFromRoleName( CswNbtObjClassRole.ChemSWAdminRoleName );
 
-            if( null != strRoleId )
-            {
-                Int32 designActId = _CswNbtSchemaModTrnsctn.Actions[Actions.CswNbtActionName.Design].ActionId;
-                Int32 createInsId = _CswNbtSchemaModTrnsctn.Actions[Actions.CswNbtActionName.Create_Inspection].ActionId;
-                CswNbtWelcomeTable.WelcomeComponentType linkType = CswNbtWelcomeTable.WelcomeComponentType.Link;
+            Int32 designActId = _CswNbtSchemaModTrnsctn.Actions[Actions.CswNbtActionName.Design].ActionId;
+            Int32 createInsId = _CswNbtSchemaModTrnsctn.Actions[Actions.CswNbtActionName.Create_Inspection].ActionId;
+            CswNbtWelcomeTable.WelcomeComponentType linkType = CswNbtWelcomeTable.WelcomeComponentType.Link;
 
-                welcomeTable.AddWelcomeItem( linkType, CswNbtView.ViewType.Action, designActId.ToString(), Int32.MinValue, "Design Mode", Int32.MinValue, Int32.MinValue, "wrench.gif", strRoleId );
-                welcomeTable.AddWelcomeItem( linkType, CswNbtView.ViewType.Action, createInsId.ToString(), Int32.MinValue, "Create Inspection", Int32.MinValue, Int32.MinValue, "createinspection.png", strRoleId );
-            }
+            welcomeTable.AddWelcomeItem( linkType, CswNbtView.ViewType.Action, designActId.ToString(), Int32.MinValue, "Design Mode", Int32.MinValue, Int32.MinValue, "wrench.gif", cswAdminRoleNode.NodeId.ToString() );
+            welcomeTable.AddWelcomeItem( linkType, CswNbtView.ViewType.Action, createInsId.ToString(), Int32.MinValue, "Create Inspection", Int32.MinValue, Int32.MinValue, "createinspection.png", cswAdminRoleNode.NodeId.ToString() );
 
         }//Update()
 
