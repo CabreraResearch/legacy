e5ef78b7058520cc30217be507dff159d1b14aa8
NBT Daily Build <b3851691-5f24-46c9-9188-5f0a2e70960a>
2012-07-17 08:17:01 +0400

    Automated commit for release: Prospero_2012.7.17.1

--- NbtSchedService/Properties/AssemblyInfo.cs
+++ NbtSchedService/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "2012.7.16.3" )]
-[assembly: AssemblyFileVersion( "2012.7.16.3" )]
+[assembly: AssemblyVersion( "2012.7.17.1" )]
+[assembly: AssemblyFileVersion( "2012.7.17.1" )]
