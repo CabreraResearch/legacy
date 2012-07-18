2682d099ffeab3e8deb87eb143b65a631efa7f9a
mbischoff@chemsw.com <unknown@kiln.example.com>
2012-07-18 22:12:25 +0400

    case 27064 - barcodes default to unique

--- NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
+++ NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
@@ -107,6 +107,7 @@ namespace ChemSW.Nbt.Schema
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchemaCase27147() ) );                     //01P-80
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchemaCase27155() ) );                     //01P-81
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchemaCase26423() ) );                     //01P-82
+            _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchemaCase27064() ) );                     //01P-83
 
 
             // This automatically detects the latest version
---
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase27064.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+using ChemSW.Core;
+using ChemSW.DB;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.ObjClasses;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update for case 27064
+    /// </summary>
+    public class CswUpdateSchemaCase27064 : CswUpdateSchemaTo
+    {
+        public override void update()
+        {
+            CswNbtMetaDataNodeType containerNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "Container" );
+            if( null != containerNT )
+            {
+                CswNbtMetaDataNodeTypeProp barcodeNTP = containerNT.getNodeTypeProp( CswNbtObjClassContainer.BarcodePropertyName );
+                if( null != barcodeNTP )
+                {
+                    barcodeNTP.setIsUnique( true );
+                }
+            }
+        }//Update()
+
+    }//class CswUpdateSchemaCase27064
+
+}//namespace ChemSW.Nbt.Schema
