e25bb714a3b44f224841a9a811fc7ce03c6b6ae8
Christopher Froehlich <unknown@kiln.example.com>
2012-07-18 22:17:56 +0400

    Case 27071. Total Quantity.

---
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase27071RequestItemProps2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+using ChemSW.Core;
+using ChemSW.DB;
+using ChemSW.Nbt.Actions;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.ObjClasses;
+using ChemSW.Nbt.PropTypes;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update for case 27071RequestItemProps2
+    /// </summary>
+    public class CswUpdateSchemaCase27071RequestItemProps2 : CswUpdateSchemaTo
+    {
+        public override void update()
+        {
+            CswNbtMetaDataObjectClass RequestItemOc = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswNbtMetaDataObjectClass.NbtObjectClass.RequestItemClass );
+            _CswNbtSchemaModTrnsctn.createObjectClassProp(new CswNbtWcfMetaDataModel.ObjectClassProp(RequestItemOc)
+                {
+                    PropName = CswNbtObjClassRequestItem.PropertyName.TotalDispensed,
+                    FieldType = CswNbtMetaDataFieldType.NbtFieldType.Quantity,
+                    ServerManaged = true
+                });
+
+        }//Update()
+
+    }//class CswUpdateSchemaCase27071RequestItemProps2
+
+}//namespace ChemSW.Nbt.Schema
