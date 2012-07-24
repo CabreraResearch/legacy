e2b255a85633ac7275023d2cfaa1640f4efad99f
pglaser@chemsw.com <unknown@kiln.example.com>
2012-07-24 17:10:58 +0400

    case 26867: reset previous schedule user name

--- NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
+++ NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
@@ -114,6 +114,7 @@ namespace ChemSW.Nbt.Schema
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchemaCase27223() ) );                     //01P-88
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchemaCase27263() ) );                     //01P-89
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchemaCase27063() ) );                     //01P-90
+            _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchemaCase26867() ) );                     //01P-91
 
             // This automatically detects the latest version
             _LatestVersion = _MinimumVersion;
---
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase26867.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update for case XXXXX
+    /// </summary>
+    public class CswUpdateSchemaCase26867 : CswUpdateSchemaTo
+    {
+        public override void update()
+        {
+            _CswNbtSchemaModTrnsctn.execArbitraryPlatformNeutralSql( "update audit_transactions set transactionusername = '" + ChemSW.Nbt.Security.SystemUserNames.SysUsr_SchemaUpdt.ToString() + "' where transactionusername='_SchemaUpdaterUser'" );
+
+        }//Update()
+
+    }//class CswUpdateSchemaCase26867
+
+}//namespace ChemSW.Nbt.Schema
