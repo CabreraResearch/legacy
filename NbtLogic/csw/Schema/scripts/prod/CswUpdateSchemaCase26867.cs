88b28d3b13d33b15120846124f636cf6cda6c1a9
pglaser@chemsw.com <unknown@kiln.example.com>
2012-07-24 20:03:51 +0400

    case 26867: under sript rationing, adding default audit settings to script (has not been pushed to main yet anyway)

--- NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase26867.cs
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase26867.cs
@@ -8,7 +8,71 @@ namespace ChemSW.Nbt.Schema
     {
         public override void update()
         {
+
+            //retroactively update the audit user name to the new standard name
             _CswNbtSchemaModTrnsctn.execArbitraryPlatformNeutralSql( "update audit_transactions set transactionusername = '" + ChemSW.Nbt.Security.SystemUserNames.SysUsr_SchemaUpdt.ToString() + "' where transactionusername='_SchemaUpdaterUser'" );
+
+            //Set auditing defaults as per TDU :-( 
+            _CswNbtSchemaModTrnsctn.execArbitraryPlatformNeutralSql( @"update nodetype_props
+                                                                       set auditlevel = 'NoAudit'
+                                                                     where nodetypeid in
+                                                                           (select nodetypeid
+                                                                              from nodetypes
+                                                                             where nodetypename in
+                                                                                   ('Aliquot', 'Container Dispense Transaction',
+                                                                                    'Assembly Document', 'Container Document',
+                                                                                    'Equipment Document', 'Material Document', 'Equipment Type',
+                                                                                    'Feedback', 'Assembly Schedule', 'Equipment Schedule',
+                                                                                    'Inspection Schedule', 'Department', 'GHS Phrase',
+                                                                                    'Lab Safety (demo)', 'Lab Safety Group (demo)',
+                                                                                    'Inventory Group', 'Mail Report', 'Notification', 'Parameter',
+                                                                                    'Print Label', 'IMCS Report', 'Report', 'SI Report', 'Test',
+                                                                                    'Unit (Time)', 'Work Unit', 'Equipment'))" );
+
+
+            _CswNbtSchemaModTrnsctn.execArbitraryPlatformNeutralSql( @"update nodetypes
+                                                                       set auditlevel = 'NoAudit'
+                                                                     where nodetypename in
+                                                                           ('Aliquot', 'Container Dispense Transaction', 'Assembly Document',
+                                                                            'Container Document', 'Equipment Document', 'Material Document',
+                                                                            'Equipment Type', 'Feedback', 'Assembly Schedule',
+                                                                            'Equipment Schedule', 'Inspection Schedule', 'Department',
+                                                                            'GHS Phrase', 'Lab Safety (demo)', 'Lab Safety Group (demo)',
+                                                                            'Inventory Group', 'Mail Report', 'Notification', 'Parameter',
+                                                                            'Print Label', 'IMCS Report', 'Report', 'SI Report', 'Test',
+                                                                            'Unit (Time)', 'Work Unit', 'Equipment')" );
+
+
+            _CswNbtSchemaModTrnsctn.execArbitraryPlatformNeutralSql( @"update nodetype_props
+                                                                       set auditlevel = 'PlainAudit'
+                                                                     where nodetypeid in
+                                                                           (select nodetypeid
+                                                                              from nodetypes
+                                                                             where nodetypename not in
+                                                                                   ('Aliquot', 'Container Dispense Transaction',
+                                                                                    'Assembly Document', 'Container Document',
+                                                                                    'Equipment Document', 'Material Document', 'Equipment Type',
+                                                                                    'Feedback', 'Assembly Schedule', 'Equipment Schedule',
+                                                                                    'Inspection Schedule', 'Department', 'GHS Phrase',
+                                                                                    'Lab Safety (demo)', 'Lab Safety Group (demo)',
+                                                                                    'Inventory Group', 'Mail Report', 'Notification', 'Parameter',
+                                                                                    'Print Label', 'IMCS Report', 'Report', 'SI Report', 'Test',
+                                                                                    'Unit (Time)', 'Work Unit', 'Equipment'))" );
+
+
+
+            _CswNbtSchemaModTrnsctn.execArbitraryPlatformNeutralSql( @"update nodetypes
+                                                                       set auditlevel = 'PlainAudit'
+                                                                     where nodetypename not in
+                                                                           ('Aliquot', 'Container Dispense Transaction', 'Assembly Document',
+                                                                            'Container Document', 'Equipment Document', 'Material Document',
+                                                                            'Equipment Type', 'Feedback', 'Assembly Schedule',
+                                                                            'Equipment Schedule', 'Inspection Schedule', 'Department',
+                                                                            'GHS Phrase', 'Lab Safety (demo)', 'Lab Safety Group (demo)',
+                                                                            'Inventory Group', 'Mail Report', 'Notification', 'Parameter',
+                                                                            'Print Label', 'IMCS Report', 'Report', 'SI Report', 'Test',
+                                                                            'Unit (Time)', 'Work Unit', 'Equipment')" );
+
 
         }//Update()
 
