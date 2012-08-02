54ab6f23caf77cfe27af42d4caadee21d23ed897
bvavra@chemsw.com <unknown@kiln.example.com>
2012-08-02 21:51:37 +0400

    27465.3.1 - countControl Required value can now be passed in.

--- NbtWebApp/js/nbt/wizards/csw.wizard.amountsgrid.js
+++ NbtWebApp/js/nbt/wizards/csw.wizard.amountsgrid.js
@@ -36,7 +36,8 @@
                         quantityName: 'Net Quantity *',
                         barcodeName: 'Barcodes (Optional)'
                     },
-                    customBarcodes: false
+                    customBarcodes: false,
+                    countControlRequired: true
                 };
                 if (options) {
                     $.extend(cswPrivate, options);
@@ -144,7 +145,7 @@
                                     ceilingVal: (cswPrivate.containerlimit - Csw.number(cswPrivate.count, 0)),
                                     width: (3 * 8) + 'px', //3 characters wide, 8 is the characters-to-pixels ratio
                                     Precision: 0,
-                                    Required: true,
+                                    Required: cswPrivate.countControlRequired,
                                     onChange: function (value) {
                                         extendNewAmount({ containerNo: value });
                                     }
