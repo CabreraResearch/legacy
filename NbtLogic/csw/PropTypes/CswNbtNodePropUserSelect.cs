3ba149273bc86be85620c6429416023c319fa8af
mbischoff@chemsw.com <unknown@kiln.example.com>
2012-08-10 23:11:38 +0400

    review K3576 - archived users do not receive MailReports or Notifications

--- NbtLogic/csw/PropTypes/CswNbtNodePropUserSelect.cs
+++ NbtLogic/csw/PropTypes/CswNbtNodePropUserSelect.cs
@@ -74,6 +74,18 @@ namespace ChemSW.Nbt.PropTypes
                     _SelectedUserIds.OnChange += _SelectedUserIds_OnChange;
                     _SelectedUserIds.FromString( _CswNbtNodePropData.GetPropRowValue( _SelectedUserIdsSubField.Column ) );
                 }
+                //removed archived users
+                for( int i = 0; i < _SelectedUserIds.Count; i++ ) //foreach won't work here because we edit the list while we iterate it
+                {
+                    CswPrimaryKey pk = new CswPrimaryKey();
+                    pk.FromString( "nodes_" + _SelectedUserIds[i] );
+                    CswNbtNode node = _CswNbtResources.Nodes.GetNode( pk );
+                    CswNbtObjClassUser nodeAsUser = (CswNbtObjClassUser) node;
+                    if( nodeAsUser.IsArchived() )
+                    {
+                        _SelectedUserIds.Remove( _SelectedUserIds[i] );
+                    }
+                }
                 return _SelectedUserIds;
             }
             set
