1bfcee1d177812596210563f92a357c62c113a32
mbischoff@chemsw.com <unknown@kiln.example.com>
2012-08-21 23:12:26 +0400

    review K3622 - simplified

--- NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase27549.cs
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase27549.cs
@@ -20,45 +20,12 @@ namespace ChemSW.Nbt.Schema
             CswNbtMetaDataNodeType materialComponentNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "Material Component" );
             if( null != materialComponentNT )
             {
-                CswNbtMetaDataObjectClass materialOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswNbtMetaDataObjectClass.NbtObjectClass.MaterialClass );
-                CswNbtMetaDataObjectClassProp tradeNameOCP = materialOC.getObjectClassProp( CswNbtObjClassMaterial.TradenamePropertyName );
-
-                CswNbtMetaDataObjectClass materialComponentOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswNbtMetaDataObjectClass.NbtObjectClass.MaterialComponentClass );
-                CswNbtMetaDataObjectClassProp constituentOCP = materialComponentOC.getObjectClassProp( CswNbtObjClassMaterialComponent.ConstituentPropertyName );
-
-                CswNbtMetaDataNodeTypeTab materialCompTab = _getTab( materialComponentNT, "Material Component", 1 );
-
-                CswNbtMetaDataNodeTypeProp matCompConstituentNTP = _CswNbtSchemaModTrnsctn.MetaData.makeNewProp(
-                    materialComponentNT,
-                    CswNbtMetaDataFieldType.NbtFieldType.PropertyReference,
-                    "Constituent Tradename",
-                    materialCompTab.TabId );
-                matCompConstituentNTP.SetFK( NbtViewPropType.ObjectClassPropId.ToString(), constituentOCP.PropId, NbtViewPropType.ObjectClassPropId.ToString(), tradeNameOCP.PropId );
-
-                matCompConstituentNTP.removeFromAllLayouts();
-
-                string templateText = CswNbtMetaData.MakeTemplateEntry( matCompConstituentNTP.PropName );
+                string templateText = CswNbtMetaData.MakeTemplateEntry( CswNbtObjClassMaterialComponent.ConstituentPropertyName );
                 materialComponentNT.setNameTemplateText( templateText );
-
             }
 
 
         }//Update()
-
-        private CswNbtMetaDataNodeTypeTab _getTab( CswNbtMetaDataNodeType nodetype, string tabName, Int32 order )
-        {
-            CswNbtMetaDataNodeTypeTab tab = nodetype.getNodeTypeTab( tabName );
-            if( null == tab )
-            {
-                tab = _CswNbtSchemaModTrnsctn.MetaData.makeNewTab( nodetype, tabName, order );
-            }
-            else
-            {
-                tab.TabOrder = order;
-            }
-            return tab;
-        } // _getTab()
-
     }
 
 }//namespace ChemSW.Nbt.Schema
