56c39557cf3f538fdeac5d2c65feedfa1db12c93
Christopher Froehlich <unknown@kiln.example.com>
2012-08-29 20:30:39 +0400

    Case 27568. Scripts.

--- NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
+++ NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
@@ -34,11 +34,11 @@ namespace ChemSW.Nbt.Schema
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchemaCase27145() ) );                                    //01Q-008
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_Requesting_Case27542() ) );                        //01Q-009
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_Logging_Case27504() ) );                           //01Q-010
-            _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchemaCase27438() ) );                                    //01Q-011
+            _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_Size_Case27438() ) );                                    //01Q-011
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchemaCase27548() ) );                                    //01Q-012                        
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_Documents_Case27435() ) );                         //01R-001 // ROMEO!            
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchemaCase27493() ) );                                    //01R-002 // ROMEO! wherefore art thou Romeo?
-            _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchemaCase27488() ) );                                    //01R-003 // ROMEO!            
+            _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_RequestItem_Case27488() ) );                                    //01R-003 // ROMEO!            
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchemaCase27500() ) );                                    //01R-004 // ROMEO!           
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_MSDS_Case27435() ) );                              //01R-005 // ROMEO!
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchemaCase27432() ) );                                    //01R-006 // ROMEO!
