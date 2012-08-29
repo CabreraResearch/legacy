6df8bed0332d2872eb06d7f43d7222741b52c1e3
Christopher Froehlich <unknown@kiln.example.com>
2012-08-29 19:54:45 +0400

    Case 27568. Fix Size view.

---
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchema_Size_Case27438.cs
@@ -0,0 +1,29 @@
+﻿using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.ObjClasses;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update for case 27438
+    /// </summary>
+    public class CswUpdateSchema_Size_Case27438 : CswUpdateSchemaTo
+    {
+        public override void update()
+        {
+            CswNbtMetaDataObjectClass SizeOc = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswNbtMetaDataObjectClass.NbtObjectClass.SizeClass );
+            CswNbtMetaDataNodeType SizeNt = SizeOc.FirstNodeType;
+
+            if( SizeNt != null )
+            {
+                //Hide Dispensable and QuantityEditable properties
+                CswNbtMetaDataNodeTypeProp DispensableNtp = SizeNt.getNodeTypePropByObjectClassProp( CswNbtObjClassSize.DispensablePropertyName );
+                DispensableNtp.removeFromAllLayouts();
+                CswNbtMetaDataNodeTypeProp QuantityEditableNtp = SizeNt.getNodeTypePropByObjectClassProp( CswNbtObjClassSize.QuantityEditablePropertyName );
+                QuantityEditableNtp.removeFromAllLayouts();
+            }
+
+        }//Update()
+
+    }//class CswUpdateSchemaCase27438
+
+}//namespace ChemSW.Nbt.Schema
