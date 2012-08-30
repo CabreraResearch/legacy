3b00f6b30206cb3d9fbef536dd6050e138934f79
Christopher Froehlich <unknown@kiln.example.com>
2012-08-30 18:33:01 +0400

    Case 27589. PropertyName. Material.

--- NbtLogic/csw/ObjClasses/CswNbtObjClassMaterial.cs
+++ NbtLogic/csw/ObjClasses/CswNbtObjClassMaterial.cs
@@ -64,7 +64,7 @@ namespace ChemSW.Nbt.ObjClasses
         }
 
         #region Inherited Events
-        
+
         public override void beforeWriteNode( bool IsCopy, bool OverrideUniqueValidation )
         {
             if( ApprovalStatus.WasModified )
@@ -258,8 +258,8 @@ namespace ChemSW.Nbt.ObjClasses
         public void getParentMaterials( ref CswCommaDelimitedString MachingMaterialIDs )
         {
             CswNbtMetaDataObjectClass materialComponentOC = _CswNbtResources.MetaData.getObjectClass( CswNbtMetaDataObjectClass.NbtObjectClass.MaterialComponentClass );
-            CswNbtMetaDataObjectClassProp constituentOCP = materialComponentOC.getObjectClassProp( CswNbtObjClassMaterialComponent.ConstituentPropertyName );
-            CswNbtMetaDataObjectClassProp mixtureOCP = materialComponentOC.getObjectClassProp( CswNbtObjClassMaterialComponent.MixturePropertyName );
+            CswNbtMetaDataObjectClassProp constituentOCP = materialComponentOC.getObjectClassProp( CswNbtObjClassMaterialComponent.PropertyName.Constituent );
+            CswNbtMetaDataObjectClassProp mixtureOCP = materialComponentOC.getObjectClassProp( CswNbtObjClassMaterialComponent.PropertyName.Mixture );
 
             CswNbtView componentsView = new CswNbtView( _CswNbtResources );
             CswNbtViewRelationship parent = componentsView.AddViewRelationship( materialComponentOC, false );
