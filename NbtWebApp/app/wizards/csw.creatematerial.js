39e7f574d7aa3d513fbb47c3991620f1d5008299
bvavra@chemsw.com <unknown@kiln.example.com>
2012-09-04 22:23:39 +0400

    Backed out changeset: e032a0dc1e0c (Case 27664, Part 0)

--- NbtWebApp/app/wizards/csw.creatematerial.js
+++ NbtWebApp/app/wizards/csw.creatematerial.js
@@ -29,7 +29,9 @@
                 stepFourComplete: false,
                 config: {
                     quantityName: 'Initial Quantity *',
-                    numberName: 'Catalog No.'
+                    numberName: 'Catalog No.',
+                    dispensibleName: 'Dispensible',
+                    quantEditableName: 'Quantity Editable'
                 },
                 rows: [],
                 tabsAndProps: null,
@@ -53,8 +55,12 @@
             };
 
             var cswPublic = {
+                physicalStateCrl: null,
                 catalogNoCtrl: null,
                 quantityCtrl: null,
+                untitsCrl: null,
+                dispensibleCtrl: null,
+                quantEditableCtrl: null,
                 sizesForm: null,
                 sizeGrid: null,
                 sizes: []
@@ -469,6 +475,8 @@
                                 quantity: '',
                                 unit: '',
                                 unitid: '',
+                                quantEditableChecked: 'false', //default?
+                                dispensibleChecked: 'false', //default?
                                 nodetypeid: cswPrivate.state.sizeNodeTypeId
                             };
 
@@ -482,7 +490,7 @@
                                 return ret;
                             };
 
-                            cswPrivate.header = [cswPrivate.config.quantityName, cswPrivate.config.numberName];
+                            cswPrivate.header = [cswPrivate.config.quantityName, cswPrivate.config.numberName, cswPrivate.config.quantEditableName, cswPrivate.config.dispensibleName];
                             if (cswPrivate.rows.length === 0) {
                                 cswPrivate.rows.push(cswPrivate.header);
                             }
@@ -500,7 +508,9 @@
                                         catalogNo: '',
                                         quantity: '',
                                         unit: '',
-                                        unitid: ''
+                                        unitid: '',
+                                        quantEditableChecked: 'false', //default?
+                                        dispensibleChecked: 'false' //default?
                                     };
 
                                     switch (columnName) {
@@ -524,6 +534,26 @@
                                                 }
                                             });
                                             break;
+                                        case cswPrivate.config.quantEditableName:
+                                            cswPublic.quantEditableCtrl = cswCell.checkBox({
+                                                ID: Csw.tryExec(Csw.makeId, 'sizeQuantEditable'),
+                                                Checked: true,
+                                                onChange: function (value) {
+                                                    thisSize.quantEditableChecked = cswPublic.quantEditableCtrl.val();
+                                                    extendNewAmount(thisSize);
+                                                }
+                                            });
+                                            break;
+                                        case cswPrivate.config.dispensibleName:
+                                            cswPublic.dispensibleCtrl = cswCell.checkBox({
+                                                ID: Csw.tryExec(Csw.makeId, 'sizeDispensible'),
+                                                Checked: true,
+                                                onChange: function (value) {
+                                                    thisSize.dispensibleChecked = cswPublic.dispensibleCtrl.val();
+                                                    extendNewAmount(thisSize);
+                                                }
+                                            });
+                                            break;
                                     }
                                     extendNewAmount(thisSize);
                                 },
@@ -543,11 +573,13 @@
                                         newSize.quantity = cswPublic.quantityCtrl.val();
                                         newSize.unit = cswPublic.unitsCtrl.val();
                                         newSize.catalogNo = cswPublic.catalogNoCtrl.val();
+                                        newSize.dispensibleChecked = cswPublic.dispensibleCtrl.val();
+                                        newSize.quantEditableChecked = cswPublic.quantEditableCtrl.val();
                                         newSize.unitid = getID(newSize.unit);
                                         if (false === Csw.isNullOrEmpty(newSize.quantity)) {
                                             if (isSizeNew(newSize)) {
                                                 cswPublic.sizes.push(extractNewAmount(newSize));
-                                                cswPublic.sizeGrid.addRows([newSize.quantity + ' ' + newSize.unit, newSize.catalogNo]);
+                                                cswPublic.sizeGrid.addRows([newSize.quantity + ' ' + newSize.unit, newSize.catalogNo, newSize.quantEditableChecked, newSize.dispensibleChecked]);
                                             } else {
                                                 $.CswDialog('AlertDialog', 'This size is already defined. Please define a new, unique size.');
                                             }
