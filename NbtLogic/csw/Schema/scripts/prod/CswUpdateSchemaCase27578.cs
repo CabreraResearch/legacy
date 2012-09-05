705134421f64f9a3ed56d7d5983955ef03e1e5db
mbischoff@chemsw.com <unknown@kiln.example.com>
2012-09-05 20:27:50 +0400

    review K3705 - getFirstNodeTypeTab()

--- NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase27578.cs
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase27578.cs
@@ -18,29 +18,26 @@ namespace ChemSW.Nbt.Schema
             CswNbtMetaDataNodeType sizeNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "Size" );
             if( null != sizeNT )
             {
-                CswNbtMetaDataNodeTypeTab sizeNTT = _CswNbtSchemaModTrnsctn.MetaData.getNodeTypeTab( sizeNT.NodeTypeId, "Size" );
-                if( null != sizeNTT )
-                {
-                    CswNbtMetaDataNodeTypeProp containerTypeNTP = _CswNbtSchemaModTrnsctn.MetaData.makeNewProp( sizeNT, CswNbtMetaDataFieldType.NbtFieldType.List, "Container Type", sizeNTT.TabId );
-                    containerTypeNTP.ListOptions = "Aboveground Tank [A]," +
-                                                   "Bag [I]," +
-                                                   "Belowground Tank [B]," +
-                                                   "Box [J]," +
-                                                   "Can [F]," +
-                                                   "Carboy [G]," +
-                                                   "Cylinder [K]," +
-                                                   "Fiberdrum [H]," +
-                                                   "Glass Bottle or Jug [L]," +
-                                                   "Plastic [M]," +
-                                                   "Plastic or Non-Metal Drum [E]," +
-                                                   "Steel Drum [D]," +
-                                                   "Tank Inside Building [C]," +
-                                                   "Tank Wagon [O]," +
-                                                   "Tote Bin [N]";
+                CswNbtMetaDataNodeTypeTab sizeNTT = sizeNT.getFirstNodeTypeTab();
+                CswNbtMetaDataNodeTypeProp containerTypeNTP = _CswNbtSchemaModTrnsctn.MetaData.makeNewProp( sizeNT, CswNbtMetaDataFieldType.NbtFieldType.List, "Container Type", sizeNTT.TabId );
+                containerTypeNTP.ListOptions = "Aboveground Tank [A]," +
+                                               "Bag [I]," +
+                                               "Belowground Tank [B]," +
+                                               "Box [J]," +
+                                               "Can [F]," +
+                                               "Carboy [G]," +
+                                               "Cylinder [K]," +
+                                               "Fiberdrum [H]," +
+                                               "Glass Bottle or Jug [L]," +
+                                               "Plastic [M]," +
+                                               "Plastic or Non-Metal Drum [E]," +
+                                               "Steel Drum [D]," +
+                                               "Tank Inside Building [C]," +
+                                               "Tank Wagon [O]," +
+                                               "Tote Bin [N]";
 
-                    _CswNbtSchemaModTrnsctn.MetaData.NodeTypeLayout.updatePropLayout( CswNbtMetaDataNodeTypeLayoutMgr.LayoutType.Add, sizeNT.NodeTypeId, containerTypeNTP.PropId, true, sizeNTT.TabId );
-                    _CswNbtSchemaModTrnsctn.MetaData.NodeTypeLayout.updatePropLayout( CswNbtMetaDataNodeTypeLayoutMgr.LayoutType.Edit, sizeNT.NodeTypeId, containerTypeNTP.PropId, true, sizeNTT.TabId );
-                }
+                _CswNbtSchemaModTrnsctn.MetaData.NodeTypeLayout.updatePropLayout( CswNbtMetaDataNodeTypeLayoutMgr.LayoutType.Add, sizeNT.NodeTypeId, containerTypeNTP.PropId, true, sizeNTT.TabId );
+                _CswNbtSchemaModTrnsctn.MetaData.NodeTypeLayout.updatePropLayout( CswNbtMetaDataNodeTypeLayoutMgr.LayoutType.Edit, sizeNT.NodeTypeId, containerTypeNTP.PropId, true, sizeNTT.TabId );
             }
 
         }//Update()
