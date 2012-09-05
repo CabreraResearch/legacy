aad1c044c33da6020613c0bde461b98ff8d510d9
Christopher Froehlich <unknown@kiln.example.com>
2012-09-05 17:07:40 +0400

    Case 25352. Better align class names.

---
+++ NbtWebApp/app/proptypes/csw.propTypeLink.js
@@ -0,0 +1,110 @@
+/// <reference path="~/app/CswApp-vsdoc.js" />
+
+
+(function ($) {
+    "use strict";
+    var pluginName = 'CswFieldTypeLink';
+
+    var methods = {
+        init: function (o) {
+
+            var propDiv = o.propDiv;
+            propDiv.empty();
+
+            var propVals = o.propData.values;
+            var text = (false === o.Multi) ? Csw.string(propVals.text).trim() : Csw.enums.multiEditDefaultValue;
+            var href = (false === o.Multi) ? Csw.string(propVals.href).trim() : Csw.enums.multiEditDefaultValue;
+
+            if (o.ReadOnly) {
+                propDiv.a({
+                    href: href,
+                    text: text
+                });
+            } else {
+                var table = propDiv.table({
+                    ID: Csw.makeId(o.ID, 'tbl')
+                });
+
+                table.cell(1, 1).a({
+                    href: href,
+                    text: text
+                });
+                var cell12 = table.cell(1, 2).div();
+
+                cell12.icon({
+                        ID: o.ID + '_edit',
+                        iconType: Csw.enums.iconType.pencil,
+                        hovertext: 'Edit',
+                        size: 16,
+                        isButton: true,
+                        onClick: function () {
+                            editTable.show();
+                        }
+                    });
+
+                var editTable = propDiv.table({
+                    ID: Csw.makeId(o.ID, 'edittbl')
+                }).hide();
+
+                editTable.cell(1, 1).span({text: 'Text'});
+
+                var editTextCell = editTable.cell(1, 2);
+                var editText = editTextCell.input({ 
+                    ID: o.ID + '_text',
+                    type: Csw.enums.inputTypes.text,
+                    value: text,
+                    onChange: o.onChange
+                });
+
+                editTable.cell(2, 1).span({text: 'URL'});
+
+                var editHrefCell = editTable.cell(2, 2);
+                var editHref = editHrefCell.input({ 
+                    ID: o.ID + '_href',
+                    type: Csw.enums.inputTypes.text,
+                    value: href,
+                    onChange: o.onChange
+                });
+
+                if (o.Required && href === '') {
+                    editTable.show();
+                    editText.addClass("required");
+                    editHref.addClass("required");
+                }
+                editText.clickOnEnter(o.saveBtn);
+                editHref.clickOnEnter(o.saveBtn);
+            }
+        },
+        save: function (o) {
+            var attributes = {
+                text: null,
+                href: null
+            };
+            var compare = {};
+            var editText = o.propDiv.find('#' + o.ID + '_text');
+            if (false === Csw.isNullOrEmpty(editText)) {
+                attributes.text = editText.val();
+                compare = attributes;
+            }
+            var editHref = o.propDiv.find('#' + o.ID + '_href');
+            if (false === Csw.isNullOrEmpty(editHref)) {
+                attributes.href = editHref.val();
+                compare = attributes;
+            }
+            Csw.preparePropJsonForSave(o.Multi, o.propData, compare);
+        }
+    };
+
+    // Method calling logic
+    $.fn.CswFieldTypeLink = function (method) {
+
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist on ' + pluginName); return false;
+        }
+
+    };
+})(jQuery);
---
+++ NbtWebApp/app/proptypes/csw.propTypeMultiList.js
@@ -0,0 +1,72 @@
+/// <reference path="~/app/CswApp-vsdoc.js" />
+
+
+(function ($) {
+    "use strict";
+    var pluginName = 'CswFieldTypeMultiList';
+
+    var methods = {
+        init: function (o) {
+
+            var propDiv = o.propDiv;
+            propDiv.empty();
+            var propVals = o.propData.values;
+            var gestalt = Csw.string(o.propData.gestalt).trim();
+            var options = propVals.options;
+
+            if (o.ReadOnly) {
+                propDiv.append(gestalt);
+            } else {
+                /* Select Box */
+                propDiv.multiSelect({ //$.CswMultiSelect({
+                    ID: o.ID,
+                    cssclass: 'selectinput',
+                    values: options,
+                    readonlyless: propVals.readonlyless,
+                    readonlymore: propVals.readonlymore,
+                    isMultiEdit: o.Multi,
+                    EditMode: o.EditMode
+                });
+            }
+        },
+        save: function (o) { //$propdiv, $xml
+
+            var attributes = { value: null },
+                multi = o.propDiv.find('#' + o.ID),
+                cachedVals,
+                distinctVals = [];
+            var compare = {};
+            if (false === Csw.isNullOrEmpty(multi)) {
+                attributes.value = multi.val().split(',').sort().join(',');
+                compare = attributes;
+            }
+
+            //multiSelect sorts the val for us, sort o.propData.values.value to make comparision work
+            if (false === Csw.isNullOrEmpty(o.propData.values.value)) {
+                cachedVals = o.propData.values.value.split(',');
+                Csw.each(cachedVals, function (value) {
+                    //Guarantee the values are distinct locally
+                    if (false === Csw.contains(distinctVals, value)) {
+                        distinctVals.push(value);
+                    }
+                });
+            }
+            o.propData.values.value = distinctVals.sort().join(',');
+
+            Csw.preparePropJsonForSave(o.Multi, o.propData, compare);
+        }
+    };
+
+    // Method calling logic
+    $.fn.CswFieldTypeMultiList = function (method) {
+
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist on ' + pluginName); return false;
+        }
+
+    };
+})(jQuery);
