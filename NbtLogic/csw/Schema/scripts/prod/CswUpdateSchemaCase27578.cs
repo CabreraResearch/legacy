017ef68056b10331e974005aa71e72ae449d3e31
mbischoff@chemsw.com <unknown@kiln.example.com>
2012-09-06 17:44:22 +0400

    review K3705 - check for null tabs

--- NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase27578.cs
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase27578.cs
@@ -19,25 +19,28 @@ namespace ChemSW.Nbt.Schema
             if( null != sizeNT )
             {
                 CswNbtMetaDataNodeTypeTab sizeNTT = sizeNT.getFirstNodeTypeTab();
-                CswNbtMetaDataNodeTypeProp containerTypeNTP = _CswNbtSchemaModTrnsctn.MetaData.makeNewProp( sizeNT, CswNbtMetaDataFieldType.NbtFieldType.List, "Container Type", sizeNTT.TabId );
-                containerTypeNTP.ListOptions = "Aboveground Tank [A]," +
-                                               "Bag [I]," +
-                                               "Belowground Tank [B]," +
-                                               "Box [J]," +
-                                               "Can [F]," +
-                                               "Carboy [G]," +
-                                               "Cylinder [K]," +
-                                               "Fiberdrum [H]," +
-                                               "Glass Bottle or Jug [L]," +
-                                               "Plastic [M]," +
-                                               "Plastic or Non-Metal Drum [E]," +
-                                               "Steel Drum [D]," +
-                                               "Tank Inside Building [C]," +
-                                               "Tank Wagon [O]," +
-                                               "Tote Bin [N]";
+                if( null != sizeNTT )
+                {
+                    CswNbtMetaDataNodeTypeProp containerTypeNTP = _CswNbtSchemaModTrnsctn.MetaData.makeNewProp( sizeNT, CswNbtMetaDataFieldType.NbtFieldType.List, "Container Type", sizeNTT.TabId );
+                    containerTypeNTP.ListOptions = "Aboveground Tank [A]," +
+                                                   "Bag [I]," +
+                                                   "Belowground Tank [B]," +
+                                                   "Box [J]," +
+                                                   "Can [F]," +
+                                                   "Carboy [G]," +
+                                                   "Cylinder [K]," +
+                                                   "Fiberdrum [H]," +
+                                                   "Glass Bottle or Jug [L]," +
+                                                   "Plastic [M]," +
+                                                   "Plastic or Non-Metal Drum [E]," +
+                                                   "Steel Drum [D]," +
+                                                   "Tank Inside Building [C]," +
+                                                   "Tank Wagon [O]," +
+                                                   "Tote Bin [N]";
 
-                _CswNbtSchemaModTrnsctn.MetaData.NodeTypeLayout.updatePropLayout( CswNbtMetaDataNodeTypeLayoutMgr.LayoutType.Add, sizeNT.NodeTypeId, containerTypeNTP.PropId, true, sizeNTT.TabId );
-                _CswNbtSchemaModTrnsctn.MetaData.NodeTypeLayout.updatePropLayout( CswNbtMetaDataNodeTypeLayoutMgr.LayoutType.Edit, sizeNT.NodeTypeId, containerTypeNTP.PropId, true, sizeNTT.TabId );
+                    _CswNbtSchemaModTrnsctn.MetaData.NodeTypeLayout.updatePropLayout( CswNbtMetaDataNodeTypeLayoutMgr.LayoutType.Add, sizeNT.NodeTypeId, containerTypeNTP.PropId, true, sizeNTT.TabId );
+                    _CswNbtSchemaModTrnsctn.MetaData.NodeTypeLayout.updatePropLayout( CswNbtMetaDataNodeTypeLayoutMgr.LayoutType.Edit, sizeNT.NodeTypeId, containerTypeNTP.PropId, true, sizeNTT.TabId );
+                }
             }
 
         }//Update()
