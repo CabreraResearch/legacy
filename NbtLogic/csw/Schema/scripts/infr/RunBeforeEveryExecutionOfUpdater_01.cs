04e5aa597357d41b0ac96371062764d6e445b293
Steven Salter <ssalter@chemsw.com>
2012-09-07 19:38:33 +0400

    case 27706

--- NbtLogic/csw/Schema/scripts/infr/RunBeforeEveryExecutionOfUpdater_01.cs
+++ NbtLogic/csw/Schema/scripts/infr/RunBeforeEveryExecutionOfUpdater_01.cs
@@ -1,3 +1,7 @@
+using ChemSW.Core;
+using ChemSW.Nbt;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.ObjClasses;
 
 namespace ChemSW.Nbt.Schema
 {
@@ -24,6 +28,43 @@ namespace ChemSW.Nbt.Schema
                 _CswNbtSchemaModTrnsctn.addStringColumn( "nodes", "sessionid", "Session ID of temporary node", logicaldelete: false, required: false, datatypesize: 50 );
             }
             _CswNbtSchemaModTrnsctn.execArbitraryPlatformNeutralSql( "update nodes set istemp='0', sessionid=''" );
+
+
+
+
+
+
+            // moved from CswUpdateSchemaCase24525 for case 27706
+            #region ADD ARCHIVED PROP TO USER
+            CswNbtMetaDataFieldType logicalFT = _CswNbtSchemaModTrnsctn.MetaData.getFieldType( CswNbtMetaDataFieldType.NbtFieldType.Logical );
+            CswNbtMetaDataObjectClass userOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswNbtMetaDataObjectClass.NbtObjectClass.UserClass );
+            if( null == userOC.getObjectClassProp( CswNbtObjClassUser.PropertyName.Archived ) )
+            {
+                CswNbtMetaDataObjectClassProp archivedOCP = _CswNbtSchemaModTrnsctn.createObjectClassProp( new CswNbtWcfMetaDataModel.ObjectClassProp( userOC )
+                {
+                    PropName = CswNbtObjClassUser.PropertyName.Archived,
+                    FieldType = CswNbtMetaDataFieldType.NbtFieldType.Logical,
+                    IsFk = false,
+                    IsRequired = true,
+                    ValuePropType = logicalFT.FieldType,
+                    ValuePropId = logicalFT.FieldTypeId,
+                } );
+                //set the default val to false - we don't want new users to be archived
+                _CswNbtSchemaModTrnsctn.MetaData.SetObjectClassPropDefaultValue( archivedOCP, archivedOCP.getFieldTypeRule().SubFields.Default.Name, Tristate.False );
+
+                _CswNbtSchemaModTrnsctn.MetaData.makeMissingNodeTypeProps();
+
+                foreach( CswNbtNode userNode in userOC.getNodes( false, false ) )
+                {
+                    userNode.Properties[CswNbtObjClassUser.PropertyName.Archived].AsLogical.Checked = Tristate.False;
+                    userNode.postChanges( false );
+                }
+            }
+            #endregion
+
+        
+        
+        
         }//Update()
 
     }//class RunBeforeEveryExecutionOfUpdater_01
