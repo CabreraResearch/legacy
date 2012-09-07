11e292b48492a57e1b94024b072d8d91779b01fe
NBT Daily Build <b3851691-5f24-46c9-9188-5f0a2e70960a>
2012-09-07 16:33:20 +0400

    Automated commit for release: Romeo_2012.9.7.2

--- NbtSchedService/Properties/AssemblyInfo.cs
+++ NbtSchedService/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "2012.9.7.1" )]
-[assembly: AssemblyFileVersion( "2012.9.7.1" )]
+[assembly: AssemblyVersion( "2012.9.7.2" )]
+[assembly: AssemblyFileVersion( "2012.9.7.2" )]
