e08765cda02fa69efa45a54b37e54ac63fca32f1
mbischoff@chemsw.com <unknown@kiln.example.com>
2012-09-11 17:31:26 +0400

    review K3734 - comment 1

--- NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase26760.cs
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase26760.cs
@@ -16,19 +16,39 @@ namespace ChemSW.Nbt.Schema
         {
 
             CswNbtMetaDataNodeType vendorNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "Vendor" );
+            bool makeSigma = true;
+            bool makeAlfa = true;
+            bool makeVWR = true;
+
+            foreach( CswNbtObjClassVendor vendorNode in vendorNT.getNodes( false, false ) )
+            {
+                if( vendorNode.VendorName.Text.Equals( "Sigma-Aldrich" ) ) makeSigma = false;
+                if( vendorNode.VendorName.Text.Equals( "VWR Scientific" ) ) makeVWR = false;
+                if( vendorNode.VendorName.Text.Equals( "Alfa-Aesar" ) ) makeAlfa = false;
+            }
+
             if( null != vendorNT )
             {
-                CswNbtObjClassVendor sigmaNode = _CswNbtSchemaModTrnsctn.Nodes.makeNodeFromNodeTypeId( vendorNT.NodeTypeId, CswNbtNodeCollection.MakeNodeOperation.WriteNode );
-                sigmaNode.VendorName.Text = "Sigma-Aldrich";
-                sigmaNode.postChanges( false );
+                if( makeSigma )
+                {
+                    CswNbtObjClassVendor sigmaNode = _CswNbtSchemaModTrnsctn.Nodes.makeNodeFromNodeTypeId( vendorNT.NodeTypeId, CswNbtNodeCollection.MakeNodeOperation.WriteNode );
+                    sigmaNode.VendorName.Text = "Sigma-Aldrich";
+                    sigmaNode.postChanges( false );
+                }
 
-                CswNbtObjClassVendor vwrNode = _CswNbtSchemaModTrnsctn.Nodes.makeNodeFromNodeTypeId( vendorNT.NodeTypeId, CswNbtNodeCollection.MakeNodeOperation.WriteNode );
-                vwrNode.VendorName.Text = "VWR Scientific";
-                vwrNode.postChanges( false );
+                if( makeVWR )
+                {
+                    CswNbtObjClassVendor vwrNode = _CswNbtSchemaModTrnsctn.Nodes.makeNodeFromNodeTypeId( vendorNT.NodeTypeId, CswNbtNodeCollection.MakeNodeOperation.WriteNode );
+                    vwrNode.VendorName.Text = "VWR Scientific";
+                    vwrNode.postChanges( false );
+                }
 
-                CswNbtObjClassVendor alfaNode = _CswNbtSchemaModTrnsctn.Nodes.makeNodeFromNodeTypeId( vendorNT.NodeTypeId, CswNbtNodeCollection.MakeNodeOperation.WriteNode );
-                alfaNode.VendorName.Text = "Alfa-Aesar";
-                alfaNode.postChanges( false );
+                if( makeAlfa )
+                {
+                    CswNbtObjClassVendor alfaNode = _CswNbtSchemaModTrnsctn.Nodes.makeNodeFromNodeTypeId( vendorNT.NodeTypeId, CswNbtNodeCollection.MakeNodeOperation.WriteNode );
+                    alfaNode.VendorName.Text = "Alfa-Aesar";
+                    alfaNode.postChanges( false );
+                }
             }
 
         }//Update()
