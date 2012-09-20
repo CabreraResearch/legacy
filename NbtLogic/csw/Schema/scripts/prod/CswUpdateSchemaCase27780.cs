27c6dfe06529f43c5bf92cb037466508eae45380
mbischoff@chemsw.com <unknown@kiln.example.com>
2012-09-20 19:40:55 +0400

    case 27780 - part 1.1 - remove settings tab and hide props on equip schedule

--- NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase27780.cs
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase27780.cs
@@ -1,5 +1,7 @@
 ﻿using ChemSW.Core;
 using ChemSW.DB;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.ObjClasses;
 using System.Data;
 using System.Collections.Generic;
 
@@ -16,7 +18,33 @@ namespace ChemSW.Nbt.Schema
         public override void update()
         {
 
-            //Delete the SI Configuration category and all the views in it
+            #region PART 1 - Hide the settings tab on the Equipment Schedule NT
+
+            CswNbtMetaDataNodeType equipmentScheduleNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "Equipment Schedule" );
+            List<int> ntpIdsToHide = new List<int>();
+            if( null != equipmentScheduleNT )
+            {
+                foreach( CswNbtMetaDataNodeTypeTab tab in equipmentScheduleNT.getNodeTypeTabs() )
+                {
+                    if( tab.TabName.Equals( "Settings" ) )
+                    {
+                        foreach( CswNbtMetaDataNodeTypeProp ntp in tab.getNodeTypeProps() )
+                        {
+                            ntpIdsToHide.Add( ntp.PropId );
+                        }
+                        _CswNbtSchemaModTrnsctn.MetaData.DeleteNodeTypeTab( tab );
+                    }
+                }
+                foreach( int ntpId in ntpIdsToHide )
+                {
+                    CswNbtMetaDataNodeTypeProp ntp = _CswNbtSchemaModTrnsctn.MetaData.getNodeTypeProp( ntpId );
+                    ntp.removeFromAllLayouts();
+                }
+            }
+
+            #endregion
+
+            #region PART 2 - Delete the SI Configuration category and all the views in it
 
             //get the doomed view ids
             CswTableSelect node_viewsTS = _CswNbtSchemaModTrnsctn.makeCswTableSelect( "deleteSIViews_27780", "node_views" );
@@ -24,7 +52,6 @@ namespace ChemSW.Nbt.Schema
             selectCols.FromString( "nodeviewid" );
             DataTable node_views = node_viewsTS.getTable( selectCols, "where category = 'SI Configuration'" );
 
-            //
             foreach( DataRow row in node_views.Rows )
             {
                 int viewidInt = CswConvert.ToInt32( row["nodeviewid"] );
@@ -36,6 +63,8 @@ namespace ChemSW.Nbt.Schema
                     doomedView.Delete();
                 }
             }
+
+            #endregion
 
         }//Update()
 
