4bad8fcd89a94aa9f66c9291a7f0d71550d4ade9
Christopher Froehlich <unknown@kiln.example.com>
2012-09-26 01:06:40 +0400

    Merge fix.

--- NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
+++ NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
@@ -24,6 +24,8 @@ namespace ChemSW.Nbt.Schema
             // This is where you add new versions.
             // e.g. _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchemaCaseXXXXX() ) );
 
+            #region Romeo
+
             // formerly quince
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_MaterialComponents_Case27462() ) );                //01R-001
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_Requesting_Case27470() ) );                        //01R-002
