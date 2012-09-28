b05a975c2e8a8e5ff8f1d71e43c074b0d8751881
bvavra@chemsw.com <unknown@kiln.example.com>
2012-09-28 19:01:57 +0400

    Case 27751

---
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase27751.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data;
+using ChemSW.Core;
+using ChemSW.DB;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.ObjClasses;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update for case 27751
+    /// </summary>
+    public class CswUpdateSchemaCase27751 : CswUpdateSchemaTo
+    {
+        public override void update()
+        {
+            _CswNbtSchemaModTrnsctn.execArbitraryPlatformNeutralSql( "update configuration_variables set issystem = '1' where variablename = 'loc_use_images'" );
+        }//Update()
+
+    }//class CswUpdateSchemaCase27751 
+
+}//namespace ChemSW.Nbt.Schema
