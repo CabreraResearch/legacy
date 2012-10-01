a03c69f919037726a26e911badb2164a05b5810e
Steven Salter <ssalter@chemsw.com>
2012-10-01 21:30:49 +0400

    added script pullDefault.bat

---
+++ Scripts/pullDefault.bat
@@ -0,0 +1,15 @@
+hg pull -R ../../../nbt/nbt -u
+
+hg pull -R ../../../Common/CswCommon -u
+hg pull -R ../../../Common/CswConfigUI -u
+hg pull -R ../../../Common/CswWebControls -u
+hg pull -R ../../../Common/CswLogService -u
+
+hg pull -R ../../../nbt/NbtImport -u
+hg pull -R ../../../nbt/nbt/NbtWebApp/help -u
+
+hg pull -R ../../../ThirdParty/ClosureCompiler -u
+hg pull -R ../../../ThirdParty/OracleDataAccess -u
+hg pull -R ../../../ThirdParty/YUICompressor -u
+
+hg pull -R ../../../Common/StructureSearch -u
