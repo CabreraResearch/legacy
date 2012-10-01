d93521f9af99d66375704dd82a2c9fb59e6bb1e5
Christopher Froehlich <unknown@kiln.example.com>
2012-10-01 20:00:58 +0400

    First working draft of API.Test project.

---
+++ API.Test/Global.asax.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.ServiceModel.Activation;
+using System.Web;
+using System.Web.Routing;
+
+namespace API.Test
+{
+    /// <summary>
+    /// Global API.TEST
+    /// </summary>
+    public class Global : System.Web.HttpApplication
+    {
+        /// <summary>
+        /// Application Start
+        /// </summary>
+        protected void Application_Start( object sender, EventArgs e )
+        {
+            WebServiceHostFactory Factory = new WebServiceHostFactory();
+            RouteTable.Routes.Add( new ServiceRoute( "Properties", Factory, typeof( Properties ) ) );
+        }
+
+        /// <summary>
+        /// Session Start
+        /// </summary>
+        protected void Session_Start( object sender, EventArgs e )
+        {
+
+        }
+
+        /// <summary>
+        /// Application Begin Request
+        /// </summary>
+        protected void Application_BeginRequest( object sender, EventArgs e )
+        {
+            HttpContext.Current.Response.Cache.SetCacheability( HttpCacheability.NoCache );
+            HttpContext.Current.Response.Cache.SetNoStore();
+
+            EnableCrossDmainAjaxCall();
+        }
+
+        /// <summary>
+        /// Enable CDA
+        /// </summary>
+        private void EnableCrossDmainAjaxCall()
+        {
+            HttpContext.Current.Response.AddHeader( "Access-Control-Allow-Origin", "*" );
+            if( HttpContext.Current.Request.HttpMethod == "OPTIONS" )
+            {
+                HttpContext.Current.Response.AddHeader( "Access-Control-Allow-Methods", "GET, POST" );
+                HttpContext.Current.Response.AddHeader( "Access-Control-Allow-Headers", "Content-Type, Accept" );
+                HttpContext.Current.Response.AddHeader( "Access-Control-Max-Age", "1728000" );
+                HttpContext.Current.Response.End();
+            }
+        }
+
+        /// <summary>
+        /// Application Authenticate Request
+        /// </summary>
+        protected void Application_AuthenticateRequest( object sender, EventArgs e )
+        {
+
+        }
+
+        /// <summary>
+        /// Application Error
+        /// </summary>
+        protected void Application_Error( object sender, EventArgs e )
+        {
+
+        }
+
+        /// <summary>
+        /// Session End
+        /// </summary>
+        protected void Session_End( object sender, EventArgs e )
+        {
+
+        }
+
+        /// <summary>
+        /// Application End
+        /// </summary>
+        protected void Application_End( object sender, EventArgs e )
+        {
+
+        }
+    }
+}
---
+++ API.Test/Services/Properties.svc.cs
@@ -0,0 +1,40 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+
+namespace API.Test
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [ServiceContract( Namespace = "API.Test.Services" )]
+    [AspNetCompatibilityRequirements( RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed )]
+    public class Properties
+    {
+        // To use HTTP GET, add [WebGet] attribute. (Default ResponseFormat is WebMessageFormat.Json)
+        // To create an operation that returns XML,
+        //     add [WebGet(ResponseFormat=WebMessageFormat.Xml)],
+        //     and include the following line in the operation body:
+        //         WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
+        [OperationContract]
+        [WebGet]
+        public void DoWork()
+        {
+            // Add your operation implementation here
+            return;
+        }
+
+        /// <summary>
+        /// Get Properties
+        /// </summary>
+        [OperationContract]
+        [WebInvoke( Method = "POST", UriTemplate = "/getProps?EditMode={EditMode}&NodeId={NodeId}&SafeNodeKey={SafeNodeKey}&TabId={TabId}&NodeTypeId={NodeTypeId}&Date={Date}&filterToPropId={filterToPropId}&Multi={Multi}&ConfigMode={ConfigMode}&RelatedNodeId={RelatedNodeId}&RelatedNodeTypeId={RelatedNodeTypeId}&RelatedObjectClassId={RelatedObjectClassId}" )]
+        public string getProps( string EditMode, string NodeId, string SafeNodeKey, string TabId, string NodeTypeId, string Date, string filterToPropId, string Multi, string ConfigMode, string RelatedNodeId, string RelatedNodeTypeId, string RelatedObjectClassId )
+        {
+            return "";
+        }
+
+
+        // Add more operations here and mark them with [OperationContract]
+    }
+}
