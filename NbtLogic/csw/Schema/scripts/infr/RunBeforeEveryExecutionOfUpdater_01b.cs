8f0ccb778309ccc509e6a389304e28d759c8197c
Steven Salter <ssalter@chemsw.com>
2012-10-04 00:01:50 +0400

    case 27720

--- NbtLogic/csw/Schema/scripts/infr/RunBeforeEveryExecutionOfUpdater_01b.cs
+++ NbtLogic/csw/Schema/scripts/infr/RunBeforeEveryExecutionOfUpdater_01b.cs
@@ -116,7 +116,10 @@ namespace ChemSW.Nbt.Schema
                 CswCommaDelimitedString Options = new CswCommaDelimitedString();
                 foreach( CswNbtObjClassMailReport.EventOption EventOpt in CswNbtObjClassMailReport.EventOption._All )
                 {
-                    Options.Add( EventOpt.ToString() );
+                    if( EventOpt != CswNbtObjClassMailReport.EventOption.Unknown )
+                    {
+                        Options.Add( EventOpt.ToString() );
+                    }
                 }
                 _CswNbtSchemaModTrnsctn.createObjectClassProp( new CswNbtWcfMetaDataModel.ObjectClassProp( MailReportOC )
                 {
@@ -137,25 +140,25 @@ namespace ChemSW.Nbt.Schema
             }
 
             // Change "Report View" from ViewPickList to ViewReference
-            
-            // map jct_nodes_props records
-            //   ViewReference: Name = field1, ViewId = field1_fk
-            //   ViewPickList: Name = gestalt, ViewId = field1
             CswNbtMetaDataObjectClassProp ReportViewOCP = MailReportOC.getObjectClassProp( CswNbtObjClassMailReport.PropertyName.ReportView);
-            
-            CswTableUpdate JctUpdate = _CswNbtSchemaModTrnsctn.makeCswTableUpdate( "27720_update_jnp", "jct_nodes_props" );
-            DataTable JctTable = JctUpdate.getTable( "where nodetypepropid in (select nodetypepropid from nodetype_props where objectclasspropid = " + ReportViewOCP.ObjectClassPropId + ")" );
-            foreach( DataRow JctRow in JctTable.Rows )
+            if( ReportViewOCP.getFieldType().FieldType == CswNbtMetaDataFieldType.NbtFieldType.ViewPickList )
             {
-                JctRow["field1_fk"] = JctRow["field1"];
-                JctRow["field1"] = JctRow["gestalt"];
-            }
-            JctUpdate.update( JctTable );
-            
-            // update the field types
-            CswNbtMetaDataFieldType ViewReferenceFT = _CswNbtSchemaModTrnsctn.MetaData.getFieldType( CswNbtMetaDataFieldType.NbtFieldType.ViewReference );
-            _CswNbtSchemaModTrnsctn.MetaData.UpdateObjectClassProp( ReportViewOCP, CswNbtMetaDataObjectClassProp.ObjectClassPropAttributes.fieldtypeid, ViewReferenceFT.FieldTypeId );
+                // map jct_nodes_props records
+                //   ViewReference: Name = field1, ViewId = field1_fk
+                //   ViewPickList: Name = gestalt, ViewId = field1
+                CswTableUpdate JctUpdate = _CswNbtSchemaModTrnsctn.makeCswTableUpdate( "27720_update_jnp", "jct_nodes_props" );
+                DataTable JctTable = JctUpdate.getTable( "where nodetypepropid in (select nodetypepropid from nodetype_props where objectclasspropid = " + ReportViewOCP.ObjectClassPropId + ")" );
+                foreach( DataRow JctRow in JctTable.Rows )
+                {
+                    JctRow["field1_fk"] = JctRow["field1"];
+                    JctRow["field1"] = JctRow["gestalt"];
+                }
+                JctUpdate.update( JctTable );
 
+                // update the field types
+                CswNbtMetaDataFieldType ViewReferenceFT = _CswNbtSchemaModTrnsctn.MetaData.getFieldType( CswNbtMetaDataFieldType.NbtFieldType.ViewReference );
+                _CswNbtSchemaModTrnsctn.MetaData.UpdateObjectClassProp( ReportViewOCP, CswNbtMetaDataObjectClassProp.ObjectClassPropAttributes.fieldtypeid, ViewReferenceFT.FieldTypeId );
+            }
             #endregion case 27720
 
             #endregion SEBASTIAN
--- NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase27720.cs
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase27720.cs
@@ -14,7 +14,7 @@ namespace ChemSW.Nbt.Schema
     {
         public override void update()
         {
-            
+
             CswNbtMetaDataObjectClass MailReportOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswNbtMetaDataObjectClass.NbtObjectClass.MailReportClass );
             foreach( CswNbtMetaDataNodeType MailReportNT in MailReportOC.getNodeTypes() )
             {
@@ -25,7 +25,51 @@ namespace ChemSW.Nbt.Schema
                 // remove Run Status from Add layout
                 CswNbtMetaDataNodeTypeProp RunStatusNTP = MailReportNT.getNodeTypePropByObjectClassProp( CswNbtObjClassMailReport.PropertyName.RunStatus );
                 RunStatusNTP.removeFromLayout( CswNbtMetaDataNodeTypeLayoutMgr.LayoutType.Add );
-            }
+
+                // set Target Type and Event to be conditional on Type = View
+                CswNbtMetaDataNodeTypeProp TypeNTP = MailReportNT.getNodeTypePropByObjectClassProp( CswNbtObjClassMailReport.PropertyName.Type );
+                CswNbtMetaDataNodeTypeProp TargetTypeNTP = MailReportNT.getNodeTypePropByObjectClassProp( CswNbtObjClassMailReport.PropertyName.TargetType );
+                CswNbtMetaDataNodeTypeProp EventNTP = MailReportNT.getNodeTypePropByObjectClassProp( CswNbtObjClassMailReport.PropertyName.Event );
+                TargetTypeNTP.setFilter( TypeNTP, TypeNTP.getFieldTypeRule().SubFields.Default, CswNbtPropFilterSql.PropertyFilterMode.Equals, CswNbtObjClassMailReport.TypeOptionView );
+                EventNTP.setFilter( TypeNTP, TypeNTP.getFieldTypeRule().SubFields.Default, CswNbtPropFilterSql.PropertyFilterMode.Equals, CswNbtObjClassMailReport.TypeOptionView );
+
+                // add help text to Report View
+                CswNbtMetaDataNodeTypeProp ReportViewNTP = MailReportNT.getNodeTypePropByObjectClassProp( CswNbtObjClassMailReport.PropertyName.ReportView );
+                ReportViewNTP.HelpText = "For 'Exists' events, a report is generated if the view returns any results that match the Target Type.  For 'Edit' events, a report is only generated if one of the properties in the view was modified.";
+
+                // set target type and event for existing mail reports
+                foreach( CswNbtObjClassMailReport MailReportNode in MailReportNT.getNodes( false, true ) )
+                {
+                    if( CswNbtObjClassMailReport.TypeOptionView == MailReportNode.Type.Value &&
+                        0 == MailReportNode.TargetType.SelectedNodeTypeIds.Count )
+                    {
+                        CswNbtView View = _CswNbtSchemaModTrnsctn.restoreView( MailReportNode.ReportView.ViewId );
+                        if( View.Root.ChildRelationships.Count > 0 )
+                        {
+                            CswNbtViewRelationship RootRel = View.Root.ChildRelationships[0];
+                            if( RootRel.SecondType == NbtViewRelatedIdType.NodeTypeId )
+                            {
+                                MailReportNode.TargetType.SelectedNodeTypeIds.Add( RootRel.SecondId.ToString() );
+                            }
+                            else
+                            {
+                                CswNbtMetaDataObjectClass RootOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( RootRel.SecondId.ToString() );
+                                if( null != RootOC.FirstNodeType )
+                                {
+                                    MailReportNode.TargetType.SelectedNodeTypeIds.Add( RootOC.FirstNodeType.NodeTypeId.ToString() );
+                                }
+                            }
+                        }
+                    }
+
+                    if( MailReportNode.Event.Empty )
+                    {
+                        MailReportNode.Event.Value = CswNbtObjClassMailReport.EventOption.Exists.ToString();
+                    }
+                    MailReportNode.postChanges( false );
+
+                } // foreach( CswNbtObjClassMailReport MailReportNode in MailReportNT.getNodes( false, true ) )
+            } // foreach( CswNbtMetaDataNodeType MailReportNT in MailReportOC.getNodeTypes() )
 
         }//Update()
 
