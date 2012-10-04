35d9fe1ed03536e32b4baa8563321d9f23c67648
bvavra@chemsw.com <unknown@kiln.example.com>
2012-10-04 23:26:44 +0400

    Case 27877, part 1 - added Sequence sub fields to Property Reference

--- NbtLogic/csw/MetaData/FieldTypeRules/CswNbtFieldTypeRulePropertyReference.cs
+++ NbtLogic/csw/MetaData/FieldTypeRules/CswNbtFieldTypeRulePropertyReference.cs
@@ -11,6 +11,12 @@ namespace ChemSW.Nbt.MetaData.FieldTypeRules
         private CswNbtFieldTypeRuleDefaultImpl _CswNbtFieldTypeRuleDefault = null;
         private CswNbtFieldResources _CswNbtFieldResources = null;
 
+        public static CswNbtSubField.PropColumn SequenceNumberColumn = CswNbtSubField.PropColumn.Field1_Numeric;
+
+        public CswNbtSubField CachedValueSubField;
+        public CswNbtSubField SequenceSubField;
+        public CswNbtSubField SequenceNumberSubField;
+
         public CswNbtFieldTypeRulePropertyReference( CswNbtFieldResources CswNbtFieldResources )
         {
             _CswNbtFieldResources = CswNbtFieldResources;
@@ -28,9 +34,31 @@ namespace ChemSW.Nbt.MetaData.FieldTypeRules
             CachedValueSubField.SupportedFilterModes.Add( CswNbtPropFilterSql.PropertyFilterMode.Null );
             SubFields.add( CachedValueSubField );
 
-        }//ctor
+            SequenceSubField = new CswNbtSubField( _CswNbtFieldResources, CswNbtSubField.PropColumn.Field2, CswNbtSubField.SubFieldName.Sequence );
+            SequenceSubField.SupportedFilterModes.Add( CswNbtPropFilterSql.PropertyFilterMode.Begins );
+            SequenceSubField.SupportedFilterModes.Add( CswNbtPropFilterSql.PropertyFilterMode.Contains );
+            SequenceSubField.SupportedFilterModes.Add( CswNbtPropFilterSql.PropertyFilterMode.Ends );
+            SequenceSubField.SupportedFilterModes.Add( CswNbtPropFilterSql.PropertyFilterMode.Equals );
+            SequenceSubField.SupportedFilterModes.Add( CswNbtPropFilterSql.PropertyFilterMode.GreaterThan );
+            SequenceSubField.SupportedFilterModes.Add( CswNbtPropFilterSql.PropertyFilterMode.LessThan );
+            SequenceSubField.SupportedFilterModes.Add( CswNbtPropFilterSql.PropertyFilterMode.NotEquals );
+            SequenceSubField.SupportedFilterModes.Add( CswNbtPropFilterSql.PropertyFilterMode.NotNull );
+            SequenceSubField.SupportedFilterModes.Add( CswNbtPropFilterSql.PropertyFilterMode.Null );
+            SubFields.add( SequenceSubField );
 
-        public CswNbtSubField CachedValueSubField;
+            SequenceNumberSubField = new CswNbtSubField( _CswNbtFieldResources, SequenceNumberColumn, CswNbtSubField.SubFieldName.Number );
+            SequenceNumberSubField.SupportedFilterModes.Add( CswNbtPropFilterSql.PropertyFilterMode.Begins );
+            SequenceNumberSubField.SupportedFilterModes.Add( CswNbtPropFilterSql.PropertyFilterMode.Contains );
+            SequenceNumberSubField.SupportedFilterModes.Add( CswNbtPropFilterSql.PropertyFilterMode.Ends );
+            SequenceNumberSubField.SupportedFilterModes.Add( CswNbtPropFilterSql.PropertyFilterMode.Equals );
+            SequenceNumberSubField.SupportedFilterModes.Add( CswNbtPropFilterSql.PropertyFilterMode.GreaterThan );
+            SequenceNumberSubField.SupportedFilterModes.Add( CswNbtPropFilterSql.PropertyFilterMode.LessThan );
+            SequenceNumberSubField.SupportedFilterModes.Add( CswNbtPropFilterSql.PropertyFilterMode.NotEquals );
+            SequenceNumberSubField.SupportedFilterModes.Add( CswNbtPropFilterSql.PropertyFilterMode.NotNull );
+            SequenceNumberSubField.SupportedFilterModes.Add( CswNbtPropFilterSql.PropertyFilterMode.Null );
+            SubFields.add( SequenceNumberSubField );
+
+        }//ctor        
 
         public CswNbtSubFieldColl SubFields
         {
@@ -71,12 +99,12 @@ namespace ChemSW.Nbt.MetaData.FieldTypeRules
 
                 //if( RelationshipProp != null && RelationshipProp.IsUserRelationship() )
                 //{
-                    if( CswNbtViewPropertyFilterIn.Value.ToLower() == "me" )
-                    {
-                        CswNbtViewPropertyFilterIn.SubfieldName = CachedValueSubField.Name;
-                        CswNbtViewPropertyFilterIn.FilterMode = CswNbtPropFilterSql.PropertyFilterMode.Equals;
-                        CswNbtViewPropertyFilterIn.Value = _CswNbtFieldResources.CswNbtResources.Nodes[RunAsUser.UserId].NodeName;
-                    }
+                if( CswNbtViewPropertyFilterIn.Value.ToLower() == "me" )
+                {
+                    CswNbtViewPropertyFilterIn.SubfieldName = CachedValueSubField.Name;
+                    CswNbtViewPropertyFilterIn.FilterMode = CswNbtPropFilterSql.PropertyFilterMode.Equals;
+                    CswNbtViewPropertyFilterIn.Value = _CswNbtFieldResources.CswNbtResources.Nodes[RunAsUser.UserId].NodeName;
+                }
                 //}
             }
             string ret = _CswNbtFieldTypeRuleDefault.renderViewPropFilter( RunAsUser, SubFields, CswNbtViewPropertyFilterIn, false );
