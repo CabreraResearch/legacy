23868fcb04751116b680e4a34eb0ac2a0a9917ca
bvavra@chemsw.com <unknown@kiln.example.com>
2012-10-10 18:01:45 +0400

    Case 27939 - Out-of-the-box Users have their Archived property set to false by default.

---
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchemaCase27939.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+using ChemSW.Core;
+using ChemSW.DB;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.ObjClasses;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update for case 27939
+    /// </summary>
+    public class CswUpdateSchemaCase27939 : CswUpdateSchemaTo
+    {
+        public override void update()
+        {
+            CswNbtMetaDataObjectClass UserOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswNbtMetaDataObjectClass.NbtObjectClass.UserClass );
+            foreach( CswNbtObjClassUser UserNode in UserOC.getNodes( false, true ) )
+            {
+                if( UserNode.Archived.Checked == Tristate.Null )
+                {
+                    UserNode.Archived.Checked = Tristate.False;
+                    UserNode.postChanges( false );
+                }
+            }
+        }//Update()
+
+    }//class CswUpdateSchemaCase27939
+
+}//namespace ChemSW.Nbt.Schema
