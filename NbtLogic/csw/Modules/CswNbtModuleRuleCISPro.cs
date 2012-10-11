84da1007f19f81068ccb87ffc746430cd985949e
mbischoff@chemsw.com <unknown@kiln.example.com>
2012-10-11 21:25:40 +0400

    Titania. Case 27767 - CISPro events

--- NbtLogic/csw/Modules/CswNbtModuleRuleCISPro.cs
+++ NbtLogic/csw/Modules/CswNbtModuleRuleCISPro.cs
@@ -2,6 +2,7 @@ using System;
 using ChemSW.Core;
 using ChemSW.Nbt.MetaData;
 using ChemSW.Nbt.ObjClasses;
+using System.Data;
 
 namespace ChemSW.Nbt
 {
@@ -55,6 +56,15 @@ namespace ChemSW.Nbt
                 UserJurisdictionNTP.updateLayout( CswNbtMetaDataNodeTypeLayoutMgr.LayoutType.Add, false );
                 UserJurisdictionNTP.updateLayout( CswNbtMetaDataNodeTypeLayoutMgr.LayoutType.Edit, true, UserNT.getFirstNodeTypeTab().TabId );
             }
+
+            //Case 27862 - show...
+            //   All CISPro roles and users
+            //   Unit of measure and work units views
+            _toggleCISPRoUsers( false );
+            _toggleCISPRoRoles( false );
+            _toggleView( false, "Work Units" );
+            _toggleView( false, "Units of Measurement" );
+
         }
 
         public override void OnDisable()
@@ -93,7 +103,79 @@ namespace ChemSW.Nbt
                 CswNbtMetaDataNodeTypeProp UserJurisdictionNTP = UserNT.getNodeTypePropByObjectClassProp( CswNbtObjClassUser.PropertyName.Jurisdiction );
                 UserJurisdictionNTP.removeFromAllLayouts();
             }
+
+            //Case 27862 - hide...
+            //   All CISPro roles and users
+            //   Unit of measure and work units views
+            _toggleCISPRoUsers( true );
+            _toggleCISPRoRoles( true );
+            _toggleView( true, "Work Units" );
+            _toggleView( true, "Units of Measurement" );
+
+
         } // OnDisable()
+
+        private void _toggleCISPRoUsers( bool hidden )
+        {
+            CswNbtMetaDataObjectClass userOC = _CswNbtResources.MetaData.getObjectClass( CswNbtMetaDataObjectClass.NbtObjectClass.UserClass );
+            CswNbtMetaDataObjectClassProp usernameOCP = userOC.getObjectClassProp( CswNbtObjClassUser.PropertyName.Username );
+            CswNbtView cispro_usersView = new CswNbtView( _CswNbtResources );
+            CswNbtViewRelationship parent = cispro_usersView.AddViewRelationship( userOC, false );
+            cispro_usersView.AddViewPropertyAndFilter( parent,
+                MetaDataProp: usernameOCP,
+                Value: "cispro",
+                SubFieldName: CswNbtSubField.SubFieldName.Text,
+                FilterMode: CswNbtPropFilterSql.PropertyFilterMode.Contains );
+
+            ICswNbtTree cisproUsersTree = _CswNbtResources.Trees.getTreeFromView( cispro_usersView, false, true, true );
+            int count = cisproUsersTree.getChildNodeCount();
+            for( int i = 0; i < count; i++ )
+            {
+                cisproUsersTree.goToNthChild( i );
+                CswNbtNode userNode = cisproUsersTree.getNodeForCurrentPosition();
+                userNode.Hidden = hidden;
+                userNode.postChanges( false );
+                cisproUsersTree.goToParentNode();
+            }
+        }
+
+        private void _toggleCISPRoRoles( bool hidden )
+        {
+            CswNbtMetaDataObjectClass roleOC = _CswNbtResources.MetaData.getObjectClass( CswNbtMetaDataObjectClass.NbtObjectClass.RoleClass );
+            CswNbtMetaDataObjectClassProp nameOCP = roleOC.getObjectClassProp( CswNbtObjClassRole.PropertyName.Name );
+            CswNbtView cispro_rolesView = new CswNbtView( _CswNbtResources );
+            CswNbtViewRelationship parent = cispro_rolesView.AddViewRelationship( roleOC, false );
+            cispro_rolesView.AddViewPropertyAndFilter( parent,
+                MetaDataProp: nameOCP,
+                Value: "cispro",
+                SubFieldName: CswNbtSubField.SubFieldName.Text,
+                FilterMode: CswNbtPropFilterSql.PropertyFilterMode.Contains );
+
+            ICswNbtTree cisproUsersTree = _CswNbtResources.Trees.getTreeFromView( cispro_rolesView, false, true, true );
+            int count = cisproUsersTree.getChildNodeCount();
+            for( int i = 0; i < count; i++ )
+            {
+                cisproUsersTree.goToNthChild( i );
+                CswNbtNode userNode = cisproUsersTree.getNodeForCurrentPosition();
+                userNode.Hidden = hidden;
+                userNode.postChanges( false );
+                cisproUsersTree.goToParentNode();
+            }
+        }
+
+        private void _toggleView( bool hidden, string viewName )
+        {
+            DataTable viewDT = _CswNbtResources.ViewSelect.getView( viewName, NbtViewVisibility.Global, null, null );
+            if( viewDT.Rows.Count == 1 )
+            {
+                CswNbtView view = _CswNbtResources.ViewSelect.restoreView( viewDT.Rows[0]["viewxml"].ToString() );
+                if( null != view )
+                {
+                    view.SetVisibility( NbtViewVisibility.Hidden, null, null );
+                    view.save();
+                }
+            }
+        }
 
     } // class CswNbtModuleCISPro
 }// namespace ChemSW.Nbt
