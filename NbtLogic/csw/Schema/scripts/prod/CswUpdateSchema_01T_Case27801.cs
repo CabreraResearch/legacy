de8b84a29b2091be835d95a35a83c4e6420e79ca
mbischoff@chemsw.com <unknown@kiln.example.com>
2012-10-15 20:48:09 +0400

    Titania. Case 27801 - specific gravity is not scientific

---
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchema_01T_Case27801.cs
@@ -0,0 +1,37 @@
+﻿using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.ObjClasses;
+using ChemSW.Nbt.PropTypes;
+using ChemSW.Nbt.csw.Dev;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update for case 27801
+    /// </summary>
+    public class CswUpdateSchema_01T_Case27801 : CswUpdateSchemaTo
+    {
+        public override void update()
+        {
+            CswNbtMetaDataObjectClass materialOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( NbtObjectClass.MaterialClass );
+            CswNbtMetaDataObjectClassProp specificGravityOCP = materialOC.getObjectClassProp( CswNbtObjClassMaterial.PropertyName.SpecificGravity );
+            CswNbtMetaDataFieldType numberFT = _CswNbtSchemaModTrnsctn.MetaData.getFieldType( CswNbtMetaDataFieldType.NbtFieldType.Number );
+            _CswNbtSchemaModTrnsctn.MetaData.UpdateObjectClassProp( specificGravityOCP, CswNbtMetaDataObjectClassProp.ObjectClassPropAttributes.fieldtypeid, numberFT.FieldTypeId );
+            _CswNbtSchemaModTrnsctn.MetaData.UpdateObjectClassProp( specificGravityOCP, CswNbtMetaDataObjectClassProp.ObjectClassPropAttributes.numberminvalue, 0 );
+            _CswNbtSchemaModTrnsctn.MetaData.UpdateObjectClassProp( specificGravityOCP, CswNbtMetaDataObjectClassProp.ObjectClassPropAttributes.numberprecision, 3 );
+        }
+
+        public override CswDeveloper Author
+        {
+            get { return CswDeveloper.MB; }
+        }
+
+        public override int CaseNo
+        {
+            get { return 27801; }
+        }
+
+        //Update()
+
+    }//class CswUpdateSchemaCase27801
+
+}//namespace ChemSW.Nbt.Schema
