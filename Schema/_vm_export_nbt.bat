0d7220b7fb0430a3d8c437603639fe72c64f3552
Unknown <unknown@kiln.example.com>
2012-10-20 00:51:28 +0400

    Sebastian. Schema is no more.

--- Schema/_vm_export_nbt.bat
+++
@@ -1,4 +0,0 @@
-del \\w2008x64db\kiln\Nbt\Nbt\Schema\Dumps\Nbt_11g.dmp
-expdp.exe nbt/nbt@w2008x64db DUMPFILE=NBT_11g.dmp DIRECTORY=NBTDUMPS
-del C:\Nbt_11g.7z
-"C:\Program Files\7-zip\7z" a -t7z C:\Nbt.7z \\w2008x64db\kiln\Nbt\Nbt\Schema\Dumps\Nbt_11g.dmp
