149f69e8abc9eea42889aff0139b2123a113238f
Christopher Froehlich <unknown@kiln.example.com>
2012-10-20 00:54:15 +0400

    Titania. Schema is no more.

--- Schema/_export_nbt_11g.bat
+++
@@ -1,5 +0,0 @@
-set /p username=
-set /p database=
-
-del Dumps\Nbt_Master_11g.dmp
-expdp.exe %username%/nbt@%database% DUMPFILE=%username%.dmp DIRECTORY=NBTDUMPS
--- Schema/_steve_reset_master.bat
+++
@@ -1,7 +0,0 @@
-echo exit | sqlplus nbt_master/nbt_master@orcl @nbt_nuke.sql
-
-impdp.exe nbt_master/nbt_master@orcl DUMPFILE=NBT_MASTER_11G.dmp DIRECTORY=NBTDUMPS
-
-C:\kiln\Nbt\Nbt\NbtSchemaUpdaterCmdLn\bin\Debug\NbtUpdt.exe -accessid master -mode prod
-
-pause
