9a953729b7475902852cafb6bdc72c9ce6b524ef
Christopher Froehlich <unknown@kiln.example.com>
2012-10-23 20:02:50 +0400

    Titania. Case 27497. Oracle projects.

---
+++ NbtOracleClr/Class1.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+
+namespace NbtOracleClr
+{
+    public class Class1
+    {
+        public static void StoredProcedure1()
+        {
+        }
+    }
+}
