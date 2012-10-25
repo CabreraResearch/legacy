03ab505df3b6fff22005c446a0308b2692ff5850
mbischoff@chemsw.com <unknown@kiln.example.com>
2012-10-25 18:33:00 +0400

    Titania. Case 27551 - part 1 - Mixture server managed

---
+++ NbtLogic/csw/Schema/scripts/prod/CswUpdateSchema_01T_Case27551.cs
@@ -0,0 +1,34 @@
+﻿using ChemSW.Nbt.csw.Dev;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.ObjClasses;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update for case 27551
+    /// </summary>
+    public class CswUpdateSchema_01T_Case27551 : CswUpdateSchemaTo
+    {
+        public override CswDeveloper Author
+        {
+            get { return CswDeveloper.MB; }
+        }
+
+        public override int CaseNo
+        {
+            get { return 27551; }
+        }
+
+        public override void update()
+        {
+            CswNbtMetaDataObjectClass materialComponentOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( NbtObjectClass.MaterialComponentClass );
+            CswNbtMetaDataObjectClassProp mixtureOCP = materialComponentOC.getObjectClassProp( CswNbtObjClassMaterialComponent.PropertyName.Mixture );
+
+            _CswNbtSchemaModTrnsctn.MetaData.UpdateObjectClassProp( mixtureOCP, CswNbtMetaDataObjectClassProp.ObjectClassPropAttributes.servermanaged, true );
+        }
+
+        //Update()
+
+    }//class CswUpdateSchemaCase27551
+
+}//namespace ChemSW.Nbt.Schema
