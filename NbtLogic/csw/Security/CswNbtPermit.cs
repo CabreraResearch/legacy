4d520af5b7f9161d0d959d33809057762c1a7352
pglaser@chemsw.com <unknown@kiln.example.com>
2012-10-26 21:10:38 +0400

    case 27793: remove cruft

--- NbtLogic/csw/Security/CswNbtPermit.cs
+++ NbtLogic/csw/Security/CswNbtPermit.cs
@@ -489,37 +489,8 @@ namespace ChemSW.Nbt.Security
                         {
                             ret = false;
                         }
-                        if( ret )
-                        {
-                            ret = ( null == MetaDataTab || canTab( Permission, _CswNbtPermitInfo.NodeType, MetaDataTab ) );
-                            if( ret )
-                            {
-                                //CswNbtMetaDataObjectClassProp OCP = MetaDataProp.getObjectClassProp();
 
-                                //// case 8218 - Certain properties on the user's preferences are not allowed to be edited
-                                //if( _CswNbtPermitInfo.NodeType.getObjectClass().ObjectClass == NbtObjectClass.UserClass &&
-                                //    false == _CswNbtPermitInfo.User.IsAdministrator() &&
-                                //    OCP != null &&
-                                //    ( OCP.PropName == CswNbtObjClassUser.PropertyName.Username ||
-                                //      OCP.PropName == CswNbtObjClassUser.PropertyName.Role ||
-                                //      OCP.PropName == CswNbtObjClassUser.PropertyName.FailedLoginCount ||
-                                //      OCP.PropName == CswNbtObjClassUser.PropertyName.AccountLocked ) )
-                                //{
-                                //    ret = false;
-                                //}
-
-                                // Only admins can change other people's passwords
-                                //if( ret &&
-                                //    _CswNbtPermitInfo.NodeType.getObjectClass().ObjectClass == NbtObjectClass.UserClass &&
-                                //    false == _CswNbtPermitInfo.User.IsAdministrator() &&
-                                //    ( null != NodePropWrapper && _CswNbtPermitInfo.User.UserId != NodePropWrapper.NodeId ) &&
-                                //    OCP != null &&
-                                //    OCP.PropName == CswNbtObjClassUser.PropertyName.Password )
-                                //{
-                                //    ret = false;
-                                //}
-                            }
-                        } //if pre-reqs are satisfied
+                        ret = ( null == MetaDataTab || canTab( Permission, _CswNbtPermitInfo.NodeType, MetaDataTab ) );
                     }
                 }
                 else
