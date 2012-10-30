5eeb7c37a198884f011a1d04dec6784cb4c1fbb3
mbischoff@chemsw.com <unknown@kiln.example.com>
2012-10-30 22:44:55 +0400

    Titania. Case 28012 - grids header items implement isRequired

--- NbtWebApp/app/wizards/csw.creatematerial.js
+++ NbtWebApp/app/wizards/csw.creatematerial.js
@@ -28,7 +28,7 @@
                 stepThreeComplete: false,
                 stepFourComplete: false,
                 config: {
-                    quantityName: 'Initial Quantity *',
+                    quantityName: 'Initial Quantity',
                     numberName: 'Catalog No.',
                     dispensibleName: 'Dispensible',
                     quantityEditableName: 'Quantity Editable',
@@ -138,7 +138,7 @@
                             }
                             cswPrivate.createMaterial();
                             if (cswPrivate.isDuplicateMaterial) {
-                                
+
                                 cswPrivate.toggleButton(cswPrivate.buttons.prev, true, true);
                             }
                         }
@@ -500,12 +500,14 @@
                                 return ret;
                             };
 
-                            cswPrivate.header = [cswPrivate.config.unitCountName, cswPrivate.config.quantityName, cswPrivate.config.numberName];
+                            cswPrivate.header = [{ "value": cswPrivate.config.unitCountName, "isRequired": false },
+                                { "value": cswPrivate.config.quantityName, "isRequired": true },
+                                { "value": cswPrivate.config.numberName, "isRequired": false}];
                             if (cswPrivate.showQuantityEditable) {
-                                cswPrivate.header = cswPrivate.header.concat([cswPrivate.config.quantityEditableName]);
+                                cswPrivate.header = cswPrivate.header.concat([{ "value": cswPrivate.config.quantityEditableName, "isRequired": false}]);
                             }
                             if (cswPrivate.showDispensable) {
-                                cswPrivate.header = cswPrivate.header.concat([cswPrivate.config.dispensibleName]);
+                                cswPrivate.header = cswPrivate.header.concat([{ "value": cswPrivate.config.dispensibleName, "isRequired": false}]);
                             }
                             if (cswPrivate.rows.length === 0) {
                                 cswPrivate.rows.push(cswPrivate.header);
@@ -544,7 +546,7 @@
                                                 }
                                             });
                                             cswPublic.rows[rowid].unitsCtrl = cswCell.select({
-                                                name:'unitsOfMeasureSelect',
+                                                name: 'unitsOfMeasureSelect',
                                                 values: unitsOfMeasure,
                                                 onChange: function (value) {
                                                     cswPublic.rows[rowid].sizeValues.unit = cswPublic.rows[rowid].unitsCtrl.val();
