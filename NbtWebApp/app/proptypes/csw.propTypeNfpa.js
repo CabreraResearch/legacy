78d5b0da84812bb89426557a9282242caa0256a9
mbischoff@chemsw.com <unknown@kiln.example.com>
2012-10-30 23:39:47 +0400

    Titania. Case 27893 - NFPA readability

--- NbtWebApp/app/proptypes/csw.propTypeNfpa.js
+++ NbtWebApp/app/proptypes/csw.propTypeNfpa.js
@@ -41,10 +41,10 @@
                     cswPrivate.propVals = cswPublic.data.propData.values;
                     cswPrivate.parent = cswPublic.data.propDiv;
 
-                    cswPrivate.red =    cswPrivate.propVals.flammability;
+                    cswPrivate.red = cswPrivate.propVals.flammability;
                     cswPrivate.yellow = cswPrivate.propVals.reactivity;
-                    cswPrivate.blue =   cswPrivate.propVals.health;
-                    cswPrivate.white =  cswPrivate.propVals.special;
+                    cswPrivate.blue = cswPrivate.propVals.health;
+                    cswPrivate.white = cswPrivate.propVals.special;
                     cswPrivate.displayMode = cswPrivate.propVals.displaymode;
                     cswPrivate.hideSpecial = cswPrivate.propVals.hidespecial;
 
@@ -90,7 +90,12 @@
                             .div({ cssclass: 'CswFieldTypeNFPA_text CswFieldTypeNFPA_whitetextLinear' });
                     }
                     cswPrivate.setValue = function (div, value) {
-                        div.text(value);
+                        if (cswPrivate.blueDiv.getId() === div.getId()) {
+                            div.empty();
+                            div.span({ text: value }).css('background-color', 'White');
+                        } else {
+                            div.text(value);
+                        }
 
                         if (value === 'W') {
                             div.addClass("strikethrough");
@@ -137,7 +142,7 @@
                         cswPrivate.editTable = cswPublic.control.cell(1, 2).table({
                             FirstCellRightAlign: true
                         });
-                        
+
                         cswPrivate.editTable.cell(1, 1).text('Flammability');
                         cswPrivate.editTable.cell(2, 1).text('Reactivity');
                         if (cswPrivate.displayMode === Csw.enums.NFPADisplayMode.Diamond) {
