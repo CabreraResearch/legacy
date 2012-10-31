0bc55a8c2d15b017dc16b2ca108b910487659605
mbischoff@chemsw.com <unknown@kiln.example.com>
2012-10-31 23:00:58 +0400

    Titania. Case 28020. Review K4096 - Material Document Default FileType is "File"

--- NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
+++ NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
@@ -84,6 +84,7 @@ namespace ChemSW.Nbt.Schema
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_01T_Case27881() ) );                         //01S-051    01T-17
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_01S_Case27528() ) );                         //01S-052    01T-18
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_01T_Case27912() ) );                         //01S-053    01T-19
+            _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_01T_Case28020() ) );                         //01S-054    01T-20
 
             #endregion TITANIA
 
