b1e5bcd50048311a3e284358a6293be3e3324eff
mbischoff@chemsw.com <unknown@kiln.example.com>
2012-11-07 22:57:06 +0500

    Ursula. Case 24524. Review Review K4138 - forgotten file

---
+++ NbtLogic/csw/Schema/scripts/prod/01U/CswUpdateSchema_01U_Case24524_MolKeysTable.cs
@@ -0,0 +1,41 @@
+﻿using ChemSW.Nbt.csw.Dev;
+using ChemSW.StructureSearch;
+using ChemSW.DB;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update for case 24524
+    /// </summary>
+    public class CswUpdateSchema_01U_Case24524_MolKeysTable : CswUpdateSchemaTo
+    {
+        public override CswDeveloper Author
+        {
+            get { return CswDeveloper.MB; }
+        }
+
+        public override int CaseNo
+        {
+            get { return 24524; }
+        }
+
+        public override void update()
+        {
+            if( false == _CswNbtSchemaModTrnsctn.isTableDefined( "mol_keys" ) )
+            {
+                _CswNbtSchemaModTrnsctn.addTable( "mol_keys", "nodeid" );
+
+                for( int i = 0; i < CswStructureSearch.keySize; i++ )
+                {
+                    _CswNbtSchemaModTrnsctn.addNumberColumn( "mol_keys", "key" + i, "", false, false, string.Empty, false, string.Empty, false );
+                }
+
+                _CswNbtSchemaModTrnsctn.addNumberColumn( "mol_keys", "atomcount", "", false, false, string.Empty, false, string.Empty, false );
+            }         
+        }
+
+        //Update()
+
+    }//class CswUpdateSchemaCase24524
+
+}//namespace ChemSW.Nbt.Schema
