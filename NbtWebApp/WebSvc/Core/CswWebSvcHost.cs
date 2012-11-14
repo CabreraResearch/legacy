92b0da280efdfec0e51318012cf769206b1341ed
Christopher Froehlich <unknown@kiln.example.com>
2012-11-14 22:27:43 +0500

    Ursula. Case 28073.

---
+++ NbtWebApp/WebSvc/Core/CswWebSvcHost.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+
+namespace NbtWebApp.WebSvc.Core
+{
+    class CswWebSvcHost : WebServiceHost
+    {
+        public CswWebSvcHost(Type serviceType, params Uri[] baseAddresses)
+            : base(serviceType, baseAddresses) {}
+
+        protected override void OnOpening()
+        {
+            base.OnOpening();
+
+            foreach (ServiceEndpoint Endpoint in Description.Endpoints)
+            {
+                if (Endpoint.Behaviors.Find<WebHttpBehavior>() != null)
+                {
+                    Endpoint.Behaviors.Remove<WebHttpBehavior>();
+                    Endpoint.Behaviors.Add(new CswWebSvcHttpBehavior());
+                }
+            }
+        }
+    }
+}
---
+++ NbtWebApp/WebSvc/Core/CswWebSvcHttpBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Web;
+
+namespace NbtWebApp.WebSvc.Core
+{
+    class CswWebSvcHttpBehavior : WebHttpBehavior
+    {
+        protected override IDispatchMessageFormatter GetReplyDispatchFormatter( OperationDescription OperationDescription,
+                                                                               ServiceEndpoint Endpoint )
+        {
+            WebGetAttribute WebGetAttribute = OperationDescription.Behaviors.Find<WebGetAttribute>();
+            CswWebSvcDynamicAttribute MapAcceptedContentTypeToResponseEncodingAttribute = OperationDescription.Behaviors.Find<CswWebSvcDynamicAttribute>();
+
+            if( WebGetAttribute != null && MapAcceptedContentTypeToResponseEncodingAttribute != null )
+            {
+                // We need two formatters, since we don't know what type we will need until runtime
+                WebGetAttribute.ResponseFormat = WebMessageFormat.Json;
+                IDispatchMessageFormatter JsonDispatchMessageFormatter = base.GetReplyDispatchFormatter( OperationDescription, Endpoint );
+
+                WebGetAttribute.ResponseFormat = WebMessageFormat.Xml;
+                IDispatchMessageFormatter XmlDispatchMessageFormatter = base.GetReplyDispatchFormatter( OperationDescription, Endpoint );
+
+                return new CswWebSvcDynamicSerializer
+                {
+                    JsonDispatchMessageFormatter = JsonDispatchMessageFormatter,
+                    XmlDispatchMessageFormatter = XmlDispatchMessageFormatter
+                };
+            }
+            return base.GetReplyDispatchFormatter( OperationDescription, Endpoint );
+        }
+    }
+}
