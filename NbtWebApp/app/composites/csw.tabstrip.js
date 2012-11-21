f29871743005ec65a5c413a2ff6e90e8f94d7e0f
Christopher Froehlich <unknown@kiln.example.com>
2012-11-22 00:43:52 +0500

    Ursula. Case 27695. Part 7. Tab strip implemented.

--- NbtWebApp/app/composites/csw.tabstrip.js
+++ NbtWebApp/app/composites/csw.tabstrip.js
@@ -7,7 +7,24 @@
 
             //#region Variables
             var cswPrivate = {
-                name: ''
+                name: '',
+                tabPanel: {
+                    title: 'Tabs',
+                    height: 400,
+                    width: 800,
+                    resizable: true,
+                    stateful: true,
+                    stateId: 'CswRequestCart'
+                },
+                tabs: [{
+                    title: 'First tab',
+                    html: 'No content has been defined for this tab',
+                    tooltip: 'First tab of many',
+                    handler: function (tab, eventObj) {
+                        
+                    }
+                }],
+                extTabs: []
             };
             var cswPublic = { };
 
@@ -29,18 +46,53 @@
 
             //cswPrivate.method = function() {};
 
-            //cswPublic.method = function() {};
+            cswPublic.addTab = function(tab) {
+                window.Ext.onReady(function() {
+                    tab = tab || {
+                        title: 'This is a tab',
+                        html: 'Hi, I am tab ',
+                        tooltip: title
+                    };
+                    var extTab = cswPublic.tabPanel.add([tab])[0];
+                    cswPrivate.extTabs.push(extTab);
+                    return extTab;
+                });
+            };
                 
             //#endregion Define Class Members
                 
 
             //#region Post-ctor
 
+            window.Ext.onReady(function() {
+
             (function _post() {
                 
+                    window.Ext.tip.QuickTipManager.init();
+
+                    cswPublic.tabPanel = window.Ext.create('Ext.tab.Panel', {
+                        height: cswPrivate.tabPanel.height,
+                        width: cswPrivate.tabPanel.width,
+                        renderTo: cswPublic.getId(),
+                        title: cswPrivate.tabPanel.title,
+                        resizable: cswPrivate.tabPanel.resizable,
+                        stateful: cswPrivate.tabPanel.stateful,
+                        stateId: cswPrivate.tabPanel.stateId,
+                        plugins: [{
+                            ptype: 'tabscrollermenu',
+                            maxText: 15,
+                            pageSize: 5
+                        },
+                        new window.Ext.ux.TabReorderer()
+                        ]
+                    });
+
+                    Csw.each(cswPrivate.tabs, function(tabObj) {
+                        cswPublic.addTab(tabObj);
+                    });
 
             }());
-
+            });
             //#endregion Post-ctor
 
             return cswPublic;
