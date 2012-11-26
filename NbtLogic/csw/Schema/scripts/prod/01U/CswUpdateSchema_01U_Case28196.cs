25842416f5c152989a6b0b4724f0a00bcba0e0f6
Colleen Muldowney <cmuldowney@chemsw.com>
2012-11-26 21:43:01 +0500

    Ursula. Case 28196: Change Container Group NT to "MLM"

---
+++ NbtLogic/csw/Schema/scripts/prod/01U/CswUpdateSchema_01U_Case28196.cs
@@ -0,0 +1,35 @@
+﻿using ChemSW.Nbt.csw.Dev;
+using ChemSW.Nbt.MetaData;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update for case 28196
+    /// </summary>
+    public class CswUpdateSchema_01U_Case28196 : CswUpdateSchemaTo
+    {
+        public override CswDeveloper Author
+        {
+            get { return CswDeveloper.CM; }
+        }
+
+        public override int CaseNo
+        {
+            get { return 28196; }
+        }
+
+        public override void update()
+        {
+            CswNbtMetaDataObjectClass ContainerGroupOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( NbtObjectClass.ContainerGroupClass );
+
+            CswNbtMetaDataNodeType ContainerGroupNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "Container Group" );
+            if( ContainerGroupNT != null )
+            {
+                ContainerGroupNT.Category = "MLM";
+            }
+
+        }//Update()
+
+    }//class CswUpdateSchema_01U_Case28196
+
+}//namespace ChemSW.Nbt.Schema
