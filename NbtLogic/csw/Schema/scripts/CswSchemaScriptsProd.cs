19728ba377433f9553c315b2bcd6ba22b1428569
mbischoff@chemsw.com <unknown@kiln.example.com>
2012-11-29 02:54:45 +0500

    Ursula. Case 28156 - Receipt Lots optional

--- NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
+++ NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
@@ -66,6 +66,7 @@ namespace ChemSW.Nbt.Schema
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_01U_Case24489_Part2() ) );                   //01T-033    01U-009
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_01U_Case28196() ) );                         //01T-034    01U-010
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_01U_Case28117() ) );                         //01T-035    01U-011
+            _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_01U_Case28156() ) );                         //01T-036    01U-012
 
 
             #endregion URSULA
---
+++ NbtLogic/csw/Schema/scripts/prod/01U/CswUpdateSchema_01U_Case28156.cs
@@ -0,0 +1,48 @@
+﻿using ChemSW.Nbt.csw.Dev;
+using ChemSW.Nbt.ObjClasses;
+using ChemSW.Nbt.MetaData;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update for case 28156
+    /// </summary>
+    public class CswUpdateSchema_01U_Case28156 : CswUpdateSchemaTo
+    {
+        public override CswDeveloper Author
+        {
+            get { return CswDeveloper.MB; }
+        }
+
+        public override int CaseNo
+        {
+            get { return 28156; }
+        }
+
+        public override void update()
+        {
+            #region remove the receipt lot objclass from jct_modules_objectclass
+
+            CswNbtMetaDataObjectClass receiptLotOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( NbtObjectClass.ReceiptLotClass );
+            _CswNbtSchemaModTrnsctn.deleteAllModuleObjectClassJunctions( receiptLotOC );
+
+            #endregion
+
+            #region make the receipt lot NTP on containers hidden by default
+
+            CswNbtMetaDataObjectClass containerOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( NbtObjectClass.ContainerClass );
+            CswNbtMetaDataObjectClassProp receiptLotOCP = containerOC.getObjectClassProp( CswNbtObjClassContainer.PropertyName.ReceiptLot );
+
+            foreach( CswNbtMetaDataNodeTypeProp receiptLotNTP in receiptLotOCP.getNodeTypeProps() )
+            {
+                receiptLotNTP.removeFromAllLayouts();
+            }
+
+            #endregion
+        }
+
+        //Update()
+
+    }//class CswUpdateSchemaCase28156
+
+}//namespace ChemSW.Nbt.Schema
