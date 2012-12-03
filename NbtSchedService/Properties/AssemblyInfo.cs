5de6fb5d6130fdae7035e5e466468d1d0141ba54
NBT Daily Build <b3851691-5f24-46c9-9188-5f0a2e70960a>
2012-12-03 12:16:31 +0500

    Automated commit for release: Ursula_2012.12.3.1

--- NbtSchedService/Properties/AssemblyInfo.cs
+++ NbtSchedService/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "2012.11.30.1" )]
-[assembly: AssemblyFileVersion( "2012.11.30.1" )]
+[assembly: AssemblyVersion( "2012.12.3.1" )]
+[assembly: AssemblyFileVersion( "2012.12.3.1" )]
