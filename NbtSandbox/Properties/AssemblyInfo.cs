0d9b6731a1c2bbde3c7cb0325b8e4cbdcb27ef50
NBT Daily Build <b3851691-5f24-46c9-9188-5f0a2e70960a>
2012-12-04 18:49:20 +0500

    Automated commit for release: Ursula_2012.12.4.1

--- NbtSandbox/Properties/AssemblyInfo.cs
+++ NbtSandbox/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "2012.12.3.3" )]
-[assembly: AssemblyFileVersion( "2012.12.3.3" )]
+[assembly: AssemblyVersion( "2012.12.4.1" )]
+[assembly: AssemblyFileVersion( "2012.12.4.1" )]
