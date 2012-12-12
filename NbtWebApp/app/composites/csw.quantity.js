8ee5b812d7bd489f18d378732b151e00057ac4ff
bvavra@chemsw.com <unknown@kiln.example.com>
2012-12-12 22:56:00 +0500

    Viola - Case 28346, Case 28347 - Fixed Quantity abnormalities in Dispensing

--- NbtWebApp/app/composites/csw.quantity.js
+++ NbtWebApp/app/composites/csw.quantity.js
@@ -23,6 +23,7 @@
                 cswPrivate.relationships = cswPrivate.relationships || [];
                 cswPrivate.quantity = cswPrivate.quantity || '';
                 cswPrivate.unit = cswPrivate.unit || '';
+                cswPrivate.unitText = cswPrivate.unitText || '';
 
                 cswPrivate.minvalue = cswPrivate.minvalue || '';
                 cswPrivate.maxvalue = cswPrivate.maxvalue || '';
@@ -131,6 +132,13 @@
                     cswPrivate.unit = cswPrivate.selectBox.val();
                 }
                 return cswPrivate.unit;
+            };
+            
+            cswPublic.selectedUnitText = function () {
+                if (cswPrivate && cswPrivate.selectBox && cswPrivate.selectBox.val) {
+                    cswPrivate.unitText = cswPrivate.selectBox.selectedText();
+                }
+                return cswPrivate.unitText;
             };
 
             cswPublic.value = function () {
