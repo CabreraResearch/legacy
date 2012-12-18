313e1ad3901de4992563006d8f45a2857829db09
Steven Salter <ssalter@chemsw.com>
2012-12-18 20:38:27 +0500

    Viola - case 25495 - Refactor search filter away from JObjects and towards WCF

--- NbtLogic/csw/Search/CswNbtSearchFilterWrapper.cs
+++ NbtLogic/csw/Search/CswNbtSearchFilterWrapper.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Data;
-using System.Linq;
+using System.Runtime.Serialization;
 using ChemSW.Core;
-using ChemSW.DB;
-using ChemSW.Exceptions;
-using ChemSW.Nbt.MetaData;
-using ChemSW.Nbt.ObjClasses;
 using Newtonsoft.Json.Linq;
 
 namespace ChemSW.Nbt.Search
@@ -29,20 +22,18 @@ namespace ChemSW.Nbt.Search
         public static readonly CswNbtSearchFilterType objectclass = new CswNbtSearchFilterType( "objectclass" );
         public static readonly CswNbtSearchFilterType propval = new CswNbtSearchFilterType( "propval" );
     }
-
-    public class CswNbtSearchFilterWrapper : IEquatable<CswNbtSearchFilterWrapper>
+    
+    [DataContract]
+    public class CswNbtSearchFilter : IEquatable<CswNbtSearchFilter>
     {
-        private JObject _FilterObj;
         public const string BlankValue = "[blank]";
 
-        public CswNbtSearchFilterWrapper( JObject FilterObj )
+        public CswNbtSearchFilter( JObject FilterObj )
         {
-            _FilterObj = FilterObj;
+            FromJObject( FilterObj );
         }
-        public CswNbtSearchFilterWrapper( string inFilterName, CswNbtSearchFilterType inType, string inFilterId, string inFilterValue, Int32 inCount, string inIcon, bool inRemoveable, CswNbtSearchPropOrder.PropOrderSourceType inSource )
+        public CswNbtSearchFilter( string inFilterName, CswNbtSearchFilterType inType, string inFilterId, string inFilterValue, Int32 inCount, string inIcon, bool inRemoveable, CswNbtSearchPropOrder.PropOrderSourceType inSource )
         {
-            _FilterObj = new JObject();
-
             FilterName = inFilterName;
             Type = inType;
             FilterId = inFilterId;
@@ -53,97 +44,96 @@ namespace ChemSW.Nbt.Search
             Source = inSource;
         }
 
+        [DataMember]
+        public string FilterName;
+        [DataMember]
+        public CswNbtSearchFilterType Type;
+        [DataMember]
+        public string FilterId;
+        [DataMember]
+        public Int32 Count;
+        [DataMember]
+        public string Icon;
+        [DataMember]
+        public bool Removeable;
+        [DataMember]
+        public CswNbtSearchPropOrder.PropOrderSourceType Source;
+        [DataMember]
+        public string FilterValue;
+
+        // for NodeType Filters
+        [DataMember]
+        public Int32 FirstVersionId;
+
+        // for Object Class Filters
+        [DataMember]
+        public Int32 ObjectClassId;
+
+        // for PropVal Filters
+        [DataMember]
+        public Int32 FirstPropVersionId;
+
+        #region Serialization
+
         public JObject ToJObject()
         {
-            return _FilterObj;
-        }
+            JObject FilterObj = new JObject();
+            FilterObj["filtername"] = FilterName;
+            FilterObj["filtertype"] = Type.ToString();
+            FilterObj["filterid"] = FilterId;
+            FilterObj["count"] = Count;
+            FilterObj["icon"] = Icon;
+            FilterObj["removeable"] = Removeable;
+            FilterObj["source"] = Source.ToString();
 
-        public string FilterName
-        {
-            get { return _FilterObj["filtername"].ToString(); }
-            set { _FilterObj["filtername"] = value; }
-        }
-        public CswNbtSearchFilterType Type
-        {
-            get { return (CswNbtSearchFilterType) _FilterObj["filtertype"].ToString(); }
-            set { _FilterObj["filtertype"] = value.ToString(); }
-        }
-        public string FilterId
-        {
-            get { return _FilterObj["filterid"].ToString(); }
-            set { _FilterObj["filterid"] = value; }
-        }
-        public Int32 Count
-        {
-            get { return CswConvert.ToInt32( _FilterObj["count"] ); }
-            set { _FilterObj["count"] = value.ToString(); }
-        }
-        public string Icon
-        {
-            get { return _FilterObj["icon"].ToString(); }
-            set { _FilterObj["icon"] = value; }
-        }
-        public bool Removeable
-        {
-            get { return CswConvert.ToBoolean( _FilterObj["removeable"] ); }
-            set { _FilterObj["removeable"] = value; }
-        }
-        public CswNbtSearchPropOrder.PropOrderSourceType Source
-        {
-            get { return (CswNbtSearchPropOrder.PropOrderSourceType) _FilterObj["source"].ToString(); }
-            set { _FilterObj["source"] = value.ToString(); }
-        }
-        public string FilterValue
-        {
-            get { return _FilterObj["filtervalue"].ToString(); }
-            set
+            if( FilterValue != string.Empty )
             {
-                if( value != string.Empty )
-                {
-                    _FilterObj["filtervalue"] = value;
-                }
-                else
-                {
-                    _FilterObj["filtervalue"] = BlankValue;
-                }
+                FilterObj["filtervalue"] = FilterValue;
             }
-        } // FilterValue
+            else
+            {
+                FilterObj["filtervalue"] = BlankValue;
+            }
 
-        #region for NodeType Filters
+            if( Int32.MinValue != FirstVersionId )
+            {
+                FilterObj["firstversionid"] = FirstVersionId;
+            }
+            if( Int32.MinValue != FirstPropVersionId )
+            {
+                FilterObj["firstpropversionid"] = FirstPropVersionId;
+            }
+            if( Int32.MinValue != ObjectClassId )
+            {
+                FilterObj["objectclassid"] = ObjectClassId;
+            }
 
-        public Int32 FirstVersionId
+            return FilterObj;
+        } // ToJObject()
+
+        public JObject FromJObject( JObject FilterObj )
         {
-            get { return CswConvert.ToInt32( _FilterObj["firstversionid"] ); }
-            set { _FilterObj["firstversionid"] = value.ToString(); }
-        }
+            FilterName = FilterObj["filtername"].ToString();
+            Type = (CswNbtSearchFilterType) FilterObj["filtertype"].ToString();
+            FilterId = FilterObj["filterid"].ToString();
+            Count = CswConvert.ToInt32( FilterObj["count"] );
+            Icon = FilterObj["icon"].ToString();
+            Removeable = CswConvert.ToBoolean( FilterObj["removeable"] );
+            Source = (CswNbtSearchPropOrder.PropOrderSourceType) FilterObj["source"].ToString();
+            FilterValue = FilterObj["filtervalue"].ToString();
+            FirstVersionId = CswConvert.ToInt32( FilterObj["firstversionid"] );
+            FirstPropVersionId = CswConvert.ToInt32( FilterObj["firstpropversionid"] );
+            ObjectClassId = CswConvert.ToInt32( FilterObj["objectclassid"] );
+            return FilterObj;
+        } // FromJObject()
 
-        #endregion for NodeType Filters
-
-        #region for Object Class Filters
-
-        public Int32 ObjectClassId
-        {
-            get { return CswConvert.ToInt32( _FilterObj["objectclassid"] ); }
-            set { _FilterObj["objectclassid"] = value.ToString(); }
-        }
-
-        #endregion for Object Class Filters
-
-        #region for PropVal Filters
-
-        public Int32 FirstPropVersionId
-        {
-            get { return CswConvert.ToInt32( _FilterObj["firstpropversionid"] ); }
-            set { _FilterObj["firstpropversionid"] = value.ToString(); }
-        }
-
-        #endregion for PropVal Filters
+        #endregion Serialization
 
         #region IEquatable
         /// <summary>
         /// IEquatable: ==
         /// </summary>
-        public static bool operator ==( CswNbtSearchFilterWrapper w1, CswNbtSearchFilterWrapper w2 )
+        public static bool operator ==( CswNbtSearchFilter w1, CswNbtSearchFilter w2 )
         {
             // If both are null, or both are same instance, return true.
             if( System.Object.ReferenceEquals( w1, w2 ) )
@@ -168,7 +158,7 @@ namespace ChemSW.Nbt.Search
         /// <summary>
         /// IEquatable: !=
         /// </summary>
-        public static bool operator !=( CswNbtSearchFilterWrapper w1, CswNbtSearchFilterWrapper w2 )
+        public static bool operator !=( CswNbtSearchFilter w1, CswNbtSearchFilter w2 )
         {
             return !( w1 == w2 );
         }
@@ -178,17 +168,17 @@ namespace ChemSW.Nbt.Search
         /// </summary>
         public override bool Equals( object obj )
         {
-            if( !( obj is CswNbtSearchFilterWrapper ) )
+            if( !( obj is CswNbtSearchFilter ) )
                 return false;
-            return this == (CswNbtSearchFilterWrapper) obj;
+            return this == (CswNbtSearchFilter) obj;
         }
 
         /// <summary>
         /// IEquatable: Equals
         /// </summary>
-        public bool Equals( CswNbtSearchFilterWrapper obj )
+        public bool Equals( CswNbtSearchFilter obj )
         {
-            return this == (CswNbtSearchFilterWrapper) obj;
+            return this == (CswNbtSearchFilter) obj;
         }
 
         /// <summary>
