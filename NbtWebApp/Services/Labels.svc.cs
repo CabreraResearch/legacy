f480c9d68550b8fc8d4c89e7d55c24234b3d0d90
Steven Salter <ssalter@chemsw.com>
2012-12-22 01:11:58 +0500

    Review K4378 - fix earlier change to Labels, for integration with print app

--- NbtWebApp/Services/Labels.svc.cs
+++ NbtWebApp/Services/Labels.svc.cs
@@ -46,8 +46,7 @@ namespace NbtWebApp
         /// 
         /// </summary>
         [OperationContract]
-        [WebGet( UriTemplate = "label/{PrintLabelId}/target/{TargetId}" )]
-        [WebInvoke( BodyStyle = WebMessageBodyStyle.Wrapped )]
+        [WebGet( UriTemplate = "label/{PrintLabelId}/target/{TargetId}", BodyStyle = WebMessageBodyStyle.Wrapped )]
         [Description( "Get a collection of EPL texts for the selected Targets" )]
         public CswNbtLabelEpl get( string PrintLabelId, string TargetId )
         {
