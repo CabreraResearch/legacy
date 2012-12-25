90df92bd3b3de02869982b3a52278bbb10cb8e79
pglaser@chemsw.com <unknown@kiln.example.com>
2012-12-25 22:43:38 +0500

    case 27699: Ensure inclusion of base rules in rule factory

--- NbtLogic/csw/Sched/core/CswScheduleLogicFactoryNbt.cs
+++ NbtLogic/csw/Sched/core/CswScheduleLogicFactoryNbt.cs
@@ -1,32 +1,27 @@
 using System.Collections.Generic;
 using ChemSW.MtSched.Core;
+using ChemSW.MtSched.Rules;
 
 namespace ChemSW.Nbt.Sched
 {
 
     public enum NbtScheduleRuleNames { Unknown, UpdtPropVals, UpdtMTBF, UpdtInspection, GenNode, GenEmailRpt, DisableChemSwAdmin, BatchOp, ExpiredContainers, MolFingerprints }
-    public class CswScheduleLogicFactoryNbt : ICswScheduleLogicFactory
+    public class CswScheduleLogicFactoryNbt : CswScheduleLogicFactoryBase
     {
 
-        public List<ICswScheduleLogic> getRules()
+        public CswScheduleLogicFactoryNbt()
         {
-            List<ICswScheduleLogic> ReturnVal = new List<ICswScheduleLogic>();
-
-            ReturnVal.Add( new CswScheduleLogicNbtBatchOps() );
-            ReturnVal.Add( new CswScheduleLogicNbtGenEmailRpt() );
-            ReturnVal.Add( new CswScheduleLogicNbtGenNode() );
-            ReturnVal.Add( new CswScheduleLogicNbtUpdtInspection() );
-            ReturnVal.Add( new CswScheduleLogicNbtUpdtMTBF() );
-            ReturnVal.Add( new CswScheduleLogicNbtUpdtPropVals() );
-            ReturnVal.Add( new CswScheduleLogicNbtDisableCswAdmin() );
-            ReturnVal.Add( new CswScheduleLogicNbtExpiredContainers() );
-            ReturnVal.Add( new CswScheduleLogicNbtMolFingerprints() );
-            ReturnVal.Add( new CswScheduleLogicNbtContainerReconciliationActions() );
-
-            return ( ReturnVal );
-
-        }//getRules()
-
+            _Rules.Add( new CswScheduleLogicNbtBatchOps() );
+            _Rules.Add( new CswScheduleLogicNbtGenEmailRpt() );
+            _Rules.Add( new CswScheduleLogicNbtGenNode() );
+            _Rules.Add( new CswScheduleLogicNbtUpdtInspection() );
+            _Rules.Add( new CswScheduleLogicNbtUpdtMTBF() );
+            _Rules.Add( new CswScheduleLogicNbtUpdtPropVals() );
+            _Rules.Add( new CswScheduleLogicNbtDisableCswAdmin() );
+            _Rules.Add( new CswScheduleLogicNbtExpiredContainers() );
+            _Rules.Add( new CswScheduleLogicNbtMolFingerprints() );
+            _Rules.Add( new CswScheduleLogicNbtContainerReconciliationActions() );
+        }
 
     }//CswReportTimingDaily
 
