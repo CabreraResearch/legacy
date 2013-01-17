3bd60f7ca00d9413cf0d42c7f57e29d015e47aba
NBT Daily Build <b3851691-5f24-46c9-9188-5f0a2e70960a>
2013-01-17 20:46:27 +0500

    Automated commit for release: Viola_2013.1.17.2

--- NbtSchedService/Properties/AssemblyInfo.cs
+++ NbtSchedService/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "2013.1.17.1" )]
-[assembly: AssemblyFileVersion( "2013.1.17.1" )]
+[assembly: AssemblyVersion( "2013.1.17.2" )]
+[assembly: AssemblyFileVersion( "2013.1.17.2" )]
