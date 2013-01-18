d7b90b6bd8fb5338ae21dcfc9bb16a7b18cf9dee
bvavra@chemsw.com <unknown@kiln.example.com>
2013-01-19 00:25:56 +0500

    Viola - Case 28283 (Revisited) - added/updated props and fixed maq data

--- NbtLogic/csw/Schema/scripts/prod/01V/CswUpdateSchema_01V_Case28281.cs
+++ NbtLogic/csw/Schema/scripts/prod/01V/CswUpdateSchema_01V_Case28281.cs
@@ -81,12 +81,57 @@ namespace ChemSW.Nbt.Schema
                 ChemicalSpecialFlagsNTP.DefaultValue.AsMultiList.Value = SpecialFlags;
 
                 ChemicalHazardCategoriesNTP.ListOptions = "F = Fire,C = Chronic (delayed),I = Immediate (acute),R = Reactive,P = Pressure";
-
-                String FireHazardClassTypes = @"Aero-1,Aero-2,Aero-3,Carc,CF/D (balled),CF/D (loose),CL-II,CL-IIIA,CL-IIIB,
-                    Corr,CRY-FG,CRY-OXY,Exp,FG (gaseous),FG (liquified),FL-1A,FL-1B,FL-1C,FL-Comb,FS,H.T.,Irr,OHH,
-                    Oxy1,Oxy2,Oxy3,Oxy4,Oxy-Gas,Oxy-Gas (liquid),Perox-Det,Perox-I,Perox-II,Perox-III,Perox-IV,Perox-V,Pyro,
-                    RAD-Alpha,RAD-Beta,RAD-Gamma,Sens,Tox,UR-1,UR-2,UR-3,UR-4,WR-1,WR-2,WR-3";
-                ChemicalHazardClassesNTP.ListOptions = FireHazardClassTypes;
+                CswCommaDelimitedString FireHazardClassTypes = new CswCommaDelimitedString {
+                    "Aero-1",
+                    "Aero-2",
+                    "Aero-3",
+                    "Carc",
+                    "CF/D (baled)",
+                    "CF/D (loose)",
+                    "CL-II",
+                    "CL-III-A",
+                    "CL-III-B",
+                    "Corr",
+                    "CRY-FG",
+                    "CRY-OXY",
+                    "Exp",
+                    "FG (gaseous)",
+                    "FG (liquified)",
+                    "FL-1A",
+                    "FL-1B",
+                    "FL-1C",
+                    "FL-Comb",
+                    "FS",
+                    "HT",
+                    "Irr",
+                    "OHH",
+                    "Oxi-1",
+                    "Oxi-2",
+                    "Oxi-3",
+                    "Oxi-4",
+                    "Oxi-Gas",
+                    "Oxi-Gas (liquid)",
+                    "Perox-Det",
+                    "Perox-I",
+                    "Perox-II",
+                    "Perox-III",
+                    "Perox-IV",
+                    "Perox-V",
+                    "Pyro",
+                    "RAD-Alpha",
+                    "RAD-Beta",
+                    "RAD-Gamma",
+                    "Sens",
+                    "Tox",
+                    "UR-1",
+                    "UR-2",
+                    "UR-3",
+                    "UR-4",
+                    "WR-1",
+                    "WR-2",
+                    "WR-3"
+                };
+                ChemicalHazardClassesNTP.ListOptions = FireHazardClassTypes.ToString();
 
                 ChemicalMaterialTypeNTP.removeFromLayout( CswNbtMetaDataNodeTypeLayoutMgr.LayoutType.Add );
                 ChemicalSpecialFlagsNTP.removeFromLayout( CswNbtMetaDataNodeTypeLayoutMgr.LayoutType.Add );
