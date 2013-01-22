d26b0768a5878b67b361b7dd8e210800442ad63b
bvavra@chemsw.com <unknown@kiln.example.com>
2013-01-23 02:01:04 +0500

    William - Case 28395 - Added container props to audit table and added Trade Secret to Chemical special flags

--- NbtLogic/csw/Schema/scripts/prod/01W/CswUpdateSchema_01W_Case28395.cs
+++ NbtLogic/csw/Schema/scripts/prod/01W/CswUpdateSchema_01W_Case28395.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChemSW.Audit;
+using ChemSW.Core;
 using ChemSW.Nbt.Actions;
 using ChemSW.Nbt.MetaData;
 using ChemSW.Nbt.ObjClasses;
@@ -26,6 +27,30 @@ namespace ChemSW.Nbt.Schema
             //Add TierII Reporting Action
             _CswNbtSchemaModTrnsctn.createAction( CswNbtActionName.Tier_II_Reporting, true, "", "Materials" );
             _CswNbtSchemaModTrnsctn.createModuleActionJunction( CswNbtModuleName.CISPro, CswNbtActionName.Tier_II_Reporting );
+
+            //Add Trade Secret Chemical Prop
+            CswNbtMetaDataNodeType ChemicalNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "Chemical" );
+            if( null != ChemicalNT )
+            {
+                CswNbtMetaDataNodeTypeProp ChemicalSpecialFlagsNTP = _CswNbtSchemaModTrnsctn.MetaData.getNodeTypeProp(ChemicalNT.NodeTypeId, "Special Flags");
+                if( null != ChemicalSpecialFlagsNTP )
+                {
+                    ChemicalSpecialFlagsNTP.ListOptions = "EHS,Waste,Not Reportable,Trade Secret";
+                }
+            }
+
+            //Add Container fire reporting props to Audit Table
+            CswNbtMetaDataObjectClass ContainerOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( NbtObjectClass.ContainerClass );
+            CswNbtMetaDataNodeType ContainerNT = ContainerOC.FirstNodeType;
+            if( null != ContainerNT )
+            {
+                CswNbtMetaDataNodeTypeProp StorageTemperatureNTP = _CswNbtSchemaModTrnsctn.MetaData.getNodeTypePropByObjectClassProp(ContainerNT.NodeTypeId, CswNbtObjClassContainer.PropertyName.StorageTemperature);
+                StorageTemperatureNTP.AuditLevel = AuditLevel.PlainAudit;
+                CswNbtMetaDataNodeTypeProp StoragePressureNTP = _CswNbtSchemaModTrnsctn.MetaData.getNodeTypePropByObjectClassProp(ContainerNT.NodeTypeId, CswNbtObjClassContainer.PropertyName.StoragePressure);
+                StoragePressureNTP.AuditLevel = AuditLevel.PlainAudit;
+                CswNbtMetaDataNodeTypeProp UseTypeNTP = _CswNbtSchemaModTrnsctn.MetaData.getNodeTypePropByObjectClassProp(ContainerNT.NodeTypeId, CswNbtObjClassContainer.PropertyName.UseType);
+                UseTypeNTP.AuditLevel = AuditLevel.PlainAudit;
+            }
         }//Update()
 
     }//class CswUpdateSchemaCase_01W_28395
