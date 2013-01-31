c5fe6d23aab3d6909c73a30e606522661cdf0f70
pglaser@chemsw.com <unknown@kiln.example.com>
2013-02-01 01:55:41 +0500

    case 28520: Added spreadsheeet mapping classes (_not_ done yet :-) )

--- NbtLogic/csw/ImportExport/CswImportTablePopulatorFromRapidLoader.cs
+++ NbtLogic/csw/ImportExport/CswImportTablePopulatorFromRapidLoader.cs
@@ -1,5 +1,11 @@
 ﻿
+using System;
+using System.Collections.Generic;
+using System.Data;
 using ChemSW.Nbt.Schema;
+
+using Microsoft.Office.Interop.Excel;
+
 namespace ChemSW.Nbt.ImportExport
 {
 
@@ -43,10 +49,92 @@ namespace ChemSW.Nbt.ImportExport
             get { return ( _Stop ); }
         }
 
+        private ImportProcessPhase _LastCompletedProcessPhase = ImportProcessPhase.NothingDoneYet;
 
+
+        private Dictionary<Int32,CswNbtMetaDataForSpreadSheetCol> _metaDataForSpreadSheet = new Dictionary<int, CswNbtMetaDataForSpreadSheetCol>();
         public bool loadImportTables( ref string Msg )
         {
             bool ReturnVal = true;
+
+            //********************************************************************************************************************
+            //Begin: Set up the excel paraphanelia
+            Microsoft.Office.Interop.Excel.Application excell_Application = null;
+            Microsoft.Office.Interop.Excel.Workbooks excell_Workbooks = null;
+            Microsoft.Office.Interop.Excel._Workbook excell_OneWorkBook = null;
+            Microsoft.Office.Interop.Excel.Sheets excell_Worksheets = null;
+            Microsoft.Office.Interop.Excel._Worksheet excell_OneWorksheet = null;
+            Microsoft.Office.Interop.Excel.Range excell_Range = null;
+            object MissingExcellValue = System.Reflection.Missing.Value;
+
+
+            excell_Application = new Microsoft.Office.Interop.Excel.Application();
+            excell_OneWorkBook = excell_Workbooks.Open( _CswNbtImportExportFrame.FilePath, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, true, "XLNormalLoad" );
+            excell_Worksheets = excell_OneWorkBook.Worksheets;
+
+            excell_OneWorksheet = (Microsoft.Office.Interop.Excel.Worksheet) excell_Worksheets.get_Item( 1 );
+            //End: Set up the excel paraphanelia
+            //********************************************************************************************************************
+
+
+            //********************************************************************************************************************
+            //Begin: Set up NBT field-types per-prop mapping
+            excell_Range = excell_OneWorksheet.UsedRange;
+
+            List<string> PropColumnNames = new List<string>();
+
+
+            int NodeTypeNameRow = 1;
+            int NodeTypePropNameRow = 3;
+
+            for( Int32 ColIdx = 2; ColIdx <= excell_Range.Columns.Count; ColIdx++)
+            {
+                string CandidateNodeTypeName = excell_Range.Cells[NodeTypeNameRow, ColIdx];
+                string CandidateNodeTypePropName = excell_Range.Cells[NodeTypePropNameRow, ColIdx];
+
+
+
+            }//iterate meta data rows of spreadsheet
+
+
+
+            //End: Set up NBT field-types per-prop mapping
+            //********************************************************************************************************************
+
+            string ImportTableInconsistencyMessage = string.Empty;
+            if( _CswImporterDbTables.areImportTablesAbsent( ref ImportTableInconsistencyMessage ) )
+            {
+                if( string.Empty == ImportTableInconsistencyMessage )
+                {
+                    _LastCompletedProcessPhase = ImportProcessPhase.LoadingInputFile;
+
+                    _CswImportExportStatusReporter.updateProcessPhase( _LastCompletedProcessPhase, 0, 0, ProcessStates.InProcess );
+
+                    _CswNbtSchemaModTrnsctn.beginTransaction();
+
+
+
+                    foreach( DataColumn CurrentColumn in TableOfPropsFromXml.Columns )
+                    {
+                        PropColumnNames.Add( CurrentColumn.ColumnName );
+                    }
+
+
+                    _CswImporterDbTables.makeImportTables( PropColumnNames );
+
+                    _CswNbtSchemaModTrnsctn.commitTransaction();
+
+                    _CswImportExportStatusReporter.updateProcessPhase( _LastCompletedProcessPhase, 0, 0, ProcessStates.Complete );
+
+                }
+                else
+                {
+                    _CswImportExportStatusReporter.reportError( "The import tables are in an inconsistent state: " + ImportTableInconsistencyMessage );
+
+                }//if-else import tables are inconsistent
+
+            }//if import tables are absent
+
 
             return ( ReturnVal );
 
---
+++ NbtLogic/csw/ImportExport/CswNbtMetaDataForSpreadSheetColReader.cs
@@ -0,0 +1,99 @@
+﻿
+using System.Collections.Generic;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.MetaData.FieldTypeRules;
+namespace ChemSW.Nbt.ImportExport
+{
+
+    public class CswNbtMetaDataForSpreadSheetColReader
+    {
+        private Dictionary<string, string> _NodeTypeNameMapper = new Dictionary<string, string>();
+        private Dictionary<string, string> _NodeTypePropNameMapper = new Dictionary<string, string>();
+        private Dictionary<string, CswNbtMetaDataNodeType> _NodeTypesPerSpreadsheetRowCols = new Dictionary<string, CswNbtMetaDataNodeType>();
+
+        private CswNbtResources _CswNbtResources = null;
+        public CswNbtMetaDataForSpreadSheetColReader( CswNbtResources CswNbtResources )
+        {
+            _CswNbtResources = CswNbtResources;
+
+            //Add nodetype name mappings to _NodeTypeNameMapper
+
+
+            //Add nodetype propname mappings to _NodeTypePropNameMapper
+
+
+        }//ctor
+
+        public CswNbtMetaDataForSpreadSheetCol read( string NodeTypeNameColVal, string PropNameColVal, ref string Message )
+        {
+            CswNbtMetaDataForSpreadSheetCol ReturnVal = new CswNbtMetaDataForSpreadSheetCol();
+
+
+            if( _NodeTypesPerSpreadsheetRowCols.ContainsKey( NodeTypeNameColVal ) )
+            {
+                ReturnVal.CswNbtMetaDataNodeType = _NodeTypesPerSpreadsheetRowCols[NodeTypeNameColVal];
+            }
+            else
+            {
+
+                if( null != ( ReturnVal.CswNbtMetaDataNodeTypeProp = _CswNbtResources.MetaData.getNodeType( NodeTypeNameColVal ) ) )
+                {
+                    _NodeTypesPerSpreadsheetRowCols.Add( NodeTypeNameColVal, ReturnVal.CswNbtMetaDataNodeType );
+
+                }
+                else
+                {
+
+                    string OfficialNodeTypeName = string.Empty;
+                    if( _NodeTypeNameMapper.ContainsKey( NodeTypeNameColVal ) )
+                    {
+                        OfficialNodeTypeName = _NodeTypeNameMapper[NodeTypeNameColVal];
+                        if( null != ( ReturnVal.CswNbtMetaDataNodeTypeProp = _CswNbtResources.MetaData.getNodeType( OfficialNodeTypeName ) ) )
+                        {
+                            _NodeTypesPerSpreadsheetRowCols.Add( NodeTypeNameColVal, ReturnVal.CswNbtMetaDataNodeType );
+                        }
+                        else
+                        {
+                            Message = "The officially mapped node type name ( " + OfficialNodeTypeName + ") for the spreadsheet column nodetype name " + NodeTypeNameColVal + " does not correspond to a known node type";
+
+                        }//even the offiial name doesn't map
+                    }
+                    else
+                    {
+                        Message = "The spreadsheet column nodetype name " + NodeTypeNameColVal + " does not correspond to a known node type and has no mapping";
+
+                    }//if we have a mapped nodetype name
+                }
+            }//if we already have the nodetype
+
+
+            if( null != ReturnVal.CswNbtMetaDataNodeType )
+            {
+
+                if( null == ( ReturnVal.CswNbtMetaDataNodeTypeProp = _CswNbtResources.MetaData.getNodeTypeProp( PropNameColVal ) ) )
+                {
+                    if( _NodeTypePropNameMapper.ContainsKey( PropNameColVal ) )
+                    {
+
+                        string OfficialPropName = _NodeTypePropNameMapper[PropNameColVal];
+                        if( null == ( ReturnVal.CswNbtMetaDataNodeTypeProp = _CswNbtResources.MetaData.getNodeTypeProp( OfficialPropName ) ) )
+                        {
+
+                        }
+
+                    }
+                    else
+                    {
+
+                    }
+                }//if the spreadsheet prop name did not get a prop
+            }
+
+            return ( ReturnVal );
+
+        }//read
+
+    } // CswNbtMetaDataForSpreadSheetColReader
+
+
+} // namespace ChemSW.Nbt
