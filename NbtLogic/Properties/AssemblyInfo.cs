44e934a062a90566b88e9d76b96741810da4d27c
NBT Daily Build <b3851691-5f24-46c9-9188-5f0a2e70960a>
2013-01-31 12:16:08 +0500

    Automated commit for release: William_2013.1.31.1

--- NbtLogic/Properties/AssemblyInfo.cs
+++ NbtLogic/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion( "2013.1.30.1" )]
-[assembly: AssemblyFileVersion( "2013.1.30.1" )]
+[assembly: AssemblyVersion( "2013.1.31.1" )]
+[assembly: AssemblyFileVersion( "2013.1.31.1" )]
