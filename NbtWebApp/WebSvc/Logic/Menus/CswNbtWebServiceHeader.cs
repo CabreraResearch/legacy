4aa3f8d5f0a5e87376f5fb3870c232e970549a45
Steven Salter <ssalter@chemsw.com>
2013-02-02 00:46:27 +0500

    William - case 28702 - PrintingLabels.html

--- NbtWebApp/WebSvc/Logic/Menus/CswNbtWebServiceHeader.cs
+++ NbtWebApp/WebSvc/Logic/Menus/CswNbtWebServiceHeader.cs
@@ -187,8 +187,6 @@ namespace ChemSW.Nbt.WebServices
             Ret["Help"]["Help"]["popup"] = "help/index.htm";
             Ret["Help"]["Clear Cache"] = new JObject();
             Ret["Help"]["Clear Cache"]["action"] = "Clear Cache";
-            Ret["Help"]["About"] = new JObject();
-            Ret["Help"]["About"]["action"] = "About";
             CswNbtMetaDataObjectClass feedbackOC = _CswNbtResources.MetaData.getObjectClass( NbtObjectClass.FeedbackClass );
             IEnumerable<CswNbtMetaDataNodeType> feedbackNodeTypes = feedbackOC.getNodeTypes();
             if( feedbackNodeTypes.Any() )
@@ -202,6 +200,10 @@ namespace ChemSW.Nbt.WebServices
                     Ret["Help"]["Give Feedback"]["nodetypeid"] = feedbackNodeType.NodeTypeId;
                 }
             }
+            Ret["Help"]["Printing Labels"] = new JObject();
+            Ret["Help"]["Printing Labels"]["popup"] = "PrintingLabels.html";
+            Ret["Help"]["About"] = new JObject();
+            Ret["Help"]["About"]["action"] = "About";
 
             Ret["Logout"] = new JObject( new JProperty( "action", "Logout" ) );
 
