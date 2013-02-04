6b881d2952be4e48fbbe35613e598f31f2174105
bvavra@chemsw.com <unknown@kiln.example.com>
2013-02-04 21:35:56 +0500

    William - Case 28713, Parts 1, 3, 4, 5, and 8 - rearranged UNCode and Fire Reporting properties, added Revision Date to SDS grid, and made Components into its own tab

--- NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
+++ NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
@@ -79,7 +79,7 @@ namespace ChemSW.Nbt.Schema
             _addVersionedScript(new CswSchemaUpdateDriver(new CswUpdateSchema_01W_Case28655()));              //01V-049   01W-019
             _addVersionedScript(new CswSchemaUpdateDriver(new CswUpdateSchema_01W_Case28534()));              //01V-050   01W-020
             _addVersionedScript(new CswSchemaUpdateDriver(new CswUpdateSchema_01W_Case28557()));              //01V-051   01W-021
-
+            _addVersionedScript(new CswSchemaUpdateDriver(new CswUpdateSchema_01W_Case28713()));              //01V-052   01W-022
 
             #endregion WILLIAM
 
