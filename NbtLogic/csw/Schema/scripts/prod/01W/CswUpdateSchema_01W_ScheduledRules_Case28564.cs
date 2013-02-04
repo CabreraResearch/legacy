02fa8b3b509c53ae804bd5db4e0f925eb5421f85
Unknown <unknown@kiln.example.com>
2013-02-04 21:04:11 +0500

    William. Case 28564.

--- NbtLogic/csw/Schema/scripts/prod/01W/CswUpdateSchema_01W_ScheduledRules_Case28564.cs
+++ NbtLogic/csw/Schema/scripts/prod/01W/CswUpdateSchema_01W_ScheduledRules_Case28564.cs
@@ -1,5 +1,8 @@
-﻿using ChemSW.Nbt.Actions;
+﻿using System;
+using ChemSW.Nbt.Actions;
 using ChemSW.Nbt.csw.Dev;
+using ChemSW.Nbt.LandingPage;
+using ChemSW.Nbt.MetaData;
 using ChemSW.Nbt.ObjClasses;
 
 namespace ChemSW.Nbt.Schema
@@ -25,8 +28,31 @@ namespace ChemSW.Nbt.Schema
             CswNbtObjClassRole CswAdmin = _CswNbtSchemaModTrnsctn.Nodes.makeRoleNodeFromRoleName( CswNbtObjClassRole.ChemSWAdminRoleName );
             if( null != CswAdmin )
             {
-                _CswNbtSchemaModTrnsctn.Permit.can( CswNbtActionName.View_Scheduled_Rules, CswAdmin );
+                _CswNbtSchemaModTrnsctn.Permit.set( CswNbtActionName.View_Scheduled_Rules, CswAdmin, value: true );
             }
+
+            CswNbtMetaDataObjectClass RoleOc = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( NbtObjectClass.RoleClass );
+            Int32 ActionId = _CswNbtSchemaModTrnsctn.getActionId( CswNbtActionName.View_Scheduled_Rules );
+
+            foreach( CswNbtNode RoleNode in RoleOc.getNodes(includeSystemNodes: false, forceReInit: true) )
+            {
+                if( _CswNbtSchemaModTrnsctn.Permit.can( CswNbtActionName.View_Scheduled_Rules, RoleNode ) )
+                {
+                    CswNbtLandingPageTable Lp = _CswNbtSchemaModTrnsctn.getLandingPageTable();
+                    Lp.addLandingPageItem(new LandingPageData.Request
+                        {
+                            RoleId = RoleNode.NodeId.ToString(),
+                            ActionId = ActionId.ToString(),
+                            Text = "View Scheduled Rules",
+                            Type = CswNbtLandingPageItemType.Link,
+                            ViewType = "Action",
+                            PkValue = ActionId.ToString(),
+                            NewRow = 1,
+                            NewColumn = 2
+                        });
+                }
+            }
+
         }
 
     }//class CswUpdateSchema_01V_CaseXXXXX
