91d8d1bea3a9787e474a58f131f7dfcfc29bc1dc
Colleen Muldowney <cmuldowney@chemsw.com>
2013-02-12 23:40:32 +0500

    William Case 28796, William Case 28792: all containers now being created in receive wizard & quantity correct

--- NbtLogic/csw/Actions/CswNbtActReceiving.cs
+++ NbtLogic/csw/Actions/CswNbtActReceiving.cs
@@ -147,70 +147,72 @@ namespace ChemSW.Nbt.Actions
                                 if( Quantities.HasValues )
                                 {
 
-                                    //JObject ContainerAddProps = CswConvert.ToJObject( ReceiptObj["props"] );
-                                    CswNbtSdTabsAndProps SdTabsAndProps = new CswNbtSdTabsAndProps( CswNbtResources );
-                                    foreach( JObject QuantityDef in Quantities )
-                                    {
-                                        Int32 NoContainers = CswConvert.ToInt32( QuantityDef["containerNo"] );
-                                        CswCommaDelimitedString Barcodes = new CswCommaDelimitedString();
-                                        Barcodes.FromString( CswConvert.ToString( QuantityDef["barcodes"] ) );
-                                        Double QuantityValue = CswConvert.ToDouble( QuantityDef["quantity"] );
-                                        CswPrimaryKey UnitId = new CswPrimaryKey();
-                                        UnitId.FromString( CswConvert.ToString( QuantityDef["unitid"] ) );
-                                        CswPrimaryKey SizeId = new CswPrimaryKey();
-                                        SizeId.FromString( CswConvert.ToString( QuantityDef["sizeid"] ) );
-                                        CswNbtObjClassSize AsSize = CswNbtResources.Nodes.GetNode( SizeId );
+                                    JObject ContainerAddProps = CswConvert.ToJObject( ReceiptObj["props"] );
 
-                                        Debug.Assert( ( NoContainers > 0 ), "The request did not specify at least one container." );
-                                        Debug.Assert( ( QuantityValue > 0 ), "The request did not specify a valid quantity." );
-                                        Debug.Assert( ( Int32.MinValue != UnitId.PrimaryKey ), "The request did not specify a valid unit." );
-                                        if( NoContainers > 0 && QuantityValue > 0 && Int32.MinValue != UnitId.PrimaryKey )
+                                    CswNbtSdTabsAndProps SdTabsAndProps = new CswNbtSdTabsAndProps( CswNbtResources );
+                                    for (int index = 0; index < Quantities.Count; index++)
+                                    {
+                                        JObject QuantityDef = CswConvert.ToJObject(Quantities[index]);
+                                        Int32 NoContainers = CswConvert.ToInt32(QuantityDef["containerNo"]);
+                                        CswCommaDelimitedString Barcodes = new CswCommaDelimitedString();
+                                        Barcodes.FromString(CswConvert.ToString(QuantityDef["barcodes"]));
+                                        Double QuantityValue = CswConvert.ToDouble(QuantityDef["quantity"]);
+                                        CswPrimaryKey UnitId = new CswPrimaryKey();
+                                        UnitId.FromString(CswConvert.ToString(QuantityDef["unitid"]));
+                                        CswPrimaryKey SizeId = new CswPrimaryKey();
+                                        SizeId.FromString(CswConvert.ToString(QuantityDef["sizeid"]));
+                                        CswNbtObjClassSize AsSize = CswNbtResources.Nodes.GetNode(SizeId);
+
+                                        Debug.Assert((NoContainers > 0), "The request did not specify at least one container.");
+                                        Debug.Assert((QuantityValue > 0), "The request did not specify a valid quantity.");
+                                        Debug.Assert((Int32.MinValue != UnitId.PrimaryKey), "The request did not specify a valid unit.");
+                                        if (NoContainers > 0 && QuantityValue > 0 && Int32.MinValue != UnitId.PrimaryKey)
                                         {
                                             JArray jBarcodes = new JArray();
                                             Ret["barcodes"] = jBarcodes;
-                                            for( Int32 C = 0; C < NoContainers; C += 1 )
+                                            for (Int32 C = 0; C < NoContainers; C += 1)
                                             {
                                                 // This includes the initial Container node that was created at 
                                                 // the start of the receive wizard -- this is done so the barcode isn't
                                                 // thrown out.
+                                                CswNbtNodeKey ContainerNodeKey;
                                                 CswNbtObjClassContainer AsContainer;
-                                                if (C == 0)
+                                                if (C == 0 && index == 0)
                                                 {
                                                     AsContainer = InitialContainerNode;
                                                 }
                                                 else
                                                 {
-                                                    AsContainer = InitialContainerNode.CopyNode();
+                                                    AsContainer = SdTabsAndProps.addNode(ContainerNt, null, ContainerAddProps, out ContainerNodeKey);
                                                 }
 
-                                                if( null != AsContainer )
+                                                if (null != AsContainer)
                                                 {
-                                                    if( Barcodes.Count <= NoContainers && false == string.IsNullOrEmpty( Barcodes[C] ) )
+                                                    if (Barcodes.Count <= NoContainers && false == string.IsNullOrEmpty(Barcodes[C]))
                                                     {
-                                                        AsContainer.Barcode.setBarcodeValueOverride( Barcodes[C], false );
+                                                        AsContainer.Barcode.setBarcodeValueOverride(Barcodes[C], false);
                                                     }
                                                     AsContainer.Size.RelatedNodeId = SizeId;
-                                                    //AsContainer.Material.RelatedNodeId = NodeAsMaterial.NodeId;
-                                                    if( AsSize.QuantityEditable.Checked != Tristate.True )
+                                                    AsContainer.Material.RelatedNodeId = NodeAsMaterial.NodeId;
+                                                    if (AsSize.QuantityEditable.Checked != Tristate.True)
                                                     {
                                                         QuantityValue = AsSize.InitialQuantity.Quantity;
                                                         UnitId = AsSize.InitialQuantity.UnitId;
                                                     }
-                                                    if( null == AsContainer.Quantity.UnitId || Int32.MinValue == AsContainer.Quantity.UnitId.PrimaryKey )
+                                                    if (null == AsContainer.Quantity.UnitId || Int32.MinValue == AsContainer.Quantity.UnitId.PrimaryKey)
                                                     {
                                                         AsContainer.Quantity.UnitId = UnitId;
                                                     }
-                                                    AsContainer.DispenseIn( CswNbtObjClassContainerDispenseTransaction.DispenseType.Receive, QuantityValue, UnitId );
+                                                    AsContainer.DispenseIn(CswNbtObjClassContainerDispenseTransaction.DispenseType.Receive, QuantityValue, UnitId);
                                                     AsContainer.Disposed.Checked = Tristate.False;
-                                                    AsContainer.Undispose.setHidden( value: true, SaveToDb: true );
-                                                    AsContainer.postChanges( true );
-                                                    ContainerIds.Add( AsContainer.NodeId );
-                                                    jBarcodes.Add( AsContainer.NodeId.ToString() );
+                                                    AsContainer.Undispose.setHidden(value: true, SaveToDb: true);
+                                                    AsContainer.postChanges(true);
+                                                    ContainerIds.Add(AsContainer.NodeId);
+                                                    jBarcodes.Add(AsContainer.NodeId.ToString());
                                                 }
-                                            }//for( Int32 C = 0; C < NoContainers; C += 1 )
+                                            } //for( Int32 C = 0; C < NoContainers; C += 1 )
                                         }
-                                    }//foreach( JObject QuantityDef in Quantities )
-
+                                    }
                                 }//if( Quantities.HasValues )
 
                             }//if( null != NodeAsMaterial )
