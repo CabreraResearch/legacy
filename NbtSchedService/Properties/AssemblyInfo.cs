aa6aa10dfe37a7ff2973f0832991927e3e6221f5
NBT Daily Build <b3851691-5f24-46c9-9188-5f0a2e70960a>
2013-02-15 20:36:27 +0500

    Automated commit for release: William_2013.2.15.2

--- NbtSchedService/Properties/AssemblyInfo.cs
+++ NbtSchedService/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "2013.2.15.1" )]
-[assembly: AssemblyFileVersion( "2013.2.15.1" )]
+[assembly: AssemblyVersion( "2013.2.15.2" )]
+[assembly: AssemblyFileVersion( "2013.2.15.2" )]
