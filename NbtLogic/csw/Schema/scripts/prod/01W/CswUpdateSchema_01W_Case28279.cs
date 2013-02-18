c5aa870ea7b46e257a204bed54c40c66eb9add90
pglaser@chemsw.com <unknown@kiln.example.com>
2013-02-19 03:46:51 +0500

    case 28279: add schema update file

---
+++ NbtLogic/csw/Schema/scripts/prod/01W/CswUpdateSchema_01W_Case28279.cs
@@ -0,0 +1,27 @@
+﻿using ChemSW.Nbt.csw.Dev;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update for case XXXXX
+    /// </summary>
+    public class CswUpdateSchema_01W_Case28279 : CswUpdateSchemaTo
+    {
+        public override CswDeveloper Author
+        {
+            get { return CswDeveloper.PG; }
+        }
+
+        public override int CaseNo
+        {
+            get { return 28279; }
+        }
+
+        public override void update()
+        {
+            // This is a placeholder script that does nothing.
+        } //Update()
+
+    }//class CswUpdateSchema_01V_CaseXXXXX
+
+}//namespace ChemSW.Nbt.Schema
