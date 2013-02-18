e8235328942956ed868ecbeea9843b5035ac5a5e
bvavra@chemsw.com <unknown@kiln.example.com>
2013-02-19 01:36:31 +0500

    William - Review K4739, Comment 2 some more.

--- NbtLogic/csw/Schema/scripts/prod/01W/CswUpdateSchema_01W_Case28650.cs
+++ NbtLogic/csw/Schema/scripts/prod/01W/CswUpdateSchema_01W_Case28650.cs
@@ -22,20 +22,12 @@ namespace ChemSW.Nbt.Schema
         public override void update()
         {
             CswNbtMetaDataObjectClass UnitOfMeasureOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( NbtObjectClass.UnitOfMeasureClass );
-            //Case 28650
             foreach(CswNbtMetaDataNodeType UoMNT in UnitOfMeasureOC.getNodeTypes())
             {
                 CswNbtMetaDataNodeTypeProp BaseUnitNTP = _CswNbtSchemaModTrnsctn.MetaData.getNodeTypePropByObjectClassProp( UoMNT.NodeTypeId, CswNbtObjClassUnitOfMeasure.PropertyName.BaseUnit );
                 BaseUnitNTP.StaticText = BaseUnitNTP.DefaultValue.AsText.Text;
                 BaseUnitNTP.updateLayout( CswNbtMetaDataNodeTypeLayoutMgr.LayoutType.Add, true );
                 BaseUnitNTP.ServerManaged = false;
-            }
-            //Case 28834
-            foreach( CswNbtObjClassUnitOfMeasure UoMNode in UnitOfMeasureOC.getNodes( false, false ) )
-            {
-                UoMNode.UnitConversion.StaticText = @"Conversion Factor should be set to the number required to make the current unit equal to the base unit.<br/>
-Example: <strong>g(1E3) = kg</strong><br/>where g is the current unit, kg is the base unit, and 1E3 is the conversion factor.";
-                UoMNode.postChanges( false );
             }
         } //Update()
 
