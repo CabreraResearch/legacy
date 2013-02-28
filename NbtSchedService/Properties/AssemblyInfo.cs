73498e84270596419bc142fb37c3ebdd2dd96d04
NBT Daily Build <b3851691-5f24-46c9-9188-5f0a2e70960a>
2013-02-28 12:16:18 +0500

    Automated commit for release: Yorick_2013.2.28.1

--- NbtSchedService/Properties/AssemblyInfo.cs
+++ NbtSchedService/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "2013.2.27.1" )]
-[assembly: AssemblyFileVersion( "2013.2.27.1" )]
+[assembly: AssemblyVersion( "2013.2.28.1" )]
+[assembly: AssemblyFileVersion( "2013.2.28.1" )]
