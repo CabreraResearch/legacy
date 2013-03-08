5bb8ac4cbde9e46cf39f5eba02f868737e1dd713
Steven Salter <ssalter@chemsw.com>
2013-03-08 21:57:13 +0500

    Yorick, case 28938 - new classes

---
+++ NbtLogic/csw/ImportExport/2D/CswNbt2DDefinition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using ChemSW.Nbt.MetaData;
+
+namespace ChemSW.Nbt.ImportExport
+{
+    public class CswNbt2DDefinition
+    {
+        private string _SheetName;
+        public string SheetName
+        {
+            get { return _SheetName; }
+            set
+            {
+                _SheetName = value;
+                if( false == _SheetName.EndsWith( "$" ) )
+                {
+                    _SheetName += "$";
+                }
+            }
+        }
+        public string ImportDataTableName;
+        public CswNbt2DBindingCollection Bindings = new CswNbt2DBindingCollection();
+        public SortedList<Int32, CswNbt2DOrder> ImportOrder = new SortedList<int, CswNbt2DOrder>();
+        public Collection<CswNbt2DRowRelationship> RowRelationships = new Collection<CswNbt2DRowRelationship>();
+    }
+}
