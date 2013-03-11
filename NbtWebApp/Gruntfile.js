1460f3f3e1aeed85db52d947725d421823eea6bc
Christopher Froehlich <cfroehlich@chemsw.com>
2013-03-12 01:02:58 +0400

    Aspen. Case 29020. First Grunt task. Switch to NbtWebApp directory and run `grunt.cmd test` to get started. See W1021 for more details. Add to Review K4936.

--- NbtWebApp/Gruntfile.js
+++ NbtWebApp/Gruntfile.js
@@ -2,45 +2,48 @@
 module.exports = function (grunt) {
     // Project configuration.
     grunt.initConfig({
-        releasePath: 'release',
+        CswAppJsFiles: ['app/ChemSW.js', 'app/types/**.js', 'app/core/**.js', 'app/events/**.js', 'app/tools/**.js', 'app/literals/**.js', 'app/controls/**.js',
+                'app/composites/**.js', 'app/actions/**.js', 'app/layouts/**.js', 'app/nodes/**.js', 'app/pagecmp/**.js', 'app/proptypes/**.js', 'app/view/**.js',
+                'app/wizard/**.js'
+        ], //Don't include 'Main.js' just yet. Unfortunately, due to racee conditions in loading, we can't blindly accept all (YET)
+        CswTestJsFiles: ['test/*.js', 'test/**/*.js'],
+        releaseMode: 'test',
         pkg: '<json:package.json>',
         meta: {
             banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %>\n' + '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' + '* No Copyright (!c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' + ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %>'
         },
         tmpHtml: {
-            release: {
-                src: '',
-                dest: ''
-            },
-            dev: {
-                src: '',
-                dest: ''
+            
+            test: {
+                src: 'test/test.tmpl',
+                dest: 'test/Test.html',
+                testJsFiles: ['test/*.js', 'test/**/*.js']
             }
         },
         concat: {
-            dist: {
-                src: '',
-                dest: ''
-            },
+            
             vendorCoreJs: {
-                src: [],
-                dest: '',
-                separator: ';\n\n\n;'
+                src: ['Scripts/modernizr-2.6.2.js', 'vendor/loggly-0.1.0.min.js', 'Scripts/es5-shim.min.js', 'vendor/es5-sham.min.js', 'vendor/es6-shim.min.js', 'Scripts/mousetrap.min.js', 'Scripts/jquery-1.7.1.min.js',
+                    'Scripts/jquery-ui-1.8.19.min.js', 'Scripts/jquery.validate.min.js', 'Scripts/additional-methods.min.js', 'Scripts/jquery.cookie.js', 'vendor/jsTree.v.1.0rc2/jquery.jstree.js',
+                    'vendor/multiselect/jquery.multiselect.min.js', 'vendor/multiselect/jquery.multiselect.filter.min.js', 'vendor/jQueryFileUpload_2011.12.15/jquery.fileupload.js',
+                    'vendor/jQueryFileUpload_2011.12.15/jquery.iframe-transport.js', 'vendor/ext-init.js', 'vendor/extjs-4.1.0/ext-all.js', 'vendor/ext-done.js'
+                ],
+                dest: 'vendor/vendor.min.js',
+                separator: ';/* next JS  */\n'
             },
             vendorCss: {
-                src: [],
-                dest: ''
+                src: ['Content/themes/cupertino/jquery-ui-1.8.19.css', 'vendor/multiselect/jquery.multiselect.css', 'vendor/multiselect/jquery.multiselect.filter.css', 'vendor/extjs-4.1.0/resources/css/ext-all-scoped.css',
+                      'vendor/extjs-4.1.0/ux/TabScrollerMenu.css', 'vendor/extjs-4.1.0/ux/css/CheckHeader.css'
+                ],
+                dest: 'vendor/vendor.min.css',
+                separator: '/*  next CSS  */'
             }
         },
         min: {
-            dist: {
-                src: [],
-                dest: '',
-                separator: ';\n\n\n;'
-            }
+            
         },
         qunit: {
-            files: []
+            files: ['test/*.html']
         },
         lint: {
             files: []
@@ -80,12 +83,6 @@ module.exports = function (grunt) {
             ast: true,
             verbose: true
         },
-        'closure-compiler': {
-
-        },
-        'append-sourcemapping': {
-
-        },
         jsdoc: {
 
         }
@@ -99,6 +96,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jsdoc');
 
     /**ENDREGION: *-contrib tasks */
 
@@ -111,8 +109,25 @@ module.exports = function (grunt) {
 
     /**ENDREGION: init tasks */
 
-    /**REGION: register tasks */
+    /**REGION: register CSW tasks */
 
+    var _buildHtmlFromTmpl = function (releaseMode, gruntConfigOpt) {
+        grunt.log.write('Starting template-to-HTML conversion for ' + releaseMode + ' mode.');
+        grunt.config('releaseMode', releaseMode);
+        var conf = grunt.config(gruntConfigOpt);
+        var tmpl = grunt.file.read(conf.src);
 
-    /**REGION: register tasks */
+        grunt.file.write(conf.dest, grunt.template.process(tmpl));
+
+        grunt.log.writeln('Generated \'' + conf.dest + '\' from \'' + conf.src + '\'');
+    };
+
+    grunt.registerTask('testTmpl', 'Generate Test/test.html from test.tmpl template.', function () {
+        //Strictly speaking, it's not required to subtask these helper functions, but's it's easier to grok later --vlad
+        _buildHtmlFromTmpl('test', 'tmpHtml.test');
+    });
+
+    grunt.registerTask('test', ['concat', 'jshint', 'concat', 'testTmpl', 'qunit']);
+
+    /**REGION: register CSW tasks */
 };
---
+++ NbtWebApp/vendor/ext-init.js
@@ -0,0 +1,7 @@
+﻿// from: http://stackoverflow.com/questions/6435833/how-to-scope-the-reset-css-so-that-is-applied-to-ext-components-using-scopereset
+// Here we define Ext for the first time
+window.Ext = {
+    buildSettings: {
+        "scopeResetCSS": true  // Thanks, but I'll do my own scoping please
+    }
+};
