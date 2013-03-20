3fdf245f1cd5cc38935fe01bdcd0a6bdbed3fadf
bvavra@chemsw.com <unknown@kiln.example.com>
2013-03-20 23:47:11 +0400

    Yorick - Case 29125, Part 1 - Inspection Type is now editable - Target Type and View removed from layout

---
+++ NbtLogic/csw/Schema/scripts/prod/01Y/CswUpdateSchema_01Y_Case29125.cs
@@ -0,0 +1,46 @@
+﻿using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.ObjClasses;
+using ChemSW.Nbt.csw.Dev;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update for case 29125
+    /// </summary>
+    public class CswUpdateSchema_01Y_Case29125 : CswUpdateSchemaTo
+    {
+        public override CswDeveloper Author
+        {
+            get { return CswDeveloper.BV; }
+        }
+
+        public override int CaseNo
+        {
+            get { return 29125; }
+        }
+
+        public override void update()
+        {
+            CswNbtMetaDataNodeType InspectionScheduleNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeTypeFirstVersion( CswNbtObjClassGenerator.InspectionGeneratorNodeTypeName );
+            if( null != InspectionScheduleNT )
+            {
+                CswNbtMetaDataNodeTypeProp InspectionGroupNTP = InspectionScheduleNT.getNodeTypePropByObjectClassProp( CswNbtObjClassGenerator.PropertyName.Owner );
+                InspectionGroupNTP.ServerManaged = false;
+                InspectionGroupNTP.ReadOnly = true;
+                CswNbtMetaDataNodeTypeProp InspectionTargetTypeNTP = InspectionScheduleNT.getNodeTypePropByObjectClassProp( CswNbtObjClassGenerator.PropertyName.ParentType );
+                InspectionTargetTypeNTP.ServerManaged = true;
+                InspectionTargetTypeNTP.removeFromAllLayouts();
+                CswNbtMetaDataNodeTypeProp InspectionTargetViewNTP = InspectionScheduleNT.getNodeTypePropByObjectClassProp( CswNbtObjClassGenerator.PropertyName.ParentView );
+                InspectionTargetViewNTP.ServerManaged = true;
+                InspectionTargetViewNTP.removeFromAllLayouts();
+                CswNbtMetaDataNodeTypeProp InspectionTypeNTP = InspectionScheduleNT.getNodeTypePropByObjectClassProp( CswNbtObjClassGenerator.PropertyName.TargetType );
+                InspectionTypeNTP.ServerManaged = false;
+                InspectionTypeNTP.ReadOnly = false;
+                InspectionTypeNTP.Multi = PropertySelectMode.Multiple;
+            }
+
+        } //Update()
+
+    }//class CswUpdateSchema_01Y_Case29125
+
+}//namespace ChemSW.Nbt.Schema
