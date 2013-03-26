c98d2f9278209efc9b2361e788f48c4e22b24d73
Steven Salter <ssalter@chemsw.com>
2013-03-26 23:21:27 +0400

    Aspen, case 29095 - Administrative override button

--- NbtLogic/csw/Security/CswNbtPermit.cs
+++ NbtLogic/csw/Security/CswNbtPermit.cs
@@ -711,7 +711,8 @@ namespace ChemSW.Nbt.Security
                   ( false == MetaDataProp.ServerManaged ) &&
                   (
                     // Anyone but an admin cannot write to read-only props  
-                    ( ( null != _CswNbtPermitInfo.User ) && ( _CswNbtPermitInfo.User.IsAdministrator() ) ) ||
+                    // but see case 29095; this is now handled in CswNbtSdTabsAndProps
+                    //( ( null != _CswNbtPermitInfo.User ) && ( _CswNbtPermitInfo.User.IsAdministrator() ) ) ||
                       //Buttons are always "writable"  
                       MetaDataProp.getFieldType().FieldType == CswNbtMetaDataFieldType.NbtFieldType.Button ||
                       (
