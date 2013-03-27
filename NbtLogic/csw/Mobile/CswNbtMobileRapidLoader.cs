07277d2586499bcfea7be76e9cc63e871f282a81
bvavra@chemsw.com <unknown@kiln.example.com>
2013-03-27 20:57:00 +0400

    Aspen - Review K5028, comment 4

--- NbtLogic/csw/Mobile/CswNbtMobileRapidLoader.cs
+++ NbtLogic/csw/Mobile/CswNbtMobileRapidLoader.cs
@@ -47,6 +47,7 @@ namespace ChemSW.Nbt.csw.Mobile
             StreamWriter sw = new StreamWriter( fs, System.Text.Encoding.Default );
             sw.Write( Request.CSVData );
             sw.Flush();
+            sw.Close();
 
             String EmailMessageSubject = "Your ChemSW Rapid Loader import is available for download";
             String EmailMessageBody = String.Format( 
