03305b9de5e50597cef503bde576cdbe09140c7b
Christopher Froehlich <cfroehlich@chemsw.com>
2013-04-05 18:15:35 +0400

    Aspen. Case 28813.

--- NbtWebApp/app/composites/csw.grid.js
+++ NbtWebApp/app/composites/csw.grid.js
@@ -553,12 +553,11 @@
                     var cols = cswPrivate.columns.filter(function (col) {
                         return colNames.contains(col.header);
                     });
-                    var i = 0;
+                    
                     Csw.each(cols, function (colObj, key) {
                         colObj.renderer = function (value, metaData, record, rowIndex, colIndex, store, view) {
                             //NOTE: this can now be moved to the viewrender event. See action column logic.
-                            i += 1;
-                            var id = cswPrivate.ID + 'nodebutton' + i;
+                            var divId = cswPrivate.name + 'button' + rowIndex + colIndex;
                             var thisBtn = cswPrivate.rows.buttons.filter(function (btn) {
                                 return btn.index === colObj.dataIndex && btn.rowno === rowIndex;
                             });
@@ -567,8 +566,8 @@
                                     //Case 28343. The problem here is that 
                                     // a) our div is not in the DOM until this method returns and 
                                     // b) we're not always guaranteed to be in the writable portion of the cell--the div we return might be thrown away by Ext
-                                    if (Csw.isElementInDom(id)) {
-                                        var div = Csw.domNode({ ID: id });
+                                    if (Csw.isElementInDom(divId)) {
+                                        var div = Csw.domNode({ ID: divId });
                                         div.nodeButton({
                                             displayName: colObj.header,
                                             size: 'small',
@@ -577,7 +576,7 @@
                                     }
                                 }, 100);
                             }
-                            return '<div id="' + id + '"></div>';
+                            return '<div id="' + divId + '"></div>';
 
                         };
                     });
