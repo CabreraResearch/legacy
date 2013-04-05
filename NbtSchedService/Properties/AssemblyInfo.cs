bcfefd95f8f8a5936cc91748ee784dc63e1f40fe
NBT Daily Build <b3851691-5f24-46c9-9188-5f0a2e70960a>
2013-04-05 10:16:09 +0400

    Automated commit for release: Buckeye_2013.4.5.1

--- NbtSchedService/Properties/AssemblyInfo.cs
+++ NbtSchedService/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "2013.4.4.1" )]
-[assembly: AssemblyFileVersion( "2013.4.4.1" )]
+[assembly: AssemblyVersion( "2013.4.5.1" )]
+[assembly: AssemblyFileVersion( "2013.4.5.1" )]
