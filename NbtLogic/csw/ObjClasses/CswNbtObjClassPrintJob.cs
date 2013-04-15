c3497b3dc22ae738576b1733bc37000fcb6d9904
Christopher Froehlich <cfroehlich@chemsw.com>
2013-04-15 20:57:23 +0400

    Buckeye. Case 29320. Add filter to the default view.

--- NbtLogic/csw/ObjClasses/CswNbtObjClassPrintJob.cs
+++ NbtLogic/csw/ObjClasses/CswNbtObjClassPrintJob.cs
@@ -92,6 +92,9 @@ namespace ChemSW.Nbt.ObjClasses
 
         public override void addDefaultViewFilters( CswNbtViewRelationship ParentRelationship )
         {
+            CswNbtMetaDataObjectClassProp JobStateOcp = _CswNbtResources.MetaData.getObjectClassProp( JobState.ObjectClassPropId );
+            ParentRelationship.View.AddViewPropertyAndFilter( ParentRelationship, JobStateOcp, Value: StateOption.Closed, FilterMode: CswEnumNbtFilterMode.NotEquals, ShowAtRuntime: true );
+            
             _CswNbtObjClassDefault.addDefaultViewFilters( ParentRelationship );
         }
         
