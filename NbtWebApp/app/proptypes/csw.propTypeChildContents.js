e525185e96bcb8b9ae3449a290981b6a6cb10c93
Christopher Froehlich <cfroehlich@chemsw.com>
2013-04-17 20:01:56 +0400

    Buckeye. Case 29390. Child Contents. Renames. Review K5221.

--- NbtWebApp/app/proptypes/csw.propTypeChildContents.js
+++ NbtWebApp/app/proptypes/csw.propTypeChildContents.js
@@ -1,131 +1,94 @@
 /// <reference path="~/app/CswApp-vsdoc.js" />
+/* globals Csw:false, $:false  */
 
 (function () {
     'use strict';
-    Csw.properties.childContents = Csw.properties.childContents ||
-        Csw.properties.register('childContents',
-            Csw.method(function (propertyOption) {
+    Csw.properties.childContents = Csw.properties.register('childContents',
+        function(nodeProperty) {
+            'use strict';
+
+            //The render function to be executed as a callback
+            var render = function() {
                 'use strict';
-                var cswPrivate = {};
-                var cswPublic = {
-                    data: propertyOption || Csw.nbt.propertyOption(propertyOption)
+
+                var cswPrivate = Csw.object();
+
+                nodeProperty.propDivTbl = nodeProperty.propDiv.table();
+
+                var nsOptions = Csw.object();
+                nsOptions.name = Csw.string(nodeProperty.propData.name).trim();
+                nsOptions.selectedNodeId = Csw.string(nodeProperty.propData.values.relatednodeid).trim();
+                nsOptions.selectedNodeLink = Csw.string(nodeProperty.propData.values.relatednodelink).trim();
+                nsOptions.selectedName = Csw.string(nodeProperty.propData.values.relatednodename).trim();
+                nsOptions.nodeTypeId = Csw.string(nodeProperty.propData.values.nodetypeid).trim();
+                //nodeSelect.viewid = Csw.string(nodeProperty.propData.values.viewid).trim();
+                nsOptions.objectClassId = Csw.string(nodeProperty.propData.values.objectclassid).trim();
+                nsOptions.allowAdd = Csw.bool(nodeProperty.propData.values.allowadd);
+                nsOptions.options = nodeProperty.propData.values.options;
+                nsOptions.useSearch = false; // Csw.bool(nodeProperty.propData.values.usesearch);
+                nsOptions.cellCol = 1;
+                nsOptions.selectedNodeType = null;
+                nsOptions.addImage = null;
+                nsOptions.onSelectNode = function(nodeObj) {
+                    // Csw.tryExec(nodeProperty.onChange, nodeObj.nodeid);
+                    // nodeProperty.onPropChange({ nodeid: nodeObj.nodeid, name: nodeObj.name, relatednodeid: nodeObj.selectedNodeId, relatednodelink: nodeObj.relatednodelink });
+
+                    cswPrivate.loadNode(nodeObj.nodeid);
+
                 };
+                nsOptions.onAfterAdd = cswPrivate.loadNode;
+                nsOptions.relatedTo = {};
+                nsOptions.relatedTo.relatednodeid = nodeProperty.tabState.nodeid;
+                nsOptions.relatedTo.relatednodetypeid = nodeProperty.tabState.nodetypeid;
+                nsOptions.relatedTo.relatednodename = nodeProperty.tabState.nodename;
+                //nodeSelect.relatedTo.relatedobjectclassid = nodeProperty.tabState.relatedobjectclassid;
 
-                //The render function to be executed as a callback
-                var render = function () {
-                    'use strict';
+                nsOptions.isRequired = nodeProperty.isRequired();
+                nsOptions.isMulti = nodeProperty.isMulti();
+                nsOptions.isReadOnly = false; // nodeProperty.isReadOnly();
+                nsOptions.isClickable = nodeProperty.tabState.EditMode !== Csw.enums.editMode.AuditHistoryInPopup; //case 28180 - relationships not clickable from audit history popup
 
-                    cswPrivate.propVals = cswPublic.data.propData.values;
-                    cswPrivate.parent = cswPublic.data.propDiv;
-                    cswPrivate.parentTbl = cswPrivate.parent.table();
+                nsOptions.doGetNodes = false;
+                nsOptions.showSelectOnLoad = true;
 
-                    var nodeSelect = {};
-                    nodeSelect.name = Csw.string(cswPublic.data.propData.name).trim();
-                    nodeSelect.selectedNodeId = Csw.string(cswPrivate.propVals.relatednodeid).trim();
-                    nodeSelect.selectedNodeLink = Csw.string(cswPrivate.propVals.relatednodelink).trim();
-                    nodeSelect.selectedName = Csw.string(cswPrivate.propVals.relatednodename).trim();
-                    nodeSelect.nodeTypeId = Csw.string(cswPrivate.propVals.nodetypeid).trim();
-                    //nodeSelect.viewid = Csw.string(cswPrivate.propVals.viewid).trim();
-                    nodeSelect.objectClassId = Csw.string(cswPrivate.propVals.objectclassid).trim();
-                    nodeSelect.allowAdd = Csw.bool(cswPrivate.propVals.allowadd);
-                    nodeSelect.options = cswPrivate.propVals.options;
-                    nodeSelect.useSearch = false; // Csw.bool(cswPrivate.propVals.usesearch);
-                    nodeSelect.cellCol = 1;
-                    nodeSelect.selectedNodeType = null;
-                    nodeSelect.addImage = null;
-                    nodeSelect.onSelectNode = function (nodeObj) {
-                        // Csw.tryExec(cswPublic.data.onChange, nodeObj.nodeid);
-                        // cswPublic.data.onPropChange({ nodeid: nodeObj.nodeid, name: nodeObj.name, relatednodeid: nodeObj.selectedNodeId, relatednodelink: nodeObj.relatednodelink });
+                var nodeSelect = nodeProperty.propDivTbl.cell(1, 1).nodeSelect(nsOptions);
 
-                        cswPrivate.loadNode(nodeObj.nodeid);
+                var editLink = nodeProperty.propDivTbl.cell(1, 2).buttonExt({
+                    name: 'childContentsEdit',
+                    icon: Csw.enums.getName(Csw.enums.iconType, Csw.enums.iconType.pencil),
+                    size: 'small',
+                    enabledText: 'Edit Selected',
+                    onClick: function() {
+                        var nodeid = nodeSelect.selectedNodeId();
+                        $.CswDialog('EditNodeDialog', {
+                            currentNodeId: nodeid,
+                            nodenames: [nodeSelect.selectedName()],
+                            onEditNode: function() {
+                                // refresh
+                                cswPrivate.loadNode(nodeid);
+                            }
+                        }); // CswDialog
+                    } // onClick
+                }); // link
+                cswPrivate.childContentsDiv = nodeProperty.propDiv.div();
 
-                    };
-                    nodeSelect.onAfterAdd = cswPrivate.loadNode;
-                    nodeSelect.relatedTo = {};
-                    nodeSelect.relatedTo.relatednodeid = cswPublic.data.tabState.nodeid;
-                    nodeSelect.relatedTo.relatednodetypeid = cswPublic.data.tabState.nodetypeid;
-                    nodeSelect.relatedTo.relatednodename = cswPublic.data.tabState.nodename;
-                    //nodeSelect.relatedTo.relatedobjectclassid = cswPublic.data.tabState.relatedobjectclassid;
+                if (false === Csw.isNullOrEmpty(nsOptions.selectedNodeId)) {
+                    cswPrivate.loadNode(nsOptions.selectedNodeId, editLink);
+                } else {
+                    editLink.hide();
+                }
+            }; // render()
 
-                    nodeSelect.isRequired = cswPublic.data.isRequired();
-                    nodeSelect.isMulti = cswPublic.data.isMulti();
-                    nodeSelect.isReadOnly = false; // cswPublic.data.isReadOnly();
-                    nodeSelect.isClickable = cswPublic.data.tabState.EditMode !== Csw.enums.editMode.AuditHistoryInPopup; //case 28180 - relationships not clickable from audit history popup
+            
 
-                    nodeSelect.doGetNodes = false;
-                    nodeSelect.showSelectOnLoad = true;
+            //Bind the callback to the render event
+            nodeProperty.bindRender(render);
 
-                    cswPublic.control = cswPrivate.parentTbl.cell(1, 1).nodeSelect(nodeSelect);
+            //Bind an unrender callback to terminate any outstanding ajax requests, if any. See propTypeGrid.
+            //nodeProperty.unBindRender();
 
-                    cswPublic.editLink = cswPrivate.parentTbl.cell(1, 2).buttonExt({
-                        name: 'childContentsEdit',
-                        icon: Csw.enums.getName(Csw.enums.iconType, Csw.enums.iconType.pencil),
-                        size: 'small',
-                        enabledText: 'Edit Selected',
-                        onClick: function () {
-                            var nodeid = cswPublic.control.selectedNodeId();
-                            $.CswDialog('EditNodeDialog', {
-                                currentNodeId: nodeid,
-                                nodenames: [cswPublic.control.selectedName()],
-                                onEditNode: function () {
-                                    // refresh
-                                    cswPrivate.loadNode(nodeid);
-                                }
-                            }); // CswDialog
-                        } // onClick
-                    }); // link
-                    cswPrivate.childContentsDiv = cswPrivate.parent.div();
-
-                    if (false === Csw.isNullOrEmpty(nodeSelect.selectedNodeId)) {
-                        cswPrivate.loadNode(nodeSelect.selectedNodeId);
-                    } else {
-                        cswPublic.editLink.hide();
-                    }
-                }; // render()
-
-                cswPrivate.loadNode = function (nodeid) {
-                    cswPublic.editLink.show();
-                    cswPrivate.childContentsDiv.empty();
-
-                    Csw.layouts.tabsAndProps(cswPrivate.childContentsDiv, {
-                        name: 'tabsAndProps',
-                        globalState: {
-                            //propertyData: cswDlgPrivate.propertyData,
-                            ShowAsReport: false,
-                            currentNodeId: nodeid
-                        },
-                        tabState: {
-                            //                            nodetypeid: cswDlgPrivate.nodetypeid,
-                            //                            relatednodeid: cswDlgPrivate.relatednodeid,
-                            //                            relatednodename: cswDlgPrivate.relatednodename,
-                            //                            relatednodetypeid: cswDlgPrivate.relatednodetypeid,
-                            //                            relatedobjectclassid: cswDlgPrivate.relatedobjectclassid,
-                            EditMode: Csw.enums.editMode.Edit,
-                            ReadOnly: true,
-                            showSaveButton: false
-                        },
-                        showTitle: false,
-                        //ReloadTabOnSave: false,
-                        //onSave: function (nodeid, nodekey, tabcount, nodename) {
-                        //                            cswPublic.close();
-                        //                            cswPublic.div.$.dialog('close');
-                        //                            Csw.tryExec(cswDlgPrivate.onAddNode, nodeid, nodekey, nodename);
-                        //                            Csw.tryExec(cswDlgPrivate.onSaveImmediate);
-                        //                        },
-                        onInitFinish: function () {
-                            //openDialog(cswPublic.div, 800, 600, null, cswPublic.title);
-                        }
-                    });
-                }; // loadNode()
-
-                //Bind the callback to the render event
-                cswPublic.data.bindRender(render);
-
-                //Bind an unrender callback to terminate any outstanding ajax requests, if any. See propTypeGrid.
-                //cswPublic.data.unBindRender();
-
-                return cswPublic;
-            }));
+            return true;
+        });
 
 } ());
 
