b5b736577ef9ab5754f3c6829f93effe80b092fd
Christopher Froehlich <cfroehlich@chemsw.com>
2013-04-17 23:14:35 +0400

    Buckeye. Case 29390. Image. Review K5221.

--- NbtWebApp/app/proptypes/csw.propTypeImage.js
+++ NbtWebApp/app/proptypes/csw.propTypeImage.js
@@ -1,155 +1,173 @@
 /// <reference path="~/app/CswApp-vsdoc.js" />
-
+/* globals Csw:false, $:false  */
 
 (function () {
     'use strict';
-    Csw.properties.image = Csw.properties.image ||
-        Csw.properties.register('image',
-            Csw.method(function (propertyOption) {
+    Csw.properties.image = Csw.properties.register('image',
+        function(nodeProperty) {
+            'use strict';
+
+            var eventName = 'onChangeImage_' + nodeProperty.propid;
+
+            //The render function to be executed as a callback
+            var render = function() {
                 'use strict';
-                var cswPrivate = {
-                    width: ''
-                };
-                var cswPublic = {
-                    data: propertyOption
-                };
 
-                //The render function to be executed as a callback
-                var render = function () {
-                    'use strict';
-                    cswPublic.data = cswPublic.data || Csw.nbt.propertyOption(propertyOption);
+                var cswPrivate = Csw.object();
+                
+                if (nodeProperty.isMulti()) {
+                    nodeProperty.propDiv.append('[Image display disabled]');
+                } else {
 
-                    cswPrivate.propVals = cswPublic.data.propData.values;
-                    cswPrivate.parent = cswPublic.data.propDiv;
+                    cswPrivate.href = nodeProperty.propData.values.href;
+                    cswPrivate.fileName = nodeProperty.propData.values.name;
+                    
+                    var width = 100;
+                    if (nodeProperty.propData.values.width > 0) {
+                        width = Math.abs(Csw.number(nodeProperty.propData.values.width, 100) - 36);
+                    } 
+                    var height = 100;
+                    if (nodeProperty.propData.values.height > 0) {
+                        height = nodeProperty.propData.values.height;
+                    }
+                    
+                    var table = nodeProperty.propDiv.table();
+                    cswPrivate.cell11 = table.cell(1, 1).propDom('colspan', '3');
+                    cswPrivate.cell21 = table.cell(2, 1).propDom('width', width);
+                    cswPrivate.cell22 = table.cell(2, 2).propDom({ align: 'right', width: '20px' }).div();
+                    cswPrivate.cell23 = table.cell(2, 3).propDom({ align: 'right', width: '20px' }).div();
 
-                    if (cswPublic.data.isMulti()) {
-                        cswPublic.control = cswPrivate.parent.append('[Image display disabled]');
-                    } else {
-
-                        cswPrivate.href = Csw.hrefString(cswPrivate.propVals.href);
-
-                        if (false === Csw.isNullOrEmpty(cswPrivate.propVals.width) &&
-                            Csw.isNumeric(cswPrivate.propVals.width)) {
-                            cswPrivate.width = Math.abs(Csw.number(cswPrivate.propVals.width, 100) - 36);
+                    Csw.properties.subscribe(eventName, function(eventObj, val) {
+                        if (cswPrivate.fileName !== val.name || cswPrivate.href != val.href) {
+                            updateProp(val);
                         }
+                    });
 
-                        cswPrivate.fileName = Csw.string(cswPrivate.propVals.name).trim();
+                    var updateProp = function(val) {
+                        cswPrivate.fileName = val.name;
+                        cswPrivate.href = val.href;
 
-                        cswPublic.control = cswPrivate.parent.table();
-                        cswPrivate.cell11 = cswPublic.control.cell(1, 1).propDom('colspan', '3');
-                        cswPrivate.cell21 = cswPublic.control.cell(2, 1).propDom('width', cswPrivate.width);
-                        cswPrivate.cell22 = cswPublic.control.cell(2, 2).propDom({ align: 'right', width: '20px' }).div();
-                        cswPrivate.cell23 = cswPublic.control.cell(2, 3).propDom({ align: 'right', width: '20px' }).div();
+                        nodeProperty.propData.values.href = val.href;
+                        nodeProperty.propData.values.fileName = val.name;
 
-                        cswPrivate.makeClr = function () {
-                            cswPrivate.cell23.empty();
-                            if (false === Csw.isNullOrEmpty(cswPrivate.fileName)) {
-                                //Clear button
-                                cswPrivate.cell23.icon({
-                                    name: 'clear',
-                                    iconType: Csw.enums.iconType.trash,
-                                    hovertext: 'Clear Image',
-                                    size: 16,
-                                    isButton: true,
-                                    onClick: function () {
-                                        /* remember: confirm is globally blocking call */
-                                        if (confirm("Are you sure you want to clear this image?")) {
-                                            var dataJson = {
-                                                propid: cswPublic.data.propData.id,
-                                                IncludeBlob: true
-                                            };
+                        makeImg(val);
+                    };
 
-                                            Csw.ajaxWcf.post({
-                                                urlMethod: 'BlobData/clearBlob',
-                                                data: dataJson,
-                                                success: function () {
-                                                    var val = {
-                                                        href: '',
-                                                        name: '',
-                                                        contenttype: ''
-                                                    };
-                                                    cswPrivate.makeImg(null);
-                                                    cswPublic.data.onPropChange(val);
-                                                    Csw.publish(Csw.enums.events.main.refreshSelected, {});
-                                                }
-                                            });
-                                        }
-                                    }
-                                }); // icon
-                            } // if (false === Csw.isNullOrEmpty(fileName)) {
-                        };
+                    var broadcastUpdate = function(val) {
+                        updateProp(val);
+                        Csw.properties.publish(eventName, val);
+                        //nodeProperty.onPropChange(val);
+                        //Csw.publish(Csw.enums.events.main.refreshSelected, {});
+                    };
 
-                        cswPrivate.makeImg = function (imgData) {
-                            cswPrivate.cell11.empty();
-                            cswPrivate.cell21.empty();
-                            if (false == Csw.isNullOrEmpty(imgData)) {
-                                cswPrivate.fileName = imgData.fileName;
-                                cswPrivate.cell11.a({
-                                    href: imgData.href,
-                                    target: '_blank'
-                                })
-                                    .img({
-                                        src: imgData.href,
-                                        alt: imgData.fileName,
-                                        height: cswPrivate.propVals.height,
-                                        width: cswPrivate.propVals.width
-                                    });
-                                cswPrivate.cell21.a({
-                                    href: imgData.href,
-                                    target: '_blank',
-                                    text: imgData.fileName
-                                });
-                            }
-                            cswPrivate.makeClr();
-                        };
-                        cswPrivate.makeImg(cswPrivate);
-
-                        if (false === cswPublic.data.isReadOnly()) {
+                    var makeClr = function() {
+                        cswPrivate.cell23.empty();
+                        if (cswPrivate.fileName) {
                             //Clear button
-                            cswPrivate.makeClr();
-
-                            //Edit button
-                            cswPrivate.cell22.icon({
-                                name: 'edit',
-                                iconType: Csw.enums.iconType.pencil,
-                                hovertext: 'Edit',
+                            cswPrivate.cell23.icon({
+                                name: 'clear',
+                                iconType: Csw.enums.iconType.trash,
+                                hovertext: 'Clear Image',
                                 size: 16,
                                 isButton: true,
-                                onClick: function () {
-                                    $.CswDialog('FileUploadDialog', {
-                                        urlMethod: 'Services/BlobData/SaveFile',
-                                        params: {
-                                            propid: cswPublic.data.propData.id
-                                        },
-                                        onSuccess: function (data) {
-                                            var val = {
+                                onClick: function() {
+                                    /* remember: confirm is globally blocking call */
+                                    if (confirm("Are you sure you want to clear this image?")) {
+                                        
+                                        Csw.ajaxWcf.post({
+                                            urlMethod: 'BlobData/clearBlob',
+                                            data: {
+                                                propid: nodeProperty.propData.id,
+                                                IncludeBlob: true
+                                            },
+                                            success: function() {
+                                                broadcastUpdate({
+                                                    href: nodeProperty.propData.values.placeholder,
+                                                    name: '',
+                                                    contenttype: ''
+                                                });
+                                            }
+                                        });
+                                    }
+                                }
+                            }); // icon
+                        } // if (false === Csw.isNullOrEmpty(fileName)) {
+                    };
+
+                    var makeImg = function(val) {
+                        cswPrivate.cell11.empty();
+                        cswPrivate.cell21.empty();
+                        if (val) {
+                            var href = val.href;
+                            if (href !== nodeProperty.propData.values.placeholder) {
+                                href = Csw.hrefString(val.href);
+                            }
+
+                            cswPrivate.fileName = val.fileName;
+                            cswPrivate.cell11.a({
+                                href: val.href,
+                                target: '_blank'
+                            })
+                                .img({
+                                    src: href,
+                                    alt: val.fileName,
+                                    height: height,
+                                    width: width
+                                });
+                            cswPrivate.cell21.a({
+                                href: href,
+                                target: '_blank',
+                                text: val.fileName
+                            });
+                        }
+                        makeClr();
+                    };
+
+                    makeImg(nodeProperty.propData.values);
+
+                    if (false === nodeProperty.isReadOnly()) {
+                        //Clear button
+                        makeClr();
+
+                        //Edit button
+                        cswPrivate.cell22.icon({
+                            name: 'edit',
+                            iconType: Csw.enums.iconType.pencil,
+                            hovertext: 'Edit',
+                            size: 16,
+                            isButton: true,
+                            onClick: function() {
+                                $.CswDialog('FileUploadDialog', {
+                                    urlMethod: 'Services/BlobData/SaveFile',
+                                    params: {
+                                        propid: nodeProperty.propData.id
+                                    },
+                                    onSuccess: function(data) {
+                                        if (data.Data.success) {
+                                            broadcastUpdate({
                                                 href: data.Data.href,
                                                 name: data.Data.filename,
                                                 fileName: data.Data.filename,
                                                 contenttype: data.Data.contenttype
-                                            };
-                                            if (data.Data.success) {
-                                                cswPrivate.makeImg(val);
-                                                cswPublic.data.onPropChange(val);
-                                            }
+                                            });
                                         }
-                                    });
-                                }
-                            }); // icon
+                                    }
+                                });
+                            }
+                        }); // icon
+                    } // if (false === o.ReadOnly && o.EditMode !== Csw.enums.editMode.Add) {
+                }
+            };
 
-                        } // if (false === o.ReadOnly && o.EditMode !== Csw.enums.editMode.Add) {
-                    }
+            //Bind the callback to the render event
+            nodeProperty.bindRender(render);
 
+            //Bind an unrender callback to terminate any outstanding ajax requests, if any. See propTypeGrid.
+            nodeProperty.unBindRender(function() {
+                Csw.properties.unsubscribe(eventName);
+            });
 
-                };
-
-                //Bind the callback to the render event
-                cswPublic.data.bindRender(render);
-
-                //Bind an unrender callback to terminate any outstanding ajax requests, if any. See propTypeGrid.
-                //cswPublic.data.unBindRender();
-
-                return cswPublic;
-            }));
+            return true;
+        });
 
 } ());
