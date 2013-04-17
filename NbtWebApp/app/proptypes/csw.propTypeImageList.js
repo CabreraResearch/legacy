6593accedd6bc43d66cf62accdaddca64f83a357
Christopher Froehlich <cfroehlich@chemsw.com>
2013-04-17 23:49:38 +0400

    Buckeye. Case 29390. Image List. Review K5221.

--- NbtWebApp/app/proptypes/csw.propTypeImageList.js
+++ NbtWebApp/app/proptypes/csw.propTypeImageList.js
@@ -3,56 +3,50 @@
 
 (function () {
     'use strict';
-    Csw.properties.imageList = Csw.properties.imageList ||
-        Csw.properties.register('imageList', Csw.method(function (propertyOption) {
+    Csw.properties.imageList = Csw.properties.register('imageList',
+        function(nodeProperty) {
             'use strict';
             var cswPrivate = {};
-            var cswPublic = {
-                data: propertyOption
-            };
-
+            
             //The render function to be executed as a callback
-            var render = function () {
+            var render = function() {
                 'use strict';
-                cswPublic.data = cswPublic.data || Csw.nbt.propertyOption(propertyOption);
 
-                cswPrivate.propVals = cswPublic.data.propData.values;
-                cswPrivate.parent = cswPublic.data.propDiv;
-                cswPrivate.value = Csw.string(cswPrivate.propVals.value).trim();
-                cswPrivate.options = cswPrivate.propVals.options;
-                cswPrivate.width = Csw.string(cswPrivate.propVals.width);
-                cswPrivate.height = Csw.string(cswPrivate.propVals.height);
-                cswPrivate.allowMultiple = Csw.bool(cswPrivate.propVals.allowmultiple);
+                cswPrivate.value = nodeProperty.propData.values.value;
+                cswPrivate.options = nodeProperty.propData.values.options;
+                cswPrivate.width = nodeProperty.propData.values.width;
+                cswPrivate.height = nodeProperty.propData.values.height;
+                cswPrivate.allowMultiple = nodeProperty.propData.values.allowmultiple;
 
-                cswPublic.control = cswPrivate.parent.table({
+                var table = nodeProperty.propDiv.table({
                     cellvalign: 'top'
                 });
 
-                cswPrivate.imageTable = cswPublic.control.cell(1, 1).table();
+                cswPrivate.imageTable = table.cell(1, 1).table();
                 cswPrivate.imgTblCol = 1;
                 cswPrivate.selectedValues = [];
 
-                cswPrivate.addImage = function (name, href, doAnimation) {
+                cswPrivate.addImage = function(name, href, doAnimation) {
                     var imageCell = cswPrivate.imageTable.cell(1, cswPrivate.imgTblCol)
-                            .css({
-                                'text-align': 'center',
-                                'padding-left': '10px'
-                            });
+                        .css({
+                            'text-align': 'center',
+                            'padding-left': '10px'
+                        });
                     imageCell.a({
                         href: href,
                         target: '_blank'
                     })
-                            .img({
-                                src: href,
-                                alt: name,
-                                width: cswPrivate.width,
-                                height: cswPrivate.height
-                            });
+                        .img({
+                            src: href,
+                            alt: name,
+                            width: cswPrivate.width,
+                            height: cswPrivate.height
+                        });
                     var nameCell = cswPrivate.imageTable.cell(2, cswPrivate.imgTblCol)
-                            .css({
-                                'text-align': 'center',
-                                'padding-left': '10px'
-                            });
+                        .css({
+                            'text-align': 'center',
+                            'padding-left': '10px'
+                        });
                     cswPrivate.imgTblCol += 1;
 
                     if (doAnimation) {
@@ -63,21 +57,21 @@
                     if (name !== href) {
                         nameCell.a({ href: href, target: '_blank', text: name });
                     }
-                    if (false === cswPublic.data.isReadOnly() && (false === cswPublic.data.isRequired() || cswPrivate.allowMultiple)) {
+                    if (false === nodeProperty.isReadOnly() && (false === nodeProperty.isRequired() || cswPrivate.allowMultiple)) {
                         nameCell.icon({
                             name: 'rembtn',
                             iconType: Csw.enums.iconType.trash,
                             hovertext: 'Remove',
                             size: 16,
                             isButton: true,
-                            onClick: function () {
+                            onClick: function() {
                                 nameCell.$.fadeOut('fast');
                                 imageCell.$.fadeOut('fast');
                                 cswPrivate.removeValue(href);
                                 if (cswPrivate.allowMultiple) {
                                     cswPrivate.imageSelectList.addOption(name, href);
                                 }
-                                Csw.tryExec(cswPublic.data.onChange);
+                                //Csw.tryExec(nodeProperty.onChange);
                             } // onClick
                         }); //
                     } // if(!o.ReadOnly)
@@ -88,19 +82,22 @@
                     }
                 }; // addImage()
 
-                cswPrivate.saveProp = function () {
-                    cswPublic.data.onPropChange({ value: cswPrivate.selectedValues.join('\n') });
+                cswPrivate.saveProp = function() {
+                    //Case 29390: No sync for Image List
+                    nodeProperty.propData.values.value = cswPrivate.selectedValues.join('\n');
+
+                    //nodeProperty.onPropChange({ value: cswPrivate.selectedValues.join('\n') });
                 };
 
-                cswPrivate.addValue = function (valueToAdd) {
+                cswPrivate.addValue = function(valueToAdd) {
                     if (false === Csw.contains(cswPrivate.selectedValues, valueToAdd) &&
-                            false === Csw.isNullOrEmpty(valueToAdd)) {
+                        false === Csw.isNullOrEmpty(valueToAdd)) {
                         cswPrivate.selectedValues.push(valueToAdd);
                     }
                     cswPrivate.saveProp();
                 };
 
-                cswPrivate.removeValue = function (valueToRemove) {
+                cswPrivate.removeValue = function(valueToRemove) {
                     var idx = cswPrivate.selectedValues.indexOf(valueToRemove);
                     if (idx !== -1) {
                         cswPrivate.selectedValues.splice(idx, 1);
@@ -108,8 +105,8 @@
                     cswPrivate.saveProp();
                 };
 
-                cswPrivate.imageSelectList = cswPublic.control.cell(1, 2).imageSelect({
-                    onSelect: function (name, href, id, imageCell, nameCell) {
+                cswPrivate.imageSelectList = table.cell(1, 2).imageSelect({
+                    onSelect: function(name, href, id, imageCell, nameCell) {
                         if (false === cswPrivate.allowMultiple) {
                             cswPrivate.imageTable.empty();
                             cswPrivate.selectedValues = [];
@@ -120,28 +117,28 @@
                             nameCell.hide();
                         }
                         cswPrivate.addImage(name, href, true);
-                        Csw.tryExec(cswPublic.data.onChange, cswPrivate.selectedValues);
+                        Csw.tryExec(nodeProperty.onChange, cswPrivate.selectedValues);
                         return false;
                     }
                 });
 
-                Csw.eachRecursive(cswPrivate.options, function (thisOpt) {
+                Csw.iterate(cswPrivate.options, function(thisOpt) {
                     if (Csw.bool(thisOpt.selected)) {
                         cswPrivate.selectedValues.push(thisOpt.value);
                         cswPrivate.addImage(thisOpt.text, thisOpt.value, false);
                     } else {
-                        if (false === cswPublic.data.isReadOnly()) {
+                        if (false === nodeProperty.isReadOnly()) {
                             cswPrivate.imageSelectList.addOption(thisOpt.text, thisOpt.value);
                         }
                     }
-                }, false);
+                }, true);
 
-                if (cswPublic.data.isReadOnly()) {
+                if (nodeProperty.isReadOnly()) {
                     cswPrivate.imageSelectList.hide();
                 }
-                cswPrivate.imageSelectList.required(cswPublic.data.isRequired());
-                if (cswPublic.data.isRequired()) {
-                    $.validator.addMethod('imageRequired', function (value, element) {
+                cswPrivate.imageSelectList.required(nodeProperty.isRequired());
+                if (nodeProperty.isRequired()) {
+                    $.validator.addMethod('imageRequired', function(value, element) {
                         return (cswPrivate.selectedValues.length > 0);
                     }, 'An image is required.');
                     cswPrivate.imageSelectList.addClass('imageRequired');
@@ -150,13 +147,13 @@
             };
 
             //Bind the callback to the render event
-            cswPublic.data.bindRender(render);
+            nodeProperty.bindRender(render);
 
             //Bind an unrender callback to terminate any outstanding ajax requests, if any. See propTypeGrid.
-            //cswPublic.data.unBindRender();
+            //nodeProperty.unBindRender();
 
-            return cswPublic;
-        }));
+            return true;
+        });
 
 } ());
 
