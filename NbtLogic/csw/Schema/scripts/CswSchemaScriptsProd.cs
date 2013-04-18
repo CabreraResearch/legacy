cb9222c24bf962d72cbbd46029f9332aca03e44f
bvavra@chemsw.com <unknown@kiln.example.com>
2013-04-19 00:38:27 +0400

    Buckeye - Case 29401, Review K5211 - Updated Assigned SDS grid

--- NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
+++ NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
@@ -53,6 +53,7 @@ namespace ChemSW.Nbt.Schema
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_02B_Case28899_01() ) );               // 02A-026   02B-006
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_02B_Case29287() ) );                  // 02A-027   02B-007
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_02B_Case25871() ) );                  // 02A-028   02B-008
+            _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_02B_Case29401() ) );                  // 02A-029   02B-009
 
             #endregion BUCKEYE
 
---
+++ NbtLogic/csw/Schema/scripts/prod/02B/CswUpdateSchema_02B_Case29401.cs
@@ -0,0 +1,73 @@
+﻿using ChemSW.Core;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.ObjClasses;
+using ChemSW.Nbt.csw.Dev;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update for case 29401
+    /// </summary>
+    public class CswUpdateSchema_02B_Case29401 : CswUpdateSchemaTo
+    {
+        public override CswEnumDeveloper Author
+        {
+            get { return CswEnumDeveloper.BV; }
+        }
+
+        public override int CaseNo
+        {
+            get { return 29401; }
+        }
+
+        public override void update()
+        {
+            CswNbtMetaDataNodeType SDSDocumentNT = _CswNbtSchemaModTrnsctn.MetaData.getNodeType( "SDS Document" );
+            if( null != SDSDocumentNT )
+            {
+                CswNbtMetaDataObjectClass MaterialOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswEnumNbtObjectClass.MaterialClass );
+                foreach( CswNbtMetaDataNodeType MaterialNT in MaterialOC.getNodeTypes() )
+                {
+                    if( MaterialNT.NodeTypeName == "Chemical" )
+                    {
+                        CswNbtMetaDataNodeTypeProp AssignedSDSNTP = MaterialNT.getNodeTypeProp( "Assigned SDS" );
+                        if( null != AssignedSDSNTP )
+                        {
+                            CswNbtView AssignedSDSView = _CswNbtSchemaModTrnsctn.ViewSelect.restoreView( AssignedSDSNTP.ViewId );
+                            AssignedSDSView.Root.ChildRelationships.Clear();
+                            CswNbtViewRelationship RootRel = AssignedSDSView.AddViewRelationship( MaterialNT, false );
+                            CswNbtMetaDataNodeTypeProp OwnerOCP = SDSDocumentNT.getNodeTypePropByObjectClassProp( CswNbtObjClassDocument.PropertyName.Owner );
+                            CswNbtViewRelationship DocRel = AssignedSDSView.AddViewRelationship( RootRel, CswEnumNbtViewPropOwnerType.Second, OwnerOCP, true );
+                            CswNbtMetaDataNodeTypeProp ArchivedNTP = SDSDocumentNT.getNodeTypePropByObjectClassProp( CswNbtObjClassDocument.PropertyName.Archived );
+                            AssignedSDSView.AddViewPropertyAndFilter( DocRel, ArchivedNTP, CswEnumTristate.False.ToString(),
+                                                             FilterMode: CswEnumNbtFilterMode.Equals,
+                                                             ShowAtRuntime: true,
+                                                             ShowInGrid: false );
+                            CswNbtMetaDataNodeTypeProp RevisionDateNTP = SDSDocumentNT.getNodeTypeProp( "Revision Date" );
+                            if( null != RevisionDateNTP )
+                            {
+                                CswNbtViewProperty RevisionDateVP = AssignedSDSView.AddViewProperty( DocRel, RevisionDateNTP );
+                                RevisionDateVP.Order = 1;
+                            }
+                            CswNbtMetaDataNodeTypeProp FileNTP = SDSDocumentNT.getNodeTypePropByObjectClassProp( CswNbtObjClassDocument.PropertyName.File );
+                            CswNbtViewProperty FileVP = AssignedSDSView.AddViewProperty( DocRel, FileNTP );
+                            FileVP.Order = 2;
+                            CswNbtMetaDataNodeTypeProp LinkNTP = SDSDocumentNT.getNodeTypePropByObjectClassProp( CswNbtObjClassDocument.PropertyName.Link );
+                            CswNbtViewProperty LinkVP = AssignedSDSView.AddViewProperty( DocRel, LinkNTP );
+                            LinkVP.Order = 3;
+                            CswNbtMetaDataNodeTypeProp LanguageNTP = SDSDocumentNT.getNodeTypePropByObjectClassProp( CswNbtObjClassDocument.PropertyName.Language );
+                            CswNbtViewProperty LanguageVP = AssignedSDSView.AddViewProperty( DocRel, LanguageNTP );
+                            LanguageVP.Order = 4;
+                            CswNbtMetaDataNodeTypeProp FormatNTP = SDSDocumentNT.getNodeTypePropByObjectClassProp( CswNbtObjClassDocument.PropertyName.Format );
+                            CswNbtViewProperty FormatVP = AssignedSDSView.AddViewProperty( DocRel, FormatNTP );
+                            FormatVP.Order = 5;
+                            AssignedSDSView.save();
+                        }
+                    }
+                }
+            }
+        } // update()
+
+    }//class CswUpdateSchema_02B_Case29401
+
+}//namespace ChemSW.Nbt.Schema
