ebb867d6089f10f4c703d33ea9cb5f3fb2127ae2
Christopher Froehlich <cfroehlich@chemsw.com>
2013-04-19 00:47:10 +0400

    Buckeye. Case 29390. Review K5220. Quantity.

--- NbtWebApp/app/proptypes/csw.propTypeQuantity.js
+++ NbtWebApp/app/proptypes/csw.propTypeQuantity.js
@@ -1,109 +1,101 @@
 /// <reference path="~/app/CswApp-vsdoc.js" />
-
+/* globals Csw:false, $:false  */
 
 (function () {
     'use strict';
-    Csw.properties.quantity = Csw.properties.quantity ||
-        Csw.properties.register('quantity',
-            Csw.method(function (propertyOption) {
+    Csw.properties.quantity = Csw.properties.register('quantity',
+        function(nodeProperty) {
+            'use strict';
+            
+            var cswPublic = {
+                
+            };
+
+            //The render function to be executed as a callback
+            var render = function() {
                 'use strict';
-                var cswPrivate = {};
-                var cswPublic = {
-                    data: propertyOption || Csw.nbt.propertyOption(propertyOption)
+                var cswPrivate = Csw.object();
+
+                cswPrivate.selectedName = nodeProperty.propData.values.name;
+                cswPrivate.cellCol = 1;
+
+                var updateProp = function() {
+                    nodeProperty.propData.values.value = cswPrivate.quntCtrl.value();
+                    nodeProperty.propData.values.nodeid = cswPrivate.quntCtrl.selectedUnit();
+                    nodeProperty.broadcastPropChange();
                 };
 
-                //The render function to be executed as a callback
-                var render = function () {
-                    'use strict';
+                var quantity = {};
+                quantity.precision = nodeProperty.propData.values.precision;
+                quantity.ceilingVal = Csw.number('999999999' + Csw.getMaxValueForPrecision(cswPrivate.precision));
+                quantity.selectedNodeId = nodeProperty.propData.values.relatednodeid;
+                quantity.unit = nodeProperty.propData.values.name;
+                quantity.nodeTypeId = nodeProperty.propData.values.nodetypeid;
+                quantity.objectClassId = nodeProperty.propData.values.objectclassid;
+                quantity.options = nodeProperty.propData.values.options;
+                quantity.relationships = [];
+                quantity.quantity = nodeProperty.propData.values.value;
+                quantity.minvalue = Csw.number(nodeProperty.propData.values.minvalue);
+                quantity.maxvalue = Csw.number(nodeProperty.propData.values.maxvalue);
+                quantity.excludeRangeLimits = nodeProperty.propData.values.excludeRangeLimits;
+                quantity.fractional = nodeProperty.propData.values.fractional;
+                quantity.quantityoptional = nodeProperty.propData.values.quantityoptional;
+                quantity.cellCol = 1;
+                quantity.name = nodeProperty.name;
+                quantity.onChange = nodeProperty.onChange;
+                quantity.isMulti = nodeProperty.isMulti();
+                quantity.isRequired = nodeProperty.isRequired();
+                quantity.isReadOnly = nodeProperty.isReadOnly();
+                quantity.propVals = nodeProperty.propData.values;
+                quantity.onNumberChange = function() {
+                    updateProp();
+                };
 
-                    cswPrivate.propVals = cswPublic.data.propData.values;
-                    cswPrivate.parent = cswPublic.data.propDiv;
 
-                    var quantity = {};
-                    quantity.precision = Csw.number(cswPrivate.propVals.precision, 6);
-                    quantity.ceilingVal = Csw.number('999999999' + Csw.getMaxValueForPrecision(cswPrivate.precision));
-                    quantity.selectedNodeId = Csw.string(cswPrivate.propVals.relatednodeid).trim();
-                    cswPrivate.selectedName = Csw.string(cswPrivate.propVals.name).trim();
-                    quantity.unit = Csw.string(cswPrivate.propVals.name).trim();
-                    quantity.nodeTypeId = Csw.string(cswPrivate.propVals.nodetypeid).trim();
-                    quantity.objectClassId = Csw.string(cswPrivate.propVals.objectclassid).trim();
-                    quantity.options = cswPrivate.propVals.options;
-                    quantity.relationships = [];
-                    quantity.quantity = Csw.string(cswPrivate.propVals.value).trim();
-                    cswPrivate.cellCol = 1;
-                    quantity.minvalue = Csw.number(cswPrivate.propVals.minvalue);
-                    quantity.maxvalue = Csw.number(cswPrivate.propVals.maxvalue);
-                    quantity.excludeRangeLimits = Csw.bool(cswPrivate.propVals.excludeRangeLimits);
-                    quantity.fractional = Csw.bool(cswPrivate.propVals.fractional);
-                    quantity.quantityoptional = Csw.bool(cswPrivate.propVals.quantityoptional);
-                    quantity.cellCol = 1;
-                    quantity.name = cswPublic.data.name;
-                    quantity.onChange = cswPublic.data.onChange;
-                    quantity.isMulti = cswPublic.data.isMulti();
-                    quantity.isRequired = cswPublic.data.isRequired();
-                    quantity.isReadOnly = cswPublic.data.isReadOnly();
-                    quantity.doPropChangeDataBind = cswPublic.data.doPropChangeDataBind;
-                    quantity.propVals = cswPrivate.propVals;
-                    quantity.onNumberChange = function () {
-                        var val = cswPrivate.quntCtrl.value();
-                        cswPublic.data.onPropChange({
-                            value: val,
-                            nodeid: cswPrivate.quntCtrl.selectedUnit()
-                        });
-                    };
-                    
-                    
-
-                    quantity.onQuantityChange = function () {
-                        var val = cswPrivate.quntCtrl.selectedUnit();
-                        Csw.iterate(quantity.options, function (relatedObj) {
-                            if (relatedObj.id === val) {
-                                quantity.fractional = Csw.bool(relatedObj.fractional);
-                            }
-                        });
-                        quantity.precision = false === cswPrivate.fractional ? 0 : Csw.number(cswPrivate.propVals.precision, 6);
-                        Csw.tryExec(quantity.onChange, val);
-                        cswPublic.data.onPropChange({
-                            value: cswPrivate.quntCtrl.value(),
-                            nodeid: val
-                        });
-                    };
-
-                    if (false === cswPrivate.fractional) {
-                        quantity.precision = 0;
-                    }
-                    if (cswPublic.data.isReadOnly()) {
-                        cswPublic.control = cswPrivate.parent.span({ text: cswPublic.data.propData.gestalt });
-                    } else {
-
-                        if (false === Csw.isNullOrEmpty(cswPublic.data.tabState.relatednodeid) &&
-                                                    Csw.isNullOrEmpty(cswPrivate.selectedNodeId) &&
-                                                    false === cswPublic.data.isMulti() &&
-                                                    (Csw.number(cswPublic.data.tabState.relatednodetypeid) === Csw.number(cswPrivate.nodeTypeId) ||
-                                                      Csw.number(cswPublic.data.tabState.relatedobjectclassid) === Csw.number(cswPrivate.objectClassId))) {
-                            cswPrivate.selectedNodeId = cswPublic.data.tabState.relatednodeid;
-                            cswPrivate.selectedName = cswPublic.data.tabState.relatednodename;
+                quantity.onQuantityChange = function() {
+                    var val = cswPrivate.quntCtrl.selectedUnit();
+                    Csw.iterate(quantity.options, function(relatedObj) {
+                        if (relatedObj.id === val) {
+                            quantity.fractional = Csw.bool(relatedObj.fractional);
                         }
-                        cswPublic.control = cswPrivate.parent.table();
-                        cswPrivate.quntCtrl = cswPublic.control.quantity(quantity);
-                        
-                        //Case 29098 - after rendering the ctrl, make sure the internal data is up to date with the selected options
-                        cswPublic.data.onPropChange({
-                            value: cswPrivate.quntCtrl.value(),
-                            nodeid: cswPrivate.quntCtrl.selectedUnit()
-                        });
-                    }
-
+                    });
+                    quantity.precision = false === cswPrivate.fractional ? 0 : nodeProperty.propData.values.precision;
+                    Csw.tryExec(quantity.onChange, val);
+                    updateProp();
                 };
 
-                //Bind the callback to the render event
-                cswPublic.data.bindRender(render);
+                if (false === cswPrivate.fractional) {
+                    quantity.precision = 0;
+                }
+                if (nodeProperty.isReadOnly()) {
+                    nodeProperty.propDiv.span({ text: nodeProperty.propData.gestalt });
+                } else {
 
-                //Bind an unrender callback to terminate any outstanding ajax requests, if any. See propTypeGrid.
-                //cswPublic.data.unBindRender();
+                    if (false === Csw.isNullOrEmpty(nodeProperty.tabState.relatednodeid) &&
+                        Csw.isNullOrEmpty(cswPrivate.selectedNodeId) &&
+                        false === nodeProperty.isMulti() &&
+                        (Csw.number(nodeProperty.tabState.relatednodetypeid) === Csw.number(cswPrivate.nodeTypeId) ||
+                            Csw.number(nodeProperty.tabState.relatedobjectclassid) === Csw.number(cswPrivate.objectClassId))) {
+                        cswPrivate.selectedNodeId = nodeProperty.tabState.relatednodeid;
+                        cswPrivate.selectedName = nodeProperty.tabState.relatednodename;
+                    }
+                    var table = nodeProperty.propDiv.table();
+                    cswPrivate.quntCtrl = table.quantity(quantity);
 
-                return cswPublic;
-            }));
+                    //Case 29098 - after rendering the ctrl, make sure the internal data is up to date with the selected options
+                    updateProp();
+                }
+
+            };
+
+            //Bind the callback to the render event
+            nodeProperty.bindRender(render);
+
+            //Bind an unrender callback to terminate any outstanding ajax requests, if any. See propTypeGrid.
+            //nodeProperty.unBindRender();
+
+            return true;
+        });
 
 } ());
         
