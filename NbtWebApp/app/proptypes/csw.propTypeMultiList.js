b8b0e42f133908e7051282d5e3f54c0541e6bd5f
Christopher Froehlich <cfroehlich@chemsw.com>
2013-04-18 22:33:44 +0400

    Buckeye. Case 29390. Review K5220. Multi List.

--- NbtWebApp/app/proptypes/csw.propTypeMultiList.js
+++ NbtWebApp/app/proptypes/csw.propTypeMultiList.js
@@ -2,60 +2,46 @@
 
 (function () {
     'use strict';
-    Csw.properties.multiList = Csw.properties.multiList ||
-        Csw.properties.register('multiList',
-            Csw.method(function (propertyOption) {
+    Csw.properties.multiList = Csw.properties.register('multiList',
+        function(nodeProperty) {
+            'use strict';
+
+            //The render function to be executed as a callback
+            var render = function() {
                 'use strict';
-                var cswPrivate = {};
-                var cswPublic = {
-                    data: propertyOption
-                };
-                //The render function to be executed as a callback
-                var render = function () {
-                    'use strict';
-                    cswPublic.data = cswPublic.data || Csw.nbt.propertyOption(propertyOption);
+                var cswPrivate = Csw.object();
+                
+                cswPrivate.gestalt = nodeProperty.propData.gestalt;
+                cswPrivate.options = nodeProperty.propData.values.options;
 
-                    cswPrivate.propVals = cswPublic.data.propData.values;
-                    cswPrivate.parent = cswPublic.data.propDiv;
-                    cswPrivate.gestalt = Csw.string(cswPublic.data.propData.gestalt).trim();
-                    cswPrivate.options = cswPrivate.propVals.options;
+                if (nodeProperty.isReadOnly()) {
+                    nodeProperty.propDiv.append(cswPrivate.gestalt);
+                } else {
+                    /* Select Box */
+                    nodeProperty.propDiv.multiSelect({
+                        name: nodeProperty.name,
+                        cssclass: 'selectinput',
+                        values: cswPrivate.options,
+                        readonlyless: nodeProperty.propData.values.readonlyless,
+                        readonlymore: nodeProperty.propData.values.readonlymore,
+                        isMultiEdit: nodeProperty.isMulti(),
+                        EditMode: nodeProperty.tabState.EditMode,
+                        onChange: function(sel, val) {
+                            //Case 29390: no sync for Multi List
+                            nodeProperty.propData.values.value = val;
+                        }
+                    });
+                }
 
-                    cswPrivate.sortVals = function() {
-                        //multiSelect sorts the val for us, sort values.value to make comparision work
-                        var uniqueVals = Csw.delimitedString(cswPrivate.propVals.value.split(','));
-                        cswPrivate.propVals.value = uniqueVals.toString();
-                    };
+            };
 
-                    if (cswPublic.data.isReadOnly()) {
-                        cswPublic.control = cswPrivate.parent.append(cswPrivate.gestalt);
-                    } else {
-                        /* Select Box */
-                        cswPublic.control = cswPrivate.parent.multiSelect({
-                            name: cswPublic.data.name,
-                            cssclass: 'selectinput',
-                            values: cswPrivate.options,
-                            readonlyless: cswPrivate.propVals.readonlyless,
-                            readonlymore: cswPrivate.propVals.readonlymore,
-                            isMultiEdit: cswPublic.data.isMulti(),
-                            EditMode: cswPublic.data.tabState.EditMode,
-                            onChange: function () {
-                               var val = cswPublic.control.val();
-                               Csw.tryExec(cswPublic.data.onChange, val);
-                               cswPrivate.sortVals();
-                               cswPublic.data.onPropChange({ value: val });
-                            }
-                        });
-                    }
+            //Bind the callback to the render event
+            nodeProperty.bindRender(render);
 
-                };
+            //Bind an unrender callback to terminate any outstanding ajax requests, if any. See propTypeGrid.
+            //nodeProperty.unBindRender();
 
-                //Bind the callback to the render event
-                cswPublic.data.bindRender(render);
-
-                //Bind an unrender callback to terminate any outstanding ajax requests, if any. See propTypeGrid.
-                //cswPublic.data.unBindRender();
-
-                return cswPublic;
-            }));
+            return true;
+        });
 
 }());
