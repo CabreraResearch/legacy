012e76145041830c0f8918be5cacb11e2a808e39
Christopher Froehlich <cfroehlich@chemsw.com>
2013-04-19 19:22:43 +0400

    Buckeye. Case 29390. Review K5220. View Reference.

--- NbtWebApp/app/proptypes/csw.propTypeViewReference.js
+++ NbtWebApp/app/proptypes/csw.propTypeViewReference.js
@@ -2,70 +2,62 @@
 
 (function () {
     'use strict';
-    Csw.properties.viewReference = Csw.properties.viewReference ||
-        Csw.properties.register('viewReference',
-            Csw.method(function (propertyOption) {
+    Csw.properties.viewReference = Csw.properties.register('viewReference',
+        function(nodeProperty) {
+            'use strict';
+
+            //The render function to be executed as a callback
+            var render = function() {
                 'use strict';
-                var cswPrivate = {};
-                var cswPublic = {
-                    data: propertyOption
-                };
+                var cswPrivate = Csw.object();
+                
+                cswPrivate.viewId = nodeProperty.propData.values.viewid;
+                cswPrivate.viewMode = nodeProperty.propData.values.viewmode;
 
-                //The render function to be executed as a callback
-                var render = function () {
-                    'use strict';
-                    cswPublic.data = cswPublic.data || Csw.nbt.propertyOption(propertyOption);
+                var table = nodeProperty.propDiv.table();
 
-                    cswPrivate.propVals = cswPublic.data.propData.values;
-                    cswPrivate.parent = cswPublic.data.propDiv;
-                    cswPrivate.viewId = Csw.string(cswPrivate.propVals.viewid).trim();
-                    cswPrivate.viewMode = Csw.string(cswPrivate.propVals.viewmode).trim().toLowerCase();
+                if (false === nodeProperty.isMulti()) {
+                    table.cell(1, 1).$.CswViewContentTree({
+                        viewid: cswPrivate.viewId
+                    });
 
-                    cswPublic.control = cswPrivate.parent.table();
+                    table.cell(1, 2).icon({
+                        name: nodeProperty.name + '_view',
+                        iconType: Csw.enums.iconType.magglass,
+                        hovertext: 'View',
+                        size: 16,
+                        isButton: true,
+                        onClick: function() {
+                            Csw.clientState.setCurrentView(cswPrivate.viewId, cswPrivate.viewMode);
+                            /* case 20958 - so that it doesn't treat the view as a Grid Property view */
+                            Csw.cookie.clear(Csw.cookie.cookieNames.CurrentNodeId);
+                            Csw.cookie.clear(Csw.cookie.cookieNames.CurrentNodeKey);
 
-                    if (false === cswPublic.data.isMulti()) {
-                        cswPublic.control.cell(1, 1).$.CswViewContentTree({
-                            viewid: cswPrivate.viewId
-                        });
-
-                        cswPublic.control.cell(1, 2).icon({
-                            name: cswPublic.data.name + '_view',
-                            iconType: Csw.enums.iconType.magglass,
-                            hovertext: 'View',
+                            Csw.window.location(Csw.getGlobalProp('homeUrl'));
+                        }
+                    });
+                    if (false === nodeProperty.isReadOnly()) {
+                        table.cell(1, 3).icon({
+                            name: nodeProperty.name + '_edit',
+                            hovertext: 'Edit',
+                            iconType: Csw.enums.iconType.pencil,
                             size: 16,
                             isButton: true,
-                            onClick: function () {
-                                Csw.clientState.setCurrentView(cswPrivate.viewId, cswPrivate.viewMode);
-                                /* case 20958 - so that it doesn't treat the view as a Grid Property view */
-                                Csw.cookie.clear(Csw.cookie.cookieNames.CurrentNodeId);
-                                Csw.cookie.clear(Csw.cookie.cookieNames.CurrentNodeKey);
-
-                                Csw.window.location(Csw.getGlobalProp('homeUrl'));
+                            onClick: function() {
+                                nodeProperty.onEditView(cswPrivate.viewId);
                             }
                         });
-                        if (false === cswPublic.data.isReadOnly()) {
-                            cswPublic.control.cell(1, 3).icon({
-                                name: cswPublic.data.name + '_edit',
-                                hovertext: 'Edit',
-                                iconType: Csw.enums.iconType.pencil,
-                                size: 16,
-                                isButton: true,
-                                onClick: function () {
-                                    cswPublic.data.onEditView(cswPrivate.viewId);
-                                }
-                            });
-                        }
-                    } 
+                    }
+                }
+            };
 
-                };
+            //Bind the callback to the render event
+            nodeProperty.bindRender(render);
 
-                //Bind the callback to the render event
-                cswPublic.data.bindRender(render);
+            //Bind an unrender callback to terminate any outstanding ajax requests, if any. See propTypeGrid.
+            //nodeProperty.unBindRender();
 
-                //Bind an unrender callback to terminate any outstanding ajax requests, if any. See propTypeGrid.
-                //cswPublic.data.unBindRender();
-
-                return cswPublic;
-            }));
+            return true;
+        });
 
 }());
