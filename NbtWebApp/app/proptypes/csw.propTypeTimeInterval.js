539ac74e307e5d82eb7c1c905a3e4ea3457fbace
Christopher Froehlich <cfroehlich@chemsw.com>
2013-04-19 19:23:45 +0400

    Buckeye. Case 29390. Review K5220. Time Interval.

--- NbtWebApp/app/proptypes/csw.propTypeTimeInterval.js
+++ NbtWebApp/app/proptypes/csw.propTypeTimeInterval.js
@@ -2,46 +2,38 @@
 
 (function () {
     'use strict';
-    Csw.properties.timeInterval = Csw.properties.timeInterval ||
-        Csw.properties.register('timeInterval', Csw.method(function (propertyOption) {
+    Csw.properties.timeInterval = Csw.properties.register('timeInterval',
+        function(nodeProperty) {
             'use strict';
-            var cswPrivate = {};
-            var cswPublic = {
-                data: propertyOption
-            };
-
+            
             //The render function to be executed as a callback
-            var render = function () {
+            var render = function() {
                 'use strict';
-                cswPublic.data = cswPublic.data || Csw.nbt.propertyOption(propertyOption);
-
-                cswPrivate.propVals = cswPublic.data.propData.values;
-                cswPrivate.parent = cswPublic.data.propDiv;
-
-                if (false === Csw.bool(cswPublic.data.isReadOnly())) {
-                    cswPublic.control = cswPrivate.parent.timeInterval({
-                        Multi: cswPublic.data.isMulti(),
-                        ReadOnly: cswPublic.data.isReadOnly(),
-                        isRequired: cswPublic.data.isRequired(),
-                        rateIntervalValue: cswPrivate.propVals.Interval.rateintervalvalue,
-                        useEditButton: cswPublic.data.tabState.EditMode !== Csw.enums.editMode.Add,
-                        onChange: function () {
-                            // We're bypassing this to avoid having to deal with the complexity of multiple copies of the RateInterval JSON
-                            // cswPublic.data.onPropChange(compare);
-                            cswPublic.data.propData.wasmodified = true;
+                var cswPrivate = Csw.object();
+                
+                if (false === Csw.bool(nodeProperty.isReadOnly())) {
+                    nodeProperty.propDiv.timeInterval({
+                        Multi: nodeProperty.isMulti(),
+                        ReadOnly: nodeProperty.isReadOnly(),
+                        isRequired: nodeProperty.isRequired(),
+                        rateIntervalValue: nodeProperty.propData.values.Interval.rateintervalvalue,
+                        useEditButton: nodeProperty.tabState.EditMode !== Csw.enums.editMode.Add,
+                        onChange: function() {
+                            //Case 29390: We're already passing by reference; no need to update. No sync for Time Interval.
+                            nodeProperty.broadcastPropChange();
                         }
                     });
                 } else {
-                    cswPublic.control = cswPrivate.parent.append(cswPublic.data.propData.gestalt);
+                    nodeProperty.propDiv.append(nodeProperty.propData.gestalt);
                 }
             };
 
             //Bind the callback to the render event
-            cswPublic.data.bindRender(render);
+            nodeProperty.bindRender(render);
 
             //Bind an unrender callback to terminate any outstanding ajax requests, if any. See propTypeGrid.
-            //cswPublic.data.unBindRender();
+            //nodeProperty.unBindRender();
 
-            return cswPublic;
-        }));
+            return true;
+        });
 } ());
