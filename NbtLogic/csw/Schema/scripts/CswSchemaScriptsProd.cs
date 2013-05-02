4507d07878157c3a4a4f6832b2aa950a8cd7bd90
bvavra@chemsw.com <unknown@kiln.example.com>
2013-05-02 17:09:07 +0400

    Cedar - fixed merge fail

--- NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
+++ NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
@@ -33,7 +33,6 @@ namespace ChemSW.Nbt.Schema
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_02B_Case29320_PrintJobs() ) );          //02B-011
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_02B_Case29335_DisableSchedules() ) );   //02B-012
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_02B_Case29254() ) );                    //02B-013
-            _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_02B_Case28768() ) );                    //02B-014
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_02B_Case29211() ) );                    //02B-015
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_02B_Case28690B() ) );                   //02B-016
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_02B_Case28690C() ) );                   //02B-017
