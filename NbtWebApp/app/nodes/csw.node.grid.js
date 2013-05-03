f6dfb8536751ced6f0eb3a1eab20fb5c2944136d
David Hessler <dhessler@chemsw.com>
2013-05-03 21:52:42 +0400

    case 29553

--- NbtWebApp/app/nodes/csw.node.grid.js
+++ NbtWebApp/app/nodes/csw.node.grid.js
@@ -59,7 +59,7 @@
                 forceFit: cswPrivate.forceFit,
                 usePaging: false === cswPrivate.forReporting,
                 showCheckboxes: cswPrivate.showCheckboxes,
-                showActionColumn: false === cswPrivate.forReporting && false === cswPrivate.readonly,
+                showActionColumn: false === cswPrivate.forReporting, // && false === cswPrivate.readonly, //case 29553
                 height: cswPrivate.height,
                 canSelectRow: cswPrivate.canSelectRow,
                 onSelect: cswPrivate.onSelect,
