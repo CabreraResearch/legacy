ce3f7bd64546f15f70b873909b95a15a68a9ff5d
Matthew Bischoff <mbischoff@chemsw.com>
2013-05-07 00:44:13 +0400

    Buckeye Case 29630 - InspectionDesign  image prop

--- NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
+++ NbtLogic/csw/Schema/scripts/CswSchemaScriptsProd.cs
@@ -42,7 +42,8 @@ namespace ChemSW.Nbt.Schema
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_02B_Case28690E() ) );                   // 02A-039   02B-019
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_02B_Case29431() ) );                    // 02A-040   02B-020
             _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_02B_Case29512() ) );                    // 02A-041   02B-021
-            _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_02B_Case29599() ) );
+            _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_02B_Case29599() ) );                    // 02A-042   02B-022
+            _addVersionedScript( new CswSchemaUpdateDriver( new CswUpdateSchema_02B_Case29630() ) );                    // 02A-043   02B-023
 
             #endregion BUCKEYE
 
