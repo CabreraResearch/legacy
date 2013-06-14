975ebeecc9e0891eeb67df330958b82fefa4598c
Christopher Froehlich <cfroehlich@chemsw.com>
2013-06-14 21:58:26 +0400

    Cedar. Case 24977. Data loaders.

--- NbtWebApp/Csw2/app/js/legos/ui/structures/tree/treeStore.js
+++ NbtWebApp/Csw2/app/js/legos/ui/structures/tree/treeStore.js
@@ -7,17 +7,17 @@
      * Private class representing an instance of a tree store. It returns a new n$.tree.treeStore instance.
      * @param rootText {String} The text to display for the root node
      * @param children {Array} [children=[]] An array of tree node children
-     * @param proxy {String} [proxy='memory'] A proxy to render the tree
+     * @param dataLoader {n$.dataLoaders.dataLoader} [dataLoader=n$.dataLoaders.dataLoaderMemory] A proxy to render the tree
     */
-    var TreeStore = function (rootText, children, proxy) {
+    var TreeStore = function (rootText, children, dataLoader) {
         'use strict';
         var that = Ext.create('Ext.data.TreeStore', {
             root: n$.trees.treeNode({
                 text: rootText,
                 expanded: true,
-                children: children
+                children: children || []
             }),
-            proxy: proxy
+            proxy: dataLoader
         });
 
         return that;
@@ -30,7 +30,7 @@
          * Create a tree object.
          * @param treeDef.rootText {String} The text to display for the root node
          * @param treeDef.children {Array} [children=[]] An array of tree node children
-         * @param treeDef.proxy {String} [proxy='memory'] A proxy to render the tree
+         * @param treeDef.dataLoader {n$.dataLoaders.dataLoader} [dataLoader=n$.dataLoaders.dataLoaderMemory] A proxy to render the tree
          * @returns {Csw.trees.treeStore} A tree store object.
         */
         function treeStoreFunc(treeDef) {
@@ -38,10 +38,13 @@
             if (!(treeDef)) {
                 throw new Error('Cannot instance a tree store without properties');
             }
-            if (!(treeDef.proxy instanceof n$.instanceOf.Proxy)) {
-                treeDef.proxy = n$.stores.proxy('memory');
+            if (!(treeDef.dataLoader instanceof n$.instanceOf.DataLoader)) {
+                treeDef.dataLoader = n$.dataLoaders.dataLoaderMemory();
             }
-            var treeStore = new TreeStore(treeDef.rootText, treeDef.children, treeDef.proxy);
+            var treeStore = new TreeStore(treeDef.rootText, treeDef.children, treeDef.dataLoader);
+            if (treeDef.load) {
+                treeStore.load();
+            }
             return treeStore;
         });
 
