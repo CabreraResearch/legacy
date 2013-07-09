cb666269c8bf676635522d82edb012a7fea76f41
NBT Daily Build <b3851691-5f24-46c9-9188-5f0a2e70960a>
2013-07-09 08:16:48 +0400

    Automated commit for release: Cedar_2013.7.9.1

--- NbtSchedService/Properties/AssemblyInfo.cs
+++ NbtSchedService/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "2013.7.8.1" )]
-[assembly: AssemblyFileVersion( "2013.7.8.1" )]
+[assembly: AssemblyVersion( "2013.7.9.1" )]
+[assembly: AssemblyFileVersion( "2013.7.9.1" )]
