d424e5c05536462bc1489ee597d61fc05644d1ee
Matthew Bischoff <mbischoff@chemsw.com>
2013-07-12 23:54:09 +0400

    Dogwood Case 27516 - menu buttons in grids

---
+++ NbtWebApp/Services/Properties.svc.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Web;
+using ChemSW.Nbt.ServiceDrivers;
+using ChemSW.Nbt.WebServices;
+using ChemSW.WebSvc;
+
+namespace NbtWebApp.Services
+{
+    [ServiceBehavior( IncludeExceptionDetailInFaults = true )]
+    [ServiceContract( Namespace = "NbtWebApp" )]
+    [AspNetCompatibilityRequirements( RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed )]
+    public class Properties
+    {
+        private HttpContext _Context = HttpContext.Current;
+
+        [OperationContract]
+        [WebInvoke( Method = "POST" )]
+        [Description( "Save a file" )]
+        [FaultContract( typeof( FaultException ) )]
+        public string GetMenuMode( string propid )
+        {
+            CswPropIdAttr PropIdAttr = new CswPropIdAttr( propid );
+
+            string ret = "Button"; //default is button
+            var SvcDriver = new CswWebSvcDriver<string, CswPropIdAttr>(
+                    CswWebSvcResourceInitializer : new CswWebSvcResourceInitializerNbt( _Context, null ),
+                    ReturnObj : ret,
+                    WebSvcMethodPtr : CswNbtWebServiceProperties.GetButtonMode,
+                    ParamObj : PropIdAttr
+                    );
+
+            SvcDriver.run();
+
+            return ret;
+        }
+
+
+    }
+}
