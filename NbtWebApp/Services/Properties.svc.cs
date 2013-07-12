85e8ff6c8404c8d8f1e0ffe2768f2ed07f95ec4b
Matthew Bischoff <mbischoff@chemsw.com>
2013-07-13 00:01:11 +0400

    Dogwood Case 27516 - get button opts

--- NbtWebApp/Services/Properties.svc.cs
+++ NbtWebApp/Services/Properties.svc.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Activation;
 using System.ServiceModel.Web;
@@ -6,9 +7,25 @@ using System.Web;
 using ChemSW.Nbt.ServiceDrivers;
 using ChemSW.Nbt.WebServices;
 using ChemSW.WebSvc;
+using NbtWebApp.WebSvc.Returns;
 
 namespace NbtWebApp.Services
 {
+    #region Returns
+
+    [DataContract]
+    public class CswNbtPropertyReturn: CswWebSvcReturn
+    {
+        public CswNbtPropertyReturn()
+        {
+            Data = new CswNbtWebServiceProperties.CswNbtButtonData();
+        }
+        [DataMember]
+        public CswNbtWebServiceProperties.CswNbtButtonData Data;
+    }
+
+    #endregion
+
     [ServiceBehavior( IncludeExceptionDetailInFaults = true )]
     [ServiceContract( Namespace = "NbtWebApp" )]
     [AspNetCompatibilityRequirements( RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed )]
@@ -37,6 +54,28 @@ namespace NbtWebApp.Services
             return ret;
         }
 
+        [OperationContract]
+        [WebInvoke( Method = "POST" )]
+        [Description( "Save a file" )]
+        [FaultContract( typeof( FaultException ) )]
+        public CswNbtPropertyReturn GetButtonOpts( string propid )
+        {
+            CswPropIdAttr PropIdAttr = new CswPropIdAttr( propid );
+
+            CswNbtPropertyReturn ret = new CswNbtPropertyReturn(); //default is button
+            var SvcDriver = new CswWebSvcDriver<CswNbtPropertyReturn, CswPropIdAttr>(
+                    CswWebSvcResourceInitializer : new CswWebSvcResourceInitializerNbt( _Context, null ),
+                    ReturnObj : ret,
+                    WebSvcMethodPtr : CswNbtWebServiceProperties.GetButtonOpts,
+                    ParamObj : PropIdAttr
+                    );
+
+            SvcDriver.run();
+
+
+
+            return ret;
+        }
 
     }
 }
