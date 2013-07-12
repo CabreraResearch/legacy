81c963e76127520656ff962ee3b017472b3ad3e6
Matthew Bischoff <mbischoff@chemsw.com>
2013-07-13 00:54:35 +0400

    Dogwood Case 27516 - bug

--- NbtWebApp/WebSvc/Logic/Nodes/CswNbtWebServiceProperties.cs
+++ NbtWebApp/WebSvc/Logic/Nodes/CswNbtWebServiceProperties.cs
@@ -3,6 +3,7 @@ using System.Runtime.Serialization;
 using ChemSW.Core;
 using ChemSW.Nbt.MetaData;
 using ChemSW.Nbt.ObjClasses;
+using ChemSW.Nbt.PropTypes;
 using ChemSW.Nbt.ServiceDrivers;
 using NbtWebApp.Services;
 
@@ -17,7 +18,7 @@ namespace ChemSW.Nbt.WebServices
         public class CswNbtButtonData
         {
             [DataMember]
-            public string Mode = "button"; ////default is button
+            public string Mode = "button"; //default is button
 
             [DataMember]
             public Collection<string> Opts = new Collection<string>();
@@ -30,7 +31,11 @@ namespace ChemSW.Nbt.WebServices
             CswNbtResources NbtResources = (CswNbtResources) Resources;
             CswNbtMetaDataNodeTypeProp BtnNTP = NbtResources.MetaData.getNodeTypeProp( PropId.NodeTypePropId );
             CswNbtNode Node = NbtResources.Nodes.GetNode( PropId.NodeId );
-            Return.Data.Mode = Node.Properties[BtnNTP].AsButton.Mode;
+            CswNbtNodePropButton btnProp = Node.Properties[BtnNTP].AsButton;
+            if( false == string.IsNullOrEmpty( btnProp.Mode ) )
+            {
+                Return.Data.Mode = Node.Properties[BtnNTP].AsButton.Mode;
+            }
         }
 
         public static void GetButtonOpts( ICswResources Resources, CswNbtPropertyReturn Return, CswPropIdAttr PropId )
