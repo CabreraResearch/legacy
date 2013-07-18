d6eb6b319a597559206f1b3e0d97a642685ae43a
Christopher Froehlich <cfroehlich@chemsw.com>
2013-07-18 18:15:59 +0400

    Dogwood. Case 29863. Review K5813. Comment 2.

---
+++ NbtLogic/csw/Schema/Layout/CswNbtSchemaUpdateLayoutMgrHlprImpl.cs
@@ -0,0 +1,119 @@
+﻿using ChemSW.Exceptions;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.ObjClasses;
+using ChemSW.Nbt.Security;
+using System;
+using System.Collections.ObjectModel;
+
+namespace ChemSW.Nbt.csw.Schema
+{
+    struct CswNbtSchemaUpdateLayoutMgrHlprImpl : ICswNbtSchemaUpdateLayoutMgrHlpr
+    {
+        private CswNbtSchemaUpdateLayoutMgr _LayoutMgr;
+        private CswNbtSchemaUpdateLayoutMgr.getTab _getTab;
+        
+        public CswNbtSchemaUpdateLayoutMgrHlprImpl( CswNbtSchemaUpdateLayoutMgr LayoutMgr, CswNbtSchemaUpdateLayoutMgr.getTab GetTab )
+        {
+            _LayoutMgr = LayoutMgr;
+            _getTab = GetTab;
+        }
+
+        private CswNbtSchemaUpdateLayoutMgr.LayoutProp _vetLayoutOp( string PropName, bool DoNotTrack, CswEnumNbtLayoutType LayoutType = null )
+        {
+            LayoutType = LayoutType ?? _LayoutMgr.LayoutType;
+            CswNbtMetaDataObjectClassProp Ocp = _LayoutMgr.ObjectClass.getObjectClassProp( PropName );
+            if ( null == Ocp )
+            {
+                throw new CswDniException( "Cannot copy a property that is not an Object Class prop. Object Class Prop {" + PropName + "} was null." );
+            }
+
+            CswNbtSchemaUpdateLayoutMgr.LayoutProp Ret = new CswNbtSchemaUpdateLayoutMgr.LayoutProp( Ocp, LayoutType, "" );
+            //I originally thought we could prevent copy/paste errors by tracking which props had been positioned where, but this turns out to be non trivial.
+            //It's not a bad idea, but not worth the overhead to do correctly right now.
+
+            //if ( _LayoutMgr.Props.Contains( Ret ) )
+            //{
+            //    if ( DoNotTrack )
+            //    {
+            //        _LayoutMgr.Props.Remove( Ret );
+            //    }
+            //    else
+            //    {
+            //        throw new CswDniException( "This property has already been placed in this layout. Prop: {" + PropName + "}, LayoutType: {" + LayoutType + "}, Tab: {" + CurrentTabName + "}." );
+            //    }
+            //}
+
+            //if ( false == DoNotTrack )
+            //{
+            //    _LayoutMgr.Props.Add( Ret );
+            //}
+            return Ret;
+        }
+
+        private void _doLayoutUpdate( string PropName, Int32 Row, Int32 Column, bool DoMove, bool DoNotTrack, CswEnumNbtLayoutType LayoutType = null )
+        {
+            CswNbtSchemaUpdateLayoutMgr.LayoutProp Prop = _vetLayoutOp( PropName, DoNotTrack, LayoutType );
+
+            foreach ( CswNbtMetaDataNodeType NodeType in _LayoutMgr.LatestVersionNodeTypes )
+            {
+                CswNbtMetaDataNodeTypeTab Tab = _getTab( _LayoutMgr.SchemaModTrnsctn, NodeType );
+                CswNbtMetaDataNodeTypeProp Ntp = NodeType.getNodeTypePropByObjectClassProp( Prop.Prop );
+                Ntp.updateLayout( Prop.Layout, TabId: Tab.TabId, DoMove: DoMove, DisplayRow: Row, DisplayColumn: Column );
+            }
+        }
+
+        /// <summary>
+        /// Copies a property to the tab. If the tab does not exist, this will create the tab.
+        /// </summary>
+        /// <param name="PropName"></param>
+        /// <param name="Row"></param>
+        /// <param name="Column"></param>
+        /// <param name="TabName">Default to CurrentTabName</param>
+        /// <param name="LayoutType">Default to CurrentLayoutType</param>
+        public void copyProp( string PropName, Int32 Row, Int32 Column, CswEnumNbtLayoutType LayoutType = null )
+        {
+            _doLayoutUpdate( PropName, Row, Column, DoMove: false, DoNotTrack: false, LayoutType: LayoutType );
+        }
+
+        /// <summary>
+        /// Moves a property on the "first" tab, which should be the tab of the same name as the NodeType else the first tab by sequence.
+        /// </summary>
+        public void moveProp( string PropName, Int32 Row, Int32 Column, CswEnumNbtLayoutType LayoutType = null )
+        {
+            _doLayoutUpdate( PropName, Row, Column, DoMove: true, DoNotTrack: true, LayoutType: LayoutType );
+        }
+
+        /// <summary>
+        /// Remove a property from the tab of a specific layout or from all tabs of a layout if the current tab is null.
+        /// </summary>
+        public void removeProp( string PropName, CswEnumNbtLayoutType LayoutType = null )
+        {
+            CswNbtSchemaUpdateLayoutMgr.LayoutProp Prop = _vetLayoutOp( PropName, true, LayoutType );
+
+            foreach ( CswNbtMetaDataNodeType NodeType in _LayoutMgr.LatestVersionNodeTypes )
+            {
+                CswNbtMetaDataNodeTypeTab Tab = _getTab( _LayoutMgr.SchemaModTrnsctn, NodeType );
+                if ( null != Tab )
+                {
+                    CswNbtMetaDataNodeTypeProp Ntp = NodeType.getNodeTypePropByObjectClassProp( Prop.Prop );
+                    Ntp.removeFromLayout( Prop.Layout, Tab.TabId );
+                }
+            }
+        }
+
+        public void setPermit( CswEnumNbtNodeTypeTabPermission Permission, bool Value, Collection<CswNbtObjClassRole> Roles )
+        {
+            foreach ( CswNbtMetaDataNodeType NodeType in _LayoutMgr.LatestVersionNodeTypes )
+            {
+                CswNbtMetaDataNodeTypeTab Tab = _getTab( _LayoutMgr.SchemaModTrnsctn, NodeType );
+                if ( null != Tab )
+                {
+                    foreach ( CswNbtObjClassRole Role in Roles )
+                    {
+                        _LayoutMgr.SchemaModTrnsctn.Permit.set( Permission, Tab, Role, Value );
+                    }
+                }
+            }
+        }
+    }
+}
--- NbtLogic/csw/Schema/scripts/prod/02D/CswUpdateSchema_02D_Case29863_UserProfile.cs
+++ NbtLogic/csw/Schema/scripts/prod/02D/CswUpdateSchema_02D_Case29863_UserProfile.cs
@@ -28,56 +28,56 @@ namespace ChemSW.Nbt.Schema
         {
             try
             {
-                CswNbtSchemaUpdateLayoutMgr LayoutMgr = new CswNbtSchemaUpdateLayoutMgr( _CswNbtSchemaModTrnsctn, CswEnumNbtObjectClass.UserClass, TabName: "Profile", LayoutType: CswEnumNbtLayoutType.Edit );
+                CswNbtSchemaUpdateLayoutMgr LayoutMgr = new CswNbtSchemaUpdateLayoutMgr( _CswNbtSchemaModTrnsctn, CswEnumNbtObjectClass.UserClass, LayoutType: CswEnumNbtLayoutType.Edit );
 
                 //User tab
-                LayoutMgr.removePropFromLayoutFirstTab( PropName: CswNbtObjClassUser.PropertyName.FavoriteActions );
-                LayoutMgr.removePropFromLayoutFirstTab( PropName: CswNbtObjClassUser.PropertyName.FavoriteViews );
-                LayoutMgr.removePropFromLayoutFirstTab( PropName: CswNbtObjClassUser.PropertyName.DateFormat );
-                LayoutMgr.removePropFromLayoutFirstTab( PropName: CswNbtObjClassUser.PropertyName.TimeFormat );
-                LayoutMgr.removePropFromLayoutFirstTab( PropName: CswNbtObjClassUser.PropertyName.Language );
-                LayoutMgr.removePropFromLayoutFirstTab( PropName: CswNbtObjClassUser.PropertyName.DefaultLocation );
-                LayoutMgr.removePropFromLayoutFirstTab( PropName: CswNbtObjClassUser.PropertyName.DefaultPrinter );
-                LayoutMgr.removePropFromLayoutFirstTab( PropName: CswNbtObjClassUser.PropertyName.PageSize );
+                LayoutMgr.First.removeProp( PropName: CswNbtObjClassUser.PropertyName.FavoriteActions );
+                LayoutMgr.First.removeProp( PropName: CswNbtObjClassUser.PropertyName.FavoriteViews );
+                LayoutMgr.First.removeProp( PropName: CswNbtObjClassUser.PropertyName.DateFormat );
+                LayoutMgr.First.removeProp( PropName: CswNbtObjClassUser.PropertyName.TimeFormat );
+                LayoutMgr.First.removeProp( PropName: CswNbtObjClassUser.PropertyName.Language );
+                LayoutMgr.First.removeProp( PropName: CswNbtObjClassUser.PropertyName.DefaultLocation );
+                LayoutMgr.First.removeProp( PropName: CswNbtObjClassUser.PropertyName.DefaultPrinter );
+                LayoutMgr.First.removeProp( PropName: CswNbtObjClassUser.PropertyName.PageSize );
 
-                LayoutMgr.movePropOnFirstTab( Row: 1, Column: 1, PropName: CswNbtObjClassUser.PropertyName.Username );
-                LayoutMgr.movePropOnFirstTab( Row: 2, Column: 1, PropName: CswNbtObjClassUser.PropertyName.FirstName );
-                LayoutMgr.movePropOnFirstTab( Row: 3, Column: 1, PropName: CswNbtObjClassUser.PropertyName.LastName );
-                LayoutMgr.movePropOnFirstTab( Row: 4, Column: 1, PropName: CswNbtObjClassUser.PropertyName.Phone );
-                LayoutMgr.movePropOnFirstTab( Row: 5, Column: 1, PropName: CswNbtObjClassUser.PropertyName.Email );
-                LayoutMgr.movePropOnFirstTab( Row: 6, Column: 1, PropName: CswNbtObjClassUser.PropertyName.Password );
-                LayoutMgr.movePropOnFirstTab( Row: 7, Column: 1, PropName: CswNbtObjClassUser.PropertyName.Jurisdiction );
+                LayoutMgr.First.moveProp( Row: 1, Column: 1, PropName: CswNbtObjClassUser.PropertyName.Username );
+                LayoutMgr.First.moveProp( Row: 2, Column: 1, PropName: CswNbtObjClassUser.PropertyName.FirstName );
+                LayoutMgr.First.moveProp( Row: 3, Column: 1, PropName: CswNbtObjClassUser.PropertyName.LastName );
+                LayoutMgr.First.moveProp( Row: 4, Column: 1, PropName: CswNbtObjClassUser.PropertyName.Phone );
+                LayoutMgr.First.moveProp( Row: 5, Column: 1, PropName: CswNbtObjClassUser.PropertyName.Email );
+                LayoutMgr.First.moveProp( Row: 6, Column: 1, PropName: CswNbtObjClassUser.PropertyName.Password );
+                LayoutMgr.First.moveProp( Row: 7, Column: 1, PropName: CswNbtObjClassUser.PropertyName.Jurisdiction );
 
-                LayoutMgr.movePropOnFirstTab( Row: 1, Column: 2, PropName: CswNbtObjClassUser.PropertyName.Role );
-                LayoutMgr.movePropOnFirstTab( Row: 2, Column: 2, PropName: CswNbtObjClassUser.PropertyName.WorkUnit );
-                LayoutMgr.movePropOnFirstTab( Row: 3, Column: 2, PropName: CswNbtObjClassUser.PropertyName.EmployeeId );
-                LayoutMgr.movePropOnFirstTab( Row: 4, Column: 2, PropName: CswNbtObjClassUser.PropertyName.Barcode );
-                LayoutMgr.movePropOnFirstTab( Row: 5, Column: 2, PropName: CswNbtObjClassUser.PropertyName.LastLogin );
-                LayoutMgr.movePropOnFirstTab( Row: 6, Column: 2, PropName: CswNbtObjClassUser.PropertyName.AccountLocked );
-                LayoutMgr.movePropOnFirstTab( Row: 7, Column: 2, PropName: CswNbtObjClassUser.PropertyName.FailedLoginCount );
-                LayoutMgr.movePropOnFirstTab( Row: 8, Column: 2, PropName: CswNbtObjClassUser.PropertyName.Archived );
+                LayoutMgr.First.moveProp( Row: 1, Column: 2, PropName: CswNbtObjClassUser.PropertyName.Role );
+                LayoutMgr.First.moveProp( Row: 2, Column: 2, PropName: CswNbtObjClassUser.PropertyName.WorkUnit );
+                LayoutMgr.First.moveProp( Row: 3, Column: 2, PropName: CswNbtObjClassUser.PropertyName.EmployeeId );
+                LayoutMgr.First.moveProp( Row: 4, Column: 2, PropName: CswNbtObjClassUser.PropertyName.Barcode );
+                LayoutMgr.First.moveProp( Row: 5, Column: 2, PropName: CswNbtObjClassUser.PropertyName.LastLogin );
+                LayoutMgr.First.moveProp( Row: 6, Column: 2, PropName: CswNbtObjClassUser.PropertyName.AccountLocked );
+                LayoutMgr.First.moveProp( Row: 7, Column: 2, PropName: CswNbtObjClassUser.PropertyName.FailedLoginCount );
+                LayoutMgr.First.moveProp( Row: 8, Column: 2, PropName: CswNbtObjClassUser.PropertyName.Archived );
 
                 //Identity tab
-                LayoutMgr.copyPropToIdentityTab( Row: 1, Column: 1, PropName: CswNbtObjClassUser.PropertyName.Username );
-                LayoutMgr.copyPropToIdentityTab( Row: 2, Column: 1, PropName: CswNbtObjClassUser.PropertyName.Barcode );
+                LayoutMgr.Identity.copyProp( Row: 1, Column: 1, PropName: CswNbtObjClassUser.PropertyName.Username );
+                LayoutMgr.Identity.copyProp( Row: 2, Column: 1, PropName: CswNbtObjClassUser.PropertyName.Barcode );
 
                 //Profile tab
-                LayoutMgr.copyPropToTab( Row: 1, Column: 1, PropName: CswNbtObjClassUser.PropertyName.Username );
-                LayoutMgr.copyPropToTab( Row: 2, Column: 1, PropName: CswNbtObjClassUser.PropertyName.FirstName );
-                LayoutMgr.copyPropToTab( Row: 3, Column: 1, PropName: CswNbtObjClassUser.PropertyName.LastName );
-                LayoutMgr.copyPropToTab( Row: 4, Column: 1, PropName: CswNbtObjClassUser.PropertyName.Phone );
-                LayoutMgr.copyPropToTab( Row: 5, Column: 1, PropName: CswNbtObjClassUser.PropertyName.Email );
-                LayoutMgr.copyPropToTab( Row: 6, Column: 1, PropName: CswNbtObjClassUser.PropertyName.Password );
-                LayoutMgr.copyPropToTab( Row: 7, Column: 1, PropName: CswNbtObjClassUser.PropertyName.PageSize );
+                LayoutMgr["Profile"].copyProp( Row: 1, Column: 1, PropName: CswNbtObjClassUser.PropertyName.Username );
+                LayoutMgr["Profile"].copyProp( Row: 2, Column: 1, PropName: CswNbtObjClassUser.PropertyName.FirstName );
+                LayoutMgr["Profile"].copyProp( Row: 3, Column: 1, PropName: CswNbtObjClassUser.PropertyName.LastName );
+                LayoutMgr["Profile"].copyProp( Row: 4, Column: 1, PropName: CswNbtObjClassUser.PropertyName.Phone );
+                LayoutMgr["Profile"].copyProp( Row: 5, Column: 1, PropName: CswNbtObjClassUser.PropertyName.Email );
+                LayoutMgr["Profile"].copyProp( Row: 6, Column: 1, PropName: CswNbtObjClassUser.PropertyName.Password );
+                LayoutMgr["Profile"].copyProp( Row: 7, Column: 1, PropName: CswNbtObjClassUser.PropertyName.PageSize );
 
-                LayoutMgr.copyPropToTab( Row: 1, Column: 2, PropName: CswNbtObjClassUser.PropertyName.FavoriteActions );
-                LayoutMgr.copyPropToTab( Row: 2, Column: 2, PropName: CswNbtObjClassUser.PropertyName.FavoriteViews );
-                LayoutMgr.copyPropToTab( Row: 3, Column: 2, PropName: CswNbtObjClassUser.PropertyName.DateFormat );
-                LayoutMgr.copyPropToTab( Row: 4, Column: 2, PropName: CswNbtObjClassUser.PropertyName.TimeFormat );
-                LayoutMgr.copyPropToTab( Row: 5, Column: 2, PropName: CswNbtObjClassUser.PropertyName.Language );
-                LayoutMgr.copyPropToTab( Row: 6, Column: 2, PropName: CswNbtObjClassUser.PropertyName.Jurisdiction );
-                LayoutMgr.copyPropToTab( Row: 7, Column: 2, PropName: CswNbtObjClassUser.PropertyName.DefaultLocation );
-                LayoutMgr.copyPropToTab( Row: 8, Column: 2, PropName: CswNbtObjClassUser.PropertyName.DefaultPrinter );
+                LayoutMgr["Profile"].copyProp( Row: 1, Column: 2, PropName: CswNbtObjClassUser.PropertyName.FavoriteActions );
+                LayoutMgr["Profile"].copyProp( Row: 2, Column: 2, PropName: CswNbtObjClassUser.PropertyName.FavoriteViews );
+                LayoutMgr["Profile"].copyProp( Row: 3, Column: 2, PropName: CswNbtObjClassUser.PropertyName.DateFormat );
+                LayoutMgr["Profile"].copyProp( Row: 4, Column: 2, PropName: CswNbtObjClassUser.PropertyName.TimeFormat );
+                LayoutMgr["Profile"].copyProp( Row: 5, Column: 2, PropName: CswNbtObjClassUser.PropertyName.Language );
+                LayoutMgr["Profile"].copyProp( Row: 6, Column: 2, PropName: CswNbtObjClassUser.PropertyName.Jurisdiction );
+                LayoutMgr["Profile"].copyProp( Row: 7, Column: 2, PropName: CswNbtObjClassUser.PropertyName.DefaultLocation );
+                LayoutMgr["Profile"].copyProp( Row: 8, Column: 2, PropName: CswNbtObjClassUser.PropertyName.DefaultPrinter );
 
                 CswNbtMetaDataObjectClass RoleOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswEnumNbtObjectClass.RoleClass );
                 Collection<CswNbtObjClassRole> Roles = new Collection<CswNbtObjClassRole>();
@@ -85,7 +85,7 @@ namespace ChemSW.Nbt.Schema
                 {
                     Roles.Add( Role );
                 }
-                LayoutMgr.setTabPermission( CswEnumNbtNodeTypeTabPermission.Edit, true, Roles );
+                LayoutMgr["Profile"].setPermit( CswEnumNbtNodeTypeTabPermission.Edit, true, Roles );
 
             }
             catch( Exception Ex )
