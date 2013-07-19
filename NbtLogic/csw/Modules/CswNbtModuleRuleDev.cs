d8efc44db3f9f175584c892d26b26f8f0842c6f1
Christopher Froehlich <cfroehlich@chemsw.com>
2013-07-19 22:07:33 +0400

    Dogwood. Case 29617. Easy Customer setup for Dev module.

--- NbtLogic/csw/Modules/CswNbtModuleRuleDev.cs
+++ NbtLogic/csw/Modules/CswNbtModuleRuleDev.cs
@@ -1,5 +1,6 @@
 
 using ChemSW.Config;
+using ChemSW.Core;
 using ChemSW.Nbt.MetaData;
 using ChemSW.Nbt.ObjClasses;
 using CswNbtMetaDataNodeType = ChemSW.Nbt.MetaData.CswNbtMetaDataNodeType;
@@ -23,6 +24,8 @@ namespace ChemSW.Nbt
             {
                 _CswNbtResources.ConfigVbls.setConfigVariableValue( CswEnumConfigurationVariableNames.Logging_Level.ToString(), "Info" );
             }
+            _CswNbtResources.SetupVbls.writeSetting( CswEnumSetupVariableNames.LogOutputToLoggly, "true" );
+            _CswNbtResources.SetupVbls.writeSetting( CswEnumSetupVariableNames.ShowFullExceptions, "true" );
 
             CswNbtMetaDataNodeType FieldTypeNt = _CswNbtResources.MetaData.getNodeType( "Csw Dev FieldType Test" );
             if( null == FieldTypeNt )
@@ -96,16 +99,36 @@ namespace ChemSW.Nbt
                 FieldTypeView.save();
 
 
-                //Creating nodes in modules causes a table lock when we update nodecounts in it's own transaction. We'll be removing the mistake that was
-                // "execArbitraryPlatformNeutralSqlInItsOwnTransaction." Until then, make your own nodes!
-                /*
                 CswNbtNode Node1 = _CswNbtResources.Nodes.makeNodeFromNodeTypeId( FieldTypeNt.NodeTypeId, CswEnumNbtMakeNodeOperation.WriteNode );
                 CswNbtNode Node2 = _CswNbtResources.Nodes.makeNodeFromNodeTypeId( FieldTypeNt.NodeTypeId, CswEnumNbtMakeNodeOperation.WriteNode );
                 Node1.IsDemo = true;
                 Node1.postChanges( ForceUpdate: false );
                 Node2.IsDemo = true;
                 Node2.postChanges( ForceUpdate: false );
-                 */
+            }
+
+            _CswNbtResources.Modules.EnableModule( CswEnumNbtModuleName.NBTManager );
+
+            CswNbtMetaDataNodeType CustomerNt = _CswNbtResources.MetaData.getNodeType( "Csw Dev Customers" );
+            if( null == CustomerNt )
+            {
+                CustomerNt = _CswNbtResources.MetaData.makeNewNodeType( new CswNbtWcfMetaDataModel.NodeType( _CswNbtResources.MetaData.getObjectClass( CswEnumNbtObjectClass.CustomerClass ) )
+                {
+                    NodeTypeName = "Csw Dev Customers"
+                } );
+
+                CswNbtView CustomersView = new CswNbtView( _CswNbtResources );
+                CustomersView.saveNew( "Csw Dev Customers", CswEnumNbtViewVisibility.Global );
+                CustomersView.Category = "Csw Dev";
+                CustomersView.AddViewRelationship( CustomerNt, IncludeDefaultFilters: true );
+                CustomersView.save();
+
+                foreach( string AccessId in _CswNbtResources.CswDbCfgInfo.AccessIds )
+                {
+                    CswNbtObjClassCustomer Cust = _CswNbtResources.Nodes.makeNodeFromNodeTypeId( CustomerNt.NodeTypeId, CswEnumNbtMakeNodeOperation.WriteNode );
+                    Cust.CompanyID.Text = AccessId;
+                    Cust.postChanges( ForceUpdate: false );
+                }
             }
         }
 
@@ -115,6 +138,8 @@ namespace ChemSW.Nbt
             {
                 _CswNbtResources.ConfigVbls.setConfigVariableValue( CswEnumConfigurationVariableNames.Logging_Level.ToString(), "None" );
             }
+            _CswNbtResources.SetupVbls.writeSetting( CswEnumSetupVariableNames.LogOutputToLoggly, "false" );
+            _CswNbtResources.SetupVbls.writeSetting( CswEnumSetupVariableNames.ShowFullExceptions, "false" );
         }
     } // class CswNbtModuleRuleDev
 }// namespace ChemSW.Nbt
