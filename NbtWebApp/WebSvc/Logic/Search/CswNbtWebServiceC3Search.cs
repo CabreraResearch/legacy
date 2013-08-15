f294c409b3283e95e9f225f3aa04eb7efd69c406
Colleen Muldowney <cmuldowney@chemsw.com>
2013-08-15 22:08:51 +0400

    Eucalyptus - Case 30482: C3 imports now work regardless of whether MOL is supported by structure search

--- NbtWebApp/WebSvc/Logic/Search/CswNbtWebServiceC3Search.cs
+++ NbtWebApp/WebSvc/Logic/Search/CswNbtWebServiceC3Search.cs
@@ -8,6 +8,7 @@ using ChemSW.Nbt.ChemCatCentral;
 using ChemSW.Nbt.MetaData;
 using ChemSW.Nbt.ObjClasses;
 using ChemSW.Nbt.ServiceDrivers;
+using ChemSW.StructureSearch;
 using NbtWebApp.WebSvc.Returns;
 using Newtonsoft.Json.Linq;
 
@@ -994,7 +995,16 @@ namespace ChemSW.Nbt.WebServices
                                     string Href;
                                     string FormattedMolString;
                                     CswNbtSdBlobData SdBlobData = new CswNbtSdBlobData( _CswNbtResources );
-                                    SdBlobData.saveMol( molData, propAttr, out Href, out FormattedMolString );
+
+                                    MolecularGraph Mol = MoleculeBuilder.CreateMolFromString( molData );
+                                    if( false == Mol.ContainsInvalidAtom() )
+                                    {
+                                        SdBlobData.saveMol( molData, propAttr, out Href, out FormattedMolString );
+                                    }
+                                    else
+                                    {
+                                        _CswNbtResources.logMessage( "During the ChemCatCentral import process, product with ProductId " + _ProductToImport.ProductId + " failed to create a fingerprint record because of an invalid atom." );
+                                    }
                                 }
                                 break;
                             default:
