765f993715531ba839150eb31dc9fd97f667510e
Christopher Froehlich <cfroehlich@chemsw.com>
2013-08-19 22:30:56 +0400

    02F - Case 30079. Finish public interface to iDb.

--- NbtWebApp/app/tools/csw.clientCache.js
+++ NbtWebApp/app/tools/csw.clientCache.js
@@ -4,27 +4,49 @@
 (function () {
     'use strict';
 
+    /**
+     * All paramaters are required
+    */
+    var validate = function (customerId, userName, webServiceName) {
+        if (!customerId) {
+            throw new Error('Customer ID is required.');
+        }
+        if (!userName) {
+            throw new Error('User Name is required.');
+        }
+        if (!webServiceName) {
+            throw new Error('Web Service Name is required.');
+        }
+    };
+    
+    /**
+     * Make a cached call for insert
+    */
+    var makeCachedCall = function (customerId, userName, webServiceName, data) {
+        validate(customerId, userName, webServiceName);
+        return { message: { dateTime: new Date(), cache: { customerId: customerId, userName: userName, webServiceName: webServiceName }, data: data } };
+    };
+
     //Wait until Main is loaded before initing
     Csw.main.onReady.then(function () {
-
-        cacheDbMgr = Csw.db.dbManager('CswLive', 1);
-
-        //newDbMgr.ddl.dropTable(tableName);
-        cacheDbMgr.ddl.createTable('CachedData', 'CachedDataId', true); //true == auto manage primary key
-        cacheDbMgr.ddl.createIndex('CachedData', 'customerId', 'cache.customerId');
-        cacheDbMgr.ddl.createIndex('CachedData', 'dateTimeId', 'dateTime');
-        cacheDbMgr.ddl.createIndex('CachedData', 'userNameId', 'cache.userName');
-        cacheDbMgr.ddl.createIndex('CachedData', 'webServiceNameId', 'cache.webServiceName');
-        cacheDbMgr.ddl.createIndex('CachedData', 'uniqueCalls', ['cache.webServiceName', 'cache.userName', 'cache.customerId'], true);
-
-        //Insert some data
-        cacheDbMgr.insert('CachedData', { message: { dateTime: new Date(), cache: { customerId: 'dev', userName: 'admin', webServiceName: 'getDashbaord' }, data: { a: 1, b: 2, c: 3 } } });
-        cacheDbMgr.insert('CachedData', { message: { dateTime: new Date(), cache: { customerId: 'qa', userName: 'bill', webServiceName: 'getWatermark' }, data: { a: 1, b: 2, c: 3 } } });
 
         var cacheDbMgr = null;
         if (window.Modernizr.indexeddb) {
             //Until we need to manage versions, there is only 1. Versioning either happens on creation, or it doesn't.
 
+            cacheDbMgr = Csw.db.dbManager('CswLive', 1);
+
+            //newDbMgr.ddl.dropTable(tableName);
+            cacheDbMgr.ddl.createTable('CachedData', 'CachedDataId', true); //true == auto manage primary key
+            cacheDbMgr.ddl.createIndex('CachedData', 'customerId', 'cache.customerId');
+            cacheDbMgr.ddl.createIndex('CachedData', 'dateTimeId', 'dateTime');
+            cacheDbMgr.ddl.createIndex('CachedData', 'userNameId', 'cache.userName');
+            cacheDbMgr.ddl.createIndex('CachedData', 'webServiceNameId', 'cache.webServiceName');
+            cacheDbMgr.ddl.createIndex('CachedData', 'uniqueCalls', ['cache.webServiceName', 'cache.userName', 'cache.customerId'], true);
+
+            //Insert some demo data
+            //cacheDbMgr.insert('CachedData', { message: { dateTime: new Date(), cache: { customerId: 'dev', userName: 'admin', webServiceName: 'getDashbaord' }, data: { a: 1, b: 2, c: 3 } } });
+            //cacheDbMgr.insert('CachedData', { message: { dateTime: new Date(), cache: { customerId: 'qa', userName: 'bill', webServiceName: 'getWatermark' }, data: { a: 1, b: 2, c: 3 } } });
         }
 
         var getCachedWebServiceCall = function(customerId, userName, webServiceName) {
@@ -34,14 +56,14 @@
                 deferred.resolve(Csw.object());
                 ret = deferred.promise;
             } else {
-                debugger;
-                ret = cacheDbMgr.select.all('CachedData');
+                validate(customerId, userName, webServiceName);
+                ret = cacheDbMgr.select.from('CachedData', 'uniqueCalls', [webServiceName, userName, customerId]);
             }
             return ret;
         };
 
         Csw.register('getCachedWebServiceCall', getCachedWebServiceCall);
-
+        
         var setCachedWebServiceCall = function(customerId, userName, webServiceName, data) {
             var deferred = Q.defer();
             var ret;
@@ -49,15 +71,13 @@
                 deferred.resolve(Csw.object());
                 ret = deferred.promise;
             } else {
-                debugger;
-                //need to do either an insert or an update
-                if (true) {
-                    cacheDbMgr.insert('CachedData', { message: { dateTime: new Date(), cache: { customerId: customerId, userName: userName, webServiceName: webServiceName }, data: data } });
-                } else {
-                    //do update
-                }
-
-                //ret = cacheDbMgr.select.all('CachedData');
+                ret = cacheDbMgr.update('CachedData', 'uniqueCalls', [webServiceName, userName, customerId], data);
+                ret.then(function(updatedRows) {
+                    if (!updatedRows || updatedRows.length === 0) {
+                        var cachedCall = makeCachedCall(customerId, userName, webServiceName);
+                        return cacheDbMgr.insert('CachedData', cachedCall);
+                    }
+                });
             }
             return ret;
         };
