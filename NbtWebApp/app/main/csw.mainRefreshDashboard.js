fd102cb5a314d561dad44d4a6533b7a355eb6c9d
Christopher Froehlich <cfroehlich@chemsw.com>
2013-08-20 23:16:02 +0400

    02F - Case 30079. Bug fix.

--- NbtWebApp/app/main/csw.mainRefreshDashboard.js
+++ NbtWebApp/app/main/csw.mainRefreshDashboard.js
@@ -3,34 +3,35 @@
 (function _initMain() {
 
     function refresh(onSuccess, data) {
-        data = data || [];
-        Csw.main.headerDashboard.empty();
+        if (data) {
+            Csw.main.headerDashboard.empty();
 
-        var table = Csw.main.headerDashboard.table({
-            name: 'DashboardTable'
-        });
-        table.addClass('DashboardTable');
+            var table = Csw.main.headerDashboard.table({
+                name: 'DashboardTable'
+            });
+            table.addClass('DashboardTable');
 
-        var $tr = table.append('<tr />');
+            var $tr = table.append('<tr />');
 
-        Csw.iterate(data, function (thisIcon, dashId) {
-            var cellcontent;
-            if (false === Csw.isNullOrEmpty(thisIcon.href)) {
-                cellcontent = '<td class="DashboardCell">' +
-                    '  <a target="_blank" href="' + thisIcon.href + '">' +
-                    '    <div title="' + thisIcon.text + '" id="' + dashId + '" class="' + dashId + '" />' +
-                    '  </a>' +
-                    '</td>';
-            } else {
-                cellcontent = '<td class="DashboardCell">' +
-                    '  <div title="' + thisIcon.text + '" id="' + dashId + '" class="' + dashId + '" />' +
-                    '</td>';
+            Csw.iterate(data, function(thisIcon, dashId) {
+                var cellcontent;
+                if (false === Csw.isNullOrEmpty(thisIcon.href)) {
+                    cellcontent = '<td class="DashboardCell">' +
+                        '  <a target="_blank" href="' + thisIcon.href + '">' +
+                        '    <div title="' + thisIcon.text + '" id="' + dashId + '" class="' + dashId + '" />' +
+                        '  </a>' +
+                        '</td>';
+                } else {
+                    cellcontent = '<td class="DashboardCell">' +
+                        '  <div title="' + thisIcon.text + '" id="' + dashId + '" class="' + dashId + '" />' +
+                        '</td>';
+                }
+                $tr.append(cellcontent);
+            });
+
+            if (onSuccess) {
+                onSuccess();
             }
-            $tr.append(cellcontent);
-        });
-
-        if (onSuccess) {
-            onSuccess();
         }
         return true;
     }
