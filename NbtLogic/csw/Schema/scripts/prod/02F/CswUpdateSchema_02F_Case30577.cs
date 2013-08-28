92587fd4e56193f1ea5e782a001903f422751c10
Matthew Obetz <mobetz@chemsw.com>
2013-08-29 00:47:29 +0400

    case 30577: Material Type is no longer a required property of chemicals

---
+++ NbtLogic/csw/Schema/scripts/prod/02F/CswUpdateSchema_02F_Case30577.cs
@@ -0,0 +1,36 @@
+﻿using ChemSW.Nbt.csw.Dev;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.ObjClasses;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update
+    /// </summary>
+    public class CswUpdateSchema_02F_Case30577: CswUpdateSchemaTo
+    {
+        public override CswEnumDeveloper Author
+        {
+            get { return CswEnumDeveloper.MO; }
+        }
+
+        public override int CaseNo
+        {
+            get { return 30577; }
+        }
+
+        public override void update()
+        {
+
+            CswNbtMetaDataObjectClass ChemicalOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswEnumNbtObjectClass.ChemicalClass );
+            CswNbtMetaDataObjectClassProp MaterialTypeOCP = ChemicalOC.getObjectClassProp( CswNbtObjClassChemical.PropertyName.MaterialType );
+
+            foreach( CswNbtMetaDataNodeTypeProp MaterialTypeNTP in MaterialTypeOCP.getNodeTypeProps() )
+            {
+                MaterialTypeNTP.IsRequired = false;
+            }
+
+        } // update()
+
+    }//class CswUpdateSchema_02F_Case30577
+}//namespace ChemSW.Nbt.Schema
