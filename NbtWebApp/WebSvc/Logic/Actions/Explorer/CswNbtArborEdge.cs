fb1dd427c0ac5bcfa266151c087a5be156c7e955
Matthew Bischoff <mbischoff@chemsw.com>
2013-09-13 18:14:25 +0400

    Ginko Case 30169 - Data Contracts

---
+++ NbtWebApp/WebSvc/Logic/Actions/Explorer/CswNbtArborEdge.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Runtime.Serialization;
+using ChemSW.Core;
+
+namespace NbtWebApp.Actions.Explorer
+{
+    [DataContract]
+    public class CswNbtArborEdge
+    {
+        private CswPrimaryKey _OwnerNodeId;
+        [DataMember( Name = "OwnerNodeId" )]
+        public string OwnerNodeIdStr
+        {
+            get { return _OwnerNodeId.ToString(); }
+            set { _OwnerNodeId = CswConvert.ToPrimaryKey( value ); }
+        }
+
+        private CswPrimaryKey _TargetNodeId;
+        [DataMember( Name = "TargetNodeId" )]
+        public string TargetNodeIdStr
+        {
+            get { return _TargetNodeId.ToString(); }
+            set { _TargetNodeId = CswConvert.ToPrimaryKey( value ); }
+        }
+
+        public CswPrimaryKey TargetNodeId { get { return _TargetNodeId; } }
+        public CswPrimaryKey OwnerNodeId { get { return _OwnerNodeId; } }
+
+        [DataMember]
+        public CswNbtArborEdgeData Data { get; set; }
+
+        [DataContract]
+        public class CswNbtArborEdgeData
+        {
+            [DataMember]
+            public int Length;
+        }
+
+    }
+}
---
+++ NbtWebApp/WebSvc/Logic/Actions/Explorer/CswNbtArborNode.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.Serialization;
+using ChemSW.Core;
+
+namespace NbtWebApp.Actions.Explorer
+{
+    [DataContract]
+    public class CswNbtArborNode
+    {
+        private CswPrimaryKey _NodeId;
+        [DataMember( Name = "NodeId" )]
+        public string NodeIdStr
+        {
+            get { return _NodeId.ToString(); }
+            set { _NodeId = CswConvert.ToPrimaryKey( value ); }
+        }
+
+        public CswPrimaryKey NodeId { get { return _NodeId; } }
+
+        [DataMember]
+        public CswNbtArborNodeData Data { get; set; }
+
+        [DataContract]
+        public class CswNbtArborNodeData
+        {
+            [DataMember]
+            public double Mass;
+
+            [DataMember]
+            public string Icon;
+
+            [DataMember]
+            public string Label { get; set; }
+        }
+    }
+}
