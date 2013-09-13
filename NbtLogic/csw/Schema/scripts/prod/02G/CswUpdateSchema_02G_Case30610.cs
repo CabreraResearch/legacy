aef6ce5d4a69e434f082be32bee9468c1acfd290
Matthew Obetz <mobetz@chemsw.com>
2013-09-13 23:40:08 +0400

    Ginkgo -- case 30610, review K6242, comment 1 - fixed errors from moving prop vals to afterMakeNode

--- NbtLogic/csw/Schema/scripts/prod/02G/CswUpdateSchema_02G_Case30610.cs
+++ NbtLogic/csw/Schema/scripts/prod/02G/CswUpdateSchema_02G_Case30610.cs
@@ -102,16 +102,11 @@ select * from (
 
                 if( null != ReportGroupNT )
                 {
-                    _CswNbtSchemaModTrnsctn.Nodes.makeNodeFromNodeTypeId( 
-                        ReportGroupNT.NodeTypeId, 
-                        IsTemp: true, 
-                        OnAfterMakeNode: ( CswNbtNode ) =>
-                            {
-                                SystemReportGroup = CswNbtNode;
-                                SystemReportGroup.IsTemp = false; //hack around logic in beforeWriteNode
-                                SystemReportGroup.Name.Text = Name;
-                            }
-                        );
+                   SystemReportGroup = _CswNbtSchemaModTrnsctn.Nodes.makeNodeFromNodeTypeId( ReportGroupNT.NodeTypeId, IsTemp: true );
+                    SystemReportGroup.IsTemp = false; //hack around logic in beforeWriteNode
+                    SystemReportGroup.Name.Text = Name;
+
+                    SystemReportGroup.postChanges( false );
 
                 }
             }
@@ -128,19 +123,20 @@ select * from (
 
             if( null != ReportNT )
             {
-                _CswNbtSchemaModTrnsctn.Nodes.makeNodeFromNodeTypeId( 
+                CswNbtObjClassReport ReportNode = _CswNbtSchemaModTrnsctn.Nodes.makeNodeFromNodeTypeId( 
                     ReportNT.NodeTypeId,
                     OnAfterMakeNode: ( CswNbtNode ) =>
                         {
-                            CswNbtObjClassReport ReportNode = CswNbtNode;
-                            ReportNode.ReportName.Text = ReportName;
-                            ReportNode.Category.Text = Category;
-                            ReportNode.ReportGroup.RelatedNodeId = Group.NodeId;
-                            ReportNode.SQL.Text = Query;
-
-                            _uploadBlobData( ReportNode, Filename );
+                            CswNbtObjClassReport NewNode = CswNbtNode;
+                            NewNode.ReportName.Text = ReportName;
+                            NewNode.Category.Text = Category;
+                            NewNode.ReportGroup.RelatedNodeId = Group.NodeId;
+                            NewNode.SQL.Text = Query;
                         }
                     );
+
+                _uploadBlobData( ReportNode, Filename );
+
             }
         }
 
