dc0ac54174d431c84fa55d2bebaaac2a5e08ca02
Steven Salter <ssalter@chemsw.com>
2013-09-24 01:09:39 +0400

    Foxglove, case 29562 - new index

---
+++ NbtLogic/csw/Schema/scripts/prod/02F/CswUpdateSchema_02F_Case29562.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using ChemSW.Nbt.csw.Dev;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.ObjClasses;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update
+    /// </summary>
+    public class CswUpdateSchema_02F_Case29562 : CswUpdateSchemaTo
+    {
+        public override CswEnumDeveloper Author
+        {
+            get { return CswEnumDeveloper.SS; }
+        }
+
+        public override int CaseNo
+        {
+            get { return 29562; }
+        }
+
+        public override string ScriptName
+        {
+            get { return "02F_" + CaseNo; }
+        }
+
+        public override string Title
+        {
+            get { return "Performance improvements"; }
+        }
+
+        public override void update()
+        {
+            _CswNbtSchemaModTrnsctn.indexColumn( "jct_nodes_props", "nodetypepropid, field1, field2, field3, field1_numeric, field1_date", "JCT10" );
+
+
+        } // update()
+
+    }
+
+}//namespace ChemSW.Nbt.Schema
