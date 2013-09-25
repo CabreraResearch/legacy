06788e5e52cc3100c93ce95fcbb09a5e0fda1a16
Steven Salter <ssalter@chemsw.com>
2013-09-25 20:23:22 +0400

    Ginkgo, case 30778 - fix ocpa valuepropid datatype

---
+++ NbtLogic/csw/Schema/scripts/prod/02G/CswUpdateDDL_02G_Case30778.cs
@@ -0,0 +1,40 @@
+﻿using ChemSW.Core;
+using ChemSW.Nbt.csw.Dev;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update
+    /// </summary>
+    public class CswUpdateDDL_02G_Case30778 : CswUpdateSchemaTo
+    {
+        public override CswEnumDeveloper Author
+        {
+            get { return CswEnumDeveloper.SS; }
+        }
+
+        public override int CaseNo
+        {
+            get { return 30778; }
+        }
+
+        public override string ScriptName
+        {
+            get { return "02G_Case30778"; }
+        }
+
+        public override string Title
+        {
+            get { return "Fix ocpa valuepropid"; }
+        }
+
+        public override void update()
+        {
+            // object_class_props_audit.valuepropid is varchar2(20), should be number(12)
+            _CswNbtSchemaModTrnsctn.changeColumnDataType( "object_class_props_audit", "valuepropid", CswEnumDataDictionaryPortableDataType.Long, 12 );
+
+        } // update()
+
+    } // class CswUpdateDDL_02G_Case30778
+
+}//namespace ChemSW.Nbt.Schema
