b89997fb0fa858445803c0933369e75bd5a3ef0d
Steven Salter <ssalter@chemsw.com>
2013-09-27 00:40:14 +0400

    post-merge fix

--- NbtLogic/csw/Schema/scripts/prod/CAF/CswSchemaScriptsCAF.cs
+++ NbtLogic/csw/Schema/scripts/prod/CAF/CswSchemaScriptsCAF.cs
@@ -34,7 +34,7 @@ namespace ChemSW.Nbt.Schema
                     new CswUpdateSchema_02F_Case30043_WorkUnits(),
                     new CswUpdateSchema_02F_Case30043_InventoryGroups(),
                     new CswUpdateSchema_02G_Case30017(),
-                    new CswUpdateSchema_02G_Case30743_Materials()
+                    new CswUpdateSchema_02G_Case30743_Materials(),
                     new CswUpdateSchema_02G_Case30793()
                 };
         } // _SchemaScripts()
