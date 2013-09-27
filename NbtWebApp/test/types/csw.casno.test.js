96e6729e50cc51231406cad2189d6a9c4dd4b2ec
bvavra@chemsw.com <unknown@kiln.example.com>
2013-09-27 23:58:35 +0400

    Ginkgo - Added JS 'types' unit tests (and fixed a couple bugs in the process)

---
+++ NbtWebApp/test/types/csw.casno.test.js
@@ -0,0 +1,16 @@
+﻿module('Csw.casno');
+test('checkSumCASNo valid CASNo', function () {
+    deepEqual(Csw.validateCASNo('12-34-0'), true, '12-34-0 is a valid CASNo');
+});
+
+test('checkSumCASNo invalid CASNo', function () {
+    deepEqual(Csw.validateCASNo('12345'), false, '12340 is an invalid CASNo');
+});
+
+test('checkSumCASNo valid CASNo', function () {
+    deepEqual(Csw.checkSumCASNo('12-34-0'), true, '12-34-0 has a valid checksum');
+});
+
+test('checkSumCASNo invalid CASNo', function () {
+    deepEqual(Csw.checkSumCASNo('12-34-5'), false, '12-34-5 has an invalid checksum');
+});
