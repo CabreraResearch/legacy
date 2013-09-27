62850305b18fb587ed77305d2f80b16eb37ebd38
Christopher Froehlich <cfroehlich@chemsw.com>
2013-09-27 20:34:57 +0400

    02F - Case 30817. (1) When creating a new user, add whichever Work Unit is selected to the list of Available Work Units automatically. (2) When changing the selected Work Unit, do not reset valid Work Unit values with a random selection. (3) Allow ChemSW_Admin to override the exception logic--selected Work Unit will be added to the Available Work Units automatically.

--- NbtLogic/csw/ObjClasses/CswNbtObjClassUser.cs
+++ NbtLogic/csw/ObjClasses/CswNbtObjClassUser.cs
@@ -16,11 +16,11 @@ using ChemSW.Security;
 
 namespace ChemSW.Nbt.ObjClasses
 {
-    public class CswNbtObjClassUser: CswNbtObjClass, ICswNbtUser
+    public class CswNbtObjClassUser : CswNbtObjClass, ICswNbtUser
     {
         public const string ChemSWAdminUsername = CswAuthenticator.ChemSWAdminUsername;
 
-        public new sealed class PropertyName: CswNbtObjClass.PropertyName
+        public new sealed class PropertyName : CswNbtObjClass.PropertyName
         {
             public const string AccountLocked = "AccountLocked";
             public const string Archived = "Archived";
@@ -186,6 +186,16 @@ namespace ChemSW.Nbt.ObjClasses
 
         public override void beforeCreateNode( bool IsCopy, bool OverrideUniqueValidation )
         {
+            //Case 30817
+            if( CswTools.IsPrimaryKey( WorkUnitId ) &&
+                false == AvailableWorkUnits.CheckValue( WorkUnitId.ToString() ) )
+            {
+                AvailableWorkUnits.AddValue( WorkUnitId.ToString() );
+                WorkUnitProperty.RelatedNodeId = WorkUnitId;
+                WorkUnitProperty.SyncGestalt();
+
+                _updateAvailableWorkUnits();
+            }
             _CswNbtObjClassDefault.beforeCreateNode( IsCopy, OverrideUniqueValidation );
         }//beforeCreateNode()
 
@@ -206,7 +216,7 @@ namespace ChemSW.Nbt.ObjClasses
 
             if( UsernameProperty.Text != string.Empty ) // case 25616
             {
-                UsernameProperty.setReadOnly( value : true, SaveToDb : true ); // BZ 5906
+                UsernameProperty.setReadOnly( value: true, SaveToDb: true ); // BZ 5906
             }
 
             // case 22512
@@ -244,7 +254,7 @@ namespace ChemSW.Nbt.ObjClasses
             {
                 clearFailedLoginCount();
             }
-            CachedData.setHidden( value : true, SaveToDb : true );
+            CachedData.setHidden( value: true, SaveToDb: true );
             // BZ 9170
             _CswNbtResources.ConfigVbls.setConfigVariableValue( "cache_lastupdated", DateTime.Now.ToString() );
 
@@ -344,8 +354,8 @@ namespace ChemSW.Nbt.ObjClasses
             //BZ 9933
             if( _CswNbtResources.CurrentNbtUser == null || !_CswNbtResources.CurrentNbtUser.IsAdministrator() )
             {
-                this.FailedLoginCount.setHidden( value : true, SaveToDb : false );
-                this.AccountLocked.setHidden( value : true, SaveToDb : false );
+                this.FailedLoginCount.setHidden( value: true, SaveToDb: false );
+                this.AccountLocked.setHidden( value: true, SaveToDb: false );
             }
 
 
@@ -396,7 +406,7 @@ namespace ChemSW.Nbt.ObjClasses
             CswNbtView view = ParentRelationship.View;
             CswNbtMetaDataObjectClass userOC = _CswNbtResources.MetaData.getObjectClass( CswEnumNbtObjectClass.UserClass );
             CswNbtMetaDataObjectClassProp archivedOCP = userOC.getObjectClassProp( PropertyName.Archived );
-            view.AddViewPropertyAndFilter( ParentRelationship, archivedOCP, FilterMode : CswEnumNbtFilterMode.NotEquals, Value : CswEnumTristate.True.ToString() );
+            view.AddViewPropertyAndFilter( ParentRelationship, archivedOCP, FilterMode: CswEnumNbtFilterMode.NotEquals, Value: CswEnumTristate.True.ToString() );
 
             _CswNbtObjClassDefault.addDefaultViewFilters( ParentRelationship );
         }
@@ -532,14 +542,17 @@ namespace ChemSW.Nbt.ObjClasses
         public void OnWorkUnitPropertyChange( CswNbtNodeProp Prop, bool Creating )
         {
             CswPrimaryKey UsersWorkUnitId = WorkUnitId;
-            if( null != UsersWorkUnitId )
+            //Case 30817
+            //if( null != UsersWorkUnitId )
+            if( false == CswTools.IsPrimaryKey( UsersWorkUnitId ) )
             {
                 UsersWorkUnitId = GetFirstAvailableWorkUnitNodeId();
             }
 
             if( false == AvailableWorkUnits.CheckValue( UsersWorkUnitId.ToString() ) )
             {
-                if( false == _CswNbtResources.CurrentNbtUser is CswNbtSystemUser )
+                if( false == _CswNbtResources.CurrentNbtUser is CswNbtSystemUser &&
+                    _CswNbtResources.CurrentNbtUser.Username != ChemSWAdminUsername )
                 {
                     throw new CswDniException( CswEnumErrorType.Warning,
                                                WorkUnitProperty.CachedNodeName +
@@ -659,7 +672,7 @@ namespace ChemSW.Nbt.ObjClasses
                     if( null != _CurrentUser )
                     {
                         _CurrentUser.CurrentCache = this;
-                        _CurrentUser.postChanges( ForceUpdate : false );
+                        _CurrentUser.postChanges( ForceUpdate: false );
                     }
                 }
             }
@@ -822,7 +835,7 @@ namespace ChemSW.Nbt.ObjClasses
                                       and (perms.userworkunit = :workunit or perms.userworkunit is null)
                                     order by userpermgroup, applyallroles, applyallworkunits";
             CswArbitrarySelect Query = _CswNbtResources.makeCswArbitrarySelect( "getUserPermissions", SQLQuery );
-            
+
             Query.addParameter( "roleocp", CswNbtPropertySetPermission.PropertyName.Role );
             Query.addParameter( "workunitocp", CswNbtPropertySetPermission.PropertyName.WorkUnit );
             Query.addParameter( "permgroupocp", CswNbtPropertySetPermission.PropertyName.PermissionGroup );
@@ -851,7 +864,7 @@ namespace ChemSW.Nbt.ObjClasses
             Dictionary<string, string> opts = new Dictionary<string, string>();
 
             CswNbtMetaDataObjectClass WorkUnitOC = _CswNbtResources.MetaData.getObjectClass( CswEnumNbtObjectClass.WorkUnitClass );
-            foreach( KeyValuePair<CswPrimaryKey, string> workUnit in WorkUnitOC.getNodeIdAndNames( false, false, RequireViewPermissions : false ) )
+            foreach( KeyValuePair<CswPrimaryKey, string> workUnit in WorkUnitOC.getNodeIdAndNames( false, false, RequireViewPermissions: false ) )
             {
                 opts[workUnit.Key.ToString()] = workUnit.Value;
             }
@@ -866,7 +879,7 @@ namespace ChemSW.Nbt.ObjClasses
         public CswPrimaryKey GetFirstAvailableWorkUnitNodeId()
         {
             CswNbtMetaDataObjectClass WorkUnitOC = _CswNbtResources.MetaData.getObjectClass( CswEnumNbtObjectClass.WorkUnitClass );
-            Dictionary<CswPrimaryKey, string> WorkUnits = WorkUnitOC.getNodeIdAndNames( false, false, RequireViewPermissions : false );
+            Dictionary<CswPrimaryKey, string> WorkUnits = WorkUnitOC.getNodeIdAndNames( false, false, RequireViewPermissions: false );
             CswPrimaryKey ret = WorkUnits.OrderBy( entry => entry.Key.PrimaryKey ).First().Key;
             return ret;
         }
