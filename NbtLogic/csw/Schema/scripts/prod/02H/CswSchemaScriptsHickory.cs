cc32325162db71b56811240a12b924905fd7928c
bvavra@chemsw.com <unknown@kiln.example.com>
2013-10-01 18:05:56 +0400

    HICKORY STARTS HERE

---
+++ NbtLogic/csw/Schema/scripts/prod/02H/CswSchemaScriptsHickory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Keeps the schema up-to-date
+    /// </summary>
+    public class CswSchemaScriptsHickory : ICswSchemaScripts
+    {
+        public Collection<CswUpdateSchemaTo> _DDLScripts()
+        {
+            return new Collection<CswUpdateSchemaTo>()
+                {
+                    //Add DDL Scripts Here
+                };
+        } // _DDLScripts()
+
+        public Collection<CswUpdateSchemaTo> _MetaDataScripts()
+        {
+            return new Collection<CswUpdateSchemaTo>()
+                {
+                    //Add MetaData Scripts Here
+                };
+        } // _MetaDataScripts()
+
+        public Collection<CswUpdateSchemaTo> _SchemaScripts()
+        {
+            return new Collection<CswUpdateSchemaTo>()
+                {
+                    //Add Data Scripts Here
+                };
+        } // _SchemaScripts()
+
+    }//class CswSchemaScriptsHickory
+}//namespace ChemSW.Nbt.Schema
