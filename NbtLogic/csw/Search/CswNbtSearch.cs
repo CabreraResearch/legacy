1c44f178336ebda1bbb52b404157ff47e8934247
Steven Salter <ssalter@chemsw.com>
2013-10-01 18:20:26 +0400

    Ginkgo, case 30828

--- NbtLogic/csw/Search/CswNbtSearch.cs
+++ NbtLogic/csw/Search/CswNbtSearch.cs
@@ -369,7 +369,7 @@ namespace ChemSW.Nbt.Search
                                                                                       where firstpropversionid = (select firstpropversionid 
                                                                                                                     from nodetype_props 
                                                                                                                    where nodetypepropid = " + NodeTypePropFirstVersionId.ToString() + @" ))
-                                                              and gestalt " + FilterStr + @") ";
+                                                              and gestaltsearch " + FilterStr + @") ";
                     }
                 } // else if( Filter.Type == CswNbtSearchFilterType.propval )
             } // foreach( CswNbtSearchFilter Filter in FiltersApplied )
