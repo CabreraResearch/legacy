f2d2eb05aa0fa16ac76d41ea78a712ee0e38c3b2
Matthew Bischoff <mbischoff@chemsw.com>
2013-10-02 20:37:48 +0400

    Ginkgo Review K6371 - documentid, not legacyid

--- NbtLogic/csw/Schema/scripts/prod/CAF/CswUpdateSchema_02G_Case30047_SDS.cs
+++ NbtLogic/csw/Schema/scripts/prod/CAF/CswUpdateSchema_02G_Case30047_SDS.cs
@@ -49,8 +49,8 @@ namespace ChemSW.Nbt.Schema
             ImpMgr.importBinding( "filename", CswNbtObjClassSDSDocument.PropertyName.File, CswEnumNbtSubFieldName.Name.ToString() );
 
             //Link and BlobData are stored in the same column, we're going to import it twice and let the "FileType" property dictate what is shown
-            ImpMgr.importBinding( "document", CswNbtObjClassSDSDocument.PropertyName.File, CswEnumNbtSubFieldName.Blob.ToString(), BlobTableName : "documents", LobDataPkColOverride : "legacyid" );
-            ImpMgr.importBinding( "document", CswNbtObjClassSDSDocument.PropertyName.Link, CswEnumNbtSubFieldName.Href.ToString(), BlobTableName : "documents", LobDataPkColOverride : "legacyid" );
+            ImpMgr.importBinding( "document", CswNbtObjClassSDSDocument.PropertyName.File, CswEnumNbtSubFieldName.Blob.ToString(), BlobTableName : "documents", LobDataPkColOverride : "documentid" );
+            ImpMgr.importBinding( "document", CswNbtObjClassSDSDocument.PropertyName.Link, CswEnumNbtSubFieldName.Href.ToString(), BlobTableName : "documents", LobDataPkColOverride : "documentid" );
 
             //Use the description as the text that displays for links
             ImpMgr.importBinding( "description", CswNbtObjClassSDSDocument.PropertyName.Link, CswEnumNbtSubFieldName.Text.ToString() );
