dffd4489a692c56443b180d85c07fb7470572df4
bvavra@chemsw.com <unknown@kiln.example.com>
2013-10-08 00:21:31 +0400

    Hickory - Case 30130 - Container Receipt Lot is now required

--- NbtLogic/csw/Schema/scripts/prod/02H/CswSchemaScriptsHickory.cs
+++ NbtLogic/csw/Schema/scripts/prod/02H/CswSchemaScriptsHickory.cs
@@ -20,6 +20,7 @@ namespace ChemSW.Nbt.Schema
             return new Collection<CswUpdateSchemaTo>()
                 {
                     new CswUpdateSchema_02H_Case30879(),
+                    new CswUpdateMetaData_02H_Case30130(),
                 };
         } // _MetaDataScripts()
 
