cc10d45687d31aa62b5b99e9d12c536382f903f1
bvavra@chemsw.com <unknown@kiln.example.com>
2013-10-15 20:05:02 +0400

    Ginkgo - Fixed bug where ImageList values were not being copied

--- NbtLogic/csw/PropTypes/CswNbtNodePropImageList.cs
+++ NbtLogic/csw/PropTypes/CswNbtNodePropImageList.cs
@@ -25,7 +25,7 @@ namespace ChemSW.Nbt.PropTypes
             _ValueSubField = ( (CswNbtFieldTypeRuleImageList) _FieldTypeRule ).ValueSubField;
 
             // Associate subfields with methods on this object, for SetSubFieldValue()
-            _SubFieldMethods.Add( _ValueSubField, new Tuple<Func<dynamic>, Action<dynamic>>( () => Value, x => Value.FromString( CswConvert.ToString( x ) ) ) );
+            _SubFieldMethods.Add( _ValueSubField, new Tuple<Func<dynamic>, Action<dynamic>>( () => Value, x => Value = _setValue( x ) ) );
         }
 
         private CswNbtSubField _ValueSubField;
@@ -65,6 +65,13 @@ namespace ChemSW.Nbt.PropTypes
                 SetPropRowValue( _ValueSubField, ValString );
                 SyncGestalt();
             }
+        }
+
+        private CswDelimitedString _setValue( string ValString )
+        {
+            CswDelimitedString _Value = new CswDelimitedString( _delimiter );
+            _Value.FromString( ValString );
+            return _Value;
         }
 
         /// <summary>
