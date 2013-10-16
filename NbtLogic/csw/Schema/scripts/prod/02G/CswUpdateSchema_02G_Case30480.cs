6f6e8f7e5ff8cd0201a69af149686f7e072bacda
bvavra@chemsw.com <unknown@kiln.example.com>
2013-10-17 00:05:05 +0400

    Ginkgo - Case 30480 - Fixed WorkUnit assignment bug on defunct InvGrpPerms

--- NbtLogic/csw/Schema/scripts/prod/02G/CswUpdateSchema_02G_Case30480.cs
+++ NbtLogic/csw/Schema/scripts/prod/02G/CswUpdateSchema_02G_Case30480.cs
@@ -121,13 +121,27 @@ namespace ChemSW.Nbt.Schema
                 //5: Get rid of deprecated CISPro Inventory Group and move their permissions over to Default Inventory Group
                 if( null != CISProInventoryGroup )
                 {
-                    foreach( CswNbtObjClassInventoryGroupPermission InventoryGroupPerm in InventoryGroupPermissionOc.getNodes( true, false, false, true ) )
+                    CswNbtMetaDataObjectClass WorkUnitOc = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswEnumNbtObjectClass.WorkUnitClass );
+                    CswNbtObjClassWorkUnit DefaultWorkUnit = null;
+                    foreach( CswNbtObjClassWorkUnit WorkUnit in WorkUnitOc.getNodes( true, false, false, true ) )
                     {
-                        if( InventoryGroupPerm.PermissionGroup.RelatedNodeId == CISProInventoryGroup.NodeId )
+                        if( WorkUnit.Name.Text == "Default Work Unit" )
                         {
-                            InventoryGroupPerm.PermissionGroup.RelatedNodeId = DefaultInventoryGroup.NodeId;
-                            InventoryGroupPerm.PermissionGroup.SyncGestalt();
-                            InventoryGroupPerm.postChanges( false );
+                            DefaultWorkUnit = WorkUnit;
+                        }
+                    }
+                    if( null != DefaultWorkUnit )
+                    {
+                        foreach( CswNbtObjClassInventoryGroupPermission InventoryGroupPerm in InventoryGroupPermissionOc.getNodes( true, false, false, true ) )
+                        {
+                            if( InventoryGroupPerm.PermissionGroup.RelatedNodeId == CISProInventoryGroup.NodeId )
+                            {
+                                InventoryGroupPerm.PermissionGroup.RelatedNodeId = DefaultInventoryGroup.NodeId;
+                                InventoryGroupPerm.PermissionGroup.SyncGestalt();
+                                InventoryGroupPerm.WorkUnit.RelatedNodeId = DefaultWorkUnit.NodeId;
+                                InventoryGroupPerm.WorkUnit.SyncGestalt();
+                                InventoryGroupPerm.postChanges( false );
+                            }
                         }
                     }
                     CISProInventoryGroup.Node.delete( true, true );
