e7d621f784ed43536d7c49f6ce1b4139193259b5
bvavra@chemsw.com <unknown@kiln.example.com>
2013-10-18 19:39:00 +0400

    Hickory - Case 30723 - Remove ReceiptLot from MLM NTP visibility logic and use layout visibility logic instead.

---
+++ NbtLogic/csw/Schema/scripts/prod/02H/CswUpdateMetaData_02H_Case30723.cs
@@ -0,0 +1,56 @@
+﻿using ChemSW.Core;
+using ChemSW.Nbt.ObjClasses;
+using ChemSW.Nbt.csw.Dev;
+using ChemSW.Nbt.MetaData;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update
+    /// </summary>
+    public class CswUpdateMetaData_02H_Case30723 : CswUpdateSchemaTo
+    {
+        public override CswEnumDeveloper Author
+        {
+            get { return CswEnumDeveloper.BV; }
+        }
+
+        public override int CaseNo
+        {
+            get { return 30723; }
+        }
+
+        public override string ScriptName
+        {
+            get { return "02H_Case" + CaseNo; }
+        }
+
+        public override string Title
+        {
+            get { return "Receipt Lot NTP no longer Hidden"; }
+        }
+
+        public override void update()
+        {
+            CswNbtMetaDataObjectClass ContainerOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswEnumNbtObjectClass.ContainerClass );
+            foreach( CswNbtMetaDataNodeType ContainerNT in ContainerOC.getNodeTypes() )
+            {
+                CswNbtMetaDataNodeTypeProp ReceiptLotNTP = ContainerNT.getNodeTypePropByObjectClassProp( CswNbtObjClassContainer.PropertyName.ReceiptLot );
+                if( null != ReceiptLotNTP )
+                {
+                    ReceiptLotNTP.Hidden = false;
+                    if( _CswNbtSchemaModTrnsctn.Modules.IsModuleEnabled( CswEnumNbtModuleName.MLM ) )
+                    {
+                        ReceiptLotNTP.updateLayout( CswEnumNbtLayoutType.Edit, true );
+                    }
+                    else
+                    {
+                        ReceiptLotNTP.removeFromAllLayouts();
+                    }
+                }
+            }
+        }
+
+    }
+
+}//namespace ChemSW.Nbt.Schema
