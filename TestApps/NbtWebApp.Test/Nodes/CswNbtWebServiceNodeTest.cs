a27ad489c380614606a9b2f4e1aeb009dfc19a93
bvavra@chemsw.com <unknown@kiln.example.com>
2013-10-21 18:37:36 +0400

    Hickory - Added NbtWebApp UnitTest project

---
+++ TestApps/NbtWebApp.Test/Nodes/CswNbtWebServiceNodeTest.cs
@@ -0,0 +1,33 @@
+﻿using ChemSW.Nbt.Test;
+using NUnit.Framework;
+
+namespace NbtWebApp.Test.WebServices
+{
+    [TestFixture]
+    public class CswNbtWebServiceNodeTest
+    {
+        #region Setup and Teardown
+
+        private TestData TestData;
+
+        [SetUp]
+        public void MyTestInitialize()
+        {
+            TestData = new TestData();
+        }
+
+        [TearDown]
+        public void MyTestCleanup()
+        {
+            TestData.Destroy();
+        }
+
+        #endregion
+
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.IsNotNull( TestData.CswNbtResources );
+        }
+    }
+}
