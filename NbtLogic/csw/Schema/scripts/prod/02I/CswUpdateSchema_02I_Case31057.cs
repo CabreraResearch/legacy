b2ac5d55ccb8f1b01d77e0e15f4e560e3def0140
Matthew Bischoff <mbischoff@chemsw.com>
2013-11-04 22:26:05 +0500

    Ironwood Case 31061 - href saves to ClobData instead of Field2, fix script

--- NbtLogic/csw/Schema/scripts/prod/02I/CswUpdateSchema_02I_Case31057.cs
+++ NbtLogic/csw/Schema/scripts/prod/02I/CswUpdateSchema_02I_Case31057.cs
@@ -36,17 +36,17 @@ namespace ChemSW.Nbt.Schema
         {
             CswNbtMetaDataFieldType LinkFieldType = _CswNbtSchemaModTrnsctn.MetaData.getFieldType( CswEnumNbtFieldType.Link );
 
-            string where = "where jctnodepropid in (select jnp.field_2, jnp.clobdata from jct_nodes_props jnp" +
+            string where = "where jctnodepropid in (select jnp.jctnodepropid from jct_nodes_props jnp" +
                          " join nodetype_props ntp on jnp.nodetypepropid = ntp.nodetypepropid" +
-                         "  where ntp.fieldtypeid = " + LinkFieldType.FieldTypeId + " and jnp.field_2 is not null)";
+                         "  where ntp.fieldtypeid = " + LinkFieldType.FieldTypeId + " and jnp.field2 is not null)";
 
             CswTableUpdate linkTU = _CswNbtSchemaModTrnsctn.makeCswTableUpdate( "31057.UpdateLinkPropsHrefCol", "jct_nodes_props" );
-            DataTable jct_nodes_props = linkTU.getTable( new CswCommaDelimitedString() { "clobdata,field_2" }, string.Empty, Int32.MinValue, where, false );
+            DataTable jct_nodes_props = linkTU.getTable( new CswCommaDelimitedString() { "clobdata,field2" }, string.Empty, Int32.MinValue, where, false );
 
             foreach( DataRow Row in jct_nodes_props.Rows )
             {
-                Row["clobdata"] = Row["field_2"];
-                Row["field_2"] = string.Empty;
+                Row["clobdata"] = Row["field2"];
+                Row["field2"] = string.Empty;
             }
             linkTU.update( jct_nodes_props );
 
