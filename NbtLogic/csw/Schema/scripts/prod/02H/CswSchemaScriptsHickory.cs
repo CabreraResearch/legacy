6e888e4538088d327c3f7e26c1677b393bf32eb0
Steven Salter <ssalter@chemsw.com>
2013-11-07 21:09:44 +0500

    Hickory, case 31110 - CISPro Report Group permissions now limited to cispro roles

--- NbtLogic/csw/Schema/scripts/prod/02H/CswSchemaScriptsHickory.cs
+++ NbtLogic/csw/Schema/scripts/prod/02H/CswSchemaScriptsHickory.cs
@@ -57,7 +57,8 @@ namespace ChemSW.Nbt.Schema
                     new CswUpdateSchema_02H_Case30914C(),
                     new CswUpdateSchema_02H_Case30658(),
                     new CswUpdateSchema_02H_Case30042(),
-                    new CswUpdateSchema_02H_Case30671()
+                    new CswUpdateSchema_02H_Case30671(),
+                    new CswUpdateSchema_02H_Case31110()
                 };
         } // _SchemaScripts()
 
