340cfd46f4ec632351b9f923d040d1600e211de3
Matthew Bischoff <mbischoff@chemsw.com>
2013-12-03 02:44:50 +0500

    JUNIPER STARTS HERE

---
+++ NbtLogic/csw/Schema/scripts/prod/02J/CswSchemaScriptsJuniper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Keeps the schema up-to-date
+    /// </summary>
+    public class CswSchemaScriptsJuniper : ICswSchemaScripts
+    {
+        public Collection<CswUpdateSchemaTo> _DDLScripts()
+        {
+            return new Collection<CswUpdateSchemaTo>()
+                {
+                    // new CswUpdateDDL_02J_CaseXXXXX()
+                };
+        } // _DDLScripts()
+
+        public Collection<CswUpdateSchemaTo> _MetaDataScripts()
+        {
+            return new Collection<CswUpdateSchemaTo>()
+                {
+                    // new CswUpdateMetaData_02J_CaseXXXXX()
+                };
+        } // _MetaDataScripts()
+
+        public Collection<CswUpdateSchemaTo> _SchemaScripts()
+        {
+            return new Collection<CswUpdateSchemaTo>()
+                {
+                    // new CswUpdateSchema_02J_CaseXXXXX()
+                };
+        } // _SchemaScripts()
+
+    }//class CswSchemaScriptsHickory
+}//namespace ChemSW.Nbt.Schema
