2ca0c54b67d724826c0c8349bda3214e6d53fee6
Matthew Bischoff <mbischoff@chemsw.com>
2013-12-05 02:33:08 +0500

    Case 30917 - copy Chemical copies SDS doc correctly

--- NbtLogic/csw/ObjClasses/CswNbtObjClassSDSDocument.cs
+++ NbtLogic/csw/ObjClasses/CswNbtObjClassSDSDocument.cs
@@ -1,17 +1,19 @@
 using System;
+using System.Data;
 using ChemSW.Core;
+using ChemSW.DB;
 using ChemSW.Nbt.MetaData;
 using ChemSW.Nbt.PropTypes;
 
 namespace ChemSW.Nbt.ObjClasses
 {
-    public class CswNbtObjClassSDSDocument : CswNbtPropertySetDocument
+    public class CswNbtObjClassSDSDocument: CswNbtPropertySetDocument
     {
         #region Enums
         /// <summary>
         /// Object Class Property Names
         /// </summary>
-        public new sealed class PropertyName : CswNbtPropertySetDocument.PropertyName
+        public new sealed class PropertyName: CswNbtPropertySetDocument.PropertyName
         {
             /// <summary>
             /// Language of the document.
@@ -58,7 +60,7 @@ namespace ChemSW.Nbt.ObjClasses
         /// <summary>
         /// Potential File Type options (Includes menu inherited from base class <see cref="CswNbtPropertySetDocument"/>)
         /// </summary>
-        public new sealed class CswEnumDocumentFileTypes : CswNbtPropertySetDocument.CswEnumDocumentFileTypes
+        public new sealed class CswEnumDocumentFileTypes: CswNbtPropertySetDocument.CswEnumDocumentFileTypes
         {
             /// <summary>
             /// Hyperlink
@@ -148,6 +150,39 @@ namespace ChemSW.Nbt.ObjClasses
             return true;
         }
 
+        public override CswNbtNode CopyNode( bool IsNodeTemp = false )
+        {
+            CswNbtNode NewNode = base.CopyNodeImpl( IsNodeTemp );
+
+            //Copy the existing file if there is any
+            CswTableUpdate blobDataTU = _CswNbtResources.makeCswTableUpdate( "CopyBlobData", "blob_data" );
+            DataTable blobDataDT = blobDataTU.getTable( "where jctnodepropid = " + this.File.JctNodePropId );
+            if( blobDataDT.Rows.Count > 0 )
+            {
+                DataRow existingRow = blobDataDT.Rows[0];
+                DataRow newRow = blobDataDT.NewRow();
+                foreach( DataColumn col in blobDataDT.Columns )
+                {
+                    if( "jctnodepropid" == col.ColumnName )
+                    {
+                        newRow["jctnodepropid"] = ( (CswNbtObjClassSDSDocument) NewNode ).File.JctNodePropId;
+                    }
+                    else if( col.ColumnName != "blobdataid" )
+                    {
+                        newRow[col] = existingRow[col];
+                    }
+                }
+                blobDataDT.Rows.Add( newRow );
+                blobDataTU.update( blobDataDT );
+            }
+
+            return NewNode;
+        }
+
+        #endregion Inherited Events
+
+        #region Custom Logic
+
         public override void archiveMatchingDocs()
         {
             //Archives existing Documents related to the same Owner.
@@ -162,7 +197,7 @@ namespace ChemSW.Nbt.ObjClasses
                     CswNbtView ExistingDocsView = new CswNbtView( _CswNbtResources );
                     CswNbtViewRelationship DocumentVr = ExistingDocsView.AddViewRelationship( NodeType, false );
                     ExistingDocsView.AddViewPropertyAndFilter( DocumentVr, Owner.NodeTypeProp, OwnerNode.NodeId.PrimaryKey.ToString(), CswEnumNbtSubFieldName.NodeID );
-                    ExistingDocsView.AddViewPropertyAndFilter( DocumentVr, Archived.NodeTypeProp, CswEnumTristate.True.ToString(), FilterMode: CswEnumNbtFilterMode.NotEquals );
+                    ExistingDocsView.AddViewPropertyAndFilter( DocumentVr, Archived.NodeTypeProp, CswEnumTristate.True.ToString(), FilterMode : CswEnumNbtFilterMode.NotEquals );
                     ExistingDocsView.AddViewPropertyAndFilter( DocumentVr, Format.NodeTypeProp, Format.Value );
                     ExistingDocsView.AddViewPropertyAndFilter( DocumentVr, Language.NodeTypeProp, Language.Value );
 
@@ -187,10 +222,6 @@ namespace ChemSW.Nbt.ObjClasses
                 }
             }
         }
-
-        #endregion Inherited Events
-
-        #region Custom Logic
 
         public static bool materialHasActiveSDS( CswNbtResources _CswNbtResources, CswPrimaryKey MaterialId )
         {
