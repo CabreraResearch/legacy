7d8c0d7345408215b9a86c8229e15ac216079fbb
bvavra@chemsw.com <unknown@kiln.example.com>
2013-12-19 01:19:28 +0500

    Ironwood - Case 31405, Part 1 - Relationships in Search mode now supports PropertySets (sort of)

--- NbtWebApp/app/composites/csw.nodeselect.js
+++ NbtWebApp/app/composites/csw.nodeselect.js
@@ -32,6 +32,7 @@
 
             cswPrivate.nodeTypeId = cswPrivate.nodeTypeId || '';
             cswPrivate.objectClassId = cswPrivate.objectClassId || '';
+            cswPrivate.propertySetId = cswPrivate.propertySetId || '';
             cswPrivate.objectClassName = cswPrivate.objectClassName || '';
             cswPrivate.addNodeDialogTitle = cswPrivate.addNodeDialogTitle || '';
 
@@ -110,6 +111,7 @@
                 data: cswPrivate.ajaxData || {
                     NodeTypeId: Csw.number(cswPrivate.nodeTypeId, 0),
                     ObjectClassId: Csw.number(cswPrivate.objectClassId, 0),
+                    PropertySetId: Csw.number(cswPrivate.propertySetId, 0),
                     ObjectClass: Csw.string(cswPrivate.objectClassName),
                     RelatedToObjectClass: Csw.string(cswPrivate.relatedTo.objectClassName),
                     RelatedToNodeId: Csw.string(cswPrivate.relatedTo.nodeId),
@@ -462,6 +464,7 @@
                         ViewId: Csw.string(cswPrivate.viewid),
                         NodeTypeId: Csw.number(cswPrivate.nodeTypeId, 0),
                         ObjectClassId: Csw.number(cswPrivate.objectClassId, 0),
+                        PropertySetId: Csw.number(cswPrivate.propertySetId, 0),
                         ObjectClass: Csw.string(cswPrivate.objectClassName),
                         RelatedToObjectClass: Csw.string(cswPrivate.relatedTo.objectClassName),
                         RelatedToNodeId: Csw.string(cswPrivate.relatedTo.nodeId)
