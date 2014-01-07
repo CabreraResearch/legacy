56e8b0788eec9939a372341c90ba32472ff3db79
Matthew Bischoff <mbischoff@chemsw.com>
2014-01-07 23:22:50 +0500

    Juniper Case 31534 - forgot file

---
+++ NbtLogic/csw/Schema/scripts/prod/02J/CswUpdateSchema_02J_Case31534.cs
@@ -0,0 +1,34 @@
+﻿using ChemSW.Nbt.csw.Dev;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update
+    /// </summary>
+    public class CswUpdateSchema_02J_Case31534: CswUpdateSchemaTo
+    {
+        public override CswEnumDeveloper Author
+        {
+            get { return CswEnumDeveloper.NBT; }
+        }
+
+        public override int CaseNo
+        {
+            get { return 31534; }
+        }
+
+        public override string Title
+        {
+            get { return "Change max containers received config var to 250"; }
+        }
+
+        public override void update()
+        {
+
+            _CswNbtSchemaModTrnsctn.ConfigVbls.setConfigVariableValue( CswEnumNbtConfigurationVariables.container_receipt_limit.ToString(), "250" );
+
+        } // update()
+
+    }
+
+}//namespace ChemSW.Nbt.Schema
