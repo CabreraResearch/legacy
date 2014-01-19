8e18271c51f64910842449719c21a29958563ea5
Colleen Muldowney <cmuldowney@chemsw.com>
2014-01-20 01:48:23 +0500

    Fixed compile issue -- script files need to be pushed

--- NbtLogic/csw/Schema/scripts/prod/02K/CswSchemaScriptsKatsura.cs
+++ NbtLogic/csw/Schema/scripts/prod/02K/CswSchemaScriptsKatsura.cs
@@ -14,7 +14,7 @@ namespace ChemSW.Nbt.Schema
                     // new CswUpdateDDL_02K_CaseXXXXX()
                     new CswUpdateDDL_02K_Case29311_AddRelationalColumn(),
                     new CswUpdateDDL_02K_Case31616(),
-                    new CswUpdateDDL_02K_Case31517()
+                    //new CswUpdateDDL_02K_Case31517() //TODO: Steve push this file & add back to csproj
                 };
         } // _DDLScripts()
 
@@ -30,7 +30,7 @@ namespace ChemSW.Nbt.Schema
                     new CswUpdateMetaData_02K_Case31542B(),
                     new CswUpdateMetaData_02K_Case31672(),
                     new CswUpdateMetaData_02K_Case31545(),
-                    new CswUpdateMetaData_02K_Case31517B()
+                    //new CswUpdateMetaData_02K_Case31517B() //TODO: Steve push this file & add back to csproj
                 };
         } // _MetaDataScripts()
 
