389b1ecda7a4a4071f75e4e3de5e4859b7dd467e
bvavra@chemsw.com <unknown@kiln.example.com>
2014-01-24 22:00:33 +0500

    Katsura.2 - Case 31576, Case 31668, Case 31691 - Inspection ObjClass buttons are working again

--- NbtLogic/csw/PropertySets/CswNbtPropertySetGeneratorTarget.cs
+++ NbtLogic/csw/PropertySets/CswNbtPropertySetGeneratorTarget.cs
@@ -89,6 +89,11 @@ namespace ChemSW.Nbt.ObjClasses
 
         public virtual bool onPropertySetButtonClick( NbtButtonData ButtonData ) { return true; }
 
+        protected override bool onButtonClick( NbtButtonData ButtonData )
+        {
+            return onPropertySetButtonClick( ButtonData );
+        }//afterPopulateProps()
+
         #endregion
 
         #region Property Set specific properties
