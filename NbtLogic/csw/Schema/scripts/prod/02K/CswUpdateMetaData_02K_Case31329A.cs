6baccc20ca1e7d19c192ca945a71d45f5eb0f992
Matthew Bischoff <mbischoff@chemsw.com>
2014-01-30 02:41:53 +0500

    Katsura.2 Case 31329 - forgot to add files

---
+++ NbtLogic/csw/Schema/scripts/prod/02K/CswUpdateMetaData_02K_Case31329A.cs
@@ -0,0 +1,45 @@
+﻿using ChemSW.Nbt.csw.Dev;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.ObjClasses;
+
+namespace ChemSW.Nbt.Schema
+{
+    public class CswUpdateMetaData_02K_Case31329A: CswUpdateSchemaTo
+    {
+        public override CswEnumDeveloper Author
+        {
+            get { return CswEnumDeveloper.MB; }
+        }
+
+        public override int CaseNo
+        {
+            get { return 31329; }
+        }
+
+        public override string AppendToScriptName()
+        {
+            return "A";
+        }
+
+        public override string Title
+        {
+            get { return "Add 'Cache Size' property to Sequences"; }
+        }
+
+        public override void update()
+        {
+
+            CswNbtMetaDataObjectClass DesignSequenceOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswEnumNbtObjectClass.DesignSequenceClass );
+
+            CswNbtMetaDataObjectClassProp CacheSizeOCP = _CswNbtSchemaModTrnsctn.createObjectClassProp( DesignSequenceOC, new CswNbtWcfMetaDataModel.ObjectClassProp( DesignSequenceOC )
+                {
+                    PropName = CswNbtObjClassDesignSequence.PropertyName.CacheSize,
+                    FieldType = CswEnumNbtFieldType.Number,
+                    IsRequired = true
+                } );
+            
+        } // update()
+
+    }//class CswUpdateMetaData_02K_Case31672
+
+}//namespace ChemSW.Nbt.Schema
---
+++ NbtLogic/csw/Schema/scripts/prod/02K/CswUpdateSchema_02K_Case31329B.cs
@@ -0,0 +1,58 @@
+﻿using ChemSW.Nbt.csw.Dev;
+using ChemSW.Nbt.MetaData;
+using ChemSW.Nbt.MetaData.FieldTypeRules;
+using ChemSW.Nbt.ObjClasses;
+
+namespace ChemSW.Nbt.Schema
+{
+    public class CswUpdateSchema_02K_Case31329B: CswUpdateSchemaTo
+    {
+        public override CswEnumDeveloper Author
+        {
+            get { return CswEnumDeveloper.MB; }
+        }
+
+        public override int CaseNo
+        {
+            get { return 31329; }
+        }
+
+        public override string Title
+        {
+            get { return "Add DesignSequence.CacheSize to add layout; Property sequences have no cache"; }
+        }
+
+        public override string AppendToScriptName()
+        {
+            return "B";
+        }
+
+        public override void update()
+        {
+            //Add CacheSize to layouts
+            CswNbtMetaDataObjectClass DesignSequenceOC = _CswNbtSchemaModTrnsctn.MetaData.getObjectClass( CswEnumNbtObjectClass.DesignSequenceClass );
+            foreach( CswNbtMetaDataNodeType DesignSequenceNT in DesignSequenceOC.getNodeTypes() )
+            {
+                CswNbtMetaDataNodeTypeProp CacheSizeNT = DesignSequenceNT.getNodeTypePropByObjectClassProp( CswNbtObjClassDesignSequence.PropertyName.CacheSize );
+                _CswNbtSchemaModTrnsctn.MetaData.NodeTypeLayout.updatePropLayout( CswEnumNbtLayoutType.Add, DesignSequenceNT.NodeTypeId, CacheSizeNT, true );
+
+                CswNbtMetaDataNodeTypeTab FirstTab = DesignSequenceNT.getFirstNodeTypeTab();
+                _CswNbtSchemaModTrnsctn.MetaData.NodeTypeLayout.updatePropLayout( CswEnumNbtLayoutType.Edit, DesignSequenceNT.NodeTypeId, CacheSizeNT, true, TabId : FirstTab.TabId );
+            }
+
+            //Update existing DesignSequences to have a cache of zero (this will also update the sequences in the DB
+            foreach( CswNbtObjClassDesignSequence Sequence in DesignSequenceOC.getNodes( false, true, false, true ) )
+            {
+                Sequence.CacheSize.Value = 0;
+                Sequence.postChanges( true ); 
+            }
+
+            //Set the CacheSize to default to zero
+            CswNbtMetaDataObjectClassProp CacheSizeOCP = DesignSequenceOC.getObjectClassProp( CswNbtObjClassDesignSequence.PropertyName.CacheSize );
+            _CswNbtSchemaModTrnsctn.MetaData.SetObjectClassPropDefaultValue( CacheSizeOCP, 0, CswNbtFieldTypeRuleNumber.SubFieldName.Value );
+
+        }
+
+    }//class CswUpdateMetaData_02K_Case31517B
+
+}//namespace ChemSW.Nbt.Schema
