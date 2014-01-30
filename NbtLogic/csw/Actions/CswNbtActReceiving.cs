cf90c49eeb01da5a1e5a981d8ee6662402981c50
Steven Salter <ssalter@chemsw.com>
2014-01-31 02:39:08 +0500

    Fixed Matt's merge

--- NbtLogic/csw/Actions/CswNbtActReceiving.cs
+++ NbtLogic/csw/Actions/CswNbtActReceiving.cs
@@ -86,11 +86,6 @@ namespace ChemSW.Nbt.Actions
             CswNbtObjClassContainer InitialContainerNode = _CswNbtResources.Nodes[ReceiptDefinition.ContainerNodeId];
             if( null != InitialContainerNode && ReceiptDefinition.Quantities.Count > 0 )
             {
-                
-                receiveContainers( ReceiptDefinition, ref processed, 1 ); //process only the first container (the initial one)
-
-                commitSDSDocNode( ReceiptDefinition.MaterialNodeId, ReceiptDefinition.SDSNodeId, ReceiptDefinition.SDSProps );
-
                 //Generate the barcodes upfront and treat them as custom barcodes so all Containers in this Receipt Def have similar numbers
                 CswNbtMetaDataNodeTypeProp BarcodeProp = (CswNbtMetaDataNodeTypeProp) InitialContainerNode.NodeType.getBarcodeProperty();
                 for( int i = 0; i < ReceiptDefinition.Quantities.Count; i++ )
@@ -112,18 +107,18 @@ namespace ChemSW.Nbt.Actions
                     }
                 }
 
+                CswNbtNode ReceiptLot = _makeReceiptLot( ReceiptDefinition.MaterialNodeId, ReceiptDefinition, InitialContainerNode.ExpirationDate.DateTimeValue );
+                if( null == ReceiptDefinition.ReceiptLotNodeId )
+                {
+                    ReceiptDefinition.ReceiptLotNodeId = ReceiptLot.NodeId;
+                }
+
                 ReceiptDefinition.Quantities[0].ContainerIds.Add( InitialContainerNode.NodeId.ToString() );
                 int processed = 0;
                 receiveContainers( ReceiptDefinition, ref processed, 1 ); //process only the first container (the initial one)
 
                 commitSDSDocNode( ReceiptDefinition.MaterialNodeId, ReceiptDefinition.SDSNodeId, ReceiptDefinition.SDSProps );
 
-                CswNbtNode ReceiptLot = _makeReceiptLot( ReceiptDefinition.MaterialNodeId, ReceiptDefinition, InitialContainerNode.ExpirationDate.DateTimeValue );
-                if( null == ReceiptDefinition.ReceiptLotNodeId )
-                {
-                    ReceiptDefinition.ReceiptLotNodeId = ReceiptLot.NodeId;
-                }
-                
                 int TotalContainersToMake = ReceiptDefinition.CountNumberContainersToMake();
                 int Threshhold = CswConvert.ToInt32( _CswNbtResources.ConfigVbls.getConfigVariableValue( "batchthreshold" ) );
                 bool UseBatchOp = TotalContainersToMake >= Threshhold;
