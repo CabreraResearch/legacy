1d1a98d8a29c1e8925d9017a9815b4c654f366c2
Steven Salter <ssalter@chemsw.com>
2014-02-04 23:32:12 +0500

    Katsura, case 31586 - the rampage through NbtLogic/Trees continues again

--- NbtLogic/csw/Tree/CswNbtTreeNodes.cs
+++ NbtLogic/csw/Tree/CswNbtTreeNodes.cs
@@ -12,6 +12,27 @@ namespace ChemSW.Nbt
 {
     public class CswNbtTreeNodes : ICswNbtTree
     {
+        private class CswNbtColumnNames
+        {
+            public string NodeId = "NodeId";
+            public string _UNodeId = "_UNodeId";
+            public string ParentNodeId = "ParentNodeId";
+            public string _UParentNodeId = "_UParentNodeId";
+            public string NodeName = "NodeName";
+            public string NodeTypeId = "NodeTypeId";
+            public string NodeTypeName = "NodeTypeName";
+            public string ObjectClassId = "ObjectClassId";
+            public string ObjectClassName = "ObjectClass";
+            public string IconFileName = "IconFileName";
+            public string IconMsSqlOrder = "MsSqlOrder";
+            public string NameTemplate = "NameTemplate";
+            public string Selectable = "Selectable";
+            public string ShowInGrid = "ShowInGrid";
+            public string ShowInTree = "ShowInTree";
+            public string Locked = "Locked";
+
+        }//class CswNbtColumnNames
+
         #region Constructor
 
         private readonly Dictionary<CswNbtNodeKey, CswNbtNodeKey> NodesAndParents;
