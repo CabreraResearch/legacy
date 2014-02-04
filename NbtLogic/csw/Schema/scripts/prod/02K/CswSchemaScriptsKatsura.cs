ec25aa6530d5be78333b959be45c06622414af4b
Matthew Bischoff <mbischoff@chemsw.com>
2014-02-05 01:38:28 +0500

    Katsura.3 Case 31829 - Kiosk Mode is in Categories in Master

--- NbtLogic/csw/Schema/scripts/prod/02K/CswSchemaScriptsKatsura.cs
+++ NbtLogic/csw/Schema/scripts/prod/02K/CswSchemaScriptsKatsura.cs
@@ -62,6 +62,7 @@ namespace ChemSW.Nbt.Schema
                     new CswUpdateSchema_02K_Case31329(),
                     new CswUpdateSchema_02K_Case31375(),
                     new CswUpdateSchema_02K_Case31801(),
+                    new CswUpdateSchema_02K_Case31829()
                 };
         } // _SchemaScripts()
 
