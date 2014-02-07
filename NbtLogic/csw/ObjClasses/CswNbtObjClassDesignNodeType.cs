a8a87bae469192cf5da825716195cf331df57286
Steven Salter <ssalter@chemsw.com>
2014-02-08 01:10:18 +0500

    Katsura, case 31839 - fix inspection checklist name default value

--- NbtLogic/csw/ObjClasses/CswNbtObjClassDesignNodeType.cs
+++ NbtLogic/csw/ObjClasses/CswNbtObjClassDesignNodeType.cs
@@ -248,9 +248,9 @@ namespace ChemSW.Nbt.ObjClasses
             CswNbtMetaDataNodeTypeProp CancelledProp = NewNodeType.getNodeTypePropByObjectClassProp( CswNbtObjClassInspectionDesign.PropertyName.Cancel );
             CswNbtMetaDataNodeTypeProp CancelReasonProp = NewNodeType.getNodeTypePropByObjectClassProp( CswNbtObjClassInspectionDesign.PropertyName.CancelReason );
 
-            //// Set 'Name' default value = nodetypename
-            //NameProp.DefaultValue.AsText.Text = NewNodeType.NodeTypeName;
-
+            // Set 'Name' default value = nodetypename
+            NameProp.getDefaultValue( true, false ).AsText.Text = NewNodeType.NodeTypeName;
+            
             // The following changes for new forms only
             if( NewNodeType.VersionNo == 1 && false == InternalCreate )
             {
