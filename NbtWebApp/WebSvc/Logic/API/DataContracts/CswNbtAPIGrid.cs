9935b5b275ef963123327c5cdb1af9daa387fdea
Matthew Bischoff <mbischoff@chemsw.com>
2014-03-05 23:46:53 +0500

    Larch.2 Case 52746 - views

---
+++ NbtWebApp/WebSvc/Logic/API/DataContracts/CswNbtAPIGrid.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+using ChemSW.Nbt;
+
+namespace NbtWebApp.WebSvc.Logic.API.DataContracts
+{
+    [DataContract]
+    public class CswNbtAPIGrid: CswNbtResourceCollection
+    {
+        [DataMember( Name = "viewname" )]
+        public string ViewName { get; set; }
+
+        [DataMember( Name = "uri" )]
+        public string URI { get; set; }
+
+        CswNbtViewId ViewId = new CswNbtViewId();
+        [DataMember( Name = "viewid" )]
+        private int _ViewId
+        {
+            get { return ViewId.get(); }
+            set { ViewId = new CswNbtViewId( value ); }
+        }
+    }
+}
---
+++ NbtWebApp/WebSvc/Logic/API/DataContracts/CswNbtAPITree.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+using ChemSW.Core;
+using ChemSW.Nbt;
+
+namespace NbtWebApp.WebSvc.Logic.API.DataContracts
+{
+    [DataContract]
+    public class CswNbtAPITree: CswNbtAPIReturn
+    {
+        [DataMember( Name = "viewname" )]
+        public string ViewName { get; set; }
+
+        [DataMember( Name = "uri" )]
+        public string URI { get; set; }
+
+        CswNbtViewId ViewId = new CswNbtViewId();
+        [DataMember( Name = "viewid" )]
+        private int _ViewId
+        {
+            get { return ViewId.get(); }
+            set { ViewId = new CswNbtViewId( value ); }
+        }
+
+        [DataMember( Name = "entities" )]
+        private Collection<CswNbtTreeResource> _Entities = new Collection<CswNbtTreeResource>();
+
+        public CswNbtTreeResource Add( string Name, CswPrimaryKey NodeId, string NodeType, string ObjClass, string PropertySet, string uri )
+        {
+            CswNbtTreeResource addedResource = new CswNbtTreeResource()
+                 {
+                     NodeId = NodeId,
+                     NodeName = Name,
+                     NodeType = NodeType,
+                     ObjectClass = ObjClass,
+                     PropertySet = PropertySet,
+                     URI = uri
+                 };
+            _Entities.Add( addedResource );
+            return addedResource;
+        }
+
+        [DataContract]
+        public class CswNbtTreeResource: CswNbtResource
+        {
+            [DataMember( Name = "children" )]
+            private Collection<CswNbtTreeResource> _Children = new Collection<CswNbtTreeResource>();
+
+            public CswNbtTreeResource Add( string Name, CswPrimaryKey NodeId, string NodeType, string ObjClass, string PropertySet, string uri )
+            {
+                CswNbtTreeResource addedResource = new CswNbtTreeResource()
+                {
+                    NodeId = NodeId,
+                    NodeName = Name,
+                    NodeType = NodeType,
+                    ObjectClass = ObjClass,
+                    PropertySet = PropertySet,
+                    URI = uri
+                };
+                _Children.Add( addedResource );
+                return addedResource;
+            }
+        }
+    }
+}
