887f2801a2382f4a0d2ab7faee4e0fe2a48a01f8
Matthew Obetz <mobetz@chemsw.com>
2014-03-25 20:11:06 +0400

    Magnolia -- CIS-52354: added password_reuse_count config var

---
+++ NbtLogic/csw/Schema/scripts/prod/02M/CswSchemaScriptsMagnolia.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Keeps the schema up-to-date
+    /// </summary>
+    public class CswSchemaScriptsMagnolia : ICswSchemaScripts
+    {
+        public Collection<CswUpdateSchemaTo> _DDLScripts()
+        {
+            return new Collection<CswUpdateSchemaTo>
+                {
+                };
+        } // _DDLScripts()
+
+        public Collection<CswUpdateSchemaTo> _MetaDataScripts()
+        {
+            return new Collection<CswUpdateSchemaTo>
+                {
+                };
+        } // _MetaDataScripts()
+
+        public Collection<CswUpdateSchemaTo> _SchemaScripts()
+        {
+            return new Collection<CswUpdateSchemaTo>
+                {
+                    new CswUpdateSchema_02M_Case52354(),
+                };
+        } // _SchemaScripts()
+
+    }//class CswSchemaScriptsLarch
+}//namespace ChemSW.Nbt.Schema
---
+++ NbtLogic/csw/Schema/scripts/prod/02M/CswUpdateSchema_02M_Case52354.cs
@@ -0,0 +1,32 @@
+﻿using ChemSW.Nbt.csw.Dev;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update
+    /// </summary>
+    public class CswUpdateSchema_02M_Case52354: CswUpdateSchemaTo
+    {
+        public override CswEnumDeveloper Author
+        {
+            get { return CswEnumDeveloper.MO; }
+        }
+
+        public override int CaseNo
+        {
+            get { return 52354; }
+        }
+
+        public override string Title
+        {
+            get { return "Add Block Prior Passwords Config Var"; }
+        }
+
+        public override void update()
+        {
+            _CswNbtSchemaModTrnsctn.ConfigVbls.addNewConfigurationValue( CswEnumNbtConfigurationVariables.password_reuse_count.ToString(), "5", "Number of previous passwords to disallow.", false );
+        } // update()
+
+    }
+
+}//namespace ChemSW.Nbt.Schema
