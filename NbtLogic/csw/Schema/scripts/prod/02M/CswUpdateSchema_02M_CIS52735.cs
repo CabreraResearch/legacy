36a559296d60b609836c8188b76644fdc9abaf99
Matthew Bischoff <mbischoff@chemsw.com>
2014-03-28 19:26:44 +0400

    Magnolia CIS-52735 - move mol data to mol_data table

---
+++ NbtLogic/csw/Schema/scripts/prod/02M/CswUpdateSchema_02M_CIS52735.cs
@@ -0,0 +1,66 @@
+﻿using System.Data;
+using System.Text;
+using ChemSW.Core;
+using ChemSW.DB;
+using ChemSW.Nbt.csw.Dev;
+using ChemSW.Nbt.MetaData;
+
+namespace ChemSW.Nbt.Schema
+{
+    /// <summary>
+    /// Schema Update
+    /// </summary>
+    public class CswUpdateSchema_02M_CIS52735: CswUpdateSchemaTo
+    {
+        public override CswEnumDeveloper Author
+        {
+            get { return CswEnumDeveloper.MB; }
+        }
+
+        public override int CaseNo
+        {
+            get { return 52735; }
+        }
+
+        public override string Title
+        {
+            get { return "Move mol clob data to mol_data table"; }
+        }
+
+        public override string AppendToScriptName()
+        {
+            return "C";
+        }
+
+        public override void update()
+        {
+            //Get all Mol NTP ids
+            CswCommaDelimitedString molNTPs = new CswCommaDelimitedString();
+            foreach( CswNbtMetaDataNodeTypeProp molNTP in _CswNbtSchemaModTrnsctn.MetaData.getNodeTypeProps( CswEnumNbtFieldType.MOL ) )
+            {
+                molNTPs.Add( molNTP.PropId.ToString() );
+            }
+
+            CswTableUpdate molDataUpdate = _CswNbtSchemaModTrnsctn.makeCswTableUpdate( "moldataUpdate_52735", "mol_data" );
+            DataTable molDataTbl = molDataUpdate.getEmptyTable();
+
+            CswTableUpdate molDataPropsTblUpdate = _CswNbtSchemaModTrnsctn.makeCswTableUpdate( "molselect_52735", "jct_nodes_props" );
+            DataTable propsWithMols = molDataPropsTblUpdate.getTable( "where nodetypepropid in (" + molNTPs.ToString() + ") and clobdata is not null" );
+            foreach( DataRow molPropRow in propsWithMols.Rows )
+            {
+                string molString = molPropRow["clobdata"].ToString();
+                int jctnodepropid = CswConvert.ToInt32( molPropRow["jctnodepropid"] );
+
+                DataRow newMolDataTblRow = molDataTbl.NewRow();
+                newMolDataTblRow["jctnodepropid"] = jctnodepropid;
+                newMolDataTblRow["orginalmol"] = Encoding.UTF8.GetBytes( molString );
+                molDataTbl.Rows.Add( newMolDataTblRow );
+
+                molPropRow["clobdata"] = string.Empty;
+            }
+
+            molDataUpdate.update( molDataTbl );
+            molDataPropsTblUpdate.update( propsWithMols );
+        }
+    }
+}
