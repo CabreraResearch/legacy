5b05abc54d92a91502ad80247ec496be487661bb
Matthew Bischoff <mbischoff@chemsw.com>
2014-04-02 17:43:41 +0400

    Magnolia CIS-52735 - script is not ready to run

--- NbtLogic/csw/Schema/scripts/prod/02M/CswSchemaScriptsMagnolia.cs
+++ NbtLogic/csw/Schema/scripts/prod/02M/CswSchemaScriptsMagnolia.cs
@@ -46,8 +46,8 @@ namespace ChemSW.Nbt.Schema
                    new CswUpdateSchema_02M_CIS53123(),
                    new CswUpdateSchema_02M_CIS52772(),
                    new CswUpdateSchema_02M_CIS52751(),
-                   new CswUpdateSchema_02M_CIS52735(),
-                   new CswUpdateSchema_02M_CIS52735D()
+                   new CswUpdateSchema_02M_CIS52735()
+                   //new CswUpdateSchema_02M_CIS52735D()
                 };
         } // _SchemaScripts()
 
