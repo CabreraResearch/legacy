771d3938ffc7cbed04d6b42030266897853e727b
Matthew Bischoff <mbischoff@chemsw.com>
2014-04-02 21:58:39 +0400

    Magnolia CIS-52735 - remove old script, module inits direct

--- NbtLogic/csw/Schema/scripts/prod/02M/CswSchemaScriptsMagnolia.cs
+++ NbtLogic/csw/Schema/scripts/prod/02M/CswSchemaScriptsMagnolia.cs
@@ -47,7 +47,6 @@ namespace ChemSW.Nbt.Schema
                    new CswUpdateSchema_02M_CIS52772(),
                    new CswUpdateSchema_02M_CIS52751(),
                    new CswUpdateSchema_02M_CIS52735()
-                   //new CswUpdateSchema_02M_CIS52735D()
                 };
         } // _SchemaScripts()
 
