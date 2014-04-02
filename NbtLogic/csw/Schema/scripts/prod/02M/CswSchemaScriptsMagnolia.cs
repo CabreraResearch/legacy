d4237da51f0b14eda670a4013ef3f724d3ce7f2c
Matthew Bischoff <mbischoff@chemsw.com>
2014-04-02 17:42:48 +0400

    Magnolia CIS-52735 - install direct per user

--- NbtLogic/csw/Schema/scripts/prod/02M/CswSchemaScriptsMagnolia.cs
+++ NbtLogic/csw/Schema/scripts/prod/02M/CswSchemaScriptsMagnolia.cs
@@ -45,7 +45,7 @@ namespace ChemSW.Nbt.Schema
                    new CswUpdateSchema_02M_CIS52670(),
                    new CswUpdateSchema_02M_CIS53123(),
                    new CswUpdateSchema_02M_CIS52772(),
-                   new CswUpdateSchema_02M_CIS52751()
+                   new CswUpdateSchema_02M_CIS52751(),
                    new CswUpdateSchema_02M_CIS52735(),
                    new CswUpdateSchema_02M_CIS52735D()
                 };
